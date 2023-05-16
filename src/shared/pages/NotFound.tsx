958abb435bdf80e89dc316fb4d55035779bbba90
Justin Barker <jbarkerpoc@gmail.com>
2023-05-16 05:20:40 -0500

    Cleaned up 404 error redir page

--- src/shared/pages/NotFound.tsx
+++ src/shared/pages/NotFound.tsx
@@ -1,11 +1,11 @@
 const NotFound = () => {
   return (
-    <section className="fourzerofour flexBoxY justifyCenter">
+    <main className="notfound">
       <div
-        className="fourzerofour__message flexBox flexColumn"
+        className="notfound__container"
         aria-label="HTTP 404 The browser was able to communicate with the server; however, the requested page could not be found."
       >
-        <h1 className="flexBox highlight">RUH ROH, HTTP 404</h1>
+        <h1 className="flexBox highlight">HTTP 404</h1>
         <p>
           The browser was able to communicate with the server; however, the requested page could not be found. Please return to the{' '}
           <a href="/" className="notFound--link highlight">
@@ -13,11 +13,9 @@ const NotFound = () => {
           </a>
           &nbsp;to continue browsing.
         </p>
-        <a href="/" className="notFound--link highlight">
-          <i className="fa-solid fa-person-walking-arrow-loop-left"></i>
-        </a>
+        <a href="/" className="notFound--link highlight"></a>
       </div>
-    </section>
+    </main>
   );
 };
 
