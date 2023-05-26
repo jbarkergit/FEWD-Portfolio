7c6b4068d34148d3d33835914a9db5340f8cf124
Justin Barker <jbarkerpoc@gmail.com>
2023-05-26 15:02:08 -0500

    Minor Dropdown prep work

--- src/ecommerce/components/navigation/header/HeaderKit.tsx
+++ src/ecommerce/components/navigation/header/HeaderKit.tsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import { useCategoryFilterContext } from '../../../context/StateProvider';
+import { useRef } from 'react';
 
 const HeaderKit = () => {
   // Thrown error is a desired outcome to utilize useState from our context while ALSO offering guard to Application Context Provider
@@ -47,26 +48,38 @@ const HeaderKit = () => {
   };
 
   const AudibleDropdown = () => {
+    function useAudibleDropdown() {
+      const audibleDropdown: HTMLElement = document.getElementById('audible')!;
+      audibleDropdown.style.display == 'none' ? (audibleDropdown.style.display = 'flex') : (audibleDropdown.style.display = 'none');
+    }
+
     return (
-      <li className="navkit__header__section__dropMenu">
-        <div>
-          Audible <i className="fa-solid fa-angle-down"></i>
+      <li className="dropMenu" onClick={useAudibleDropdown}>
+        <div
+          className="dropMenu__indicator"
+          style={{
+            color: useLocation().pathname === '/ecommerce' ? 'white' : 'black',
+          }}
+        >
+          <span className="dropMenu__indicator--interior">
+            Audible <i className="fa-solid fa-angle-down"></i>
+          </span>
         </div>
         <div>
-          <ul className="navkit__headersection__links">
-            <li className="navkit__headersection__links--link">
+          <ul className="dropMenu__links" id="audible">
+            <li className="dropMenu__links__link">
               <Link
                 to="/ecommerce/headphones"
                 className="flexBox"
                 style={{
-                  color: useLocation().pathname === '/ecommerce' ? 'white' : 'hsl(0, 0%, 19.607843137254903%)',
+                  color: useLocation().pathname === '/ecommerce' ? 'white' : 'black',
                 }}
                 onClick={() => setCategoryFilter('headphone')}
               >
                 {'Headphones'}
               </Link>
             </li>
-            <li className="navkit__headersection__links--link">
+            <li className="dropMenu__links__link">
               <Link
                 to="/ecommerce/amps-dacs"
                 className="flexBox"
@@ -78,7 +91,7 @@ const HeaderKit = () => {
                 {'Amps & Dacs'}
               </Link>
             </li>
-            <li className="navkit__headersection__links--link">
+            <li className="dropMenu__links__link">
               <Link
                 to="/ecommerce/equalizers"
                 className="flexBox"
@@ -97,13 +110,25 @@ const HeaderKit = () => {
   };
 
   const VocalDropdown = () => {
+    function useVocalDropdown() {
+      const vocalDropdown: HTMLElement = document.getElementById('vocal')!;
+      vocalDropdown.style.display == 'none' ? (vocalDropdown.style.display = 'flex') : (vocalDropdown.style.display = 'none');
+    }
+
     return (
-      <li className="navkit__header__section--dropdown">
-        <div>
-          Vocal <i className="fa-solid fa-angle-down"></i>
+      <li className="dropMenu" onClick={useVocalDropdown}>
+        <div
+          className="dropMenu__indicator"
+          style={{
+            color: useLocation().pathname === '/ecommerce' ? 'white' : 'black',
+          }}
+        >
+          <span className="dropMenu__indicator--interior">
+            Vocal <i className="fa-solid fa-angle-down"></i>
+          </span>
         </div>
-        <ul className="navkit__headersection__links">
-          <li className="navkit__headersection__links--link">
+        <ul className="dropMenu__links" id="vocal">
+          <li className="dropMenu__links__link">
             <Link
               to="/ecommerce/microphones"
               className="flexBox"
@@ -114,8 +139,8 @@ const HeaderKit = () => {
             >
               {'Microphones'}
             </Link>
-          </li>{' '}
-          <li className="navkit__headersection__links--link">
+          </li>
+          <li className="dropMenu__links__link">
             <Link
               to="/ecommerce/interfaces"
               className="flexBox"
