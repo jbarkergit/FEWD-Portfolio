9506c93adc24c808bd26d0b122bda7306b23ceee
Justin Barker <jbarkerpoc@gmail.com>
2023-05-26 22:17:34 -0500

    Converted TS to TSX, moved Hooks to local scope

--- src/portfolio/layouts/PortfolioFooter.tsx
+++ src/portfolio/layouts/PortfolioFooter.tsx
@@ -1,34 +1,77 @@
-import { NavigateLeft, NavigateRight } from '../hooks/useDemoNavigation';
-import { useActiveDemo } from '../hooks/useActiveDemo';
-
+import { useState } from 'react';
 import NavigationLabel from '../components/props/footer/NavigationLabelProps';
 
 const PortfolioFooter = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0),
+    demos: HTMLCollection = document.getElementsByClassName('demoSection');
+
+  function useLeftNav() {
+    const nextIndex: number = activeIndex - 1 >= 0 ? activeIndex - 1 : demos.length - 1,
+      activeDemo: HTMLElement = document.querySelector(`[data-index="${activeIndex}"]`)!,
+      nextDemo: HTMLElement = document.querySelector(`[data-index="${nextIndex}"]`)!;
+
+    activeDemo instanceof HTMLElement ? (activeDemo.dataset.status = 'after') : console.log(Error());
+    nextDemo instanceof HTMLElement ? (nextDemo.dataset.status = 'active-from-before') : console.log(Error());
+
+    setTimeout(() => {
+      nextDemo.dataset.status = 'active';
+      setActiveIndex(nextIndex);
+    }, 50);
+  }
+
+  function useRightNav() {
+    const nextIndex: number = activeIndex + 1 <= demos.length - 1 ? activeIndex + 1 : 0,
+      activeDemo: HTMLElement = document.querySelector(`[data-index="${activeIndex}"]`)!,
+      nextDemo: HTMLElement = document.querySelector(`[data-index="${nextIndex}"]`)!;
+
+    activeDemo instanceof HTMLElement ? (activeDemo.dataset.status = 'before') : console.log(Error());
+    nextDemo instanceof HTMLElement ? (nextDemo.dataset.status = 'active-from-after') : console.log(Error());
+
+    setTimeout(() => {
+      nextDemo.dataset.status = 'active';
+      setActiveIndex(nextIndex);
+    }, 50);
+  }
+
+  function useActiveDemo() {
+    if (activeIndex === 0) {
+      const portfolio = document.querySelector('.portfolio')!;
+      portfolio.classList.toggle('invalidShakeAnimation');
+      setTimeout(function () {
+        portfolio.classList.toggle('invalidShakeAnimation');
+      }, 165);
+    } else if (activeIndex === 1) {
+      location.href = '/ecommerce';
+    } else {
+      location.href = '*';
+    }
+  }
+
   return (
     <section>
       <footer aria-label="Footer Navigation">
         <nav className="portNav">
           <section className="portNav__block">
-            <button type="button" role="tab" tab-index={6} id="footerBtnLeft" onClick={NavigateLeft} aria-label="Previous Project Button">
+            <button type="button" role="tab" tab-index={6} onClick={useLeftNav} aria-label="Navigate to Previous Project">
               <span className="portNav__block__inline">
                 <i className="fa-solid fa-angles-left"></i>
               </span>
             </button>
           </section>
           <section className="portNav__block --labels flexBox">
-            <NavigationLabel projectLink="" tabIndex={7} ariaLabel="Navigation Button for Project One" htmlFor="demo-1" id="" />
-            <NavigationLabel projectLink="" tabIndex={8} ariaLabel="Navigation Button for Project Two" htmlFor="demo-2" id="" />
-            <NavigationLabel projectLink="" tabIndex={9} ariaLabel="Navigation Button for Project Three" htmlFor="demo-3" id="" />
+            <NavigationLabel projectLink="" tabIndex={7} ariaLabel="Navigate to Project One" htmlFor="demo-1" id="" />
+            <NavigationLabel projectLink="" tabIndex={8} ariaLabel="Navigate to Project Two" htmlFor="demo-2" id="" />
+            <NavigationLabel projectLink="" tabIndex={9} ariaLabel="Navigate to Project Three" htmlFor="demo-3" id="" />
           </section>
           <section className="portNav__block">
-            <button type="button" role="tab" tab-index={10} id="footerBtnRight" onClick={NavigateRight} aria-label="Next Project Button">
+            <button type="button" role="tab" tab-index={10} onClick={useRightNav} aria-label="Navigate to Next Project">
               <span className="portNav__block__inline">
                 <i className="fa-solid fa-angles-right"></i>
               </span>
             </button>
           </section>
           <section className="portNav__block">
-            <button role="tab" tab-index={11} onClick={useActiveDemo} aria-label="Navigate Button to Current Project">
+            <button role="tab" tab-index={11} onClick={useActiveDemo} aria-label="Navigate to Current Project">
               <span className="portNav__block__inline" id="activeDemoInline">
                 <i className="fa-solid fa-expand"></i>
                 <h4>Live Demo</h4>
