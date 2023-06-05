c3538e55e887707eb59cf8b129720fb34dfaf065
Justin Barker <jbarkerpoc@gmail.com>
2023-06-05 18:29:14 -0500

    Removed Fragmentation

--- src/App.tsx
+++ src/App.tsx
@@ -13,30 +13,28 @@ const ProductPage = lazy(() => import('./ecommerce/pages/ProductPage'));
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Suspense fallback={SuspenseLoader()}>
-          {/* @ts-ignore */}
-          <CategoryFilterProvider>
-            <Routes>
-              <Route path="/suspense-loader" element={<SuspenseLoader />} />
-              <Route path="*" element={<NotFound />} />
+    <BrowserRouter>
+      <Suspense fallback={SuspenseLoader()}>
+        {/* @ts-ignore */}
+        <CategoryFilterProvider>
+          <Routes>
+            <Route path="/suspense-loader" element={<SuspenseLoader />} />
+            <Route path="*" element={<NotFound />} />
 
-              <Route path="/" element={<Portfolio />} />
+            <Route path="/" element={<Portfolio />} />
 
-              <Route path="/ecommerce" element={<Ecommerce />} />
-              <Route path="/ecommerce/products" element={<BrowseProduct />} />
-              <Route path="/ecommerce/headphones" element={<BrowseProduct />} />
-              <Route path="/ecommerce/amps-dacs" element={<BrowseProduct />} />
-              <Route path="/ecommerce/equalizers" element={<BrowseProduct />} />
-              <Route path="/ecommerce/microphones" element={<BrowseProduct />} />
-              <Route path="/ecommerce/interfaces" element={<BrowseProduct />} />
-              <Route path="/ecommerce/product/:paramId" element={<ProductPage />} />
-            </Routes>
-          </CategoryFilterProvider>
-        </Suspense>
-      </BrowserRouter>
-    </>
+            <Route path="/ecommerce" element={<Ecommerce />} />
+            <Route path="/ecommerce/products" element={<BrowseProduct />} />
+            <Route path="/ecommerce/headphones" element={<BrowseProduct />} />
+            <Route path="/ecommerce/amps-dacs" element={<BrowseProduct />} />
+            <Route path="/ecommerce/equalizers" element={<BrowseProduct />} />
+            <Route path="/ecommerce/microphones" element={<BrowseProduct />} />
+            <Route path="/ecommerce/interfaces" element={<BrowseProduct />} />
+            <Route path="/ecommerce/product/:paramId" element={<ProductPage />} />
+          </Routes>
+        </CategoryFilterProvider>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
