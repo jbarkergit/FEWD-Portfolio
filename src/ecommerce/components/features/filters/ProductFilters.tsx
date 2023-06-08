3dfb47ea21c846897c8195d249960b7ed71830ce
Justin Barker <jbarkerpoc@gmail.com>
2023-06-07 21:55:02 -0500

    Added Filter by Style for products

--- src/ecommerce/components/features/filters/ProductFilters.tsx
+++ src/ecommerce/components/features/filters/ProductFilters.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useCategoryFilterContext } from '../../../context/products/categoryFilterProvider';
 import { ProductDatabase } from '../../../assets/production-data/ProductDatabase';
@@ -41,31 +41,63 @@ const useCompanies = () => {
 };
 
 const ProductFilters = () => {
-  useEffect(() => {
-    const selectMenu = document.querySelector('.selectMenu')!;
-    const menu = document.querySelector('.selectMenu__menu')!;
+  const selectMenu = useRef<HTMLDivElement>(null!);
+  const menu = useRef<HTMLUListElement>(null!);
 
+  const styleFilterSelectMenu = useRef<HTMLDivElement>(null!);
+  const styleFilterMenu = useRef<HTMLUListElement>(null!);
+
+  // @ts-ignore:
+  const { setCategoryFilter } = useCategoryFilterContext(); // Thrown error is a desired outcome to utilize useState from our context while ALSO offering guard to Application Context Provider
+
+  useEffect(() => {
     const useSelectMenu = (event: any) => {
-      if (menu.getAttribute('data-activity') === 'active' && !selectMenu.contains(event.target)) {
-        menu.setAttribute('data-activity', 'inactive');
-      } else if (menu.getAttribute('data-activity') === 'inactive' && !selectMenu.contains(event.target)) {
+      if (menu?.current?.getAttribute('data-activity') === 'active' && !selectMenu?.current?.contains(event.target)) {
+        menu?.current?.setAttribute('data-activity', 'inactive');
+      } else if (menu?.current?.getAttribute('data-activity') === 'inactive' && !selectMenu?.current?.contains(event.target)) {
         null;
-      } else if (menu.getAttribute('data-activity') === 'active' && selectMenu.contains(event.target)) {
-        menu.setAttribute('data-activity', 'inactive');
-      } else if (menu.getAttribute('data-activity') === 'inactive') {
-        menu.setAttribute('data-activity', 'active');
-      } else if (menu.getAttribute('data-activity') === 'active') {
-        menu.setAttribute('data-activity', 'inactive');
+      } else if (menu?.current?.getAttribute('data-activity') === 'active' && selectMenu?.current?.contains(event.target)) {
+        menu?.current?.setAttribute('data-activity', 'inactive');
+      } else if (menu?.current?.getAttribute('data-activity') === 'inactive') {
+        menu?.current?.setAttribute('data-activity', 'active');
+      } else if (menu?.current?.getAttribute('data-activity') === 'active') {
+        menu?.current?.setAttribute('data-activity', 'inactive');
       } else {
         null;
       }
     };
 
-    selectMenu.addEventListener('click', useSelectMenu);
+    selectMenu?.current?.addEventListener('click', useSelectMenu);
     document.body.addEventListener('click', useSelectMenu, true);
 
     return () => {
-      selectMenu.removeEventListener('click', useSelectMenu);
+      selectMenu?.current?.removeEventListener('click', useSelectMenu);
+      document.body.removeEventListener('click', useSelectMenu);
+    };
+  }, []);
+
+  useEffect(() => {
+    const useSelectMenu = (event: any) => {
+      if (styleFilterMenu?.current?.getAttribute('data-activity') === 'active' && !styleFilterSelectMenu?.current?.contains(event.target)) {
+        styleFilterMenu?.current?.setAttribute('data-activity', 'inactive');
+      } else if (styleFilterMenu?.current?.getAttribute('data-activity') === 'inactive' && !styleFilterSelectMenu?.current?.contains(event.target)) {
+        null;
+      } else if (styleFilterMenu?.current?.getAttribute('data-activity') === 'active' && styleFilterSelectMenu?.current?.contains(event.target)) {
+        styleFilterMenu?.current?.setAttribute('data-activity', 'inactive');
+      } else if (styleFilterMenu?.current?.getAttribute('data-activity') === 'inactive') {
+        styleFilterMenu?.current?.setAttribute('data-activity', 'active');
+      } else if (styleFilterMenu?.current?.getAttribute('data-activity') === 'active') {
+        styleFilterMenu?.current?.setAttribute('data-activity', 'inactive');
+      } else {
+        null;
+      }
+    };
+
+    styleFilterSelectMenu?.current?.addEventListener('click', useSelectMenu);
+    document.body.addEventListener('click', useSelectMenu, true);
+
+    return () => {
+      styleFilterSelectMenu?.current?.removeEventListener('click', useSelectMenu);
       document.body.removeEventListener('click', useSelectMenu);
     };
   }, []);
@@ -73,9 +105,27 @@ const ProductFilters = () => {
   return (
     <section className="productFilters flexBox">
       <div className="productFilters__panel">{useBreadcrumbs()}</div>
-
       <div className="productFilters__panel">
-        <div className="selectMenu">
+        <div className="selectMenu" ref={styleFilterSelectMenu} key="styleFilter">
+          <div className="selectMenu__selection">
+            <span className="selectMenu__selection__indicator">
+              <span className="selectMenu__selection__indicator--area">Filter by Style</span>
+              <span className="selectMenu__selection__indicator--area">
+                <i className="fa-solid fa-sort"></i>
+              </span>
+            </span>
+            <div className="selectMenu--divider"></div>
+          </div>
+          <ul className="selectMenu__menu" data-activity="inactive" ref={styleFilterMenu}>
+            <li className="selectMenu__menu--option" key="openbackheadphone">
+              <button onClick={() => setCategoryFilter('openbackheadphone')}>{'Open-Back'}</button>
+            </li>
+            <li className="selectMenu__menu--option" key="closedbackheadphone">
+              <button onClick={() => setCategoryFilter('closedbackheadphone')}>{'Closed-Back'}</button>
+            </li>
+          </ul>
+        </div>
+        <div className="selectMenu" ref={selectMenu} key="brandFilter">
           <div className="selectMenu__selection">
             <span className="selectMenu__selection__indicator">
               <span className="selectMenu__selection__indicator--area">Filter by Brand</span>
@@ -85,7 +135,7 @@ const ProductFilters = () => {
             </span>
             <div className="selectMenu--divider"></div>
           </div>
-          <ul className="selectMenu__menu" data-activity="inactive">
+          <ul className="selectMenu__menu" data-activity="inactive" ref={menu}>
             {useCompanies()}
           </ul>
         </div>
