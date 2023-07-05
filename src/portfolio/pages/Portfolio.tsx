2daa0c1899b7e1b9fb14fd36717310a91a40a6fe
Justin Barker <jbarkerpoc@gmail.com>
2023-07-05 16:28:11 -0500

    Portfolio, wip

--- src/portfolio/pages/Portfolio.tsx
+++ src/portfolio/pages/Portfolio.tsx
@@ -1,5 +1,15 @@
+import PortFooter from '../components/navigation/footer/PortFooter';
+import MainContent from '../components/main/MainContent';
+import PortHeader from '../components/navigation/header/PortHeader';
+
 const Portfolio = () => {
-  return <div className="portfolio themeToggler"></div>;
+  return (
+    <div id="portfolio">
+      <PortHeader />
+      <MainContent />
+      <PortFooter />
+    </div>
+  );
 };
 
 export default Portfolio;
