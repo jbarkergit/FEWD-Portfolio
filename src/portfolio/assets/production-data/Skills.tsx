592e0e3562a887bf7d0f4e819b68ebb4b86033a4
Justin Barker <jbarkerpoc@gmail.com>
2023-07-18 23:05:46 -0500

    Database Assets push to repo

---
+++ src/portfolio/assets/production-data/Skills.tsx
@@ -0,0 +1,76 @@
+const Skills = [
+  {
+    id: 'developer',
+    detail: 'My name is',
+    name: 'Justin Barker',
+    description: `I'm a React Front-End Developer. My expertise lies in constructing performance-focused, dynamic, and responsive layouts for the web. I strive to bring websites to life with efficiency and elegance.`,
+  },
+  {
+    id: 'html5',
+    detail: '.HTML',
+    name: 'HyperText Markup Language',
+    description: 'Search engine optimization, consistent code, class optimization and file size reduction.',
+  },
+  {
+    id: 'css3',
+    detail: '.CSS',
+    name: 'Cascading Style Sheets',
+    description: 'Performance oriented syntax with consistent global styling, class optimization and css focused solutions.',
+  },
+  {
+    id: 'sass',
+    detail: '.SASS',
+    name: 'Syntactically Awesome Style Sheets',
+    description: 'Efficient, performance oriented CSS preprocessor that reduces repetitious keystrokes.',
+  },
+  {
+    id: 'javascript',
+    detail: '.JS',
+    name: 'JavaScript',
+    description: 'Enhances site functionality, handles API interactions and aids in solving convoluted problems.',
+  },
+  {
+    id: 'typescript',
+    detail: '.TS',
+    name: 'TypeScript',
+    description: 'Strict syntactical superset of JavaScript that adds static typing.',
+  },
+  {
+    id: 'nodejs',
+    detail: 'Server Environment',
+    name: 'Node.js',
+    description: 'JavaScript runtime used for server-side programming, deployment of servers and back-end API services.',
+  },
+  {
+    id: 'npm',
+    detail: 'NPM',
+    name: 'Node Package Manager',
+    description: 'Library and registry for JavaScript software packages. Installs packages and dependencies.',
+  },
+  {
+    id: 'vite',
+    detail: 'Frontend Tooling',
+    name: 'VITE',
+    description: 'Development tool for modern web applications.',
+  },
+  {
+    id: 'vitest',
+    detail: 'Unit Test Framework',
+    name: 'Vitest',
+    description: 'Testing environment for source code.',
+  },
+  {
+    id: 'react',
+    detail: '.JSX',
+    name: 'React',
+    description: 'Creates interactive applications for the web and mobile devices while reducing JavaScript code.',
+  },
+  {
+    id: 'github',
+    detail: 'GITHUB / GIT',
+    name: 'Global Information Tracker',
+    description: 'Version Control System used to track and log changes, useful for coordination with multiple programmers.',
+  },
+];
+
+export default Skills;
