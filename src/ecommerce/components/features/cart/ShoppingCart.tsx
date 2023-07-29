81c9be45af95bc89aba7839b0673076ec2c406e4
Justin Barker <jbarkerpoc@gmail.com>
2023-07-29 09:22:42 -0500

    Minor styling

--- src/ecommerce/components/features/cart/ShoppingCart.tsx
+++ src/ecommerce/components/features/cart/ShoppingCart.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import useCart from '../../../hooks/useCart';
 import { Discover, Klarna, Mastercard, Stripe, Visa } from './PaymentMethods';
