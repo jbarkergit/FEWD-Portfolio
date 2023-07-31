46e1e00d721d9091635d0405497361d8cbf443e7
Justin Barker <jbarkerpoc@gmail.com>
2023-07-31 08:09:10 -0500

    Removal of buggy code

--- src/ecommerce/components/features/filters/BrandFilter.tsx
+++ src/ecommerce/components/features/filters/BrandFilter.tsx
@@ -2,35 +2,8 @@ import { useRef, useEffect, useState } from 'react';
 import Companies from './Companies';
 
 const BrandFilter = (): JSX.Element => {
-  const selectMenuRef = useRef<HTMLDivElement>(null!),
-    accordionRef = useRef<HTMLUListElement>(null!);
-  const [isAccordionOpen, setIsAccordionOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    const useBrandFilter = (e: MouseEvent): void => {
-      const target = e.target as HTMLElement;
-      const isSelectMenuClicked = target.classList.contains('selectMenu__selection');
-      console.log(target);
-      console.log(isSelectMenuClicked);
-      if (selectMenuRef.current && accordionRef.current) {
-        isSelectMenuClicked && !isAccordionOpen ? setIsAccordionOpen(true) : null;
-        isSelectMenuClicked && isAccordionOpen ? setIsAccordionOpen(false) : null;
-      }
-    };
-
-    if (selectMenuRef.current) selectMenuRef.current.addEventListener('click', useBrandFilter);
-    document.addEventListener('click', useBrandFilter);
-
-    return () => {
-      if (selectMenuRef.current) selectMenuRef.current.removeEventListener('click', useBrandFilter);
-      document.removeEventListener('click', useBrandFilter);
-    };
-  }, [selectMenuRef.current, accordionRef.current]);
-
-  useEffect(() => {
-    if (accordionRef.current)
-      isAccordionOpen ? accordionRef.current.setAttribute('data-status', 'active') : accordionRef.current.setAttribute('data-status', 'false');
-  }, [isAccordionOpen]);
+  const selectMenuRef = useRef<HTMLDivElement>(null),
+    accordionRef = useRef<HTMLUListElement>(null);
 
   return (
     <div className="selectMenu" ref={selectMenuRef}>
