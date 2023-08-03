66f76a7e7d576c1d6d073a090736cbeeff4e6fe0
Justin Barker <jbarkerpoc@gmail.com>
2023-08-03 11:46:43 -0500

    Product Page Tweaks/File Structure

--- src/ecommerce/pages/BrowseProduct.tsx
+++ src/ecommerce/pages/BrowseProduct.tsx
@@ -1,27 +1,16 @@
-import { v4 as uuidv4 } from 'uuid';
 import Header from '../components/navigation/header/Header';
 import Footer from '../components/navigation/footer/eFooter';
 import ProductFilters from '../components/features/product-filters/ProductFilters';
-import ProductProvider from '../components/product/ProductProvider';
-import { ProductType } from '../types/ProductType';
-import useCart from '../hooks/useCart';
-import useProductFilter from '../hooks/useProductFilter';
+import ProductCatalog from '../components/product/product-catalog/ProductCatalog';
 
 const BrowseProduct = (): JSX.Element => {
-  const { dispatch, REDUCER_ACTIONS } = useCart();
   return (
     <div data-theme="eco-light-mode">
       <Header />
       <section className="browseProduct">
         <ProductFilters />
         <main>
-          <ul className="productGrid">
-            {useProductFilter()
-              .sort((a: ProductType, b: ProductType) => (a.company > b.company ? 1 : -1))
-              .map((product: ProductType) => (
-                <ProductProvider key={uuidv4()} product={product} dispatch={dispatch} REDUCER_ACTIONS={REDUCER_ACTIONS} />
-              ))}
-          </ul>
+          <ProductCatalog />
         </main>
       </section>
       <Footer />
