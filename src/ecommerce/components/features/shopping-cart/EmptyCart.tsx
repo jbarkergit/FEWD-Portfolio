70bf59db76aba4f104e9fabf3645ce310d087f73
Justin Barker <jbarkerpoc@gmail.com>
2023-08-07 17:55:14 -0500

    Completed Navigation for Shopping Cart

--- src/ecommerce/components/features/shopping-cart/EmptyCart.tsx
+++ src/ecommerce/components/features/shopping-cart/EmptyCart.tsx
@@ -1,15 +1,30 @@
+import { Link } from 'react-router-dom';
+import { useCategoryFilterContext } from '../../../context/CategoryFilterContext';
+
 const EmptyCart = () => {
+  // @ts-ignore:
+  const { setCategoryFilter } = useCategoryFilterContext();
   return (
     <section className="ecoModal__products__emptyCart">
       <div className="ecoModal__products__emptyCart__header">
         <span>{'Your cart is empty. Ready to upgrade your setup?'}</span>
       </div>
-      <div className="ecoModal__products__emptyCart__buttons">
-        <button>{'Shop Headphones'}</button>
-        <button>{'Shop Amps & Dacs'}</button>
-        <button>{'Shop Microphones'}</button>
-        <button>{'Shop Interfaces'}</button>
-        <button>{'Browse All Products'}</button>
+      <div className="ecoModal__products__emptyCart__nav">
+        <Link to="/ecommerce/headphones" onClick={() => setCategoryFilter('')}>
+          {'Shop Headphones'}
+        </Link>
+        <Link to="/ecommerce/amps-dacs" onClick={() => setCategoryFilter('headphone')}>
+          {'Shop Amps & Dacs'}
+        </Link>
+        <Link to="/ecommerce/microphones" onClick={() => setCategoryFilter('amp', 'dac')}>
+          {'Shop Microphones'}
+        </Link>
+        <Link to="/ecommerce/interfaces" onClick={() => setCategoryFilter('microphone')}>
+          {'Shop Interfaces'}
+        </Link>
+        <Link to="/ecommerce/products" onClick={() => setCategoryFilter('interface')}>
+          {'Browse All Products'}
+        </Link>
       </div>
     </section>
   );
