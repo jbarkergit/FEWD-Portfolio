c0793cc12b1655469b3b60c5ef24c2dde628c9b4
Justin Barker <jbarkerpoc@gmail.com>
2023-08-12 16:25:04 -0500

    Improved documentation for notes to refactor

--- src/ecommerce/components/product/product-provider/ProductProvider.tsx
+++ src/ecommerce/components/product/product-provider/ProductProvider.tsx
@@ -7,21 +7,21 @@ import ProductProp from './ProductProp';
 import { useCategoryFilterContext } from '../../../context/CategoryFilterContext';
 
 const ProductProvider = () => {
-  const [fetchedSets, setFetchedSets] = useState<setArrayType[]>([]); //holds currently fetched sets for query
-  const paginatedSets: setArrayType[] = usePaginatedSets(); //localize custom hook import to bypass react warnings
-  //usePaginatedSets utilizes useProductFilter, which uses useState[categoryFilter] Context Hook to render appropriate product arrays
-
-  // @ts-ignore //categoryFilter is a single global state, which is used by useProductFilter -> rerenders ProductProvider Prop upon navigation
+  // @ts-ignore
   const { categoryFilter } = useCategoryFilterContext();
+  const [fetchedSets, setFetchedSets] = useState<setArrayType[]>([]); //Initialize empty array to hold paginated product sets
+  const paginatedSets: setArrayType[] = usePaginatedSets(); //Localize custom hook import to bypass warnings
+  //usePaginatedSets utilizes useProductFilter, which uses useState[categoryFilter] Context Hook to render appropriate product arrays
+  //categoryFilter is a single global state, which is used by useProductFilter -> rerenders ProductProvider Prop upon navigation
 
-  useEffect(() => setFetchedSets([paginatedSets[0]]), [categoryFilter]); //sets first product set from paginatedSets(usePaginatedSets Hook) as initState on nav
-  const lastProductRef = useRef<HTMLLIElement>(null); //ref for last rendered element for observer
+  useEffect(() => setFetchedSets([paginatedSets[0]]), [categoryFilter]); //Resets rendered product sets on page reload via paginatedSets(usePaginatedSets Hook)
+  const lastProductRef = useRef<HTMLLIElement>(null); //useRef to be placed on last rendered element (for the observer)
 
+  //Retrieves data and pushes to dom on scroll: If the last product is in the viewport, spread 9 new products into fetchedSets state
   useEffect(() => {
     if (lastProductRef.current) {
       const observer = new IntersectionObserver(
         (entries) => {
-          //if last product is in the viewport, spread new set into fetchedSets state
           if (entries[0].isIntersecting && fetchedSets) setFetchedSets((prevSets) => [...prevSets, paginatedSets[fetchedSets.length]]);
         },
         {
