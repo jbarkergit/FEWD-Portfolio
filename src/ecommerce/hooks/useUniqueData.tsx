543457f5e31d792e2e22669fac1aaf8a9cd478d4
Justin Barker <jbarkerpoc@gmail.com>
2023-08-12 01:39:08 -0500

    Created new set for unique microphone companies

--- src/ecommerce/hooks/useUniqueData.tsx
+++ src/ecommerce/hooks/useUniqueData.tsx
@@ -10,6 +10,7 @@ const useUniqueData = () => {
         uniquePolarPatternSet: Set<string>;
         uniqueWearStyleSet: Set<string>;
         uniqueHeadphoneCompanies: Set<string>;
+        uniqueMicrophoneCompanies: Set<string>;
       },
       product: ProductType
     ) => {
@@ -23,6 +24,8 @@ const useUniqueData = () => {
 
       if (product.category === 'headphones') result.uniqueHeadphoneCompanies.add(product.company as string); //uniqueHeadphoneCompanies
 
+      if (product.category === 'microphones') result.uniqueMicrophoneCompanies.add(product.company as string); //uniqueMicrophoneCompanies
+
       return result; //Return accumulator (stores filtered data)
     },
     //Initialize new Sets
@@ -31,6 +34,7 @@ const useUniqueData = () => {
       uniquePolarPatternSet: new Set<string>(),
       uniqueWearStyleSet: new Set<string>(),
       uniqueHeadphoneCompanies: new Set<string>(),
+      uniqueMicrophoneCompanies: new Set<string>(),
     }
   );
 
@@ -39,9 +43,10 @@ const useUniqueData = () => {
   const useUniquePolarPatterns: string[] = Array.from(uniqueDataProps.uniquePolarPatternSet).sort((a, b) => (a > b ? 1 : -1));
   const useUniqueWearStyles: string[] = Array.from(uniqueDataProps.uniqueWearStyleSet).sort((a, b) => (a > b ? 1 : -1));
   const useUniqueHeadphoneCompanies: string[] = Array.from(uniqueDataProps.uniqueHeadphoneCompanies).sort((a, b) => (a > b ? 1 : -1));
+  const useUniqueMicrophoneCompanies: string[] = Array.from(uniqueDataProps.uniqueMicrophoneCompanies).sort((a, b) => (a > b ? 1 : -1));
 
   //Returns sorted arrays
-  return { useUniqueCompanies, useUniquePolarPatterns, useUniqueWearStyles, useUniqueHeadphoneCompanies };
+  return { useUniqueCompanies, useUniquePolarPatterns, useUniqueWearStyles, useUniqueHeadphoneCompanies, useUniqueMicrophoneCompanies };
 };
 
 export default useUniqueData;
