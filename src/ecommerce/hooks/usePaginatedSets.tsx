52f5a4d6489a3ec0a97b7a5e98ff87a7a811ede1
Justin Barker <jbarkerpoc@gmail.com>
2023-08-12 18:31:25 -0500

    Added null check

--- src/ecommerce/hooks/usePaginatedSets.tsx
+++ src/ecommerce/hooks/usePaginatedSets.tsx
@@ -4,11 +4,10 @@ import useProductFilter from './useProductFilter';
 
 const usePaginatedSets = (): setArrayType[] => {
   const filteredProducts: ProductType[] | null = useProductFilter(); //Pull product data //useMemo not necessary, useProductFilter reruns on navigation.
-  console.log(filteredProducts);
   const paginatedProducts: setArrayType[] = []; //Initialize empty array for paginated sets
 
   //Iterate over products, slice into arrays of 7, push to storage array: paginatedProducts
-  if (filteredProducts != null)
+  if (filteredProducts)
     for (let i = 0; i < filteredProducts.length; i += 7) {
       const setItems = filteredProducts.slice(i, i + 7);
       paginatedProducts.push({ id: paginatedProducts.length + 1, products: setItems });
