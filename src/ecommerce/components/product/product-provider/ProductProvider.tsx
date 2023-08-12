8babc1fc95d7546fdf04a7b2f738605ecf406cfd
Justin Barker <jbarkerpoc@gmail.com>
2023-08-12 17:36:06 -0500

    Identified data rendering issue-> useProductFilter

--- src/ecommerce/components/product/product-provider/ProductProvider.tsx
+++ src/ecommerce/components/product/product-provider/ProductProvider.tsx
@@ -9,45 +9,44 @@ import { useCategoryFilterContext } from '../../../context/CategoryFilterContext
 const ProductProvider = () => {
   // @ts-ignore
   const { categoryFilter } = useCategoryFilterContext();
-  const [fetchedSets, setFetchedSets] = useState<setArrayType[]>([]); //Initialize empty array to hold paginated product sets
-  const paginatedSets: setArrayType[] = usePaginatedSets(); //Localize custom hook import to bypass warnings
   //usePaginatedSets utilizes useProductFilter, which uses useState[categoryFilter] Context Hook to render appropriate product arrays
   //categoryFilter is a single global state, which is used by useProductFilter -> rerenders ProductProvider Prop upon navigation
 
-  useEffect(() => setFetchedSets([paginatedSets[0]]), [categoryFilter]); //Resets rendered product sets on page reload via paginatedSets(usePaginatedSets Hook)
+  const paginatedSets: setArrayType[] = usePaginatedSets(); //Filtered & paginated products -> localize hook to prevent warnings
+  // console.log(paginatedSets);
+  const [renderedSets, setRenderedSets] = useState<setArrayType[]>([paginatedSets[0]]); //Initialize empty array to hold paginated product sets
+  useEffect(() => setRenderedSets([paginatedSets[0]]), [categoryFilter]); //Resets renderedSets state to hold the first set of products
+
   const lastProductRef = useRef<HTMLLIElement>(null); //useRef to be placed on last rendered element (for the observer)
 
-  //Retrieves data and pushes to dom on scroll: If the last product is in the viewport, spread 9 new products into fetchedSets state
+  //Retrieves data and pushes to fetchedSetsStorage when last product is in the viewport
   useEffect(() => {
-    if (lastProductRef.current) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting && fetchedSets) setFetchedSets((prevSets) => [...prevSets, paginatedSets[fetchedSets.length]]);
-        },
-        {
-          threshold: 0.5,
-        }
-      );
-      observer.observe(lastProductRef.current);
-      return () => {
-        if (lastProductRef.current) observer.unobserve(lastProductRef.current);
-      };
-    }
-  }, [lastProductRef, fetchedSets]);
+    const observer = new IntersectionObserver((entries) => {
+      //Check if the last product element is visible
+      if (entries[0].isIntersecting) setRenderedSets((renderedProductSets) => [...renderedProductSets, paginatedSets[renderedSets.length]]);
+    });
+
+    if (lastProductRef.current) observer.observe(lastProductRef.current);
+    return () => {
+      if (lastProductRef.current) observer.unobserve(lastProductRef.current);
+      observer.disconnect;
+    };
+  }, []);
 
   return (
     <ul className="productGrid">
-      {fetchedSets.length > 0 &&
-        fetchedSets.map((productSet: setArrayType) =>
-          productSet?.products.map((product: ProductType, index: number) => {
-            return (
-              <Fragment key={uuidv4()}>
-                <ProductProp product={product} />
-                {fetchedSets.length - 1 === index && fetchedSets[fetchedSets.length - 1] === productSet && <li key="lastProduct" ref={lastProductRef} />}
-              </Fragment>
-            );
-          })
-        )}
+      {renderedSets.length > 0
+        ? renderedSets.map((productSet: setArrayType) =>
+            productSet?.products.map((product: ProductType, index: number) => {
+              return (
+                <Fragment key={uuidv4()}>
+                  <ProductProp product={product} />
+                  {renderedSets.length - 1 === index && renderedSets[renderedSets.length - 1] === productSet && <li key="lastProduct" ref={lastProductRef} />}
+                </Fragment>
+              );
+            })
+          )
+        : null}
     </ul>
   );
 };
--- src/ecommerce/hooks/usePaginatedSets.tsx
+++ src/ecommerce/hooks/usePaginatedSets.tsx
@@ -1,16 +1,15 @@
-import { useMemo } from 'react';
 import { ProductType } from '../types/ProductType';
 import { setArrayType } from '../types/SetArrayType';
 import useProductFilter from './useProductFilter';
 
 const usePaginatedSets = (): setArrayType[] => {
-  const useFilteredProducts: ProductType[] = useProductFilter(); //Pull product data
-  const filteredProducts: ProductType[] = useFilteredProducts; //useMemo not necessary, useProductFilter reruns on navigation.
+  const filteredProducts: ProductType[] = useProductFilter(); //Pull product data //useMemo not necessary, useProductFilter reruns on navigation.
   const paginatedProducts: setArrayType[] = []; //Initialize empty array for paginated sets
 
-  //Iterate over products, slice into arrays of 9, push to storage array: paginatedProducts
-  for (let i = 0; i < filteredProducts.length; i += 9) {
-    const setItems = filteredProducts.slice(i, i + 9);
+  console.log(filteredProducts);
+  //Iterate over products, slice into arrays of 7, push to storage array: paginatedProducts
+  for (let i = 0; i < filteredProducts.length; i += 7) {
+    const setItems = filteredProducts.slice(i, i + 7);
     paginatedProducts.push({ id: paginatedProducts.length + 1, products: setItems });
   }
 
