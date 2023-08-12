a61b76a63327d06589276ab0bc886e03cf621984
Justin Barker <jbarkerpoc@gmail.com>
2023-08-12 01:23:31 -0500

    Seperated+Memoized custom hook data

--- src/ecommerce/components/features/product-filters/ConditionallyRenderedProductFilters.tsx
+++ src/ecommerce/components/features/product-filters/ConditionallyRenderedProductFilters.tsx
@@ -1,20 +1,22 @@
+import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useCategoryFilterContext } from '../../../context/CategoryFilterContext';
 import useUniqueData from '../../../hooks/useUniqueData';
 import ProductFilterConstructor from './ProductFilterConstructor';
 
 const ConditionallyRenderedProductFilters = () => {
-  //   @ts-ignore
-  const { categoryFilter } = useCategoryFilterContext();
+  //Memoized data dependencies
+  const uniqueWearStyles = useMemo(() => useUniqueData().useUniqueWearStyles, [useUniqueData().useUniqueCompanies]);
+  const uniquePolarPatterns = useMemo(() => useUniqueData().useUniquePolarPatterns, [useUniqueData().useUniquePolarPatterns]);
   //Filter Components built with ProductFilterConstructor: takes initial filter name and custom hook that returns data
   //Data filtered via reduce method using useUniqueData custom hook
-  const PolarPatternFilter = (): JSX.Element => ProductFilterConstructor('Filter by Polar Pattern', useUniqueData().useUniquePolarPatterns);
-  const WearStyleFilter = (): JSX.Element => ProductFilterConstructor('Filter by Wear Style', useUniqueData().useUniqueWearStyles);
+  const WearStyleFilter = (): JSX.Element => ProductFilterConstructor('Filter by Wear Style', uniqueWearStyles);
+  const PolarPatternFilter = (): JSX.Element => ProductFilterConstructor('Filter by Polar Pattern', uniquePolarPatterns);
+
   //Condition for renders
   const useLoc = useLocation().pathname.replace('/ecommerce/', '');
 
-  if (useLoc === 'headphones' || useUniqueData().useUniqueWearStyles.includes(useLoc)) return <WearStyleFilter />;
-  else if (useLoc === 'microphones' || useUniqueData().useUniquePolarPatterns.includes(useLoc)) return <PolarPatternFilter />;
+  if (useLoc === 'headphones' || uniqueWearStyles.includes(useLoc)) return <WearStyleFilter />;
+  else if (useLoc === 'microphones' || uniquePolarPatterns.includes(useLoc)) return <PolarPatternFilter />;
 };
 
 export default ConditionallyRenderedProductFilters;
