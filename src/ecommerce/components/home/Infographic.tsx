b4acf02e68e977cd3a998437aa2b0908515a1d21
Justin Barker <jbarkerpoc@gmail.com>
2023-08-19 06:39:37 -0500

    Reworked infographic styling

--- src/ecommerce/components/home/Infographic.tsx
+++ src/ecommerce/components/home/Infographic.tsx
@@ -6,28 +6,34 @@ const Infographic = (): JSX.Element => {
       <picture className="infographic__graphic">
         <span className="imgFadeGradient" />
         <img
-          src="/src/ecommerce/assets/production-images/compressed-home-page/infographic/img-by-ilias-chebbi-on-unsplash.jpg"
-          alt="Dynamic Audio - Your go-to source for audiophile grade HiFi audio equipment"
+          src="src/ecommerce/assets/production-images/compressed-home-page/infographic/img-by-ilias-chebbi-on-unsplash.jpg"
+          alt="Dynamic Audio - Your new go-to source for audiophile grade HiFi audio equipment"
           decoding="async"
           fetchpriority="high"
         />
       </picture>
       <article className="infographic__information">
-        <hgroup>
-          <h2>Your go-to source for audiophile grade HiFi audio equipment</h2>
-          <h1>
-            Unparalleled HiFi audio and superior manufacturing for discerning audiophiles. Crystal-clear sound, premium materials, meticulous craftsmanship, and
-            lasting durability.
-          </h1>
-        </hgroup>
-        <span className="infographic__information__cta">
-          <Link to={''} role="button">
-            Learn More
-          </Link>
-          <Link to={''} role="button">
-            Save Up To 25%
-          </Link>
-        </span>
+        <div className="infographic__information__details">
+          <hgroup>
+            <h3>
+              now partnered with <span className="highlight">beyerdynamic</span>
+            </h3>
+            <h2>the new hifi shop</h2>
+            <h1>
+              Unparalleled HiFi audio and <span className="highlight">superior</span> manufacturing for discerning audiophiles. Crystal-clear sound,{' '}
+              <span className="highlight">premium </span>
+              materials, meticulous <span className="highlight">craftsmanship</span>, and lasting durability.
+            </h1>
+          </hgroup>
+          <div className="infographic__information__details__cta">
+            <Link to={''} role="button">
+              Learn More
+            </Link>
+            <Link to={''} role="button">
+              Save Up To 25%
+            </Link>
+          </div>
+        </div>
         <picture className="infographic__information__logos">
           <img src="src/ecommerce/assets/production-images/compressed-company-logos/audientLogo.svg" decoding="async" fetchpriority="low" />
           <img src="src/ecommerce/assets/production-images/compressed-company-logos/beyerdynamicLogo.svg" decoding="async" fetchpriority="low" />
