5cc51f6c2a65c2863caaa927c257971aa31f7ed9
Justin Barker <jbarkerpoc@gmail.com>
2023-08-20 19:43:24 -0500

    Vitest dom env w testing-library/react & jest-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,6 +11,11 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts']
   },
   build: { copyPublicDir: false },
 });
