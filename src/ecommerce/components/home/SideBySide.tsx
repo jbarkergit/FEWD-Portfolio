25e637cdccf057223778994c17c9850d0387bc45
Justin Barker <jbarkerpoc@gmail.com>
2023-08-21 23:24:57 -0500

    Media queries

--- src/ecommerce/components/home/SideBySide.tsx
+++ src/ecommerce/components/home/SideBySide.tsx
@@ -2,10 +2,9 @@ const SideBySide = (): JSX.Element => {
   return (
     <article className="sideBySide">
       <div className="sideBySide__textArea">
-        <h3>
-          <span className="highlight">Satisfaction Guaranteed</span>
-        </h3>
-        <h2>Superior Audio Backed by Groundbreaking Tech</h2>
+        <h2>
+          Groundbreaking Tech, <span className="highlight">superior</span> audio
+        </h2>
         <p>
           Our mission at Dynamic Audio is to offer a diverse selection of superior HiFi audio products from renowned brands, to ensure top-notch sound quality for
           all. Our curated collection features high-fidelity headphones crafted by industry-leading manufacturers known for their commitment to excellence. With
