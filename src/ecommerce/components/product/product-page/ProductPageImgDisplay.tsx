0d46cd05505ada82fc3826b982461e767b477863
Justin Barker <jbarkerpoc@gmail.com>
2023-08-22 09:12:03 -0500

    Media Queries

--- src/ecommerce/components/product/product-page/ProductPageImgDisplay.tsx
+++ src/ecommerce/components/product/product-page/ProductPageImgDisplay.tsx
@@ -22,19 +22,23 @@ const ProductPageImgDisplay = ({ findProduct, activeDisplay, setActiveDisplay }:
       {images!.length === 1 ? null : (
         <div className="skuPage__grid__display__nav">
           <button onClick={() => (activeDisplay === 0 ? setActiveDisplay(lastSlide) : setActiveDisplay(activeDisplay - 1))}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="1.8em" height="1.8em" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="m8.165 11.63l6.63-6.43C15.21 4.799 16 5.042 16 5.57v12.86c0 .528-.79.771-1.205.37l-6.63-6.43a.499.499 0 0 1 0-.74Z"
-              ></path>
-            </svg>
-            Previous Image
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.8em" height="1.8em" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="m8.165 11.63l6.63-6.43C15.21 4.799 16 5.042 16 5.57v12.86c0 .528-.79.771-1.205.37l-6.63-6.43a.499.499 0 0 1 0-.74Z"
+                ></path>
+              </svg>
+              <span>Previous Image</span>
+            </span>
           </button>
           <button onClick={() => (activeDisplay === lastSlide ? setActiveDisplay(1) : setActiveDisplay(activeDisplay + 1))}>
-            Next Image
-            <svg xmlns="http://www.w3.org/2000/svg" width="1.8em" height="1.8em" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M15.835 11.63L9.205 5.2C8.79 4.799 8 5.042 8 5.57v12.86c0 .528.79.771 1.205.37l6.63-6.43a.498.498 0 0 0 0-.74Z"></path>
-            </svg>
+            <span>
+              <span>Next Image</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.8em" height="1.8em" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M15.835 11.63L9.205 5.2C8.79 4.799 8 5.042 8 5.57v12.86c0 .528.79.771 1.205.37l6.63-6.43a.498.498 0 0 0 0-.74Z"></path>
+              </svg>
+            </span>
           </button>
         </div>
       )}
