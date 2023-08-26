350cb555df89cd13dd1db96d79e81a9be54a0e5b
Justin Barker <jbarkerpoc@gmail.com>
2023-08-26 02:39:57 -0500

    Text update

--- src/ecommerce/components/home/Infographic.tsx
+++ src/ecommerce/components/home/Infographic.tsx
@@ -16,7 +16,7 @@ const Infographic = (): JSX.Element => {
         <div className="infographic__information__details">
           <hgroup>
             <h3>
-              Now partnered with <span className="highlight">beyerdynamic</span>
+              New drops from <span className="highlight">beyerdynamic</span>
             </h3>
             <h2>the new hifi shop</h2>
             <h1>
