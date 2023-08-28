191eda28ec281883e9bfca7af27c0237d0c7730f
Justin Barker <jbarkerpoc@gmail.com>
2023-08-27 22:46:52 -0500

    Conditional check for render

--- src/portfolio/components/navigation/footer/PortFooter.tsx
+++ src/portfolio/components/navigation/footer/PortFooter.tsx
@@ -43,11 +43,15 @@ const PortFooter = ({ stateIndex }: ProjectNavPropType) => {
     <footer className="portFooter">
       <nav className="portFooter__nav">
         <section className="portFooter__nav__mobileLeft">
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-              <path fill="#ffffff" d="M16 18v2H5v-2h11Zm5-7v2H3v-2h18Zm-2-7v2H8V4h11Z"></path>
-            </svg>
-          </button>
+          {getDemoLink() ? (
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
+                <path fill="#ffffff" d="M16 18v2H5v-2h11Zm5-7v2H3v-2h18Zm-2-7v2H8V4h11Z"></path>
+              </svg>
+            </button>
+          ) : (
+            <span className="portfoliofooterigation__unavailable">this project is unavailable</span>
+          )}
         </section>
         <section className="portFooter__nav__left">
           {getExtendedInfo() ? <button>Project Overview</button> : <span className="portfoliofooterigation__unavailable">this project is unavailable</span>}
