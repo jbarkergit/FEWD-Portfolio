f4be44d8856a1238ed4750bc1d9d344d842046e2
Justin Barker <jbarkerpoc@gmail.com>
2023-08-27 19:00:54 -0500

    Removed custom attribute extension

--- extended-attributes.tsx
+++ extended-attributes.tsx
@@ -3,6 +3,5 @@ import { AriaAttributes, DOMAttributes } from 'react';
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     fetchpriority?: 'high' | 'low' | 'auto';
-    prevPercentage?: string;
   }
 }
