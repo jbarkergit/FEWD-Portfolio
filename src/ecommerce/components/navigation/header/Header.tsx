82fac155c348f899d505deaca6208c2945944253
Justin Barker <jbarkerpoc@gmail.com>
2023-09-10 16:17:25 -0500

    Moved modals out of header

--- src/ecommerce/components/navigation/header/Header.tsx
+++ src/ecommerce/components/navigation/header/Header.tsx
@@ -11,20 +11,22 @@ const PrimaryNav = (): JSX.Element => {
   const [uiModal, setUiModal] = useState<string>('');
 
   return (
-    <header
-      className='navkit'
-      style={{
-        position: useLocation().pathname === '/ecommerce' ? 'absolute' : 'relative',
-        left: useLocation().pathname === '/ecommerce' ? '50%' : '0',
-        transform: useLocation().pathname === '/ecommerce' ? 'translateX(-50%)' : 'translateX(0)',
-      }}>
-      <LogoArea />
-      <NavigationLinks />
-      <UserInteractions uiModal={uiModal} setUiModal={setUiModal} />
-      <UserAccountModal uiModal={uiModal} setUiModal={setUiModal} />
+    <>
+      <header
+        className='navkit'
+        style={{
+          position: useLocation().pathname === '/ecommerce' ? 'absolute' : 'relative',
+          left: useLocation().pathname === '/ecommerce' ? '50%' : '0',
+          transform: useLocation().pathname === '/ecommerce' ? 'translateX(-50%)' : 'translateX(0)',
+        }}>
+        <LogoArea />
+        <NavigationLinks />
+        <UserInteractions uiModal={uiModal} setUiModal={setUiModal} />
+      </header>
       {uiModal === 'shoppingCart' ? <ShoppingCart uiModal={uiModal} setUiModal={setUiModal} /> : null}
       {uiModal === 'mobileMenu' ? <MobileMenu uiModal={uiModal} setUiModal={setUiModal} /> : null}
-    </header>
+      <UserAccountModal uiModal={uiModal} setUiModal={setUiModal} />
+    </>
   );
 };
 
