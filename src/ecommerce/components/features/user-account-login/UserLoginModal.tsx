5fd502cf7341495a1da72e2ddae7782da8107d0c
Justin Barker <jbarkerpoc@gmail.com>
2023-09-10 14:52:50 -0500

    RegExp patterns

--- src/ecommerce/components/features/user-account-login/UserLoginModal.tsx
+++ src/ecommerce/components/features/user-account-login/UserLoginModal.tsx
@@ -27,6 +27,13 @@ const UserLoginModal = ({ uiModal, setUiModal }: PropType): JSX.Element => {
     return () => document.body.removeEventListener('pointerdown', handleExteriorClick);
   }, []);
 
+  //Username Regex Pattern //RFC2822 Standard
+  const userEmailAddressRegex: RegExp =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+  //Password Regex Pattern //8 chars, 1 uppercase, 1 lowercase, 1 number, special chars enabled
+  const userPasswordRegex: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+
   //Form validation
   const [emailAddress, setEmailAddress] = useState<string>('test@email.com');
   const [password, setPassword] = useState<string>('test');
