3651048aeb87a12944730ba4f34c0168c9454223
Justin Barker <jbarkerpoc@gmail.com>
2023-09-17 22:41:23 -0500

    Wrapped svgs in links

--- src/ecommerce/components/navigation/footer/additions/social-media/SocialMedia.tsx
+++ src/ecommerce/components/navigation/footer/additions/social-media/SocialMedia.tsx
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const SocialMedia = () => {
   return (
     <>
-      <span>
+      <Link to=''>
         <svg xmlns='http://www.w3.org/2000/svg' width='1.6em' height='1.6em' viewBox='0 0 128 128'>
           <rect width='118.35' height='118.35' x='4.83' y='4.83' fill='#3d5a98' rx='6.53' ry='6.53'></rect>
           <path
             fill='#fff'
             d='M86.48 123.17V77.34h15.38l2.3-17.86H86.48v-11.4c0-5.17 1.44-8.7 8.85-8.7h9.46v-16A126.56 126.56 0 0 0 91 22.7c-13.62 0-23 8.3-23 23.61v13.17H52.62v17.86H68v45.83z'></path>
         </svg>
-      </span>
-      <span>
+      </Link>
+      <Link to=''>
         <svg xmlns='http://www.w3.org/2000/svg' width='1.6em' height='1.6em' viewBox='0 0 256 256'>
           <g fill='none'>
             <rect width='256' height='256' fill='url(#skillIconsInstagram0)' rx='60'></rect>
@@ -44,8 +46,8 @@ const SocialMedia = () => {
             </defs>
           </g>
         </svg>
-      </span>
-      <span>
+      </Link>
+      <Link to=''>
         <svg xmlns='http://www.w3.org/2000/svg' width='1.6em' height='1.6em' viewBox='0 0 256 256'>
           <g fill='none'>
             <rect width='256' height='256' fill='#fff' rx='60'></rect>
@@ -55,7 +57,7 @@ const SocialMedia = () => {
               d='M199.572 91.411c.11 1.587.11 3.174.11 4.776c0 48.797-37.148 105.075-105.075 105.075v-.03A104.54 104.54 0 0 1 38 184.677c2.918.351 5.85.526 8.79.533a74.154 74.154 0 0 0 45.864-15.839a36.976 36.976 0 0 1-34.5-25.645a36.811 36.811 0 0 0 16.672-.636c-17.228-3.481-29.623-18.618-29.623-36.198v-.468a36.705 36.705 0 0 0 16.76 4.622c-16.226-10.845-21.228-32.432-11.43-49.31a104.814 104.814 0 0 0 76.111 38.582a36.95 36.95 0 0 1 10.683-35.283c14.874-13.982 38.267-13.265 52.249 1.601a74.105 74.105 0 0 0 23.451-8.965a37.061 37.061 0 0 1-16.234 20.424A73.446 73.446 0 0 0 218 72.282a75.023 75.023 0 0 1-18.428 19.13Z'></path>
           </g>
         </svg>
-      </span>
+      </Link>
     </>
   );
 };
