ad964a56c73129c758dce6a69af02e76ff09505b
Justin Barker <jbarkerpoc@gmail.com>
2023-09-18 19:22:51 -0500

    Moved semantic tag back into map

--- src/ecommerce/components/product/product-page/product-infographic/ProductPageImgSelect.tsx
+++ src/ecommerce/components/product/product-page/product-infographic/ProductPageImgSelect.tsx
@@ -1,6 +1,5 @@
 import { Dispatch, SetStateAction } from 'react';
 import { ProductType } from '../../../../types/ProductType';
-import { v4 as uuidv4 } from 'uuid';
 
 type PropType = {
   findProduct: ProductType;
@@ -13,11 +12,11 @@ const ProductPageImgSelect = ({ findProduct, setActiveDisplay }: PropType) => {
   return (
     <aside className='skuPage__grid__imgSelection'>
       <div className='skuPage__grid__imgSelection__images'>
-        <picture>
-          {images!.map((image, index) => (
-            <img src={image} alt={company + unit} role='presentation' decoding='async' fetchpriority='high' onClick={() => setActiveDisplay(index)} key={uuidv4()} />
-          ))}
-        </picture>
+        {images!.map((image, index) => (
+          <picture key={image}>
+            <img src={image} alt={company + unit} role='presentation' decoding='async' fetchpriority='high' onClick={() => setActiveDisplay(index)} />
+          </picture>
+        ))}
       </div>
     </aside>
   );
