82660a2ba8f5db93b34dfac04e447640fe4b2edd
Justin Barker <jbarkerpoc@gmail.com>
2023-09-23 19:11:43 -0500

    Overhauled Infographic logos

--- src/ecommerce/components/home/Infographic.tsx
+++ src/ecommerce/components/home/Infographic.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import EPartners from '../navigation/footer/partners/EPartners';
 
 const Infographic = (): JSX.Element => {
   return (
@@ -35,138 +34,177 @@ const Infographic = (): JSX.Element => {
             </Link>
           </div>
         </div>
-        <picture className='infographic__information__logos'>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Apos.png'
-              alt='Apos'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Audient.svg'
-              alt='Audient'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Beyerdynamic.svg'
-              alt='Beyerdynamic'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/ElectroVoice.png'
-              alt='Electro Voice'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Evo.svg'
-              alt='Evo'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Focusrite.png'
-              alt='Focusrite'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Neumann.webp'
-              alt='Neumann'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Rode.svg'
-              alt='Rode'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Schiit.png'
-              alt='Schiit'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/Shure.svg'
-              alt='Shure'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/SMSL.png'
-              alt='SMSL'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/SolidStateLogic.svg'
-              alt='Solid State Logic'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-          <picture>
-            <img
-              src='src/ecommerce/assets/production-images/compressed-company-logos/UAudio.png'
-              alt='UAudio'
-              draggable='false'
-              loading='lazy'
-              decoding='async'
-              fetchpriority='low'
-            />
-          </picture>
-        </picture>
+        <div className='infographic__information__logos'>
+          <Link to='http://localhost:5173/ecommerce/Apos'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Apos.png'
+                alt='Apos'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Audient'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Audient.svg'
+                alt='Audient'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Avid'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Avid.png'
+                alt='Avid'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Beyerdynamic'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Beyerdynamic.svg'
+                alt='Beyerdynamic'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Electro-Voice'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/ElectroVoice.png'
+                alt='Electro Voice'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Evo'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Evo.svg'
+                alt='Evo'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Focusrite'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Focusrite.png'
+                alt='Focusrite'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Neumann'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Neumann.webp'
+                alt='Neumann'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Rode'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Rode.svg'
+                alt='Rode'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/S.M.S.L'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/SMSL.png'
+                alt='SMSL'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Schiit'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Schiit.png'
+                alt='Schiit'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/Shure'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/Shure.svg'
+                alt='Shure'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+
+          <Link to='http://localhost:5173/ecommerce/Solid%20State%20Logic'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/SolidStateLogic.svg'
+                alt='Solid State Logic'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+          <Link to='http://localhost:5173/ecommerce/UAudio'>
+            <picture>
+              <img
+                src='src/ecommerce/assets/production-images/compressed-company-logos/UAudio.png'
+                alt='UAudio'
+                draggable='false'
+                loading='lazy'
+                decoding='async'
+                fetchpriority='low'
+              />
+            </picture>
+          </Link>
+        </div>
       </article>
     </main>
   );
