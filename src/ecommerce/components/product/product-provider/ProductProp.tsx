ded1656382f2e86995b77ee04f26613808401ae5
Justin Barker <jbarkerpoc@gmail.com>
2023-09-26 09:54:41 -0500

    Prep work for image optimization overhaul

--- src/ecommerce/components/product/product-provider/ProductProp.tsx
+++ src/ecommerce/components/product/product-provider/ProductProp.tsx
@@ -1,7 +1,7 @@
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductType } from '../../../types/ProductType';
 import useCart from '../../../hooks/useCart';
-import { useEffect } from 'react';
 
 type ProductPropType = {
   product: ProductType;
@@ -11,19 +11,28 @@ const ProductProp = ({ product }: ProductPropType): JSX.Element => {
   const { sku, company, unit, description, price, images } = product;
   const { dispatch, REDUCER_ACTIONS, shoppingCart, cartProductQuantity } = useCart();
 
-  //set Shopping Cart array of products in local Storage
+  //Set Shopping Cart array of products in local Storage
+  useEffect(() => localStorage.setItem('shoppingCartState', JSON.stringify(shoppingCart)), [cartProductQuantity]);
+
+  //Optimize image rendering by dynamically setting dimensions
+  const pictureRef = useRef<HTMLElement>(null);
+  const [pictureDimensions, setPictureDimensions] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
+
   useEffect(() => {
-    localStorage.setItem('shoppingCartState', JSON.stringify(shoppingCart));
-  }, [cartProductQuantity]);
+    if (pictureRef.current) setPictureDimensions({ width: pictureRef.current.offsetWidth, height: pictureRef.current.offsetHeight });
+
+    // width={`${pictureDimensions.width}`}
+    // height={`${pictureDimensions.height}`}
+  }, [pictureRef.current]);
 
   return (
-    <article className="productGrid__product" key={product.sku}>
-      <Link to={`/ecommerce/product/${sku}`} className="productGrid__product__pictureLink">
-        <span className="productGrid__product--containedHover">
-          <picture>{images ? <img src={images![0]} alt={unit} loading="lazy" decoding="async" fetchpriority="high" /> : null}</picture>
+    <article className='productGrid__product' key={product.sku}>
+      <Link to={`/ecommerce/product/${sku}`} className='productGrid__product__pictureLink'>
+        <span className='productGrid__product--containedHover'>
+          <picture ref={pictureRef}>{images ? <img src={images![0]} alt={unit} loading='lazy' decoding='async' fetchpriority='high' /> : null}</picture>
         </span>
       </Link>
-      <div className="productGrid__product__information">
+      <div className='productGrid__product__information'>
         <Link to={`/ecommerce/product/${sku}`}>
           <h2>
             {company} {unit}
@@ -31,11 +40,10 @@ const ProductProp = ({ product }: ProductPropType): JSX.Element => {
         </Link>
         <p>{description}</p>
         <button onClick={() => dispatch({ type: REDUCER_ACTIONS.ADD, payload: { ...product, quantity: 1 } })}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em" viewBox="0 0 24 24">
+          <svg xmlns='http://www.w3.org/2000/svg' width='1.4em' height='1.4em' viewBox='0 0 24 24'>
             <path
-              fill="hsl(0, 0%, 100%)"
-              d="M2.237 2.288a.75.75 0 1 0-.474 1.423l.265.089c.676.225 1.124.376 1.453.529c.312.145.447.262.533.382c.087.12.155.284.194.626c.041.361.042.833.042 1.546v2.672c0 1.367 0 2.47.117 3.337c.12.9.38 1.658.982 2.26c.601.602 1.36.86 2.26.981c.866.117 1.969.117 3.336.117H18a.75.75 0 0 0 0-1.5h-7c-1.435 0-2.436-.002-3.192-.103c-.733-.099-1.122-.28-1.399-.556c-.235-.235-.4-.551-.506-1.091h10.12c.959 0 1.438 0 1.814-.248c.376-.248.565-.688.943-1.57l.428-1c.81-1.89 1.215-2.834.77-3.508C19.533 6 18.506 6 16.45 6H5.745a8.996 8.996 0 0 0-.047-.833c-.055-.485-.176-.93-.467-1.333c-.291-.404-.675-.66-1.117-.865c-.417-.194-.946-.37-1.572-.58l-.305-.1ZM7.5 18a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3Zm9 0a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3Z"
-            ></path>
+              fill='hsl(0, 0%, 100%)'
+              d='M2.237 2.288a.75.75 0 1 0-.474 1.423l.265.089c.676.225 1.124.376 1.453.529c.312.145.447.262.533.382c.087.12.155.284.194.626c.041.361.042.833.042 1.546v2.672c0 1.367 0 2.47.117 3.337c.12.9.38 1.658.982 2.26c.601.602 1.36.86 2.26.981c.866.117 1.969.117 3.336.117H18a.75.75 0 0 0 0-1.5h-7c-1.435 0-2.436-.002-3.192-.103c-.733-.099-1.122-.28-1.399-.556c-.235-.235-.4-.551-.506-1.091h10.12c.959 0 1.438 0 1.814-.248c.376-.248.565-.688.943-1.57l.428-1c.81-1.89 1.215-2.834.77-3.508C19.533 6 18.506 6 16.45 6H5.745a8.996 8.996 0 0 0-.047-.833c-.055-.485-.176-.93-.467-1.333c-.291-.404-.675-.66-1.117-.865c-.417-.194-.946-.37-1.572-.58l-.305-.1ZM7.5 18a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3Zm9 0a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3Z'></path>
           </svg>
           {Intl.NumberFormat('en-us', { currency: 'USD', style: 'currency' }).format(price)}
         </button>
