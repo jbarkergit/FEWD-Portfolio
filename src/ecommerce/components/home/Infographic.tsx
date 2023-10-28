144a6e51df7646ad8913d3afe74ff320a335fe81
Justin Barker <jbarkerpoc@gmail.com>
2023-10-28 09:08:38 -0500

    Rewriting hero section, WIP

--- src/ecommerce/components/home/Infographic.tsx
+++ src/ecommerce/components/home/Infographic.tsx
@@ -3,42 +3,79 @@ import { Link } from 'react-router-dom';
 const Infographic = (): JSX.Element => {
   return (
     <main className='infographic'>
-      <picture className='infographic__graphic'>
-        <span className='imgFadeGradient' />
-        <source
-          media='(max-width: 1528px)'
-          srcSet='/src/ecommerce/assets/production-images/compressed-home-page/infographic/1528x915/img-by-ilias-chebbi-on-unsplash.jpg'
-        />
-        <img
-          src='/src/ecommerce/assets/production-images/compressed-home-page/infographic/1920x1080/img-by-ilias-chebbi-on-unsplash.jpg'
-          alt='Dynamic Audio - Your new go-to source for audiophile grade HiFi audio equipment'
-          decoding='async'
-          fetchpriority='high'
-        />
-      </picture>
-      <div className='infographic__information'>
-        <article className='infographic__information__details'>
-          <hgroup>
-            <h3>
-              New drops from <span className='highlight'>beyerdynamic</span>
-            </h3>
-            <h2>the new hifi shop</h2>
-            <h1>
-              Unparalleled HiFi audio and <span className='highlight'>superior</span> manufacturing for discerning audiophiles. Crystal-clear sound,{' '}
-              <span className='highlight'>premium </span>
-              materials, meticulous <span className='highlight'>craftsmanship</span>, and lasting durability.
-            </h1>
-          </hgroup>
-          <div className='infographic__information__details__cta'>
-            <Link to={''} role='button'>
-              Learn More
-            </Link>
-            <Link to={''} role='button'>
-              Save Up To 25%
+      <section className='infographic__textArea'>
+        <article className='infographic__textArea__primary'>
+          <strong>
+            <span>THE NEW</span>
+            <span>HiFi AUDIO</span>
+            <span>EXPERIENCE</span>
+          </strong>
+          <div className='infographic--divider' />
+          <h1>
+            Unparalleled HiFi audio and <span className='highlight'>superior</span> manufacturing for discerning audiophiles. Crystal-clear sound,{' '}
+            <span className='highlight'>premium </span>
+            materials, meticulous <span className='highlight'>craftsmanship</span>, and lasting durability.
+          </h1>
+        </article>
+      </section>
+      <section className='infographic__focus'>
+        <figure>
+          <picture>
+            <source
+              media='(max-width: 1528px)'
+              srcSet='/src/ecommerce/assets/production-images/compressed-home-page/infographic/1528x915/img-by-ilias-chebbi-on-unsplash.jpg'
+            />
+            <img
+              src='src/ecommerce/assets/production-images/compressed-home-page/infographic/1528x915/img-by-ilias-chebbi-on-unsplash.jpg'
+              alt='Dynamic Audio - Your new go-to source for audiophile grade HiFi audio equipment'
+              decoding='async'
+              fetchpriority='high'
+            />
+            <figcaption>Dynamic Audio - Your new go-to source for audiophile grade HiFi audio equipment</figcaption>
+          </picture>
+        </figure>
+      </section>
+      <section className='infographic__cta'>
+        <article className='infographic__cta__news'>
+          <div className='infographic__cta__news__beyerdynamic'>
+            <p>
+              Thanks to our friends over at{' '}
+              <Link to='http://localhost:5173/ecommerce/Beyerdynamic'>
+                <figure>
+                  <picture>
+                    <img
+                      src='src/ecommerce/assets/production-images/compressed-company-logos/Beyerdynamic.svg'
+                      alt='Beyerdynamic'
+                      decoding='async'
+                      fetchpriority='high'
+                    />
+                    <figcaption>Beyerdynamic</figcaption>
+                  </picture>
+                </figure>
+              </Link>
+              , we're proud to introduce a new line of headphones sporting all new Tesla technology.
+            </p>
+            <Link to='http://localhost:5173/ecommerce/Beyerdynamic' className='infographic__cta__news--learnMore'>
+              Learn more
             </Link>
           </div>
+          <figure className='infographic__cta__news__presentation'>
+            <picture>
+              <source
+                media='(max-width: 1528px)'
+                srcSet='/src/ecommerce/assets/production-images/compressed-home-page/infographic/1528x915/img-by-ilias-chebbi-on-unsplash.jpg'
+              />
+              <img
+                src='src/ecommerce/assets/production-images/compressed-home-page/infographic/t1.png'
+                alt='Dynamic Audio - Your new go-to source for audiophile grade HiFi audio equipment'
+                decoding='async'
+                fetchpriority='high'
+              />
+              <figcaption>Dynamic Audio - Your new go-to source for audiophile grade HiFi audio equipment</figcaption>
+            </picture>
+          </figure>
         </article>
-      </div>
+      </section>
     </main>
   );
 };
