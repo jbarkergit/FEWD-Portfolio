e490f430482b95175667bf0031de1ea4f2479089
Justin Barker <jbarkerpoc@gmail.com>
2023-10-31 11:42:43 -0500

    Cleaned up app, removed context from global

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
 import { Suspense, lazy, useEffect, useMemo } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
-//Shared
+/** 404 Error */
 const NotFound = lazy(() => import('./shared/pages/not-found/NotFound'));
-import SuspensePathHandler from './shared/suspense/SuspensePathHandler';
 
-//Portfolio
+/** Portfolio */
 const Portfolio = lazy(() => import('./portfolio/pages/Portfolio'));
+const ThreeSlider = lazy(() => import('./portfolio/components/main/ThreeSlider'));
 
-//Ecommerce
+/** Ecommerce */
 const Home = lazy(() => import('./ecommerce/pages/Home'));
 const ProductCatalog = lazy(() => import('./ecommerce/pages/ProductCatalog'));
 const ProductDetailPage = lazy(() => import('./ecommerce/pages/ProductDetailPage'));
-//Ecommerce: Skeletons
+// Skeletons
 import HomeSkeleton from './ecommerce/skeletons/pages/HomeSkeleton';
 import ProductCatalogSkeleton from './ecommerce/skeletons/pages/ProductCatalogSkeleton';
 import ProductDetailPageSkeleton from './ecommerce/skeletons/pages/ProductDetailPageSkeleton';
-//Ecommerce: Context
+// Context
 import { CategoryFilterProvider } from './ecommerce/context/CategoryFilterContext';
 import { CartProvider } from './ecommerce/context/CartContext';
-//Ecommerce: Hooks
+// Pathing via Hooks
 import useUniqueData from './ecommerce/hooks/useUniqueData';
 import useCart from './ecommerce/hooks/useCart';
 
-const ThreeSlider = lazy(() => import('./portfolio/components/main/ThreeSlider'));
+/** Suspense */
+function SuspensePathHandler() {
+  const location = useLocation();
 
-//Application
+  switch (location.pathname) {
+    case '/ecommerce':
+      return <HomeSkeleton />;
+    case '/ecommerce/products':
+    case '/ecommerce/headphones':
+    case '/ecommerce/amps-dacs':
+    case '/ecommerce/microphones':
+    case '/ecommerce/interfaces':
+      return <ProductCatalogSkeleton />;
+    default:
+      return <ProductDetailPageSkeleton />;
+  }
+}
+
 function App() {
-  //** ECOMMERCE: Set Shopping Cart array of products in local Storage */
+  /** Ecommerce Shopping Cart localStorage setter */
   const { shoppingCart, cartProductQuantity } = useCart();
   useEffect(() => localStorage.setItem('shoppingCartState', JSON.stringify(shoppingCart)), [cartProductQuantity]);
 
-  //** Sets for route mapping */
+  /** Ecommerce Route mapping hooks */
   const uniqueCompanies = useMemo(() => useUniqueData().useUniqueCompanies, [useUniqueData().useUniqueCompanies]);
   const uniqueWearStyles = useMemo(() => useUniqueData().useUniqueWearStyles, [useUniqueData().useUniqueCompanies]);
   const uniquePolarPatterns = useMemo(() => useUniqueData().useUniquePolarPatterns, [useUniqueData().useUniquePolarPatterns]);
@@ -39,37 +54,124 @@ function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<SuspensePathHandler />}>
-        <CategoryFilterProvider>
-          <CartProvider>
-            <Routes>
-              <Route path='*' element={<NotFound />} />
+        <Routes>
+          <Route path='*' element={<NotFound />} />
+          <Route path='/' element={<Portfolio />} />
+          <Route path='/three' element={<ThreeSlider />} />
 
-              <Route path='/' element={<Portfolio />} />
-
-              <Route path='/three' element={<ThreeSlider />} />
-
-              <Route path='/ecommerce' element={<Home />} />
-              <Route path='/ecommerce/products' element={<ProductCatalog />} />
-              <Route path='/ecommerce/headphones' element={<ProductCatalog />} />
-              <Route path='/ecommerce/amps-dacs' element={<ProductCatalog />} />
-              <Route path='/ecommerce/microphones' element={<ProductCatalog />} />
-              <Route path='/ecommerce/interfaces' element={<ProductCatalog />} />
-              <Route path='/ecommerce/product/:paramId' element={<ProductDetailPage />} />
-              {uniqueCompanies.map((company: string) => (
-                <Route path={`/ecommerce/${company}`} element={<ProductCatalog />} key={company} />
-              ))}
-              {uniqueWearStyles.map((wearStyle) => (
-                <Route path={`/ecommerce/${wearStyle}`} element={<ProductCatalog />} key={wearStyle} />
-              ))}
-              {uniquePolarPatterns.map((polarPattern) => (
-                <Route path={`/ecommerce/${polarPattern}`} element={<ProductCatalog />} key={polarPattern} />
-              ))}
-              {/* <Route path='/home-skeleton' element={<HomeSkeleton />} /> */}
-              {/* <Route path='/product-catalog-skeleton' element={<ProductCatalogSkeleton />} /> */}
-              {/* <Route path='/product-detail-page-skeleton' element={<ProductDetailPageSkeleton />} /> */}
-            </Routes>
-          </CartProvider>
-        </CategoryFilterProvider>
+          <Route
+            path='/ecommerce'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <Home />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          <Route
+            path='/ecommerce/products'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <ProductCatalog />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          <Route
+            path='/ecommerce/headphones'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <ProductCatalog />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          <Route
+            path='/ecommerce/amps-dacs'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <ProductCatalog />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          <Route
+            path='/ecommerce/microphones'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <ProductCatalog />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          <Route
+            path='/ecommerce/interfaces'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <ProductCatalog />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          <Route
+            path='/ecommerce/product/:paramId'
+            element={
+              <CategoryFilterProvider>
+                <CartProvider>
+                  <ProductDetailPage />
+                </CartProvider>
+              </CategoryFilterProvider>
+            }
+          />
+          {uniqueCompanies.map((company: string) => (
+            <Route
+              path={`/ecommerce/${company}`}
+              element={
+                <CategoryFilterProvider>
+                  <CartProvider>
+                    <ProductCatalog />
+                  </CartProvider>
+                </CategoryFilterProvider>
+              }
+              key={company}
+            />
+          ))}
+          {uniqueWearStyles.map((wearStyle) => (
+            <Route
+              path={`/ecommerce/${wearStyle}`}
+              element={
+                <CategoryFilterProvider>
+                  <CartProvider>
+                    <ProductCatalog />
+                  </CartProvider>
+                </CategoryFilterProvider>
+              }
+              key={wearStyle}
+            />
+          ))}
+          {uniquePolarPatterns.map((polarPattern) => (
+            <Route
+              path={`/ecommerce/${polarPattern}`}
+              element={
+                <CategoryFilterProvider>
+                  <CartProvider>
+                    <ProductCatalog />
+                  </CartProvider>
+                </CategoryFilterProvider>
+              }
+              key={polarPattern}
+            />
+          ))}
+          {/* <Route path='/home-skeleton' element={<HomeSkeleton />} /> */}
+          {/* <Route path='/product-catalog-skeleton' element={<ProductCatalogSkeleton />} /> */}
+          {/* <Route path='/product-detail-page-skeleton' element={<ProductDetailPageSkeleton />} /> */}
+        </Routes>
       </Suspense>
     </BrowserRouter>
   );
