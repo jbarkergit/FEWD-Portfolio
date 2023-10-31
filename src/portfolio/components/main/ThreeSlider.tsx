b9f1a491c9d53deaacd9c6c99ece9bb21306f8ef
Justin Barker <jbarkerpoc@gmail.com>
2023-10-31 10:21:41 -0500

    prog. save

--- src/portfolio/components/main/ThreeSlider.tsx
+++ src/portfolio/components/main/ThreeSlider.tsx
@@ -17,20 +17,20 @@ const ThreeSlider = () => {
 
     // Directional Light
     const directionalLight = new THREE.DirectionalLight(0xffffff, 10); // Color, Intensity
-    directionalLight.position.set(1, 1, 1); // Set the light's position
+    directionalLight.position.set(0, 0, 1); // Set the light's position
     scene.add(directionalLight);
 
     /** Camera */
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 5); // Set camera position
-    camera.lookAt(0, 0, 0);
+    camera.position.set(0, 0, 10); // Set camera position
+    camera.lookAt(0, 0, 1);
 
     /** Renderer */
     const renderer = new THREE.WebGLRenderer({ canvas: canvas.current as HTMLCanvasElement });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     /** Object Constructor */
-    myProjects.forEach((project) => {
+    myProjects.forEach((project, index) => {
       // Dynamically load textures from myProjects array of objects into textureLoader
       const texture = new THREE.TextureLoader().load(project.imageSrc);
 
@@ -40,8 +40,14 @@ const ThreeSlider = () => {
       // Geometry
       const planeGeometry = new THREE.PlaneGeometry(1, 1);
 
+      // Geometry object
+      const object = new THREE.Mesh(planeGeometry, material);
+
+      // Geometry object positioning
+      object.position.x = index * 2;
+
       // Add new geometry object to scene
-      scene.add(new THREE.Mesh(planeGeometry, material));
+      scene.add(object);
     });
 
     /** Render Scene && attach to DOM element */
@@ -66,10 +72,6 @@ const ThreeSlider = () => {
     // window.addEventListener('resize', windowResizeHandler);
     // return () => window.removeEventListener('resize', windowResizeHandler);
   }, []);
-
-  useEffect(() => {
-    console.log(canvas.current);
-  }, [canvas.current]);
 
   return (
     <div id='threeSlider'>
