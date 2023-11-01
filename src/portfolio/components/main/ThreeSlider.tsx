ba6930b3ac47a6449e8438021922d3de5c098abf
Justin Barker <jbarkerpoc@gmail.com>
2023-11-01 12:41:55 -0500

    Save, not sure if I'm sticking w three for portfol

--- src/portfolio/components/main/ThreeSlider.tsx
+++ src/portfolio/components/main/ThreeSlider.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
-import { myProjects } from './data/myProjects';
+
+const myProjects = [
+  { key: 'ecommerce', imageSrc: '/src/portfolio/assets/compressed-project-images/Ecommerce-Landing.png' },
+  { key: 'hyundai', imageSrc: '/src/portfolio/assets/compressed-project-images/hyundai-preview.jpg' },
+];
 
 const ThreeSlider = () => {
   /** useRef */
   const canvas = useRef<HTMLCanvasElement>(null);
-  const [layout, setLayout] = useState<string>('row');
+
+  /** Global Scope variables */
+  const [globalScope, setGlobalScope] = useState({ windowX: window.innerWidth, windowY: window.innerHeight, windowPxRatio: window.devicePixelRatio });
 
   useEffect(() => {
     /** three.js Scene */
@@ -14,56 +20,73 @@ const ThreeSlider = () => {
 
     /** Renderer */
     const renderer = new THREE.WebGLRenderer({ canvas: canvas.current as HTMLCanvasElement });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(globalScope.windowX, globalScope.windowY);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     /** Camera */
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, globalScope.windowX / globalScope.windowY, 0.1, 1000);
     camera.position.set(0, 0, 1);
-    camera.rotation.set(0, 0, -10);
+    // camera.rotation.set(0, 0, 0);
     camera.scale.set(1, 1.25, 1);
 
     /** Lighting */
-    const spotlight = new THREE.SpotLight(0xffffff, 1);
-    spotlight.position.set(5, 10, 7.5);
-    spotlight.angle = 0.135;
-    scene.add(spotlight);
+    const spotLight = new THREE.SpotLight(0xffffff, 100);
+    spotLight.position.set(5, 10, 7.5);
+    spotLight.angle = 0.135;
+    spotLight.distance = 0;
+    scene.add(spotLight);
+
+    // Lighting Helper
+    const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+    // scene.add(spotLightHelper);
+
+    /** Geometry */
+    const planeGeometry = new THREE.PlaneGeometry(1, 1, 1);
+
+    /** Texture Loader */
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.setCrossOrigin('anonymous');
 
     /** Object Constructor */
-    const objectConstructor = (imageUrl: string, index: number) => {
-      // Geometry
-      const planeGeometry = new THREE.PlaneGeometry(1, 1, 1);
-
-      // Texture Loader
-      const textureLoader = new THREE.TextureLoader();
-      textureLoader.setCrossOrigin('anonymous');
-
-      // Material Constructor
-      const materialConstructor = (imageUrl: string) => {
-        return new Promise<THREE.MeshStandardMaterial>((resolve) => {
-          textureLoader.load(imageUrl, (texture) => {
-            const material = new THREE.MeshStandardMaterial({ map: texture, side: THREE.FrontSide });
-            resolve(material);
-          });
-        });
-      };
-
-      // Object Creation
-      materialConstructor(imageUrl).then((material) => {
+    myProjects.forEach((project, index: number) => {
+      textureLoader.load(project.imageSrc, (texture) => {
+        const material = new THREE.MeshStandardMaterial({ map: texture, side: THREE.FrontSide });
         const object = new THREE.Mesh(planeGeometry, material);
-        object.position.set(0, index * -2, 0);
-        object.rotation.set(0, 0, 0);
-        object.scale.set(1, 1, 1);
-
+        object.position.set(index * 2, 0, 0);
+        // object.rotation.set(0, 0, 0);
+        // object.scale.set(1, 1, 1);
         scene.add(object);
         renderer.render(scene, camera);
       });
-    };
-
-    /** Envoke Object Constructor */
-    myProjects.forEach((project, index: number) => {
-      const imageUrl: string = project.imageSrc;
-      objectConstructor(imageUrl, index);
     });
+
+    function windowResizeHandler() {
+      // setGlobalScope({ ...globalScope, windowX = window.innerWidth, windowY = window.innerHeight });
+      // camera.aspect = globalScope.windowX / globalScope.windowY;
+      // camera.updateProjectionMatrix();
+      // renderer.setSize(globalScope.windowX, globalScope.windowY);
+      // renderer.setPixelRatio(Math.min(globalScope.windowPxRatio, 2));
+    }
+
+    function userWheel(e: WheelEvent) {
+      if (e.deltaY < 0) {
+        // Up
+      } else {
+        // Down
+      }
+    }
+
+    function userPointerUp(e: PointerEvent) {}
+
+    window.addEventListener('resize', windowResizeHandler);
+    window.addEventListener('wheel', userWheel);
+    canvas.current?.addEventListener('pointerup', userPointerUp);
+
+    return () => {
+      window.removeEventListener('resize', windowResizeHandler);
+      window.removeEventListener('wheel', userWheel);
+      canvas.current?.removeEventListener('pointerup', userPointerUp);
+    };
   }, []);
 
   return (
