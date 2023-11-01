982d1f92be6494aa34b9832cbae2a5a5cdb4f5ec
Justin Barker <jbarkerpoc@gmail.com>
2023-11-01 05:24:24 -0500

    Rewrote everything from scratch

--- src/portfolio/components/main/ThreeSlider.tsx
+++ src/portfolio/components/main/ThreeSlider.tsx
@@ -1,74 +1,69 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { myProjects } from './data/myProjects';
 
 const ThreeSlider = () => {
   /** useRef */
   const canvas = useRef<HTMLCanvasElement>(null);
+  const [layout, setLayout] = useState<string>('row');
 
-  /** Three.js */
   useEffect(() => {
-    /** Initialize Scene */
+    /** three.js Scene */
     const scene = new THREE.Scene();
-
-    /** Lighting */
-    // Ambient light
-    scene.add(new THREE.AmbientLight(0xffffff, 0.2));
-
-    // Directional Light
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Color, Intensity
-    directionalLight.position.set(0, 0, 1); // Set the light's position
-    scene.add(directionalLight);
-
-    /** Camera */
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 2); // Set camera position
-    camera.lookAt(0, 0, 1);
+    scene.background = new THREE.Color().setHSL(0, 0, 0);
 
     /** Renderer */
     const renderer = new THREE.WebGLRenderer({ canvas: canvas.current as HTMLCanvasElement });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+    /** Camera */
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 1);
+    camera.rotation.set(0, 0, -10);
+    camera.scale.set(1, 1.25, 1);
+
+    /** Lighting */
+    const spotlight = new THREE.SpotLight(0xffffff, 1);
+    spotlight.position.set(5, 10, 7.5);
+    spotlight.angle = 0.135;
+    scene.add(spotlight);
+
     /** Object Constructor */
-    myProjects.forEach((project, index) => {
-      //Texture Loader
+    const objectConstructor = (imageUrl: string, index: number) => {
+      // Geometry
+      const planeGeometry = new THREE.PlaneGeometry(1, 1, 1);
+
+      // Texture Loader
       const textureLoader = new THREE.TextureLoader();
       textureLoader.setCrossOrigin('anonymous');
 
-      // Geometry
-      const planeGeometry = new THREE.PlaneGeometry(1, 1);
+      // Material Constructor
+      const materialConstructor = (imageUrl: string) => {
+        return new Promise<THREE.MeshStandardMaterial>((resolve) => {
+          textureLoader.load(imageUrl, (texture) => {
+            const material = new THREE.MeshStandardMaterial({ map: texture, side: THREE.FrontSide });
+            resolve(material);
+          });
+        });
+      };
 
-      // Dynamically load textures from myProjects array of objects into textureLoader
-      textureLoader.load(project.imageSrc, (texture) => {
-        // Create material with loaded textures
-        const material = new THREE.MeshStandardMaterial({ map: texture, side: THREE.FrontSide });
-
-        // Geometry object
+      // Object Creation
+      materialConstructor(imageUrl).then((material) => {
         const object = new THREE.Mesh(planeGeometry, material);
+        object.position.set(0, index * -2, 0);
+        object.rotation.set(0, 0, 0);
+        object.scale.set(1, 1, 1);
 
-        // Geometry object positioning
-        object.position.y = index * -2;
-
-        // Add new geometry object to scene
         scene.add(object);
-
-        /** Render Scene && attach to DOM element */
         renderer.render(scene, camera);
       });
+    };
+
+    /** Envoke Object Constructor */
+    myProjects.forEach((project, index: number) => {
+      const imageUrl: string = project.imageSrc;
+      objectConstructor(imageUrl, index);
     });
-
-    /** Window resize handler */
-    // const windowResizeHandler = () => {
-    //   camera.aspect = window.innerWidth / window.innerHeight;
-    //   camera.updateProjectionMatrix();
-    //   new THREE.WebGLRenderer({ canvas: canvas.current as HTMLCanvasElement }).setSize(window.innerWidth, window.innerHeight);
-    // };
-
-    // /** Ensure the scene is rendered and mounted correctly on DOM */
-    // windowResizeHandler();
-
-    // window.addEventListener('resize', windowResizeHandler);
-    // return () => window.removeEventListener('resize', windowResizeHandler);
   }, []);
 
   return (
