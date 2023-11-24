bdc389aa3650d6dd56de616c611f38f0919e1e1b
Justin Barker <jbarkerpoc@gmail.com>
2023-11-23 19:54:07 -0600

    +flex layout +coordinate slider anim

--- src/portfolio/pages/Portfolio.tsx
+++ src/portfolio/pages/Portfolio.tsx
@@ -25,14 +25,44 @@ const Portfolio = (): JSX.Element => {
   }, [portfolioRef.current]);
 
   /** Component transitions */
+  const portfolioWrapper = useRef<HTMLDivElement>(null);
+
   const [featureState, setFeatureState] = useState({
     projectDetailsActive: false,
     contactFormActive: false,
     techStackActive: false,
   });
 
+  const setPortfolioCoordinates = (value: number) => {
+    if (portfolioWrapper.current)
+      portfolioWrapper.current.scrollTo({
+        left: value,
+        top: 0,
+        behavior: 'smooth',
+      });
+  };
+
+  useEffect(() => {
+    const portfolioWidth = portfolioWrapper.current?.scrollWidth || 0;
+    const featurePosition = portfolioWidth / 3 || 0;
+
+    switch (true) {
+      case featureState.projectDetailsActive:
+        setPortfolioCoordinates(featurePosition * 1);
+        break;
+      case featureState.contactFormActive:
+        setPortfolioCoordinates(featurePosition * 2);
+        break;
+      case featureState.techStackActive:
+        setPortfolioCoordinates(featurePosition * 3);
+        break;
+      default:
+        break;
+    }
+  }, [featureState, portfolioWrapper]);
+
   return (
-    <div id='portfolioWrapper'>
+    <div id='portfolioWrapper' ref={portfolioWrapper}>
       <section id='portfolio' ref={portfolioRef}>
         <h2>Project Hub</h2>
         <PortHeader
@@ -45,9 +75,9 @@ const Portfolio = (): JSX.Element => {
         <MainContent mountAnimation={mountAnimation} projectSlideIndex={projectSlideIndex} setProjectSlideIndex={setProjectSlideIndex} />
         <PortFooter mountAnimation={mountAnimation} projectSlideIndex={projectSlideIndex} featureState={featureState} setFeatureState={setFeatureState} />
       </section>
+      <ProjectDetails projectSlideIndex={projectSlideIndex} />
       <ContactForm />
       <EcommerceTechStack featureState={featureState} setFeatureState={setFeatureState} />
-      <ProjectDetails projectSlideIndex={projectSlideIndex} />
     </div>
   );
 };
