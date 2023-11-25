e8a23412c0b31aae15676e553c33f89f482cb462
Justin Barker <jbarkerpoc@gmail.com>
2023-11-25 06:32:55 -0600

    Added about section

--- src/portfolio/components/features/contact/ContactInformation.tsx
+++ src/portfolio/components/features/contact/ContactInformation.tsx
@@ -68,14 +68,23 @@ const ContactFormInformation = () => {
 
   return (
     <section className='contactForm__section'>
-      <h2>General Contact Information</h2>
+      <h2>Developer and Contact Information</h2>
+      <article className='contact__introduction'>
+        <h3>Let's link up.</h3>
+        <p>
+          <span>
+            👋 Hello! I'm Justin, a <abbr title='React: A JavaScript library for building user interfaces'>React</abbr> Front End Developer. I build responsive,
+            interactive, <abbr title='Search Engine Optimization'>SEO</abbr>-optimized, and{' '}
+            <abbr title='Web Accessibility Initiative - Accessible Rich Internet Applications'>WAI-ARIA</abbr>-compliant web applications; with performance-oriented
+            Core Web Vital metrics.
+          </span>
+          <span>
+            If you're looking to inquire further information in regards of potential employment, kindly share the requested contact information and I'll be sure to
+            respond in a timely fashion.
+          </span>
+        </p>
+      </article>
       <article className='contact__questions'>
-        <div className='contact__questions__intro'>
-          <h3 className='contact__questions__intro--welcome'>Let's link up.</h3>
-          <p className='contact__questions__intro--prompt'>
-            👋 Kindly share the requested contact information and I'll be sure to respond in a timely fashion. Thank you.
-          </p>
-        </div>
         <details className='contact__questions__responseTime' ref={primaryQuestionRef} open>
           <summary className='contact__questions__responseTime--question'>
             How long until I receive a response?
