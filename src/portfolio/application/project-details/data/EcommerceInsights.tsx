a5d28ce61c7de90ee9bdd662c4490e2377982035
Justin Barker <jbarkerpoc@gmail.com>
2023-12-01 10:47:30 -0600

    Additional tweaks

--- src/portfolio/application/project-details/data/EcommerceInsights.tsx
+++ src/portfolio/application/project-details/data/EcommerceInsights.tsx
@@ -13,21 +13,24 @@ const EcommerceInsights = (): JSX.Element => {
         touch-ups, aspect-ratio matching, and large batch exports to satisfy Web Core Vitals for three device breakpoints. If that wasn't bad enough, I initially
         wrote the database in TSX format, then post-application completion, converted it to JSON. Utilizing the product database, I built numerous custom hooks that
         filtered and returned alphabetized unique data. Naturally, this paved the way for dynamic route mapping, product search functionality, product filters, and
-        product categories. The vast majority of the application's data was dealt with, and routes could now be dynamically mapped, resolving any pending issues.
+        product categories. The vast majority of the application's data was dealt with, resolving any pending issues.
       </p>
       <p>
         Incorporating reusable, type-safe functional components that adhered to best practices, such as lazy loading and leveraging the Intersection Observer API for
         efficient querying, became an integral integration to reduce bloat and optimize performance. With React's performance hooks, constructor patterns, BEM
         methodology, and DRY, documented logic, refactoring component architecture was less of an issue than SEO and WAI-ARIA compliance. Side effect management,
-        event propagation, and conditional rendering conflicts with animations took the most effort. Conceptualization of the application's UX and UI was the next
-        big priority. By blending style structures from miscellaneous domains, I created something imperfect, experimental, and fun. I put a lot of emphasis on
-        subtle element interactions throughout the application. Additionally, covered all edge cases for conditional mapping of images and data. Please note that if
-        your monitor(s) are not calibrated, the neumorphic styling that is not suitable for production applications, may be lost.
+        event propagation, and conditional rendering conflicts with animations took the most effort.
       </p>
       <p>
-        To truly mark this as an eCommerce project, user registry and authentication had to be simulated. Given I am a front-end developer, I employed localStorage
-        to handle data and Regular Expressions to handle field validation. The last step was shopping cart functionality, which gave me the great pleasure of writing
-        useReducer boilerplate. I, of course, have not built out user account pages nor integrated Stripe due to a lack of a back-end.
+        Conceptualization of the application's UX and UI was the next big priority. By blending style structures from miscellaneous domains, I created something
+        imperfect, experimental, and fun. I put a lot of emphasis on subtle element interactions throughout the application. Additionally, covered all edge cases for
+        conditional mapping of images and data. Please note that if your monitor(s) are not calibrated, the neumorphic styling that is not suitable for production
+        applications, may be lost.
+      </p>
+      <p>
+        To truly mark this as an eCommerce project, user registry and authentication had to be simulated. I employed localStorage to handle data and Regular
+        Expressions to handle field validation. The last step was shopping cart functionality, which gave me the great pleasure of writing useReducer boilerplate. I,
+        of course, have not built out user account pages nor integrated Stripe due to a lack of a back-end.
       </p>
     </>
   );
