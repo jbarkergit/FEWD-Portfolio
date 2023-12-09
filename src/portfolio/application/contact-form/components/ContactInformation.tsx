f59acd4bb213d0faa4c1572ed2338aea16a31676
Justin Barker <jbarkerpoc@gmail.com>
2023-12-09 00:09:39 -0600

    Mapped react component inline

--- src/portfolio/application/contact-form/components/ContactInformation.tsx
+++ src/portfolio/application/contact-form/components/ContactInformation.tsx
@@ -15,6 +15,41 @@ const EmailAddressOL = () => {
   );
 };
 
+/** Assistance Questions */
+const assistanceData = [
+  {
+    summary: `How long until I receive a response?`,
+    details: `My availability extends throughout the entire week; however, I do not respond to inquiries after business hours.`,
+  },
+  {
+    summary: `Will the information I share be secure?`,
+    details: `I do not collect personal information for malicious intentions. This form and it's contents are redirected to my personal Google Mail inbox. The information requested is solely to comprehend and determine whether or not I can satisfy your inquiry in advance. I have no control over what third party services do with said information. Please provide only the information you're comfortable sharing. If you are uncomfortable with using the provided form, you may contact me using the following email address.`,
+  },
+  {
+    summary: `Inclusivity and Accessibility`,
+    details: `I will, without question, welcome the opportunity to connect and/or work with individuals of any race, background, disability, or circumstance. There will be no reservations or communication barriers between any person(s) who may employ me. I will adapt to ensure you or an individual you know is treated with the utmost respect and dignity.`,
+  },
+  {
+    summary: `I'm contacting on behalf of another person.`,
+    details: `If you have permission/consent from the inquiring person(s), please premise the inquiry with, "I am contacting you on behalf of
+              (person's FIRST name). If you do not have permission/consent, please fill the contact form out with your information. NOTICE - A person commits the offense of identity theft of another person if he possesses or uses, through any means, identifying information of
+              another person without the consent of that other person to further any unlawful purpose.`,
+  },
+  {
+    summary: `I'm having issues with the email address.`,
+    details: `You will be prompted with an error in the event that: The email address contains special characters with the exception of asperands (@) and a period (.) and/or the email address is not registered on the following domains:`,
+    ol: <EmailAddressOL />,
+  },
+  {
+    summary: `My contact number is being rejected.`,
+    details: `Your contact number must be ten digits in length excluding the extension. Example: 123-456-7890`,
+  },
+  {
+    summary: `I can't submit my website url.`,
+    details: `I will not visit insecure URLS; therefore, any provided URL must be secure (https://).`,
+  },
+];
+
 const ContactFormInformation = () => {
   const parentArticleRef = useRef<HTMLElement>(null);
 
@@ -62,41 +97,6 @@ const ContactFormInformation = () => {
     }
   }, [activeDetailsIndex]);
 
-  /** Assistance Questions */
-  const assistanceData = [
-    {
-      summary: `How long until I receive a response?`,
-      details: `My availability extends throughout the entire week; however, I do not respond to inquiries after business hours.`,
-    },
-    {
-      summary: `Will the information I share be secure?`,
-      details: `I do not collect personal information for malicious intentions. This form and it's contents are redirected to my personal Google Mail inbox. The information requested is solely to comprehend and determine whether or not I can satisfy your inquiry in advance. I have no control over what third party services do with said information. Please provide only the information you're comfortable sharing. If you are uncomfortable with using the provided form, you may contact me using the following email address.`,
-    },
-    {
-      summary: `Inclusivity and Accessibility`,
-      details: `I will, without question, welcome the opportunity to connect and/or work with individuals of any race, background, disability, or circumstance. There will be no reservations or communication barriers between any person(s) who may employ me. I will adapt to ensure you or an individual you know is treated with the utmost respect and dignity.`,
-    },
-    {
-      summary: `I'm contacting on behalf of another person.`,
-      details: `If you have permission/consent from the inquiring person(s), please premise the inquiry with, "I am contacting you on behalf of
-              (person's FIRST name). If you do not have permission/consent, please fill the contact form out with your information. NOTICE - A person commits the offense of identity theft of another person if he possesses or uses, through any means, identifying information of
-              another person without the consent of that other person to further any unlawful purpose.`,
-    },
-    {
-      summary: `I'm having issues with the email address.`,
-      details: `You will be prompted with an error in the event that: The email address contains special characters with the exception of asperands (@) and a period (.) and/or the email address is not registered on the following domains:`,
-      ol: <EmailAddressOL />,
-    },
-    {
-      summary: `My contact number is being rejected.`,
-      details: `Your contact number must be ten digits in length excluding the extension. Example: 123-456-7890`,
-    },
-    {
-      summary: `I can't submit my website url.`,
-      details: `I will not visit insecure URLS; therefore, any provided URL must be secure (https://).`,
-    },
-  ];
-
   return (
     <section className='contactForm__section' ref={parentArticleRef}>
       <h2>Contact information</h2>
@@ -115,6 +115,7 @@ const ContactFormInformation = () => {
                 </svg>
               </div>
               <p ref={detailsInformationReference}>{object.details}</p>
+              {object.ol ? object.ol : null}
             </div>
           ))}
         </>
