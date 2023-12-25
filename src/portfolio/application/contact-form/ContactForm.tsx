38b6ae8ff1f1ee110876269a8875e9a2f95a7f88
Justin Barker <jbarkerpoc@gmail.com>
2023-12-25 14:57:22 -0600

    Blob removal

--- src/portfolio/application/contact-form/ContactForm.tsx
+++ src/portfolio/application/contact-form/ContactForm.tsx
@@ -5,37 +5,9 @@ import ContactFormStandard from './components/ContactFormStandard';
 type PropDrillType = { featureState: Record<string, boolean>; setFeatureState: Dispatch<SetStateAction<Record<string, boolean>>> };
 
 const ContactForm = ({ featureState, setFeatureState }: PropDrillType) => {
-  const blob = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const useBlobSizer = () => {
-      if (blob.current) blob.current.style.width = `${window.innerWidth / 3}px`;
-    };
-
-    useBlobSizer();
-
-    const useBlob = (e: PointerEvent) => {
-      if (blob.current) {
-        blob.current.animate({ top: `${e.clientY}px`, left: `${e.clientX}px` }, { duration: 6000, fill: 'forwards' });
-      }
-    };
-
-    window.addEventListener('resize', useBlobSizer);
-    window.addEventListener('pointermove', useBlob);
-
-    return () => {
-      window.removeEventListener('resize', useBlobSizer);
-      window.removeEventListener('pointermove', useBlob);
-    };
-  }, []);
-
   return (
     <section className='contactForm'>
       <h2>Developer contact form and general information</h2>
-      <div className='contactForm__blob'>
-        <div className='contactForm__blob--blob' ref={blob} />
-        <div className='contactForm__blob--blur' />
-      </div>
       <section className='contactForm__container'>
         <AboutDeveloper />
         <ContactFormStandard />
