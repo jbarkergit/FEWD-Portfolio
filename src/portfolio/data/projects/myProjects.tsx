9c3f4752369dabdb3208c4368d0a95ea911ac5ed
Justin Barker <jbarkerpoc@gmail.com>
2023-12-27 12:08:55 -0600

    Removed key value pair 'posterSrc'

--- src/portfolio/data/projects/myProjects.tsx
+++ src/portfolio/data/projects/myProjects.tsx
@@ -12,7 +12,6 @@ export const myProjects = [
     },
     insights: <EcommerceInsights />,
     imgSrc: '/src/portfolio/assets/compressed-project-images/ecommerce-landing.png',
-    posterSrc: 'src/ecommerce/assets/production-images/compressed-home-page/infographic/infographic-3.jpg',
     imgAlt: 'Ecommerce Web Development Project',
     dataStatus: 'active',
   },
@@ -20,7 +19,7 @@ export const myProjects = [
     key: '2024 portfolio',
     url: '',
     technologies: {
-      libraries: ['react', 'react router'],
+      libraries: ['react', 'react router', 'lenis'],
       web_technologies: ['html', 'css', 'sass', 'javascript', 'typescript'],
       code_quality: ['eslint', 'prettier'],
       build_tools: ['vite'],
@@ -29,8 +28,6 @@ export const myProjects = [
     },
     insights: <PortfolioInsights />,
     imgSrc:
-      'https://images.unsplash.com/photo-1594904351111-a072f80b1a71?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    posterSrc:
       'https://images.unsplash.com/photo-1594904351111-a072f80b1a71?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     imgAlt: 'Portfolio Project Hub',
     dataStatus: 'disabled',
