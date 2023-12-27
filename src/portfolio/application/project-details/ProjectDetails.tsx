540d0fa2696b43b53644804bd2f59855dd0edbf1
Justin Barker <jbarkerpoc@gmail.com>
2023-12-27 11:50:55 -0600

    Minor verbage tweak

--- src/portfolio/application/project-details/ProjectDetails.tsx
+++ src/portfolio/application/project-details/ProjectDetails.tsx
@@ -29,12 +29,12 @@ const ProjectDetails = ({ projectSlideIndex, setProjectSlideIndex, featureState,
               <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'>
                 <path fill='#ffffff' d='m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6z'></path>
               </svg>
-              Previous Project
+              Previous
             </span>
           </button>
           <button aria-label='View next project insights' onClick={() => setProjectSlideIndex((state) => (state === myProjects.length - 1 ? 0 : state + 1))}>
             <span>
-              Next Project{' '}
+              Next{' '}
               <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'>
                 <path fill='#ffffff' d='M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6z'></path>
               </svg>
