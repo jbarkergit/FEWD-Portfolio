2edcf7a9916889e83787acffcbceea95255d6fb2
Justin Barker <jbarkerpoc@gmail.com>
2024-02-04 17:09:49 -0600

    Processing env vars via VITE

--- src/film-database/hooks/useApiRequest.tsx
+++ src/film-database/hooks/useApiRequest.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
 
-export const useApiRequest = async (endPoint: string) => {
-  const API_KEY: string = '';
-  const [responseLimiter, setResponseLimiter] = useState<number>(0);
+export const useApiRequest = async (endPoint: string, AUTH_KEY: string) => {
+  const [responseLimited, setResponseLimited] = useState<boolean>(false);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `${AUTH_KEY}`,
+    },
+  };
 
   try {
-    const response: Response = await fetch(`${endPoint}`);
+    const response: Response = await fetch(`${endPoint}`, options);
 
-    if (!response.ok && responseLimiter === 0) {
-      setResponseLimiter((previousState) => previousState + 1);
+    if (!response.ok) {
+      if (responseLimited === false) setResponseLimited(true);
       throw new Error(`${response.status}`);
-    } else if (responseLimiter === 0) {
-      setResponseLimiter((previousState) => previousState + 1);
+    } else if (responseLimited === false) {
+      setResponseLimited(true);
       return (await response.json()) as [] | {} | unknown;
     } else {
-      console.log('Response limit met, refusing request.');
+      console.log('Responses rate limit met, refusing request.');
     }
   } catch (error) {
     console.error('Request failure status:', error);
