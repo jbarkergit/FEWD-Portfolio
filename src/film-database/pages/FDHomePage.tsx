6e1515d92cc9bc70ebe4fcc763ac67f74604b9e8
Justin Barker <jbarkerpoc@gmail.com>
2024-02-04 17:10:07 -0600

    Processing environment vars via VITE

--- src/film-database/pages/FDHomePage.tsx
+++ src/film-database/pages/FDHomePage.tsx
@@ -1,18 +1,15 @@
 import useCreateCarousel from '../component-creation/useCreateCarousel';
 import FDFooter from '../components/navigation/footer/FDFooter';
 import FDHeader from '../components/navigation/header/FDHeader';
+import { useApiRequest } from '../hooks/useApiRequest';
 
 const FDHomePage = () => {
+  useApiRequest('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', `${import.meta.env.VITE_TMDB_AUTH_KEY}`).then((data) => console.log(data));
   return (
     <div className='filmDatabase'>
       <div className='filmDatabase--backdrop' />
       <FDHeader />
-      <section className='filmDatabase__content'>
-        {useCreateCarousel({ heading: 'test', landscape: true })}
-        {useCreateCarousel({ heading: 'test', landscape: true })}
-        {useCreateCarousel({ heading: 'test', landscape: true })}
-        {useCreateCarousel({ heading: 'test', landscape: true })}
-      </section>
+      <section className='filmDatabase__content'>{useCreateCarousel({ heading: 'test', landscape: true })}</section>
       <FDFooter />
     </div>
   );
