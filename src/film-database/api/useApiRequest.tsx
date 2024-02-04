c8369ca09bd002e02c3334349ff8d13763cfe7d5
Justin Barker <jbarkerpoc@gmail.com>
2024-02-04 16:12:45 -0600

    Limited response requests, will error handle later

--- src/film-database/api/useApiRequest.tsx
+++ src/film-database/api/useApiRequest.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+
 export const useApiRequest = async (endPoint: string) => {
+  const apiKey: string = '';
+  const [responseLimiter, setResponseLimiter] = useState<number>(0);
+
   try {
-    const apiKey: string = '';
     const response: Response = await fetch(`${endPoint}`);
 
-    if (!response.ok) {
-      //refire
-      throw new Error(`Failed to fetch data. Status: ${response.status}`);
-    } else {
+    if (!response.ok && responseLimiter === 0) {
+      setResponseLimiter((previousState) => previousState + 1);
+      throw new Error(`${response.status}`);
+    } else if (responseLimiter === 0) {
+      setResponseLimiter((previousState) => previousState + 1);
       return (await response.json()) as [] | {} | unknown;
+    } else {
+      console.log('Response limit met, refusing request.');
     }
   } catch (error) {
-    console.error('Request failure status', error);
+    console.error('Request failure status:', error);
     throw error;
   }
 };
