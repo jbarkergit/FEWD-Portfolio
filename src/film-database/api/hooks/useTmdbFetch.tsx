f5d346e24577f2c3c63b20a4acac684e0832a62b
Justin Barker <jbarkerpoc@gmail.com>
2024-02-06 18:14:41 -0600

    Docs, dynamic params, improved efficiency

--- src/film-database/api/hooks/useTmdbFetch.tsx
+++ src/film-database/api/hooks/useTmdbFetch.tsx
@@ -1,4 +1,19 @@
-export const useTmdbFetch = async (endPoint: string, movie_id?: string, person_id?: string): Promise<unknown> => {
+/**
+ * TMDB API Fetch
+ * @param endPoint
+ * @param movie_id
+ * @param person_id
+ * @returns
+ */
+
+type tmdbApiFetchTypes = {
+  endPoint: string;
+  movie_id?: string;
+  person_id?: string;
+};
+
+const tmdbApiFetch = async ({ endPoint, movie_id, person_id }: tmdbApiFetchTypes): Promise<unknown> => {
+  // authorization options for TMDB API
   const options: RequestInit = {
     method: 'GET',
     headers: {
@@ -8,11 +23,66 @@ export const useTmdbFetch = async (endPoint: string, movie_id?: string, person_i
   };
 
   try {
-    const response: Response = await fetch(`${endPoint}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`, options);
+    // check for optional paramaters, alter response accordingly
+    let url: string;
+
+    switch (true) {
+      case !!movie_id:
+        url = `${endPoint.replace('/{movie_id}', movie_id)}`;
+        break;
+      case !!person_id:
+        url = `${endPoint.replace('/person_id}', person_id)}`;
+        break;
+      default:
+        url = `${endPoint}`;
+    }
+
+    // fetch data
+    const response: Response = await fetch(`${url}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`, options);
     const rawData: unknown = await response.json();
+
+    // ensure response status is OK
     if (!response.ok) throw new Error(`${response.status}`);
+    // return response
     else return rawData;
+
+    // catch errors
   } catch (error) {
     console.error('Request failure status:', error);
+  }
+};
+
+/**
+ * Fetch information by specified params utilizing tmdbApiFetch()
+ * @param category
+ * @returns
+ */
+
+type useTmdbFetchTypes = {
+  category: { key: string; endPoint: string }[];
+  movie_id?: string;
+  person_id?: string;
+};
+
+export const useTmdbFetch = async ({ category, movie_id, person_id }: useTmdbFetchTypes) => {
+  // initialize empty array to be returned for useState storage
+  const dataArray: {}[] = [];
+
+  try {
+    // utilize Promise.all to fetch requests in parallel
+    await Promise.all(
+      // create a new array based on category keyValuePairs
+      category.map(async (keyValuePair) => {
+        // await end points
+        const data = await tmdbApiFetch({ endPoint: keyValuePair.endPoint, movie_id, person_id });
+        // push new array into dataArray
+        dataArray.push({ [keyValuePair.key]: [{ key: keyValuePair.key, data: data }] });
+      })
+    );
+
+    // return our new array for storage
+    return dataArray;
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
 };
