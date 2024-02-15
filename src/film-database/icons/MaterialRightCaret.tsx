3e34996c4465cb42fa7762fe68c0bd02daf0a886
Justin Barker <jbarkerpoc@gmail.com>
2024-02-15 14:40:31 -0600

    Updated caret icons

--- src/film-database/icons/MaterialRightCaret.tsx
+++ src/film-database/icons/MaterialRightCaret.tsx
@@ -1,7 +1,7 @@
 const MaterialRightCaret = () => {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-      <path fill='currentColor' d='M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6z'></path>
+      <path fill='currentColor' d='m13.292 12l-4.6-4.6l.708-.708L14.708 12L9.4 17.308l-.708-.708z'></path>
     </svg>
   );
 };
