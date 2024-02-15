11e84f27b37187ce9a7c155b3c4c730488fd5222
Justin Barker <jbarkerpoc@gmail.com>
2024-02-15 13:29:48 -0600

    Navigation overlay + styling, html semantic tweaks

--- src/film-database/hooks/useCreateCarousel.tsx
+++ src/film-database/hooks/useCreateCarousel.tsx
@@ -3,8 +3,13 @@ import FDCarouselHeading from '../components/carousels/carousel-headers/FDCarous
 import FDCarouselChildLP from '../components/carousels/carousel-children/FDCarouselChildLP';
 import FDCarouselChildTopTen from '../components/carousels/carousel-children/FDCarouselChildTopTen';
 
+// icons
+import MaterialLeftCaret from '../icons/MaterialLeftCaret';
+import MaterialRightCaret from '../icons/MaterialRightCaret';
+
 // types
 import { TmdbDataUnionArrayType } from '../api/types/TmdbDataTypes';
+import useCreatePicture from './useCreatePicture';
 
 // Param Types
 type useCreateCarouselParamTypes = {
@@ -18,11 +23,13 @@ const useCreateCarousel = ({ heading, topten, dataObject }: useCreateCarouselPar
   return (
     <section className='fdCarousel'>
       <FDCarouselHeading heading={heading} />
-      <div className='fdCarousel__wrapper'>{topten ? <FDCarouselChildTopTen dataObject={dataObject} /> : <FDCarouselChildLP dataObject={dataObject} />}</div>{' '}
-      <nav className='fdCarousel__navigation'>
-        <button className='fdCarousel__navigation--button'></button>
-        <button className='fdCarousel__navigation--button'></button>
-      </nav>
+      <div className='fdCarousel__wrapper'>
+        {topten ? <FDCarouselChildTopTen dataObject={dataObject} /> : <FDCarouselChildLP dataObject={dataObject} />}
+        <nav className='fdCarousel__wrapper__navigation'>
+          <button className='fdCarousel__wrapper__navigation--button'>{useCreatePicture({ svg: <MaterialLeftCaret />, alt: 'Show Previous' })}</button>
+          <button className='fdCarousel__wrapper__navigation--button'>{useCreatePicture({ svg: <MaterialRightCaret />, alt: 'Show More' })}</button>
+        </nav>
+      </div>
     </section>
   );
 };
---
+++ src/film-database/icons/MaterialLeftCaret.tsx
@@ -0,0 +1,8 @@
+const MaterialLeftCaret = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+      <path fill='currentColor' d='M14 17.308L8.692 12L14 6.692l.708.708l-4.6 4.6l4.6 4.6z'></path>
+    </svg>
+  );
+};
+export default MaterialLeftCaret;
