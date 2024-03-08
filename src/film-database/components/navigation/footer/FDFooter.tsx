a2957a51976efd62ebece32f09d0ead2ca5f02e3
Justin Barker <jbarkerpoc@gmail.com>
2024-03-08 00:13:06 -0600

    Minor change, not sure

--- src/film-database/components/navigation/footer/FDFooter.tsx
+++ src/film-database/components/navigation/footer/FDFooter.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import useCreatePicture from '../../../hooks/useCreatePicture';
-import TmdbLogoShort from '../../../icons/TmdbLogoShort';
+import TmdbLogoShort from '../../../assets/logo/TmdbLogoShort';
 
 const FDFooter = () => {
   return (
