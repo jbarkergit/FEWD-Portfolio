653640856e2640ff5beb3ea7b534f12c700dd51b
Justin Barker <jbarkerpoc@gmail.com>
2024-03-08 02:12:12 -0600

    Added hover overlay for cta interactions

--- src/film-database/components/flat-grid/FDFlatGrid.tsx
+++ src/film-database/components/flat-grid/FDFlatGrid.tsx
@@ -1,4 +1,6 @@
 import { Type_Tmdb_ApiCallUnion_Obj } from '../../api/types/TmdbDataTypes';
+import { MaterialPlayCircle } from '../../assets/svg-icons/MaterialPlayCircle';
+import { BootstrapThreeDotsVertical } from '../../assets/svg-icons/PhDotsThreeVerticalBold';
 import useCreatePicture from '../../hooks/useCreatePicture';
 import { useFormatApiKey } from '../../hooks/useFormatApiKey';
 import { useFormatDate } from '../../hooks/useFormatDate';
@@ -18,7 +20,19 @@ const FDFlatGrid = ({ mapKey, mapValue }: Type_PropDrill) => {
         {mapValue.map((values) => (
           <li className='fdFlatGrid__wrapper__li' key={values.id}>
             <article className='fdFlatGrid__wrapper__li__article'>
-              {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${values.poster_path}.svg`, alt: values.title as string })}
+              <div className='fdFlatGrid__wrapper__li__article__graphic'>
+                {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${values.poster_path}.svg`, alt: values.title as string })}
+
+                <div className='fdFlatGrid__wrapper__li__article__graphic__overlay'>
+                  <button className='fdFlatGrid__wrapper__li__article__graphic__overlay--play' aria-label='Play Trailer'>
+                    {useCreatePicture({ svg: <MaterialPlayCircle />, alt: 'Play Trailer' })}
+                  </button>
+                  <button className='fdFlatGrid__wrapper__li__article__graphic__overlay--moreInfo' aria-label='More Information'>
+                    {useCreatePicture({ svg: <BootstrapThreeDotsVertical />, alt: 'More Information' })}
+                  </button>
+                </div>
+              </div>
+
               <hgroup className='fdFlatGrid__wrapper__li__article__hgroup'>
                 <h2 className='fdFlatGrid__wrapper__li__article__hgroup--h2'>{values.title}</h2>
                 <h3 className='fdFlatGrid__wrapper__li__article__hgroup--h3'>{useFormatDate(values.release_date)}</h3>
