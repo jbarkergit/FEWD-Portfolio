ac09f1ea5878c798cfb13984952a0cffad8932cd
Justin Barker <jbarkerpoc@gmail.com>
2024-03-08 00:12:09 -0600

    Moved icons into assets folder, +file structure

---
+++ src/film-database/assets/svg-icons/MaterialRightCaret.tsx
@@ -0,0 +1,8 @@
+const MaterialRightCaret = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+      <path fill='currentColor' d='m13.292 12l-4.6-4.6l.708-.708L14.708 12L9.4 17.308l-.708-.708z'></path>
+    </svg>
+  );
+};
+export default MaterialRightCaret;
