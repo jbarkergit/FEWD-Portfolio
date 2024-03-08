18cb1949c70154f91c046646576a142c08729e11
Justin Barker <jbarkerpoc@gmail.com>
2024-03-08 01:51:39 -0600

    Rebuilt footer + restyled

--- src/film-database/components/navigation/footer/FDFooter.tsx
+++ src/film-database/components/navigation/footer/FDFooter.tsx
@@ -5,45 +5,47 @@ import TmdbLogoShort from '../../../assets/logo/TmdbLogoShort';
 const FDFooter = () => {
   return (
     <footer className='fdFooter'>
-      <section className='fdFooter__block'>
-        <table>
-          <thead>
-            <tr>
-              <th>Browse</th>
-              <th>TV Shows</th>
-              <th>Membership</th>
-              <th>Membership Plans</th>
-              <th>Help</th>
-              <th>About Us</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>x</td>
-              <td>y</td>
-              <td>z</td>
-            </tr>
-            <tr>
-              <td>x</td>
-              <td>y</td>
-              <td>z</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-
-      <section className='fdFooter__block'>
-        <nav className='fdFooter__block__nav'>
-          <div className='fdFooter__block__nav__socials'>{/* socials here */}</div>
-          <div className='fdFooter__block__nav__additions'>{/* additional info here */}</div>
+      <section className='fdFooter__section'>
+        <nav className='fdFooter__section__nav'>
+          <ul className='fdFooter__section__nav__ul'>
+            <header className='fdFooter__section__nav__ul__header'>
+              <h2>Browse</h2>
+            </header>
+            {['Streaming Library', 'Movies', 'Television', 'Kids'].map((item: string) => (
+              <li className='fdFooter__section__nav__ul__li'>
+                <Link to='' aria-label={`Navigate to ${item}`}>
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className='fdFooter__section__nav__ul'>
+            <header className='fdFooter__section__nav__ul__header'>
+              <h2>Help</h2>
+            </header>
+            {[
+              'What to Watch',
+              'About Ads',
+              'Privacy Policy',
+              'Do Not Sell or Share My Personal Information',
+              'Your US State Privacy Rights',
+              'Terms of Service',
+            ].map((item: string) => (
+              <li className='fdFooter__section__nav__ul__li'>
+                <Link to='' aria-label={`Navigate to ${item}`}>
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </nav>
       </section>
 
-      <section className='fdFooter__block'>
+      <section className='fdFooter__blockquote'>
         <Link to='https://www.themoviedb.org/' target='_blank'>
           {useCreatePicture({ svg: <TmdbLogoShort />, alt: 'TMDB API Logo' })}
         </Link>
-        <blockquote>
+        <blockquote className='fdFooter__blockquote__tmdbApiCredit'>
           <p>
             All media content along with associated information, images, and videos are provided courtesy of{' '}
             <Link to='https://www.themoviedb.org/' target='_blank'>
