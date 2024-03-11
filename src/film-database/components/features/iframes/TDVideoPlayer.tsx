d798b83711773c2974386390e838fce1a39fbe34
Justin Barker <jbarkerpoc@gmail.com>
2024-03-11 10:36:09 -0500

    Video player now utilizes react-youtube lib

--- src/film-database/components/features/iframes/TDVideoPlayer.tsx
+++ src/film-database/components/features/iframes/TDVideoPlayer.tsx
@@ -1,30 +1,53 @@
-import { useEffect } from 'react';
+import { Dispatch, SetStateAction } from 'react';
+import YouTube from 'react-youtube';
 import { Type_Tmdb_Parent_StateObjArr, Type_Tmdb_Trailer_Obj } from '../../../api/types/TmdbDataTypes';
+import { Options } from 'youtube-player/dist/types';
+
+/** This component utilizes YouTube Player API
+ * https://developers.google.com/youtube/iframe_api_reference
+ * via third party library https://github.com/tjallingt/react-youtube
+ */
 
 type Type_PropDrill = {
   videoPlayerState: boolean;
   videoPlayerVideos: Type_Tmdb_Parent_StateObjArr;
+  setVideoPlayerState: Dispatch<SetStateAction<boolean>>;
 };
 
-const FDVideoPlayer = ({ videoPlayerState, videoPlayerVideos }: Type_PropDrill) => {
-  const trailerKey: string | undefined =
+const FDVideoPlayer = ({ videoPlayerState, videoPlayerVideos, setVideoPlayerState }: Type_PropDrill) => {
+  const trailerObj: Type_Tmdb_Trailer_Obj | undefined =
     videoPlayerVideos && videoPlayerVideos.length > 0
-      ? (videoPlayerVideos[0]?.value as Type_Tmdb_Trailer_Obj[])?.find((object) => object.site === 'YouTube' && object.type === 'Trailer')?.key
+      ? (videoPlayerVideos[0]?.value as Type_Tmdb_Trailer_Obj[])?.find((object) => object.site === 'YouTube' && object.type === 'Trailer')
       : undefined;
 
-  useEffect(() => {
-    console.log(videoPlayerVideos);
-  }, [videoPlayerVideos]);
+  const opts: Options = {
+    height: undefined,
+    width: undefined,
+    playerVars: {
+      origin: 'http://localhost:5173/film-database',
+      hl: 'eng',
+      cc_lang_pref: 'eng',
+      cc_load_policy: 1,
+      iv_load_policy: 3,
+      autoplay: 1,
+      rel: 0,
+      playsinline: 0,
+    },
+  };
 
-  if (trailerKey)
+  if (trailerObj?.key)
     return (
       <section className='fdVideoPlayer' data-status={videoPlayerState}>
-        <iframe
-          className='fdVideoPlayer--iframe'
-          src={`https://www.youtube.com/embed/${trailerKey}`}
-          title='YouTube video player'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowFullScreen
+        <h2 className='fdVideoPlayer--h2'>{trailerObj.name}</h2>
+        <YouTube
+          videoId={trailerObj.key}
+          className='fdVideoPlayer__wrapper'
+          iframeClassName='fdVideoPlayer__wrapper--iframe'
+          title={`YouTube video player: ${trailerObj.name}`}
+          // loading={string}
+          opts={opts}
+          onEnd={() => setVideoPlayerState(false)}
+          onError={() => setVideoPlayerState(false)}
         />
       </section>
     );
