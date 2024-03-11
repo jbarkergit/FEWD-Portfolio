fb3894d403a1a54af0320cafb1f4c996f901016b
Justin Barker <jbarkerpoc@gmail.com>
2024-03-11 07:26:57 -0500

    Removed custom hook in favor of pages scope

--- src/film-database/api/hooks/useTmdbVideos.tsx
+++
@@ -1,6 +0,0 @@
-import { tmdbEndPoints } from '../data/tmdbEndPoints';
-import { useTmdbApi } from './useTmdbApi';
-
-export const useTmdbVideos = async (propertyId: string) => {
-  return await useTmdbApi({ tmdbEndPointKeyValuePairArr: tmdbEndPoints.movies.find((obj) => obj.key === 'videos'), movie_id: `${propertyId}` });
-};
