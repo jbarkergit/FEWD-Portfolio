3d79d9f6fed554a20a9afb734841e1cb9a6a248d
Justin Barker <jbarkerpoc@gmail.com>
2024-03-28 11:37:35 -0500

    Removed informational hgroup

--- src/film-database/components/media/poster/FDPosterProp.tsx
+++ src/film-database/components/media/poster/FDPosterProp.tsx
@@ -1,8 +1,7 @@
 // React
 import { Link } from 'react-router-dom';
 // Assets
-import { MaterialPlayCircle } from '../../../assets/svg-icons/MaterialPlayCircle';
-import { BootstrapThreeDotsVertical } from '../../../assets/svg-icons/PhDotsThreeVerticalBold';
+import { BootstrapThreeDotsVertical } from '../../../assets/svg-icons/BootstrapThreeDotsVertical';
 // Hooks
 import useCreatePicture from '../../../hooks/component-creation/useCreatePicture';
 import { useFormatDate } from '../../../hooks/formatters/useFormatDate';
@@ -22,26 +21,26 @@ const FDPosterProp = ({ mapValue, useVideoPlayer }: Type_PropDrill) => {
           {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${mapValue.poster_path}.svg`, alt: mapValue.title as string })}
 
           <div className='FDMediaGrid__wrapper__ul__li__article__graphic__overlay'>
-            <button
+            {/* <button
               className='FDMediaGrid__wrapper__ul__li__article__graphic__overlay--play'
               aria-label='Play Trailer'
               onClick={() => useVideoPlayer(`${mapValue.id}`)}>
               {useCreatePicture({ svg: <MaterialPlayCircle />, alt: 'Play Trailer' })}
-            </button>
+            </button> */}
             <button className='FDMediaGrid__wrapper__ul__li__article__graphic__overlay--moreInfo' aria-label='More Information'>
               {useCreatePicture({ svg: <BootstrapThreeDotsVertical />, alt: 'More Information' })}
             </button>
           </div>
         </div>
 
-        <hgroup className='FDMediaGrid__wrapper__ul__li__article__hgroup'>
+        {/* <hgroup className='FDMediaGrid__wrapper__ul__li__article__hgroup'>
           <Link to='' aria-label={mapValue.title}>
             <h2 className='FDMediaGrid__wrapper__ul__li__article__hgroup--h2'>{mapValue.title}</h2>
           </Link>
           <Link to='' aria-label={mapValue.title}>
             <h3 className='FDMediaGrid__wrapper__ul__li__article__hgroup--h3'>{useFormatDate(mapValue.release_date)}</h3>
           </Link>
-        </hgroup>
+        </hgroup> */}
       </article>
     </li>
   );
