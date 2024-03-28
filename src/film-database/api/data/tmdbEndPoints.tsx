c860e1ab66e4f2dd8097cd7e5b20f98b42822cf6
Justin Barker <jbarkerpoc@gmail.com>
2024-03-28 18:38:46 -0500

    Removed unnecessary data, organized, tweaked tree

--- src/film-database/api/data/tmdbEndPoints.tsx
+++ src/film-database/api/data/tmdbEndPoints.tsx
@@ -5,51 +5,22 @@
  */
 
 export const tmdbEndPoints = {
-  // end points to acess movie lists
-  movieLists: [
+  movie_lists: [
     { key: 'nowPlaying', endPoint: 'https://api.themoviedb.org/3/movie/now_playing' },
     { key: 'popular', endPoint: 'https://api.themoviedb.org/3/movie/popular' },
     { key: 'topRated', endPoint: 'https://api.themoviedb.org/3/movie/top_rated' },
+    { key: 'trending', endPoint: 'https://api.themoviedb.org/3/trending/movie/{time_window}' },
     { key: 'upcoming', endPoint: 'https://api.themoviedb.org/3/movie/upcoming' },
   ],
 
-  // end points to acess movie details
-  movies: [
+  movie_information: [
     { key: 'details', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}' },
-    { key: 'alternativeTitles', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/alternative_titles' },
-    { key: 'credits', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/credits' },
-    { key: 'images', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/images' },
-    { key: 'lists', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/lists' },
-    { key: 'recommendations', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations' },
     { key: 'releaseDates', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/release_dates' },
-    { key: 'reviews', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/reviews' },
-    { key: 'similar', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/similar' },
-    { key: 'videos', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/videos' },
     { key: 'watchProviders', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers' },
+    { key: 'credits', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/credits' },
+    { key: 'similar', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/similar' },
+    { key: 'recommendations', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations' },
   ],
 
-  // end points to acess people lists
-  peopleLists: [{ key: 'popular', endPoint: 'https://api.themoviedb.org/3/person/popular' }],
-
-  // end points to acess people details
-  people: [
-    { key: 'details', endPoint: 'https://api.themoviedb.org/3/person/{person_id}' },
-    { key: 'combinedCredits', endPoint: 'https://api.themoviedb.org/3/person/{person_id}/combined_credits' },
-    { key: 'images', endPoint: 'https://api.themoviedb.org/3/person/{person_id}/images' },
-    { key: 'movieCredits', endPoint: 'https://api.themoviedb.org/3/person/{person_id}/movie_credits' },
-    { key: 'tvCredits', endPoint: 'https://api.themoviedb.org/3/person/{person_id}/tv_credits' },
-  ],
-
-  // end points to acess search functionality
-  search: [
-    { key: 'movie', endPoint: 'https://api.themoviedb.org/3/search/movie' },
-    { key: 'person', endPoint: 'https://api.themoviedb.org/3/search/person' },
-    { key: 'tv', endPoint: 'https://api.themoviedb.org/3/search/tv' },
-  ],
-
-  // end points to acess trending movies/tv
-  trending: [
-    { key: 'trendingMovies', endPoint: 'https://api.themoviedb.org/3/trending/movie/{time_window}' },
-    { key: 'trendingTv', endPoint: 'https://api.themoviedb.org/3/trending/tv/{time_window}' },
-  ],
+  movie_search: { key: 'keywords', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/keywords' },
 };
