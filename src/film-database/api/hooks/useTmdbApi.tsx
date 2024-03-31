cfeabeb1c40a220295931633556a14bea752990d
Justin Barker <jbarkerpoc@gmail.com>
2024-03-31 14:13:18 -0500

    Updated types, added conditional checks

--- src/film-database/api/hooks/useTmdbApi.tsx
+++ src/film-database/api/hooks/useTmdbApi.tsx
@@ -5,6 +5,7 @@ import {
   Type_Tmdb_Discover_Obj_isUndefined,
   Type_Tmdb_Fetcher_Obj,
   Type_Tmdb_KeyValuePair_Obj,
+  Type_Tmdb_Mixed_Union_isUndefined,
   Type_Tmdb_MovieId_Obj_isUndefined,
   Type_Tmdb_Payload_Union,
   Type_Tmdb_PersonId_Obj_isUndefined,
@@ -21,7 +22,7 @@ import {
 
 export const useTmdbApi = async ({ controller, tmdbKeyValuePairUnion }: Type_Tmdb_Payload_Union): Promise<Type_Tmdb_useApiReturn_Obj[]> => {
   // Handle events where singular objects are passed by converting entries to an obj arr
-  const tmdbKeyValuePairUnionArrConversion = () => {
+  const tmdbKeyValuePairUnionArrConversion = (): (Type_Tmdb_Mixed_Union_isUndefined | undefined)[] => {
     return Array.isArray(tmdbKeyValuePairUnion) ? tmdbKeyValuePairUnion : [tmdbKeyValuePairUnion];
   };
 
@@ -30,43 +31,47 @@ export const useTmdbApi = async ({ controller, tmdbKeyValuePairUnion }: Type_Tmd
     tmdbKeyValuePairUnionArrConversion().map(async (keyValuePair) => {
       // Utilizes Type Guard to return key-value pair's properties
       const destructureKeyValuePair = () => {
-        let typedKeyValuePair;
+        if (keyValuePair) {
+          let typedKeyValuePair;
 
-        switch (true) {
-          case 'movie_id' in keyValuePair:
-            typedKeyValuePair = keyValuePair as Type_Tmdb_MovieId_Obj_isUndefined;
-            return {
-              key: typedKeyValuePair.tmdbEndPointObj.key,
-              label: typedKeyValuePair.tmdbEndPointObj.label,
-              endPoint: typedKeyValuePair.tmdbEndPointObj.endPoint,
-              parameter: typedKeyValuePair.movie_id,
-            };
+          switch (true) {
+            case 'movie_id' in keyValuePair:
+              typedKeyValuePair = keyValuePair as Type_Tmdb_MovieId_Obj_isUndefined;
+              return {
+                key: typedKeyValuePair.tmdbEndPointObj.key,
+                label: typedKeyValuePair.tmdbEndPointObj.label,
+                endPoint: typedKeyValuePair.tmdbEndPointObj.endPoint,
+                parameter: typedKeyValuePair.movie_id,
+              };
 
-          case 'person_id' in keyValuePair:
-            typedKeyValuePair = keyValuePair as Type_Tmdb_PersonId_Obj_isUndefined;
-            return {
-              key: typedKeyValuePair.tmdbEndPointObj.key,
-              label: typedKeyValuePair.tmdbEndPointObj.label,
-              endPoint: typedKeyValuePair.tmdbEndPointObj.endPoint,
-              parameter: typedKeyValuePair.person_id,
-            };
+            case 'person_id' in keyValuePair:
+              typedKeyValuePair = keyValuePair as Type_Tmdb_PersonId_Obj_isUndefined;
+              return {
+                key: typedKeyValuePair.tmdbEndPointObj.key,
+                label: typedKeyValuePair.tmdbEndPointObj.label,
+                endPoint: typedKeyValuePair.tmdbEndPointObj.endPoint,
+                parameter: typedKeyValuePair.person_id,
+              };
 
-          case 'discover' in keyValuePair:
-            typedKeyValuePair = keyValuePair as Type_Tmdb_Discover_Obj_isUndefined;
-            return {
-              key: typedKeyValuePair.tmdbEndPointObj.key,
-              label: typedKeyValuePair.tmdbEndPointObj.label,
-              endPoint: typedKeyValuePair.tmdbEndPointObj.endPoint,
-              parameter: typedKeyValuePair.discover,
-            };
+            case 'discover' in keyValuePair:
+              typedKeyValuePair = keyValuePair as Type_Tmdb_Discover_Obj_isUndefined;
+              return {
+                key: typedKeyValuePair.tmdbEndPointObj.key,
+                label: typedKeyValuePair.tmdbEndPointObj.label,
+                endPoint: typedKeyValuePair.tmdbEndPointObj.endPoint,
+                parameter: typedKeyValuePair.discover,
+              };
 
-          default:
-            typedKeyValuePair = keyValuePair as Type_Tmdb_KeyValuePair_Obj;
-            return {
-              key: typedKeyValuePair.key,
-              label: typedKeyValuePair.label,
-              endPoint: typedKeyValuePair.endPoint,
-            };
+            default:
+              typedKeyValuePair = keyValuePair as Type_Tmdb_KeyValuePair_Obj;
+              return {
+                key: typedKeyValuePair.key,
+                label: typedKeyValuePair.label,
+                endPoint: typedKeyValuePair.endPoint,
+              };
+          }
+        } else {
+          console.error(`Key-value pair cannot be processed.`);
         }
       };
 
@@ -75,15 +80,15 @@ export const useTmdbApi = async ({ controller, tmdbKeyValuePairUnion }: Type_Tmd
       // tmdbFetcher
       const fetchDataPromise: Type_Tmdb_Fetcher_Obj | undefined = await tmdbFetcher({
         controller: controller,
-        keyValuePairEndPoint: destructuredData.endPoint,
-        parameter: destructuredData.parameter,
+        keyValuePairEndPoint: destructuredData?.endPoint,
+        parameter: destructuredData?.parameter,
       });
 
       // Identify bad api call without leaking api key to console
       if (!fetchDataPromise || !fetchDataPromise.results) {
-        console.error(`Unfulfilled fetch response for key: ${destructuredData.key}`);
+        console.error(`Unfulfilled fetch response for key: ${destructuredData?.key}`);
       } else {
-        return { key: destructuredData.key, label: destructuredData.label, value: fetchDataPromise.results };
+        return { key: destructuredData?.key, label: destructuredData?.label, value: fetchDataPromise.results };
       }
     })
   );
