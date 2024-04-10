ec71941eeed064b6b7d4e37496c420d887141764
Justin Barker <jbarkerpoc@gmail.com>
2024-04-10 15:56:49 -0500

    Moved informational HTML to new page for modal

--- src/film-database/components/media/poster/FDPosterProp.tsx
+++ src/film-database/components/media/poster/FDPosterProp.tsx
@@ -1,9 +1,5 @@
-import { Link } from 'react-router-dom';
-// Components
-import FDiFrame from '../iFrame/FDiFrame';
 // Component Hooks
 import useCreatePicture from '../../../hooks/component-creation/useCreatePicture';
-import { useFormatDate } from '../../../hooks/formatters/useFormatDate';
 // API Types
 import { Type_Tmdb_ApiCall_Union, Type_Tmdb_ApiCallMovieList_Obj } from '../../../api/types/TmdbDataTypes';
 import { Type_useFilmDatabaseWebStorage_Obj } from '../../../hooks/web-storage-api/useFilmDatabaseWebStorage';
@@ -24,22 +20,6 @@ const FDPosterProp = ({ mapValue, grid, trailerCache, useFetchTrailer }: Type_Pr
         <div className='FDMediaGrid__wrapper__ul__li__article__graphic'>
           {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${value.poster_path}.svg`, alt: value.title as string })}
         </div>
-
-        {!trailerCache?.some((obj) => obj.trailer_id === value.id) ? (
-          <section className='FDMediaGrid__wrapper__ul__li__article__poster'>
-            <hgroup className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup'>
-              <Link to='' aria-label={value.title}>
-                <h2 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h2'>{value.title}</h2>
-              </Link>
-              <Link to='' aria-label={value.title}>
-                <h3 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h3'>{useFormatDate(value.release_date)}</h3>
-              </Link>
-            </hgroup>
-            {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${value.poster_path}.svg`, alt: value.title as string })}
-          </section>
-        ) : null}
-
-        <FDiFrame trailerCache={trailerCache} mappedTrailerId={value.id} />
       </article>
     </li>
   );
---
+++ src/film-database/components/modal/FDMediaModal.tsx
@@ -0,0 +1,23 @@
+const FDMediaModal = () => {
+  return (
+    <section className='FDMediaModal'>
+      {' '}
+      {/* {!trailerCache?.some((obj) => obj.trailer_id === value.id) ? (
+    <section className='FDMediaGrid__wrapper__ul__li__article__poster'>
+      <hgroup className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup'>
+        <Link to='' aria-label={value.title}>
+          <h2 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h2'>{value.title}</h2>
+        </Link>
+        <Link to='' aria-label={value.title}>
+          <h3 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h3'>{useFormatDate(value.release_date)}</h3>
+        </Link>
+      </hgroup>
+      {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${value.poster_path}.svg`, alt: value.title as string })}
+    </section>
+  ) : null}
+
+  <FDiFrame trailerCache={trailerCache} mappedTrailerId={value.id} /> */}
+    </section>
+  );
+};
+export default FDMediaModal;
