089a287e21c7491e0b300375a5a9f24cd71c4b20
Justin Barker <jbarkerpoc@gmail.com>
2024-04-09 21:01:21 -0500

    Dynam visible nodes, dynam trailer fetch, kb nav

--- src/film-database/components/media/FDMediaGrid.tsx
+++ src/film-database/components/media/FDMediaGrid.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 // Lib
 import { v4 as uuidv4 } from 'uuid';
-// Types
-import { Type_Tmdb_ApiCall_Union } from '../../api/types/TmdbDataTypes';
+// API Data
+import { tmdbEndPoints } from '../../api/data/tmdbEndPoints';
+// API Types
+import { Type_Tmdb_ApiCallTrailer_Obj, Type_Tmdb_ApiCall_Union, Type_Tmdb_OptParamloadTrailer_Obj } from '../../api/types/TmdbDataTypes';
+// API Hooks
+import { useTmdbApi } from '../../api/hooks/useTmdbApi';
 // Components
 import FDCarouselOverlay from './carousel-overlay/FDCarouselOverlay';
 import FDPosterProp from './poster/FDPosterProp';
+import { Type_useFilmDatabaseWebStorage_Obj, useFilmDatabaseWebStorage } from '../../hooks/web-storage-api/useFilmDatabaseWebStorage';
+import { useLocation } from 'react-router-dom';
 
 type Type_PropDrill = {
   dataKey: string;
@@ -15,25 +21,92 @@ type Type_PropDrill = {
 };
 
 const FDMediaGrid = ({ dataKey, dataLabel, dataValue, grid }: Type_PropDrill) => {
-  const [setIndex, setSetIndex] = useState<{ prevIndex: number; currIndex: number }>({ prevIndex: 1, currIndex: 1 });
+  const userLocation = useLocation();
 
-  /** Data Pagination && Carousel Navigation */
-  const [paginatedData, setPaginatedData] = useState<Type_Tmdb_ApiCall_Union[]>([]);
+  /** Refs */
+  const carouselWrapper = useRef<HTMLDivElement>(null);
   const carouselUl = useRef<HTMLUListElement>(null);
 
+  /** Button Navigation State Index */
+  const [btnNavIndex, setBtnNavIndex] = useState<{ prevIndex: number; currIndex: number }>({ prevIndex: 1, currIndex: 1 });
+
+  /** Carousel Visible Nodes State
+   * Employed observers to watch for visible children nodes of carouselUl.current
+   * Note: Intersectional Observer does not detect changes that may occur
+   * Employment of Mutation Observer is required to ensure our observations are concurrent
+   */
+  const [visibleNodesCount, setVisibleNodesCount] = useState<number>(8);
+  useEffect(() => console.log(visibleNodesCount), [visibleNodesCount]);
+
+  useEffect(() => {
+    const observer: IntersectionObserver = new IntersectionObserver(
+      // Filter entries that are intersecting (visible in DOM), pass length to state
+      (entries) => setVisibleNodesCount(entries.filter((entry) => entry.isIntersecting).length),
+      // Observer OPTS Note: Threshold is set to 1 to ensure we're observing ONLY 100% visible nodes
+      { root: carouselUl.current, rootMargin: '0px', threshold: 1 }
+    );
+
+    const observeNodes = () => {
+      // Create array from our ref's children && observe each node
+      if (carouselUl.current) Array.from(carouselUl.current.children).forEach((node) => observer.observe(node));
+    };
+
+    // Initial observation
+    observeNodes();
+    // Re-observe when carouselUl.current changes e.g. when media queries change the amount of visible nodes
+    const mutationObserver: MutationObserver = new MutationObserver(observeNodes);
+    mutationObserver.observe(carouselUl.current!, { childList: true });
+
+    return () => {
+      observer.disconnect();
+      mutationObserver.disconnect();
+    };
+  }, []);
+
+  /** Update navigation overlay button height dynamically
+   * Due to the informational footer, I'm opting to use JavaScript to set heights of the buttons in order to prevent any potential visual mishaps.
+   * Due to HTML structure, there isn't an easy way to pass refs upwards; therefore, I opted to drill dom nodes (I understand this isn't the best practice)
+   * Note: Temporary until I finish HTML structure -- WILL replace with resize Observer OR forwardRef
+   */
+  const [posterDimensions, setPosterDimensions] = useState<{ width: number | undefined; height: number | undefined }>({ width: undefined, height: undefined });
+
+  useEffect(() => {
+    const updatePosterDimensions = () => {
+      if (carouselUl.current) {
+        const childrenNestOne = carouselUl.current.children[0] as HTMLLIElement | undefined;
+        if (!childrenNestOne) return;
+
+        const childrenNestTwo = childrenNestOne.children[0] as HTMLElement | undefined;
+        if (!childrenNestTwo) return;
+
+        const childrenNestThree = childrenNestTwo.children[0] as HTMLDivElement | undefined;
+        if (!childrenNestThree) return;
+
+        const posterOverlayRect: DOMRect = childrenNestThree.getBoundingClientRect();
+        setPosterDimensions({ width: posterOverlayRect?.width, height: posterOverlayRect?.height });
+      }
+    };
+
+    updatePosterDimensions(); // Initial height
+    window.addEventListener('resize', updatePosterDimensions);
+    return () => window.removeEventListener('resize', updatePosterDimensions);
+  }, [carouselUl.current]);
+
   /** Data Pagination: Pushes data into state when setIndex changes (the user navigates) */
+  const [paginatedData, setPaginatedData] = useState<Type_Tmdb_ApiCall_Union[]>([]);
+
   useEffect(() => {
     if (!grid) {
       setPaginatedData((prevData: Type_Tmdb_ApiCall_Union[]) => {
         const prevDataLength: number = prevData.length - 1;
         const startIndex: number = prevDataLength === 0 ? 0 : prevDataLength + 1;
-        const endIndex: number = 8 * setIndex.currIndex;
+        const endIndex: number = visibleNodesCount * btnNavIndex.currIndex;
         return [...prevData, ...dataValue.slice(startIndex, endIndex)] as Type_Tmdb_ApiCall_Union[];
       });
     }
-  }, [setIndex]);
+  }, [btnNavIndex]);
 
-  /** INFINITE LOOP
+  /** INFINITE LOOP BUTTON NAVIGATION
    * [EMPLOYED] Animation && End of Loop Wrapping: CSS Scroll-Snapping && JavaScript Scroll Methods
    * Requires perfect min max boundaries for indexing
    * Reduces overall JavaScript logic, doesn't require CSS animations
@@ -47,8 +120,8 @@ const FDMediaGrid = ({ dataKey, dataLabel, dataValue, grid }: Type_PropDrill) =>
    */
   useEffect(() => {
     if (carouselUl.current) {
-      const firstPossibleIndex: boolean = setIndex.currIndex === 1;
-      const lastPossibleIndex: boolean = setIndex.currIndex === Math.ceil(dataValue.length / 8);
+      const firstPossibleIndex: boolean = btnNavIndex.currIndex === 1;
+      const lastPossibleIndex: boolean = btnNavIndex.currIndex === Math.ceil(dataValue.length / visibleNodesCount);
 
       let nextChildsIndex: number;
 
@@ -62,12 +135,13 @@ const FDMediaGrid = ({ dataKey, dataLabel, dataValue, grid }: Type_PropDrill) =>
           break;
 
         default:
-          nextChildsIndex = (setIndex.currIndex - 1) * 8 - 1;
+          nextChildsIndex = (btnNavIndex.currIndex - 1) * visibleNodesCount - 1;
           break;
       }
 
       const carouselChildren: HTMLCollection = carouselUl.current.children;
       const nextChild = carouselChildren[nextChildsIndex] as HTMLLIElement;
+
       if (nextChild) {
         const scrollDistance: number = nextChild.offsetLeft - carouselUl.current.offsetLeft;
         carouselUl.current.scrollTo({ left: scrollDistance, behavior: 'smooth' });
@@ -75,33 +149,84 @@ const FDMediaGrid = ({ dataKey, dataLabel, dataValue, grid }: Type_PropDrill) =>
     }
   }, [paginatedData]);
 
-  /** Update navigation overlay button height dynamically
-   * Due to the informational footer, I'm opting to use JavaScript to set heights of the buttons in order to prevent any potential visual mishaps.
-   * Due to HTML structure, there isn't an easy way to pass refs upwards; therefore, I opted to drill dom nodes (I understand this isn't the best practice)
+  /** KEYBOARD NAVIGATION
+   * #1. Index handler
+   * #2. Dom manipulation
    */
-  const [posterDimensions, setPosterDimensions] = useState<{ width: number | undefined; height: number | undefined }>({ width: undefined, height: undefined });
-
-  const updatePosterDimensions = () => {
-    if (carouselUl.current) {
-      const childrenNestOne = carouselUl.current.children[0] as HTMLLIElement | undefined;
-      if (!childrenNestOne) return;
-
-      const childrenNestTwo = childrenNestOne.children[0] as HTMLElement | undefined;
-      if (!childrenNestTwo) return;
-
-      const childrenNestThree = childrenNestTwo.children[0] as HTMLDivElement | undefined;
-      if (!childrenNestThree) return;
-
-      const posterOverlayRect: DOMRect = childrenNestThree.getBoundingClientRect();
-      setPosterDimensions({ width: posterOverlayRect?.width, height: posterOverlayRect?.height });
-    }
-  };
+  const [activeChild, setActiveChild] = useState<number>(0);
 
   useEffect(() => {
-    updatePosterDimensions(); // Initial height
-    window.addEventListener('resize', updatePosterDimensions);
-    return () => window.removeEventListener('resize', updatePosterDimensions);
-  }, [carouselUl.current]);
+    const keyboardStrokeHandler = (event: KeyboardEvent) => {
+      switch (true) {
+        case event.key === 'ArrowLeft':
+          setActiveChild(activeChild === 0 ? dataValue.length : activeChild - 1);
+          break;
+
+        case event.key === 'ArrowRight':
+          setActiveChild(activeChild === dataValue.length ? 0 : activeChild + 1);
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', keyboardStrokeHandler);
+    return () => window.removeEventListener('keydown', keyboardStrokeHandler);
+  }, []);
+
+  useEffect(() => {
+    const carouselChildren: HTMLCollection | undefined = carouselUl.current?.children;
+    const activeNode = carouselChildren ? (carouselChildren[activeChild] as HTMLLIElement) : null;
+
+    if (carouselUl.current && carouselChildren && activeNode) {
+      activeNode.scrollTo({ left: activeNode.offsetLeft - carouselUl.current?.offsetLeft, behavior: 'smooth' });
+      activeNode.setAttribute('data-trailer-status', 'active');
+    }
+  }, [activeChild]);
+
+  /** VIDEO PLAYER STATE
+   * This set of state variables enables the application to utilize a single YouTube iFrame component to produce trailer results for media.
+   * This component makes use of the react-youtube API to handle iFrames.
+   * videoPlayerTrailer stores API data and is used to find trailers directly from YouTube opposed to alternative sources.
+   */
+  const [trailerCache, setTrailerCache] = useState<Type_useFilmDatabaseWebStorage_Obj[]>();
+
+  useEffect(() => {
+    const controller: AbortController = new AbortController();
+    const cachedTrailers = useFilmDatabaseWebStorage({ userLocation: userLocation, cacheKey: 'trailerCache' }).getData() as Type_useFilmDatabaseWebStorage_Obj[];
+    const isCachedTrailer: boolean = cachedTrailers?.some((obj) => obj.trailer_id === dataValue[activeChild].id);
+
+    if (!isCachedTrailer) {
+      (async (): Promise<void> => {
+        const trailerObjData = await useTmdbApi({
+          controller: controller,
+          payload: {
+            tmdbEndPointObj: tmdbEndPoints.movie_trailer_videos,
+            trailer_id: { typeGuardKey: 'trailer_id', propValue: `${dataValue[activeChild].id}` },
+          } as unknown as Type_Tmdb_OptParamloadTrailer_Obj,
+        });
+
+        const trailerObj: Type_useFilmDatabaseWebStorage_Obj[] = [
+          {
+            trailer_id: dataValue[activeChild].id,
+            trailer: (trailerObjData as Type_Tmdb_ApiCallTrailer_Obj[])?.find((object) => object.site === 'YouTube' && object.type === 'Trailer'),
+          },
+        ];
+
+        if (trailerObjData && trailerObj) {
+          useFilmDatabaseWebStorage({ userLocation: userLocation, data: trailerObj, cacheKey: 'trailerCache' }).setData();
+
+          setTrailerCache((prevData: Type_useFilmDatabaseWebStorage_Obj[] | undefined) => {
+            if (prevData) return [...prevData, ...cachedTrailers];
+            else return cachedTrailers;
+          });
+        }
+      })();
+    } else {
+      setTrailerCache(cachedTrailers);
+    }
+  }, [activeChild]);
 
   /** Component */
   return (
@@ -109,13 +234,13 @@ const FDMediaGrid = ({ dataKey, dataLabel, dataValue, grid }: Type_PropDrill) =>
       <div className='FDMediaGrid__header'>
         <h2 className='FDMediaGrid__header--h2'>{dataLabel ? dataLabel : dataKey.replace('_', ' ')}</h2>
       </div>
-      <div className='FDMediaGrid__wrapper' data-status={grid ? 'grid' : 'carousel'}>
+      <div className='FDMediaGrid__wrapper' data-status={grid ? 'grid' : 'carousel'} ref={carouselWrapper}>
         <ul className='FDMediaGrid__wrapper__ul' data-status={grid ? 'grid' : 'carousel'} ref={carouselUl}>
           {grid
-            ? dataValue.map((values) => <FDPosterProp mapValue={values} grid={grid} key={uuidv4()} />)
-            : paginatedData.map((values) => <FDPosterProp mapValue={values} grid={grid} key={uuidv4()} />)}
+            ? dataValue.map((values) => <FDPosterProp mapValue={values} grid={grid} trailerCache={trailerCache} key={uuidv4()} />)
+            : paginatedData.map((values) => <FDPosterProp mapValue={values} grid={grid} trailerCache={trailerCache} key={uuidv4()} />)}
         </ul>
-        {grid ? null : <FDCarouselOverlay posterDimensions={posterDimensions} tmdbArrLength={dataValue.length - 1} setSetIndex={setSetIndex} />}
+        {grid ? null : <FDCarouselOverlay posterDimensions={posterDimensions} tmdbArrLength={dataValue.length - 1} setBtnNavIndex={setBtnNavIndex} />}
       </div>
     </section>
   );
