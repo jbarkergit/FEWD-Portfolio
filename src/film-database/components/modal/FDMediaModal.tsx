518ad6a93a7f1a94cf1de0eee3240c6906838803
Justin Barker <jbarkerpoc@gmail.com>
2024-04-10 16:10:53 -0500

    Opted to use a modal to keep app spa

--- src/film-database/components/modal/FDMediaModal.tsx
+++ src/film-database/components/modal/FDMediaModal.tsx
@@ -1,22 +1,24 @@
+import { Link } from 'react-router-dom';
+import useCreatePicture from '../../hooks/component-creation/useCreatePicture';
+import { useFormatDate } from '../../hooks/formatters/useFormatDate';
+import FDiFrame from '../media/iFrame/FDiFrame';
+
 const FDMediaModal = () => {
   return (
     <section className='FDMediaModal'>
-      {' '}
-      {/* {!trailerCache?.some((obj) => obj.trailer_id === value.id) ? (
-    <section className='FDMediaGrid__wrapper__ul__li__article__poster'>
-      <hgroup className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup'>
-        <Link to='' aria-label={value.title}>
-          <h2 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h2'>{value.title}</h2>
-        </Link>
-        <Link to='' aria-label={value.title}>
-          <h3 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h3'>{useFormatDate(value.release_date)}</h3>
-        </Link>
-      </hgroup>
-      {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${value.poster_path}.svg`, alt: value.title as string })}
-    </section>
-  ) : null}
+      <section className='FDMediaGrid__wrapper__ul__li__article__poster'>
+        <hgroup className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup'>
+          <Link to='' aria-label={value.title}>
+            <h2 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h2'>{value.title}</h2>
+          </Link>
+          <Link to='' aria-label={value.title}>
+            <h3 className='FDMediaGrid__wrapper__ul__li__article__poster__hgroup--h3'>{useFormatDate(value.release_date)}</h3>
+          </Link>
+        </hgroup>
+        {useCreatePicture({ src: `https://image.tmdb.org/t/p/original/${value.poster_path}.svg`, alt: value.title as string })}
+      </section>
 
-  <FDiFrame trailerCache={trailerCache} mappedTrailerId={value.id} /> */}
+      <FDiFrame trailerCache={trailerCache} mappedTrailerId={value.id} />
     </section>
   );
 };
