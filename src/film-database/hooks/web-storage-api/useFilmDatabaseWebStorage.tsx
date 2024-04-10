be29bb7234bc4b96fe3d1db1ea3a2baabe296b58
Justin Barker <jbarkerpoc@gmail.com>
2024-04-09 21:03:42 -0500

    Now caching trailer objects -- needs work

--- src/film-database/hooks/web-storage-api/useFilmDatabaseWebStorage.tsx
+++ src/film-database/hooks/web-storage-api/useFilmDatabaseWebStorage.tsx
@@ -1,28 +1,50 @@
 import { filmDatabaseKeyValuePairs } from '../../../App';
-import { Type_Tmdb_useApiReturn_Obj } from '../../api/types/TmdbDataTypes';
+import { Type_Tmdb_ApiCallTrailer_Obj, Type_Tmdb_useApiReturn_Obj } from '../../api/types/TmdbDataTypes';
 
-export const useFilmDatabaseWebStorage = (userLocation: any, data?: Type_Tmdb_useApiReturn_Obj[]) => {
+export type Type_useFilmDatabaseWebStorage_Obj = {
+  trailer_id: string | number;
+  trailer: Type_Tmdb_ApiCallTrailer_Obj | undefined;
+};
+
+interface Type_Tmdb_useWebStorage_Interface {
+  userLocation: any;
+  cacheKey: 'movieCache' | 'trailerCache';
+  data?: Type_Tmdb_useApiReturn_Obj[] | Type_useFilmDatabaseWebStorage_Obj[];
+}
+
+export const useFilmDatabaseWebStorage = ({ userLocation, cacheKey, data }: Type_Tmdb_useWebStorage_Interface) => {
   /** Custom Cross-Origin restriction safety protocol (utilizes project paths from custom app routing solution) */
   const isSameOrigin = (): boolean => {
     return filmDatabaseKeyValuePairs.some((obj) => obj.path === `${userLocation.pathname}`);
   };
 
-  /** Web Storage Key */
-  const filmDatabaseStorageKey: string = 'filmDatabaseCache';
-
   /** Data Setter */
   const setData = (): void => {
-    if (isSameOrigin()) localStorage.setItem(filmDatabaseStorageKey, JSON.stringify(data));
-    else console.error('Cross-Origin access to localStorage is not allowed.');
+    if (isSameOrigin()) {
+      if (cacheKey === 'trailerCache') {
+        const trailerData = data as Type_useFilmDatabaseWebStorage_Obj[];
+        const cachedTrailers = getData() as Type_useFilmDatabaseWebStorage_Obj[];
+
+        if (trailerData && cachedTrailers) {
+          localStorage.setItem(cacheKey, JSON.stringify([...trailerData, ...cachedTrailers]));
+        } else {
+          localStorage.setItem(cacheKey, JSON.stringify(data));
+        }
+      } else {
+        localStorage.setItem(cacheKey, JSON.stringify(data));
+      }
+    } else {
+      console.error('Cross-Origin access to localStorage is not allowed.');
+    }
   };
 
   /** Data getter - Utilizes Web Storage to prevent unnecessary API calls */
-  const getData = (): Type_Tmdb_useApiReturn_Obj[] | null => {
+  const getData = (): Type_Tmdb_useApiReturn_Obj[] | Type_useFilmDatabaseWebStorage_Obj[] | null => {
     if (isSameOrigin()) {
-      const cachedData = localStorage.getItem(filmDatabaseStorageKey);
+      const cachedData = localStorage.getItem(cacheKey);
 
       if (cachedData) {
-        return JSON.parse(cachedData) as Type_Tmdb_useApiReturn_Obj[];
+        return JSON.parse(cachedData);
       } else {
         console.error('Could not retrieve data from Web Storage.');
         return null;
