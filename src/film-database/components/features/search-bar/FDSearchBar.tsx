4cedaf37f2588fb9e31108ee2e2a7ff7dae96d9d
Justin Barker <jbarkerpoc@gmail.com>
2024-04-16 16:44:58 -0500

    Handled search bar expansion with state

--- src/film-database/components/features/search-bar/FDSearchBar.tsx
+++ src/film-database/components/features/search-bar/FDSearchBar.tsx
@@ -5,17 +5,26 @@ import { tmdbEndPoints } from '../../../api/data/tmdbEndPoints';
 import { useTmdbApi } from '../../../api/hooks/useTmdbApi';
 
 const FDSearchBar = () => {
-  const input = useRef<HTMLInputElement>(null);
   const searchBar = useRef<HTMLDivElement>(null);
+
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [searchResults, setSearchResults] = useState<Type_Tmdb_useApiReturn_Obj[]>([]);
 
   /** Exterior click handler */
-  useEffect(() => {
-    const exteriorClickHandler = (e: PointerEvent): void => {
-      if (!searchBar.current?.contains(e.target as HTMLElement)) setSearchTerm('');
-    };
+  const exteriorClickHandler = (event: PointerEvent): void => {
+    switch (true) {
+      case !searchBar.current?.contains((event.target as HTMLElement) || (event.target as HTMLButtonElement)):
+        setSearchTerm('');
+        setIsInputFocused(false);
+        break;
 
+      default:
+        null;
+        break;
+    }
+  };
+
+  useEffect(() => {
     searchBar.current?.addEventListener('pointerup', exteriorClickHandler);
     document.body.addEventListener('pointerup', exteriorClickHandler);
 
@@ -24,6 +33,28 @@ const FDSearchBar = () => {
       document.body.removeEventListener('pointerup', exteriorClickHandler);
     };
   }, []);
+
+  /** Expanding search bar */
+  const input = useRef<HTMLInputElement>(null);
+  const label = useRef<HTMLLabelElement>(null);
+  const button = useRef<HTMLButtonElement>(null);
+
+  const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isInputFocused) {
+      input.current?.focus();
+      [input.current, label.current, button.current].forEach((ref: HTMLInputElement | HTMLLabelElement | HTMLButtonElement | null) => {
+        ref?.setAttribute('data-status', 'active');
+      });
+    } else {
+      setIsInputFocused(false);
+      [input.current, label.current, button.current].forEach((ref: HTMLInputElement | HTMLLabelElement | HTMLButtonElement | null) => {
+        ref?.setAttribute('data-status', 'disabled');
+      });
+      setSearchTerm('');
+    }
+  }, [isInputFocused]);
 
   /** Search Results
    * Note: Checking tmdbDataArr for existing options would significantly increase loading times; therefore, I've opted to fetch on every search request
@@ -45,58 +76,61 @@ const FDSearchBar = () => {
     })();
   }, [searchTerm]);
 
-  const values = searchResults[0].value.map((data) => data);
+  const values: Type_Tmdb_ApiCall_Union[] | undefined = searchResults[0] ? searchResults[0].value.map((data) => data) : undefined;
 
   /** JSX */
   return (
     <div className='fdSearchBar' ref={searchBar}>
-      <div className='fdSearchBar__searcher'>
-        <fieldset className='fdSearchBar__searcher__fieldset'>
-          <legend>Find the entertainment you're looking for</legend>
+      <fieldset className='fdSearchBar__fieldset'>
+        <legend>Find the entertainment you're looking for</legend>
 
-          <input
-            className='fdSearchBar__searcher__fieldset__input'
-            id='fdSearchBar__searcher__fieldset__input'
-            ref={input}
-            data-focus={searchTerm.length > 0 ? true : false}
-            type='text'
-            placeholder=''
-            value={searchTerm.replace('-', ' ')}
-            autoCapitalize='none'
-            autoComplete='none'
-            autoCorrect='off'
-            spellCheck='false'
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e?.target.value.replace(' ', '-'))}
-            onBlur={() => input.current?.removeAttribute('data-status')}
-          />
-          {searchTerm.length === 0 ? (
-            <label className='fdSearchBar__searcher__fieldset__label' htmlFor='fdSearchBar__searcher__fieldset__input'>
-              Find the entertainment you're looking for
-            </label>
-          ) : null}
+        <input
+          className='fdSearchBar__fieldset__input'
+          id='fdSearchBar__fieldset__input'
+          ref={input}
+          data-focus={searchTerm.length > 0 ? true : false}
+          type='text'
+          placeholder=''
+          value={searchTerm.replace('-', ' ')}
+          autoCapitalize='none'
+          autoComplete='none'
+          autoCorrect='off'
+          spellCheck='false'
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e?.target.value.replace(' ', '-'))}
+          onFocus={() => setIsInputFocused(true)}
+          data-status='disabled'
+        />
+        {searchTerm.length === 0 ? (
+          <label className='fdSearchBar__fieldset__label' htmlFor='fdSearchBar__fieldset__input' data-status='disabled' ref={label}>
+            Find the entertainment you're looking for
+          </label>
+        ) : null}
 
-          <button
-            className='fdSearchBar__searcher__fieldset__button'
-            onClick={() => {
-              input.current?.setAttribute('data-status', 'expanded');
-              input.current?.focus();
-            }}
-            aria-label={`Search for ${searchTerm}`}>
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256'>
-              <path
-                fill='#888888'
-                d='M34 64a6 6 0 0 1 6-6h176a6 6 0 0 1 0 12H40a6 6 0 0 1-6-6m6 70h72a6 6 0 0 0 0-12H40a6 6 0 0 0 0 12m88 52H40a6 6 0 0 0 0 12h88a6 6 0 0 0 0-12m108.24 10.24a6 6 0 0 1-8.48 0l-21.49-21.48a38.06 38.06 0 1 1 8.49-8.49l21.48 21.49a6 6 0 0 1 0 8.48M184 170a26 26 0 1 0-26-26a26 26 0 0 0 26 26'></path>
-            </svg>
-          </button>
-        </fieldset>
-      </div>
+        <button
+          className='fdSearchBar__fieldset__button'
+          ref={button}
+          onClick={() => {
+            if (!isInputFocused) setIsInputFocused(true);
+          }}
+          aria-label={`Search for ${searchTerm}`}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256'>
+            <path
+              fill='#888888'
+              d='M34 64a6 6 0 0 1 6-6h176a6 6 0 0 1 0 12H40a6 6 0 0 1-6-6m6 70h72a6 6 0 0 0 0-12H40a6 6 0 0 0 0 12m88 52H40a6 6 0 0 0 0 12h88a6 6 0 0 0 0-12m108.24 10.24a6 6 0 0 1-8.48 0l-21.49-21.48a38.06 38.06 0 1 1 8.49-8.49l21.48 21.49a6 6 0 0 1 0 8.48M184 170a26 26 0 1 0-26-26a26 26 0 0 0 26 26'></path>
+          </svg>
+        </button>
+      </fieldset>
 
-      <ul className='fdSearchBar__results' tabIndex={-1}>
-        {searchResults.length > 0 ? (
-          values.slice(0, 6).map((obj) => <li key={uuidv4()}>{(obj as Type_Tmdb_ApiCallMovie_Obj).title}</li>)
-        ) : (
+      <ul className='fdSearchBar__results' data-status={values && values.length > 0 && searchTerm.length > 0 ? 'active' : 'disabled'}>
+        {values && searchResults.length > 0 ? (
+          values.slice(0, 8).map((obj) => (
+            <li className='fdSearchBar__results__li' key={uuidv4()}>
+              {(obj as Type_Tmdb_ApiCallMovie_Obj).title}
+            </li>
+          ))
+        ) : searchTerm.length > 0 ? (
           <li>No results found.</li>
-        )}
+        ) : null}
       </ul>
     </div>
   );
