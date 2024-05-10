9282afecc4cde7793b45fc91735e5f9364c3d37f
Justin Barker <jbarkerpoc@gmail.com>
2024-05-10 10:26:52 -0500

    Complete refactor with data type related bug fixes

--- src/film-database/composables/web-storage-api/useFilmDatabaseWebStorage.tsx
+++ src/film-database/composables/web-storage-api/useFilmDatabaseWebStorage.tsx
@@ -1,60 +1,107 @@
 import { filmDatabaseKeyValuePairs } from '../../../App';
 import { Type_Tmdb_ApiCallTrailer_Obj, Type_Tmdb_useApiReturn_Obj } from '../tmdb-api/types/TmdbDataTypes';
 
-export type Type_useFilmDatabaseWebStorage_Obj = {
+export type Type_useFDWebStorage_Trailer_Obj = {
   trailer_id: string | number;
   trailer: Type_Tmdb_ApiCallTrailer_Obj | undefined;
 };
 
+type Type_useFDWebStorage_setData_ArrUnion = Type_Tmdb_useApiReturn_Obj[] | Type_useFDWebStorage_Trailer_Obj[] | undefined;
+type Type_useFDWebStorage_getData_Union = Type_Tmdb_useApiReturn_Obj | Type_useFDWebStorage_Trailer_Obj | undefined;
+
 interface Type_Tmdb_useWebStorage_Interface {
   userLocation: any;
-  cacheKey: 'movieCache' | 'trailerCache' | string;
-  data?: Type_Tmdb_useApiReturn_Obj[] | Type_useFilmDatabaseWebStorage_Obj[];
+  cacheKey: string | string[];
+  data?: Type_useFDWebStorage_setData_ArrUnion;
 }
 
 export const useFilmDatabaseWebStorage = ({ userLocation, cacheKey, data }: Type_Tmdb_useWebStorage_Interface) => {
-  /** Custom Cross-Origin restriction safety protocol (utilizes project paths from custom app routing solution) */
-  const isSameOrigin = (): boolean => {
-    return filmDatabaseKeyValuePairs.some((obj) => obj.path === `${userLocation.pathname}`);
-  };
+  /** Errors */
+  const fireCrossOriginError = (): void => console.error('Cross-Origin access to localStorage is not permitted.');
+  const fireRetrievalError = (): void => console.error('Could not retrieve data from Web Storage.');
+
+  /** Simple Cross-Origin restriction safety protocol (utilizes project paths from custom app routing solution) */
+  const isSameOrigin: boolean = filmDatabaseKeyValuePairs.some((obj) => obj.path === userLocation);
 
   /** Data Setter */
   const setData = (): void => {
-    if (isSameOrigin()) {
-      if (cacheKey === 'trailerCache') {
-        const trailerData = data as Type_useFilmDatabaseWebStorage_Obj[];
-        const cachedTrailers = getData() as Type_useFilmDatabaseWebStorage_Obj[];
+    if (isSameOrigin) {
+      const handleDataCaching = (key: string, value: Type_useFDWebStorage_setData_ArrUnion): void => localStorage.setItem(key, JSON.stringify(value));
 
-        if (trailerData && cachedTrailers) {
-          localStorage.setItem(cacheKey, JSON.stringify([...trailerData, ...cachedTrailers]));
-        } else {
-          localStorage.setItem(cacheKey, JSON.stringify(data));
-        }
-      } else {
-        localStorage.setItem(cacheKey, JSON.stringify(data));
+      switch (cacheKey) {
+        case 'trailers':
+          const trailerData = data as Type_useFDWebStorage_Trailer_Obj[];
+          const cachedTrailers = getData() as Type_useFDWebStorage_Trailer_Obj[];
+          const mergedData = [...(trailerData || []), ...(cachedTrailers || [])];
+          handleDataCaching(cacheKey, mergedData);
+          break;
+
+        default:
+          if (Array.isArray(cacheKey)) cacheKey.forEach((key) => handleDataCaching(key, data));
+          else handleDataCaching(cacheKey, data);
+          break;
       }
     } else {
-      console.error('Cross-Origin access to localStorage is not allowed.');
+      fireCrossOriginError();
+      return;
     }
   };
 
   /** Data getter - Utilizes Web Storage to prevent unnecessary API calls */
-  const getData = (): Type_Tmdb_useApiReturn_Obj[] | Type_useFilmDatabaseWebStorage_Obj[] | null => {
-    if (isSameOrigin()) {
-      const cachedData = localStorage.getItem(cacheKey);
+  const getData = (): Type_useFDWebStorage_getData_Union[] | undefined => {
+    let cachedDataArr: Type_useFDWebStorage_getData_Union[] | undefined = [];
 
-      if (cachedData) {
-        return JSON.parse(cachedData);
+    // Get && Parse Function
+    const getParsedWebStorageKey = (key: string) => {
+      const cachedDataString: string | null = localStorage.getItem(key);
+
+      if (cachedDataString !== null) {
+        const parsedData: Type_Tmdb_useApiReturn_Obj[] | Type_useFDWebStorage_Trailer_Obj[] = JSON.parse(cachedDataString);
+
+        switch (true) {
+          case parsedData.some((obj) => 'key' in obj):
+            return parsedData as Type_Tmdb_useApiReturn_Obj[];
+
+          case parsedData.some((obj) => 'trailer_id' in obj):
+            return parsedData as Type_useFDWebStorage_Trailer_Obj[];
+
+          default:
+            console.error('Web Storage Key is missing or undefined.');
+            return undefined;
+        }
       } else {
-        console.error('Could not retrieve data from Web Storage.');
-        return null;
+        fireRetrievalError();
+        return undefined;
+      }
+    };
+
+    // Fire Get && Parse Function
+    if (isSameOrigin) {
+      switch (true) {
+        case Array.isArray(cacheKey):
+          cacheKey.forEach((key) => {
+            const parsedData = getParsedWebStorageKey(key);
+            if (parsedData) cachedDataArr.push(...parsedData);
+          });
+          break;
+
+        case typeof cacheKey === 'string':
+          const parsedData = getParsedWebStorageKey(cacheKey);
+          if (parsedData) cachedDataArr.push(...parsedData);
+          break;
+
+        default:
+          fireRetrievalError();
+          return undefined;
       }
     } else {
-      console.error('Cross-Origin access to localStorage is not allowed.');
-      return null;
+      fireCrossOriginError();
+      return;
     }
+
+    return cachedDataArr;
   };
 
-  // Return nested functions
+  // Return nested functions in useFilmDatabaseWebStorage scope
   return { setData, getData };
 };
