8e6661db7e6c8eee5ae191c11d41380eaa0e072a
Justin Barker <jbarkerpoc@gmail.com>
2024-05-18 07:12:09 -0700

    Refactored movie endpoint structure

--- src/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
+++ src/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
@@ -4,27 +4,31 @@
  * each object represents an api "category" of endpoints
  */
 
-export const tmdbEndPoints = {
-  movie_lists: {
-    nowPlaying: { key: 'movie_lists_nowPlaying', label: 'Now Playing', endPoint: 'https://api.themoviedb.org/3/movie/now_playing' },
-    popular: { key: 'movie_lists_popular', label: 'Popular', endPoint: 'https://api.themoviedb.org/3/movie/popular' },
-    topRated: { key: 'movie_lists_topRated', label: 'Top Rated', endPoint: 'https://api.themoviedb.org/3/movie/top_rated' },
-    upcoming: { key: 'movie_lists_upcoming', label: 'Upcoming', endPoint: 'https://api.themoviedb.org/3/movie/upcoming' },
+export const tmdbMovieEndpoints = {
+  theatrical: {
+    now_playing: 'https://api.themoviedb.org/3/movie/now_playing',
+    upcoming: 'https://api.themoviedb.org/3/movie/upcoming',
   },
-  movie_trending: {
-    trendingDay: { key: 'movie_trending_trendingDay', label: 'Trending Today', endPoint: 'https://api.themoviedb.org/3/trending/movie/day' },
-    trendingWeek: { key: 'movie_trending_trendingWeek', label: 'Trending This Week', endPoint: 'https://api.themoviedb.org/3/trending/movie/week' },
+  hot: {
+    popular: 'https://api.themoviedb.org/3/movie/popular',
+    top_rated: 'https://api.themoviedb.org/3/movie/top_rated',
   },
-  movie_information: {
-    details: { key: 'movie_information_details', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}' },
-    releaseDates: { key: 'movie_information_releaseDates', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/release_dates' },
-    watchProviders: { key: 'movie_information_watchProviders', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers' },
-    credits: { key: 'movie_information_credits', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/credits' },
-    similar: { key: 'movie_information_similar', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/similar' },
-    recommendations: { key: 'movie_information_recommendations', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations' },
+  trending: {
+    trending_today: 'https://api.themoviedb.org/3/trending/movie/day',
+    trending_this_week: 'https://api.themoviedb.org/3/trending/movie/week',
   },
-  movie_search_movie: { key: 'movie_search_movie', endPoint: 'https://api.themoviedb.org/3/search/movie' },
-  movie_search_keyword: { key: 'movie_search_keyword', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/keywords' },
-  movie_trailer_videos: { key: 'movie_trailer_videos', endPoint: 'https://api.themoviedb.org/3/movie/{movie_id}/videos' },
-  movie_discover: { key: 'movie_discover', endPoint: 'https://api.themoviedb.org/3/discover/movie' },
+  informationById: {
+    details: 'https://api.themoviedb.org/3/movie/{movie_id}',
+    releaseDates: 'https://api.themoviedb.org/3/movie/{movie_id}/release_dates',
+    watchProviders: 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers',
+    credits: 'https://api.themoviedb.org/3/movie/{movie_id}/credits',
+    similar: 'https://api.themoviedb.org/3/movie/{movie_id}/similar',
+    recommendations: 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations',
+  },
+  searchById: {
+    movie: 'https://api.themoviedb.org/3/search/movie',
+    discover: 'https://api.themoviedb.org/3/discover/movie',
+    keyword: 'https://api.themoviedb.org/3/movie/{movie_id}/keywords',
+    trailers: 'https://api.themoviedb.org/3/movie/{movie_id}/videos',
+  },
 };
