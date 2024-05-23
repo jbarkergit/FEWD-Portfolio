2edf49d5968f2427a69aa9a3a36eca3a22f78cf6
Justin Barker <jbarkerpoc@gmail.com>
2024-05-23 14:43:57 -0700

    refactor(data/types): rewrote all related functions to optimize performance and individualize comps
    BREAKING CHANGE: Data types changed requiring refactor of all child components

---
+++ src/film-database/pages/FilmDatabase.tsx
@@ -0,0 +1,152 @@
+import { ReactElement, ReactNode, createContext, useContext, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+// Api Data
+import { Type_Tmdb_Movie_Keys_Union, tmdbMovieEndpoints } from '../composables/tmdb-api/data/tmdbEndPoints';
+// Api Types
+import { Type_Tmdb_Api_Union } from '../composables/tmdb-api/types/TmdbDataTypes';
+// Api Hooks
+import { Type_useFetchTmdbResponse_KeyValuePairArr, useFetchTmdbResponse } from '../composables/tmdb-api/hooks/useFetchTmdbResponse';
+// Components
+import FDCarousel from '../components/carousel/FDCarousel';
+import FDHeader from '../components/header/FDHeader';
+import FDFooter from '../components/footer/FDFooter';
+import FDHero from '../components/hero/FDHero';
+import FDCarouselSkeleton from '../components/carousel/FDCarouselSkeleton';
+// Types
+type Type_paginatedDataMap = Map<string, Type_Tmdb_Api_Union[]>;
+
+const FilmDatabase = () => {
+  // References
+  const fdMediaRef = useRef<HTMLElement>(null);
+  const carouselUlRefReceiver = useRef<HTMLUListElement>(null);
+  const carouselUlRef = carouselUlRefReceiver;
+
+  // Global Variables
+  const useLocationPathname = useLocation().pathname;
+
+  /** Fetch and set data */
+  const [tmdbDataArr, setTmdbDataArr] = useState<Type_useFetchTmdbResponse_KeyValuePairArr>([]);
+
+  useEffect(() => {
+    // Fetch data based on the user's location (will change when state navigation is implemented)
+    let keyValuePairArr: [string, string][] = [];
+
+    switch (useLocationPathname) {
+      case '/film-database/genre/horror':
+        keyValuePairArr = [];
+        break;
+
+      default:
+        keyValuePairArr = Array.from(tmdbMovieEndpoints.theatrical.entries());
+        break;
+    }
+
+    // Initialize fetch, set state
+    useFetchTmdbResponse({ endPoint_keyValuePairArr: keyValuePairArr }).then((data) => {
+      if (data) setTmdbDataArr(data);
+    });
+  }, []);
+
+  /** Initial data pagination */
+  const [paginatedData, setPaginatedData] = useState<Type_paginatedDataMap>(new Map());
+  const [visibleNodesCount, setVisibleNodesCount] = useState<number>(8);
+
+  const paginateTmdbDataArrOnMount = () => {
+    let dataMap: Type_paginatedDataMap = new Map();
+
+    tmdbDataArr.forEach(([key, dataArr]) => {
+      dataMap.set(key, dataArr.slice(0, visibleNodesCount));
+    });
+
+    setPaginatedData(dataMap);
+  };
+
+  useEffect(() => paginateTmdbDataArrOnMount(), [tmdbDataArr]);
+
+  /** Carousel Visible Nodes State
+   * Employed observers to watch for visible children nodes of carouselUl.current
+   * Note: Intersectional Observer does not detect changes that may occur
+   * Employment of Mutation Observer is required to ensure our observations are concurrent
+   * Note: State may seem unnecessary; however, it's important to update the visible node count due to viewport sizing to refire our logic to force a rerender
+   */
+
+  const observeCarouselNodes = () => {
+    const observer: IntersectionObserver = new IntersectionObserver(
+      // Filter entries that are intersecting (visible in DOM), pass length to state
+      (entries: IntersectionObserverEntry[]) => {
+        const filteredEntriesLength: number = entries.filter((entry: IntersectionObserverEntry) => entry.isIntersecting).length;
+        if (filteredEntriesLength > 0) setVisibleNodesCount(filteredEntriesLength);
+      },
+      // Observer OPTS Note: Threshold is set to .2 to ensure we're observing ALL visible nodes; partial or not.
+      { root: carouselUlRef.current, rootMargin: '0px', threshold: 0.2 }
+    );
+
+    // Observe each list-item node of the first carousel
+    const observeNodes = () => {
+      if (carouselUlRef.current && carouselUlRef.current.children) {
+        Array.from(carouselUlRef.current.children).forEach((node) => observer.observe(node));
+      }
+    };
+
+    // Initial observation
+    observeNodes();
+
+    // Re-observe when carouselUlRef.current changes e.g. when media queries change the amount of visible nodes
+    const mutationObserver: MutationObserver = new MutationObserver(observeNodes);
+    if (carouselUlRef.current) mutationObserver.observe(carouselUlRef.current, { childList: true });
+
+    return () => {
+      observer.disconnect();
+      mutationObserver.disconnect();
+    };
+  };
+
+  useEffect(() => observeCarouselNodes(), [carouselUlRef.current?.children]);
+
+  /** Component props context */
+  const componentProps = {
+    fdMediaRef: fdMediaRef,
+    ref: carouselUlRefReceiver,
+    carouselUlRef: carouselUlRef,
+    visibleNodesCount: visibleNodesCount,
+  };
+
+  const ComponentPropsContext = createContext<typeof componentProps>(componentProps);
+
+  const ComponentPropsProvider = ({ children }: { children?: ReactNode }): ReactElement => {
+    return <ComponentPropsContext.Provider value={componentProps}>{children}</ComponentPropsContext.Provider>;
+  };
+
+  const useComponentProps = useContext(ComponentPropsContext);
+
+  /** Get paginated data by key (autofill provided) */
+  const createComponentByMapKey = (key: Type_Tmdb_Movie_Keys_Union): JSX.Element | null => {
+    const isKeyInMap: boolean = paginatedData.has(key);
+
+    if (!isKeyInMap) {
+      console.error('Failure to identify key within data map.');
+      return null;
+    } else {
+      const getPaginatedData: Type_Tmdb_Api_Union[] | undefined = paginatedData.get(key);
+
+      switch (getPaginatedData) {
+        case undefined:
+          return <FDCarouselSkeleton />;
+        default:
+          return <FDCarousel key={key} data={getPaginatedData} useComponentProps={useComponentProps} />;
+      }
+    }
+  };
+
+  /** Component */
+  return (
+    <div className='filmDatabase'>
+      <FDHeader />
+      <main className='fdMedia' ref={fdMediaRef}>
+        <ComponentPropsProvider>{createComponentByMapKey('now_playing')}</ComponentPropsProvider>
+      </main>
+    </div>
+  );
+};
+
+export default FilmDatabase;
