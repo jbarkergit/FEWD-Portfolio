a855f1efe21771f0b244adbd8ec670f043684804
Justin Barker <jbarkerpoc@gmail.com>
2024-05-23 15:14:46 -0700

    chore(rename): renamed component

---
+++ src/film-database/components/carousel/FDCarouselArticles.tsx
@@ -0,0 +1,36 @@
+import { useTmdbProps } from '../../composables/tmdb-api/hooks/useTmdbProps';
+import { Type_Tmdb_Api_Union } from '../../composables/tmdb-api/types/TmdbDataTypes';
+
+type PropDrill = {
+  data: Type_Tmdb_Api_Union[];
+};
+
+const FDCarouselArticles = ({ data }: PropDrill) => {
+  return (
+    <ul className='fdMedia__carousel__wrapper__ul'>
+      {data.map((obj: Type_Tmdb_Api_Union) => {
+        const props = useTmdbProps(obj);
+
+        return (
+          <li className='fdMedia__carousel__wrapper__ul__li'>
+            <article className='fdMedia__carousel__wrapper__ul__li__article'>
+              <header className='fdMedia__carousel__wrapper__ul__li__article__header'>
+                <h2>{props?.heading}</h2>
+              </header>
+              <div className='fdMedia__carousel__wrapper__ul__li__article__graphic'>
+                <figure>
+                  <picture>
+                    <img src={`https://image.tmdb.org/t/p/original/${props?.poster_path}`} alt={`${props?.alt}`} />
+                    <figcaption>{`${props?.alt}`}</figcaption>
+                  </picture>
+                </figure>
+              </div>
+            </article>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FDCarouselArticles;
