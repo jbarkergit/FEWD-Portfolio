d572fd8f7bdcd35e821834408a3b909cc9f26474
Justin Barker <jbarkerpoc@gmail.com>
2024-05-23 17:06:55 -0700

    refactor(carousel): altered conditional to narrow types

--- src/film-database/composables/tmdb-api/hooks/useTmdbProps.tsx
+++ src/film-database/composables/tmdb-api/hooks/useTmdbProps.tsx
@@ -1,17 +1,17 @@
 import { Type_Tmdb_Api_Union, Type_Tmdb_ApiMovieList_Obj, Type_Tmdb_ApiMovie_Obj } from '../types/TmdbDataTypes';
 
 export const useTmdbProps = (obj: Type_Tmdb_Api_Union) => {
-  switch (obj.type_guard) {
-    case 'movielist':
+  switch (true) {
+    case 'genre_ids' in obj:
       const movieList = obj as Type_Tmdb_ApiMovieList_Obj;
       return { heading: movieList.title, poster_path: movieList.poster_path, alt: movieList.title };
 
-    case 'movie':
+    case 'imdb_id' in obj:
       const movie = obj as Type_Tmdb_ApiMovie_Obj;
       const poster_path: string = movie.poster_path ? (movie.poster_path as string) : (movie.backdrop_path as string);
       return { heading: movie.title, poster_path: poster_path, alt: movie.title };
 
-    case 'trailer':
+    case 'published_at' in obj:
       console.error('useTmdbProps does not accept trailers, please refer to useFetchTmdbTrailer.');
       break;
 
