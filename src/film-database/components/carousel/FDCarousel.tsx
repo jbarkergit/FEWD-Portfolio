3a688883e3664b69e1931223b3afe5c702aca044
Justin Barker <jbarkerpoc@gmail.com>
2024-05-25 15:49:38 -0700

    feat(carousel): handled rendering of paginated data sets

--- src/film-database/components/carousel/FDCarousel.tsx
+++ src/film-database/components/carousel/FDCarousel.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import MaterialLeftCaret from '../../assets/svg-icons/MaterialLeftCaret';
 import MaterialRightCaret from '../../assets/svg-icons/MaterialRightCaret';
 import { Type_Tmdb_Api_Union } from '../../composables/tmdb-api/types/TmdbDataTypes';
@@ -9,18 +11,34 @@ type Type_FilmDatabase_Props = { dataKey: string; mapValue: Type_Tmdb_Api_Union[
 
 const FDCarousel = ({ dataKey, mapValue }: Type_FilmDatabase_Props) => {
   const formattedDataKey: string = dataKey.replaceAll('_', ' ');
-  const flattenedArray = mapValue.flatMap((innerArray) => innerArray);
 
-  const updateNavigationIndex = () => {};
+  const [articles, setArticles] = useState<Type_Tmdb_Api_Union[][]>([mapValue[0]]);
+  const [carouselIndex, setCarouselIndex] = useState<number>(0);
+
+  const updateCarouselIndex = (delta: number) => {
+    setCarouselIndex(Math.max(0, Math.min(carouselIndex + delta, mapValue.length - 1)));
+  };
+
+  const addDataSet = () => {
+    const isIndexInArticles: boolean = articles.some((_, index) => index === carouselIndex);
+
+    if (!isIndexInArticles) {
+      setArticles((prevState) => {
+        return [...prevState, mapValue[carouselIndex]];
+      });
+    }
+  };
+
+  useEffect(() => addDataSet(), [carouselIndex]);
 
   return (
     <section className='fdMedia__carousel' aria-label={`${formattedDataKey} Section`}>
       <h2 className='fdMedia__carousel__header'>{formattedDataKey}</h2>
       <div className='fdMedia__carousel__wrapper'>
-        <FDCarouselArticles articles={flattenedArray} />
+        {articles ? <FDCarouselArticles articles={articles.flatMap((innerArray) => innerArray)} /> : null}
         <nav className='fdMedia__carousel__wrapper__navigation'>
-          <FDCarouselButton caption={'Show Previous'} icon={<MaterialLeftCaret />} func={updateNavigationIndex} funcIndex={-1} />
-          <FDCarouselButton caption={'Show More'} icon={<MaterialRightCaret />} func={updateNavigationIndex} funcIndex={1} />
+          <FDCarouselButton caption={'Show Previous'} icon={<MaterialLeftCaret />} func={updateCarouselIndex} funcDelta={-1} />
+          <FDCarouselButton caption={'Show More'} icon={<MaterialRightCaret />} func={updateCarouselIndex} funcDelta={1} />
         </nav>
       </div>
     </section>
