0c96fcc8978c8686731f3a932cf912408889adea
Justin Barker <jbarkerpoc@gmail.com>
2024-05-25 13:41:54 -0700

    refactor: made carousel navigation buttons reusable components to keep things dry

---
+++ src/film-database/components/carousel/FDCarouselButton.tsx
@@ -0,0 +1,21 @@
+type Type_PropDrill = {
+  caption: string;
+  icon: JSX.Element;
+  func: (delta: number) => void;
+  funcIndex: number;
+};
+
+const FDCarouselButton = ({ caption, icon, func, funcIndex }: Type_PropDrill) => {
+  return (
+    <button className='fdMedia__carousel__wrapper__navigation--button' aria-label={caption} onClick={() => func(funcIndex)}>
+      <figure>
+        <picture>
+          {icon}
+          <figcaption>{caption}</figcaption>
+        </picture>
+      </figure>
+    </button>
+  );
+};
+
+export default FDCarouselButton;
