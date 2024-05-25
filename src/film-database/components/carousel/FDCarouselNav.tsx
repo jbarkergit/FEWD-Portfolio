520a294d946012669256c56f233057780dad47e1
Justin Barker <jbarkerpoc@gmail.com>
2024-05-24 18:48:38 -0700

    feat(carouselnav): handled carousel pagination / navigation index

--- src/film-database/components/carousel/FDCarouselNav.tsx
+++ src/film-database/components/carousel/FDCarouselNav.tsx
@@ -1,17 +1,24 @@
+import { useEffect, useState } from 'react';
 import MaterialLeftCaret from '../../assets/svg-icons/MaterialLeftCaret';
 import MaterialRightCaret from '../../assets/svg-icons/MaterialRightCaret';
 
-const FDCarouselNav = () => {
+type Type_PropDrill = { paginate: (paginationIndex: number) => void; maxPaginationIndex: number | undefined };
+
+const FDCarouselNav = ({ paginate, maxPaginationIndex }: Type_PropDrill) => {
+  const [carouselNavIndex, setCarouselNavIndex] = useState<number>(1);
+  useEffect(() => paginate(carouselNavIndex), [carouselNavIndex]);
+
   return (
     <nav className='fdMedia__carousel__wrapper__navigation'>
       <button
         className='fdMedia__carousel__wrapper__navigation--button'
         aria-label='Show Previous'
         onClick={() => {
-          // setCarouselNavIndex((prevIndex) => {
-          //   const clampedIndex: number = Math.max(1, Math.min(prevIndex - 1, mapObjMaxIndex));
-          //   return prevIndex === 1 ? mapObjMaxIndex : clampedIndex;
-          // });
+          if (maxPaginationIndex)
+            setCarouselNavIndex((prevIndex) => {
+              const clampedIndex: number = Math.max(1, Math.min(prevIndex - 1, maxPaginationIndex));
+              return prevIndex === 1 ? maxPaginationIndex : clampedIndex;
+            });
         }}>
         <figure>
           <picture>
@@ -25,10 +32,11 @@ const FDCarouselNav = () => {
         className='fdMedia__carousel__wrapper__navigation--button'
         aria-label='Show More'
         onClick={() => {
-          // setCarouselNavIndex((prevIndex) => {
-          //   const clampedIndex: number = Math.max(1, Math.min(prevIndex + 1, mapObjMaxIndex));
-          //   return prevIndex === mapObjMaxIndex ? 1 : clampedIndex;
-          // });
+          if (maxPaginationIndex)
+            setCarouselNavIndex((prevIndex) => {
+              const clampedIndex: number = Math.max(1, Math.min(prevIndex + 1, maxPaginationIndex));
+              return prevIndex === maxPaginationIndex ? 1 : clampedIndex;
+            });
         }}>
         <figure>
           <picture>
