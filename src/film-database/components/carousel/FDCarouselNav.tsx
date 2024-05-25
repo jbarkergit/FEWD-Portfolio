a5207209d20589d1963163ba66f08d6e0a0d4545
Justin Barker <jbarkerpoc@gmail.com>
2024-05-24 19:04:14 -0700

    refactor(carouselnav): moved logic from onclick to functions in the event i add more logic

--- src/film-database/components/carousel/FDCarouselNav.tsx
+++ src/film-database/components/carousel/FDCarouselNav.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import MaterialLeftCaret from '../../assets/svg-icons/MaterialLeftCaret';
 import MaterialRightCaret from '../../assets/svg-icons/MaterialRightCaret';
 
@@ -8,18 +8,38 @@ const FDCarouselNav = ({ paginate, maxPaginationIndex }: Type_PropDrill) => {
   const [carouselNavIndex, setCarouselNavIndex] = useState<number>(1);
   useEffect(() => paginate(carouselNavIndex), [carouselNavIndex]);
 
+  const decrement = () => {
+    if (maxPaginationIndex)
+      setCarouselNavIndex((prevIndex) => {
+        const clampedIndex: number = Math.max(1, Math.min(prevIndex - 1, maxPaginationIndex));
+        return prevIndex === 1 ? maxPaginationIndex : clampedIndex;
+      });
+  };
+
+  const increment = () => {
+    if (maxPaginationIndex)
+      setCarouselNavIndex((prevIndex) => {
+        const clampedIndex: number = Math.max(1, Math.min(prevIndex + 1, maxPaginationIndex));
+        return prevIndex === maxPaginationIndex ? 1 : clampedIndex;
+      });
+  };
+
+  const decrementRef = useRef<HTMLButtonElement>(null);
+  const incrementRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    decrementRef.current?.addEventListener('pointerup', decrement);
+    incrementRef.current?.addEventListener('pointerup', increment);
+
+    return () => {
+      decrementRef.current?.removeEventListener('pointerup', decrement);
+      incrementRef.current?.removeEventListener('pointerup', increment);
+    };
+  }, []);
+
   return (
     <nav className='fdMedia__carousel__wrapper__navigation'>
-      <button
-        className='fdMedia__carousel__wrapper__navigation--button'
-        aria-label='Show Previous'
-        onClick={() => {
-          if (maxPaginationIndex)
-            setCarouselNavIndex((prevIndex) => {
-              const clampedIndex: number = Math.max(1, Math.min(prevIndex - 1, maxPaginationIndex));
-              return prevIndex === 1 ? maxPaginationIndex : clampedIndex;
-            });
-        }}>
+      <button className='fdMedia__carousel__wrapper__navigation--button' aria-label='Show Previous' ref={decrementRef}>
         <figure>
           <picture>
             <MaterialLeftCaret />
@@ -28,16 +48,7 @@ const FDCarouselNav = ({ paginate, maxPaginationIndex }: Type_PropDrill) => {
         </figure>
       </button>
 
-      <button
-        className='fdMedia__carousel__wrapper__navigation--button'
-        aria-label='Show More'
-        onClick={() => {
-          if (maxPaginationIndex)
-            setCarouselNavIndex((prevIndex) => {
-              const clampedIndex: number = Math.max(1, Math.min(prevIndex + 1, maxPaginationIndex));
-              return prevIndex === maxPaginationIndex ? 1 : clampedIndex;
-            });
-        }}>
+      <button className='fdMedia__carousel__wrapper__navigation--button' aria-label='Show More' ref={incrementRef}>
         <figure>
           <picture>
             <MaterialRightCaret />
