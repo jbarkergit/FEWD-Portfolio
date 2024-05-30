788111eb9e59c90b2dd879a5906149605b021560
Justin Barker <jbarkerpoc@gmail.com>
2024-05-30 15:55:30 -0700

    feat(hero): passing data to state on article click

--- src/film-database/components/carousel/FDCarouselArticles.tsx
+++ src/film-database/components/carousel/FDCarouselArticles.tsx
@@ -3,19 +3,21 @@ import { v4 as uuidv4 } from 'uuid';
 import { useTmdbProps } from '../../composables/tmdb-api/hooks/useTmdbProps';
 
 import { Type_Tmdb_Api_Union } from '../../composables/tmdb-api/types/TmdbDataTypes';
+import { Dispatch, SetStateAction } from 'react';
 
 type Type_FilmDatabase_Props = {
   articles: Type_Tmdb_Api_Union[];
+  setHeroData: Dispatch<SetStateAction<Type_Tmdb_Api_Union | null>>;
 };
 
-const FDCarouselArticles = ({ articles }: Type_FilmDatabase_Props) => {
+const FDCarouselArticles = ({ articles, setHeroData }: Type_FilmDatabase_Props) => {
   return (
     <>
       {articles.map((article) => {
         const props = useTmdbProps(article);
 
         return (
-          <li className='fdMedia__carousel__wrapper__ul__li' key={uuidv4()}>
+          <li className='fdMedia__carousel__wrapper__ul__li' key={uuidv4()} onClick={() => setHeroData(article)}>
             <article className='fdMedia__carousel__wrapper__ul__li__article'>
               <header className='fdMedia__carousel__wrapper__ul__li__article__header'>
                 <h2 className='fdMedia__carousel__wrapper__ul__li__article__header--h2'>{props?.heading}</h2>
