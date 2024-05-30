206ac8995f3939d6d7c0f5d17c1b9344f84082b1
Justin Barker <jbarkerpoc@gmail.com>
2024-05-30 15:00:51 -0700

    feat(props): init component properties

--- src/film-database/components/hero/FDHero.tsx
+++ src/film-database/components/hero/FDHero.tsx
@@ -1,23 +1,35 @@
-import { v4 as uuidv4 } from 'uuid';
+import { useTmdbProps } from '../../composables/tmdb-api/hooks/useTmdbProps';
 
-const FDHero = () => {
-  return (
-    <section className='fdHero'>
-      <div className='fdHero__infographic'>
-        <div className='fdHero__infographic__slide' key={uuidv4()}>
-          <hgroup className='fdHero__infographic__details'>
-            {/* <h2>{values.title}</h2> */}
-            {/* <p>{values.overview}</p> */}
-            {/* <span>{values.vote_average}</span> */}
-          </hgroup>
-          <figure>
-            <picture>{/* <img src={`https://image.tmdb.org/t/p/original/${values.backdrop_path}`} alt={values.title} /> */}</picture>
-            <figcaption></figcaption>
-          </figure>
-        </div>
-      </div>
-    </section>
-  );
+import { Type_Tmdb_Api_Union } from '../../composables/tmdb-api/types/TmdbDataTypes';
+
+type Type_PropDrill = {
+  heroData: Type_Tmdb_Api_Union | null;
+};
+
+const FDHero = ({ heroData }: Type_PropDrill) => {
+  if (heroData) {
+    const props = useTmdbProps(heroData);
+    if (props)
+      return (
+        <section className='fdHero'>
+          <div className='fdHero__infographic'>
+            <div className='fdHero__infographic__slide'>
+              <hgroup className='fdHero__infographic__details'>
+                <h2>{props.heading}</h2>
+                <p>{props.overview}</p>
+                <span>{props.vote_average}</span>
+              </hgroup>
+              <figure>
+                <picture>
+                  <img src={`https://image.tmdb.org/t/p/original/${props.backdrop_path}`} alt={props.heading} />
+                </picture>
+                <figcaption></figcaption>
+              </figure>
+            </div>
+          </div>
+        </section>
+      );
+  }
 };
 
 export default FDHero;
