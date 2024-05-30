a93379a7961ea1c30877abd9f6d7b9b96ab060e8
Justin Barker <jbarkerpoc@gmail.com>
2024-05-30 14:59:17 -0700

    feat(props): destructured more props from api vals

--- src/film-database/composables/tmdb-api/hooks/useTmdbProps.tsx
+++ src/film-database/composables/tmdb-api/hooks/useTmdbProps.tsx
@@ -4,12 +4,26 @@ export const useTmdbProps = (obj: Type_Tmdb_Api_Union) => {
   switch (true) {
     case 'genre_ids' in obj:
       const movieList = obj as Type_Tmdb_ApiMovieList_Obj;
-      return { heading: movieList.title, poster_path: movieList.poster_path, alt: movieList.title };
+      return {
+        heading: movieList.title,
+        overview: movieList.overview,
+        vote_average: movieList.vote_average,
+        poster_path: movieList.poster_path,
+        backdrop_path: movieList.backdrop_path,
+        alt: movieList.title,
+      };
 
     case 'imdb_id' in obj:
       const movie = obj as Type_Tmdb_ApiMovie_Obj;
       const poster_path: string = movie.poster_path ? (movie.poster_path as string) : (movie.backdrop_path as string);
-      return { heading: movie.title, poster_path: poster_path, alt: movie.title };
+      return {
+        heading: movie.title,
+        overview: movie.overview,
+        vote_average: movie.vote_average,
+        poster_path: poster_path,
+        backdrop_path: movie.backdrop_path,
+        alt: movie.title,
+      };
 
     case 'published_at' in obj:
       console.error('useTmdbProps does not accept trailers, please refer to useFetchTmdbTrailer.');
