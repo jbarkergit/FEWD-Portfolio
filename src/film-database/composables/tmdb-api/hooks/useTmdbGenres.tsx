fb3ec5aa6b9cffe63fdf1c440964d2e09ab76b9f
Justin Barker <jbarkerpoc@gmail.com>
2024-05-31 00:20:31 -0700

    chore(useTmdbGenres): renamed hook to better reflect associated hooks

---
+++ src/film-database/composables/tmdb-api/hooks/useTmdbGenres.tsx
@@ -0,0 +1,52 @@
+// const tvGenres: Record<string, number> = {
+//   action_adventure: 10759,
+//   animation: 16,
+//   comedy: 35,
+//   crime: 80,
+//   documentary: 99,
+//   drama: 18,
+//   family: 10751,
+//   kids: 10762,
+//   mystery: 9648,
+//   news: 10763,
+//   reality: 10764,
+//   sciFi_fantasy: 10765,
+//   soap: 10766,
+//   talk: 10767,
+//   war_politics: 10768,
+//   western: 37,
+// };
+
+// type Type_TvGenre_Keys = keyof typeof tvGenres;
+
+const movieGenres: Record<string, number> = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  crime: 80,
+  documentary: 99,
+  drama: 18,
+  family: 10751,
+  fantasy: 14,
+  history: 36,
+  horror: 27,
+  music: 10402,
+  mystery: 9648,
+  romance: 10749,
+  science_fiction: 878,
+  tv_movie: 10770,
+  thriller: 53,
+  war: 10752,
+  western: 37,
+};
+
+type Type_MovieGenre_Keys = keyof typeof movieGenres;
+
+export const useTmdbGenres = (genre?: Type_MovieGenre_Keys): number | string[] => {
+  if (genre) {
+    return movieGenres[genre!.toLowerCase() as Type_MovieGenre_Keys];
+  } else {
+    return [...Object.keys(movieGenres)].sort().map((key) => key.replaceAll('_', ' '));
+  }
+};
