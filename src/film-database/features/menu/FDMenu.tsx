0be97d5a308f007f4eccd33943ff7a90f5557dbb
Justin Barker <jbarkerpoc@gmail.com>
2024-06-01 16:11:27 -0700

    feat(menu): general html layout, wip

--- src/film-database/features/menu/FDMenu.tsx
+++ src/film-database/features/menu/FDMenu.tsx
@@ -2,11 +2,9 @@ import { Link } from 'react-router-dom';
 
 import { useTmdbGenres } from '../../composables/tmdb-api/hooks/useTmdbGenres';
 
-import { MaterialSymbolsHomeRounded, MaterialSymbolsLegendToggleRounded, TheMovieDatabaseLogo } from '../../assets/google-material-symbols/googleMaterialSymbols';
+import { MaterialSymbolsHomeRounded, MaterialSymbolsMenuRounded, MaterialSymbolsSearch, TheMovieDatabaseLogo } from '../../assets/googleMaterialSymbols';
 
 const FDMenu = () => {
-  const genresStringArr = useTmdbGenres() as string[];
-
   /**
    * expandable left to right menus
    * buttons on left
@@ -17,50 +15,44 @@ const FDMenu = () => {
    * super nested menus, will require components
    */
   return (
-    <nav className='fdMenu'>
-      <section className='fdMenu__section'>
-        <Link to='https://www.themoviedb.org/' target='_blank'>
-          <figure>
-            <picture>
-              <TheMovieDatabaseLogo />
-            </picture>
-            <figcaption>TMDB API</figcaption>
-          </figure>
-        </Link>
-
-        <ul className='fdMenu__section__ul'>
-          <li className='fdMenu__section__ul__li'>
-            <button className='fdMenu__section__ul__li--button'>
-              <MaterialSymbolsHomeRounded /> Home
-            </button>
-          </li>
-          <li className='fdMenu__section__ul__li'>
-            <button className='fdMenu__section__ul__li--button'>
-              <MaterialSymbolsLegendToggleRounded /> Genres
-            </button>
-          </li>
-        </ul>
-
-        {/* <blockquote className='fdFooter__blockquote__tmdbApiCredit'>
-          <p>
-            All media content along with associated information, images, and videos are provided courtesy of{' '}
-            <Link to='https://www.themoviedb.org/' target='_blank'>
-              themoviedb.org.
-            </Link>
-          </p>
-        </blockquote> */}
-      </section>
-      <section className='fdMenu__section'>
-        <h2 className='fdMenu__section--heading'>Genres</h2>
-        <ul className='fdMenu__section__ul'>
-          {genresStringArr.map((genre) => (
-            <li className='fdMenu__section__ul__li'>
-              <button className='fdMenu__section__ul__li--button'>{genre}</button>
+    <section className='fdMenu'>
+      <section className='fdMenu__toolbar'>
+        <ul className='fdMenu__toolbar__cta'>
+          {[
+            ['Search', <MaterialSymbolsSearch />],
+            ['Home', <MaterialSymbolsHomeRounded />],
+            ['Genres', <MaterialSymbolsMenuRounded />],
+          ].map(([key, icon]) => (
+            <li className='fdMenu__toolbar__cta__li'>
+              <button className='fdMenu__toolbar__cta__li--button'>
+                <span className='fdMenu__toolbar__cta__li--button--icon'>{icon}</span>
+                <span className='fdMenu__toolbar__cta__li--button--key'>{key}</span>
+              </button>
             </li>
           ))}
         </ul>
+
+        <blockquote className='fdMenu__toolbar__tmdbCredit'>
+          <p className='fdMenu__toolbar__tmdbCredit--p'>
+            All media content along with associated information, images, and videos are provided courtesy of{' '}
+            <Link to='https://www.themoviedb.org/' target='_blank'>
+              themoviedb.org
+            </Link>
+            .
+          </p>
+        </blockquote>
       </section>
-    </nav>
+
+      <nav className='fdMenu__genres'>
+        <ul className='fdMenu__genres__ul'>
+          {(useTmdbGenres() as string[]).map((genre) => (
+            <li className='fdMenu__genres__ul__li'>
+              <button className='fdMenu__genres__ul__li--button'>{genre}</button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </section>
   );
 };
 
