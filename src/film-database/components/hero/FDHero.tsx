8c9a570b34d0cc7afd75db19e6cc536933ef7df9
Justin Barker <jbarkerpoc@gmail.com>
2024-06-01 19:32:51 -0700

    feat(hero): simple mount anim

--- src/film-database/components/hero/FDHero.tsx
+++ src/film-database/components/hero/FDHero.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import { useTmdbProps } from '../../composables/tmdb-api/hooks/useTmdbProps';
 
 import { Type_Tmdb_Api_Union } from '../../composables/tmdb-api/types/TmdbDataTypes';
@@ -9,13 +11,20 @@ type Type_PropDrill = {
 };
 
 const FDHero = ({ heroData }: Type_PropDrill) => {
+  const backdropRef = useRef<HTMLImageElement>(null);
+  const headerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => headerRef.current?.setAttribute('data-anim', 'fade-in'), [heroData]);
+  useEffect(() => backdropRef.current?.setAttribute('data-anim', 'fade-out'), [heroData]);
+
   if (heroData) {
     const props = useTmdbProps(heroData);
+
     if (props)
       return (
         <section className='fdHero'>
           <article className='fdHero__article'>
-            <header className='fdHero__article__header'>
+            <header className='fdHero__article__header' ref={headerRef}>
               <TheMovieDatabaseLogo />
               <hgroup className='fdHero__article__header__details'>
                 <h2>{props.heading}</h2>
@@ -24,7 +33,7 @@ const FDHero = ({ heroData }: Type_PropDrill) => {
               </hgroup>
             </header>
             <figure>
-              <picture>
+              <picture ref={backdropRef}>
                 <img src={`https://image.tmdb.org/t/p/original/${props.backdrop_path}`} alt={props.heading} />
               </picture>
               <figcaption>{props.heading}</figcaption>
