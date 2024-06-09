9762560025f36dea87b64203820098b43b2a4b17
Justin Barker <jbarkerpoc@gmail.com>
2024-06-09 05:37:52 -0700

    refactor(iframe-controller-playPause): swapped attrs for jsx state

--- src/film-database/components/iframe-controller/IFrameControllerPlayPause.tsx
+++ src/film-database/components/iframe-controller/IFrameControllerPlayPause.tsx
@@ -1,12 +1,11 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 import { YouTubeEvent } from 'react-youtube';
 
 import { MaterialSymbolsPlayArrow, MaterialSymbolsPause } from '../../assets/iFrameSymbols';
 
 const IFrameControllerPlayPause = ({ player }: { player: YouTubeEvent | undefined }) => {
-  const playRef = useRef<HTMLSpanElement>(null);
-  const pauseRef = useRef<HTMLSpanElement>(null);
+  const [symbolComponent, setSymbolComponent] = useState<JSX.Element>(<MaterialSymbolsPause />);
 
   const playPause = async (): Promise<void> => {
     if (!player) return;
@@ -15,24 +14,17 @@ const IFrameControllerPlayPause = ({ player }: { player: YouTubeEvent | undefine
     if (isMuted) {
       player.target.unMute();
       player.target.playVideo();
-      playRef.current?.setAttribute('data-visible', 'true');
-      pauseRef.current?.setAttribute('data-visible', 'false');
+      setSymbolComponent(<MaterialSymbolsPlayArrow />);
     } else {
       player.target.mute();
       player.target.pauseVideo();
-      playRef.current?.setAttribute('data-visible', 'false');
-      pauseRef.current?.setAttribute('data-visible', 'true');
+      setSymbolComponent(<MaterialSymbolsPause />);
     }
   };
 
   return (
     <button className='iFrameController__features__button' onClick={() => playPause()}>
-      <span className='iFrameController__features__button--play' ref={playRef} data-visible='false'>
-        <MaterialSymbolsPlayArrow />
-      </span>
-      <span className='iFrameController__features__button--pause' ref={pauseRef} data-visible='true'>
-        <MaterialSymbolsPause />
-      </span>
+      <span className='iFrameController__features__button--symbol'>{symbolComponent}</span>
     </button>
   );
 };
