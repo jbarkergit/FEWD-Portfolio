03a77eb1707b3981880f9cb42d045076ddd0e459
Justin Barker <jbarkerpoc@gmail.com>
2024-06-09 05:23:10 -0700

    feat(volume): hoisted state to track player volume

---
+++ src/film-database/components/iframe-controller/IFrameControllerVolume.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+import { YouTubeEvent } from 'react-youtube';
+
+import IFrameControllerVolumeIndicator from './IFrameControllerVolumeIndicator';
+import IFrameControllerVolumeSlider from './IFrameControllerVolumeSlider';
+
+const IFrameControllerVolume = ({ player }: { player: YouTubeEvent | undefined }) => {
+  const [playerVolume, setPlayerVolume] = useState<number>(0);
+
+  useEffect(() => {
+    if (player) player.target.setVolume(playerVolume);
+  }, [playerVolume]);
+
+  return (
+    <>
+      <IFrameControllerVolumeIndicator player={player} playerVolume={playerVolume} />
+      <IFrameControllerVolumeSlider player={player} setPlayerVolume={setPlayerVolume} />
+    </>
+  );
+};
+
+export default IFrameControllerVolume;
