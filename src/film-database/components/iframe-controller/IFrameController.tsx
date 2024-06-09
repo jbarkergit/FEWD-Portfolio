d2692949db3974e9ab6ce1f32abf9cd6c250d960
Justin Barker <jbarkerpoc@gmail.com>
2024-06-09 05:24:10 -0700

    chore(iframe-controller): updated volume component import

--- src/film-database/components/iframe-controller/IFrameController.tsx
+++ src/film-database/components/iframe-controller/IFrameController.tsx
@@ -1,9 +1,8 @@
 import { YouTubeEvent } from 'react-youtube';
 
-import IFramePlayPause from './IFrameControllerPlayPause';
-import IFramePlayback from './IFrameControllerPlayback';
-import IFrameVolumeIndicator from './IFrameControllerVolumeIndicator';
-import IFrameVolumeSlider from './IFrameControllerVolumeSlider';
+import IFrameControllerPlayPause from './IFrameControllerPlayPause';
+import IFrameControllerPlayback from './IFrameControllerPlayback';
+import IFrameControllerVolume from './IFrameControllerVolume';
 
 type TYPE_IFRAMECONTROLLER_PROP_REFERENCE = {
   player: YouTubeEvent | undefined;
@@ -13,10 +12,9 @@ const IFrameController = ({ player }: { player: YouTubeEvent | undefined }) => {
   return (
     <div className='iFrameController'>
       <div className='iFrameController__features'>
-        <IFramePlayPause player={player} />
-        <IFrameVolumeIndicator player={player} />
-        <IFrameVolumeSlider player={player} />
-        <IFramePlayback player={player} />
+        <IFrameControllerPlayPause player={player} />
+        <IFrameControllerVolume player={player} />
+        <IFrameControllerPlayback player={player} />
       </div>
 
       <div className='iFrameController__features'></div>
