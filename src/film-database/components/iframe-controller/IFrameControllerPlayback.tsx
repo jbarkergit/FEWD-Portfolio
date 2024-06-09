d9942e11d61792c0640a414a7fae2072534db6b3
Justin Barker <jbarkerpoc@gmail.com>
2024-06-08 19:03:41 -0700

    refactor(controller-playback): added seperator

--- src/film-database/components/iframe-controller/IFrameControllerPlayback.tsx
+++ src/film-database/components/iframe-controller/IFrameControllerPlayback.tsx
@@ -3,8 +3,9 @@ import { Type_ReactYouTube_YouTubeEvent } from '../../features/iframe/FDiFrame';
 const IFrameControllerPlayback = ({ player }: Type_ReactYouTube_YouTubeEvent) => {
   return (
     <div className='iFrameController__features__playback'>
-      <div className='iFrameController__features__playback--current'></div>
-      <div className='iFrameController__features__playback--duration'></div>
+      <div className='iFrameController__features__playback--current'>0:03</div>
+      <div className='iFrameController__features__playback--separator'> / </div>
+      <div className='iFrameController__features__playback--duration'>12:09</div>
     </div>
   );
 };
