5c649e100cb4cd071788cf35161bb14ae785d212
Justin Barker <jbarkerpoc@gmail.com>
2024-06-09 01:14:41 -0700

    feat(volume-slider): slider logic + set volume

--- src/film-database/components/iframe-controller/IFrameControllerVolumeSlider.tsx
+++ src/film-database/components/iframe-controller/IFrameControllerVolumeSlider.tsx
@@ -1,10 +1,32 @@
+import { useRef, useState, PointerEvent } from 'react';
 import { Type_ReactYouTube_YouTubeEvent } from '../../features/iframe/FDiFrame';
 
 const IFrameControllerVolumeSlider = ({ player }: Type_ReactYouTube_YouTubeEvent) => {
+  const sliderRef = useRef<HTMLButtonElement>(null);
+  const handleRef = useRef<HTMLSpanElement>(null);
+
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const moveHandle = (e: PointerEvent<HTMLButtonElement>) => {
+    if (player && sliderRef.current && handleRef.current && isDragging) {
+      const sliderRect: DOMRect = sliderRef.current.getBoundingClientRect();
+      const xOffset: number = e.clientX - sliderRect.left;
+      const position: number = (xOffset / sliderRect.width) * 100;
+      const clampedPosition: number = Math.min(100, Math.max(0, position));
+      handleRef.current.style.left = `${clampedPosition}%`;
+      player.target.setVolume(clampedPosition);
+    }
+  };
+
   return (
-    <button className='iFrameController__features__slider'>
+    <button
+      className='iFrameController__features__slider'
+      ref={sliderRef}
+      onPointerDown={() => setIsDragging(true)}
+      onPointerUp={() => setIsDragging(false)}
+      onPointerMove={(e: PointerEvent<HTMLButtonElement>) => moveHandle(e)}>
       <span className='iFrameController__features__slider--range' />
-      <span className='iFrameController__features__slider--handle' />
+      <span className='iFrameController__features__slider--handle' ref={handleRef} style={{ left: '0%' }} />
     </button>
   );
 };
