1df9dc7b53374920d4e82d5e4d7951265e9700c9
Justin Barker <jbarkerpoc@gmail.com>
2024-06-12 08:27:06 -0700

    refactor(controller): hoisted volume state from volume component, prop drilled playerStates

--- src/film-database/components/iframe-controller/IFrameController.tsx
+++ src/film-database/components/iframe-controller/IFrameController.tsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from 'react';
+
 import { YouTubePlayer } from 'react-youtube';
+import PlayerStates from 'youtube-player/dist/constants/PlayerStates';
 
 import IFrameControllerPlayPause from './IFrameControllerPlayPause';
 import IFrameControllerPlayback from './IFrameControllerPlayback';
-import IFrameControllerVolume from './IFrameControllerVolume';
+import IFrameControllerVolumeIndicator from './IFrameControllerVolumeIndicator';
+import IFrameControllerVolumeSlider from './IFrameControllerVolumeSlider';
 
-const IFrameController = ({ player }: { player: YouTubePlayer | undefined }) => {
+const IFrameController = ({ player, playerStates }: { player: YouTubePlayer | undefined; playerStates: PlayerStates | undefined }) => {
+  const [playerVolume, setPlayerVolume] = useState<number>(0);
+
+  /** Keep player volume aligned with state */
+  const alignPlayerVolume = async () => {
+    player?.setVolume(playerVolume);
+  };
+
+  useEffect(() => {
+    alignPlayerVolume();
+  }, [playerVolume]);
+
+  /** Component */
   return (
     <div className='iFrameController'>
-      <IFrameControllerPlayPause player={player} />
-      <IFrameControllerVolume player={player} />
+      <IFrameControllerPlayPause player={player} playerStates={playerStates} />
+      <IFrameControllerVolumeIndicator player={player} playerVolume={playerVolume} />
+      <IFrameControllerVolumeSlider player={player} playerVolume={playerVolume} setPlayerVolume={setPlayerVolume} />
       <IFrameControllerPlayback player={player} />
     </div>
   );
