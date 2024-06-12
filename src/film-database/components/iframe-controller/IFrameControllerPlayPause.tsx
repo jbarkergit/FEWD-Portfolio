5d6bf8e63c490b18eb86994a6ae93d449d6b9de5
Justin Barker <jbarkerpoc@gmail.com>
2024-06-12 08:28:08 -0700

    fix(iframe playPause): init click not firing play/pause function

--- src/film-database/components/iframe-controller/IFrameControllerPlayPause.tsx
+++ src/film-database/components/iframe-controller/IFrameControllerPlayPause.tsx
@@ -1,24 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { YouTubePlayer } from 'react-youtube';
+import PlayerStates from 'youtube-player/dist/constants/PlayerStates';
 
-import { MaterialSymbolsPlayArrow, MaterialSymbolsPause } from '../../assets/iFrameSymbols';
+import { MaterialSymbolsPlayArrow, MaterialSymbolsPause, SvgSpinnersRingResize } from '../../assets/iFrameSymbols';
 
-const IFrameControllerPlayPause = ({ player }: { player: YouTubePlayer | undefined }) => {
-  const [symbolComponent, setSymbolComponent] = useState<JSX.Element>(<MaterialSymbolsPause />);
+const IFrameControllerPlayPause = ({ player, playerStates }: { player: YouTubePlayer | undefined; playerStates: PlayerStates | undefined }) => {
+  const [symbolComponent, setSymbolComponent] = useState<JSX.Element>(<SvgSpinnersRingResize />);
+  const [isPlaying, setIsPlaying] = useState<boolean>(true);
 
-  const playPause = async (): Promise<void> => {
+  /** react-youtube PlayerStates
+   * Player.on('stateChange') breaks app
+   * (event as YouTubeEvent).target.addEventListener('stateChange') doesn't work
+   * (event as YouTube).data provides nothing
+   * getPlayerStates() type is Promise<enum> and doesn't provide types
+   *
+   * Solution: mount onStateChange to lib component, prop drill
+   */
+
+  const handleSymbols = async (): Promise<void> => {
     if (!player) return;
-    const isMuted: boolean = await player.isMuted();
 
-    if (isMuted) {
-      player.unMute();
-      player.playVideo();
-      setSymbolComponent(<MaterialSymbolsPlayArrow />);
-    } else {
-      player.mute();
+    switch (playerStates) {
+      case 1:
+        setSymbolComponent(<MaterialSymbolsPlayArrow />);
+        setIsPlaying(true);
+        break;
+
+      case 2:
+        setSymbolComponent(<MaterialSymbolsPause />);
+        setIsPlaying(false);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    if (playerStates) handleSymbols();
+  }, [playerStates]);
+
+  const playPause = async () => {
+    if (!player) return;
+
+    if (isPlaying) {
       player.pauseVideo();
-      setSymbolComponent(<MaterialSymbolsPause />);
+    } else {
+      player.playVideo();
     }
   };
 
