a11928d3211f1fabe839423fae68bfa5723af24b
Justin Barker <jbarkerpoc@gmail.com>
2024-06-30 13:47:18 -0700

    refactor(tmdbEndPoints): overhaul

--- src/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
+++ src/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
@@ -4,46 +4,37 @@
  * each object represents an api "category" of endpoints
  */
 
-type Theatrical_Keys = 'now_playing' | 'upcoming';
-type Hot_Keys = 'popular' | 'top_rated';
-type Trending_Keys = 'trending_today' | 'trending_this_week';
-type InformationById_Keys = 'details' | 'releaseDates' | 'watchProviders' | 'credits' | 'similar' | 'recommendations';
-type SearchById_Keys = 'genre' | 'movie' | 'discover' | 'keyword' | 'trailers';
+type Prefab_List_Keys = 'now_playing' | 'upcoming' | 'popular' | 'top_rated' | 'trending_today' | 'trending_this_week';
+type GetIdList_Keys = 'discover';
+type GetIdData_Keys = 'details' | 'credits' | 'keywords' | 'recommendations' | 'releaseDates' | 'reviews' | 'similar' | 'videos' | 'watchProviders';
 
-export type Type_Tmdb_Movie_Keys_Union = Theatrical_Keys | Hot_Keys | Trending_Keys | InformationById_Keys | SearchById_Keys;
+export type Type_Tmdb_Movie_Keys_Union = Prefab_List_Keys | GetIdData_Keys | GetIdList_Keys;
 
 export type Type_Tmdb_Movie_Endpoints = {
-  theatrical: Map<Theatrical_Keys, string>;
-  hot: Map<Hot_Keys, string>;
-  trending: Map<Trending_Keys, string>;
-  informationById: Map<InformationById_Keys, string>;
-  searchById: Map<SearchById_Keys, string>;
+  prefabList: [Prefab_List_Keys, string][];
+  getIdData: [GetIdData_Keys, string][];
+  getIdList: [GetIdList_Keys, string][];
 };
 
 export const tmdbMovieEndpoints: Type_Tmdb_Movie_Endpoints = {
-  theatrical: new Map([
+  prefabList: [
     ['now_playing', 'https://api.themoviedb.org/3/movie/now_playing'],
     ['upcoming', 'https://api.themoviedb.org/3/movie/upcoming'],
-  ]),
-  hot: new Map([
     ['popular', 'https://api.themoviedb.org/3/movie/popular'],
     ['top_rated', 'https://api.themoviedb.org/3/movie/top_rated'],
-  ]),
-  trending: new Map([
     ['trending_today', 'https://api.themoviedb.org/3/trending/movie/day'],
     ['trending_this_week', 'https://api.themoviedb.org/3/trending/movie/week'],
-  ]),
-  informationById: new Map([
+  ],
+  getIdList: [['discover', 'https://api.themoviedb.org/3/discover/movie{genre_ids}']],
+  getIdData: [
     ['details', 'https://api.themoviedb.org/3/movie/{movie_id}'],
-    ['releaseDates', 'https://api.themoviedb.org/3/movie/{movie_id}/release_dates'],
-    ['watchProviders', 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers'],
     ['credits', 'https://api.themoviedb.org/3/movie/{movie_id}/credits'],
-    ['similar', 'https://api.themoviedb.org/3/movie/{movie_id}/similar'],
+    ['keywords', 'https://api.themoviedb.org/3/movie/{movie_id}/keywords'],
     ['recommendations', 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations'],
-  ]),
-  searchById: new Map([
-    ['discover', 'https://api.themoviedb.org/3/discover/movie{genre_ids}'],
-    ['keyword', 'https://api.themoviedb.org/3/movie/{movie_id}/keywords'],
-    ['trailers', 'https://api.themoviedb.org/3/movie/{movie_id}/videos'],
-  ]),
+    ['releaseDates', 'https://api.themoviedb.org/3/movie/{movie_id}/release_dates'],
+    ['reviews', 'https://api.themoviedb.org/3/movie/{movie_id}/reviews'],
+    ['similar', 'https://api.themoviedb.org/3/movie/{movie_id}/similar'],
+    ['videos', 'https://api.themoviedb.org/3/movie/{movie_id}/videos'],
+    ['watchProviders', 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers'],
+  ],
 };
