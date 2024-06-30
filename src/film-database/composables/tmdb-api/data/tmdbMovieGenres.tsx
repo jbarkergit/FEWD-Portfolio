25e494733dd72024e1afcc63e9918e72a215c11d
Justin Barker <jbarkerpoc@gmail.com>
2024-06-30 11:56:13 -0700

    refactor(composable-tmdb): hook seperation

---
+++ src/film-database/composables/tmdb-api/data/tmdbMovieGenres.tsx
@@ -0,0 +1,42 @@
+export type Type_MovieGenre_Keys =
+  | 'action'
+  | 'adventure'
+  | 'animation'
+  | 'comedy'
+  | 'crime'
+  | 'documentary'
+  | 'drama'
+  | 'family'
+  | 'fantasy'
+  | 'history'
+  | 'horror'
+  | 'music'
+  | 'mystery'
+  | 'romance'
+  | 'science_fiction'
+  | 'tv_movie'
+  | 'thriller'
+  | 'war'
+  | 'western';
+
+export const tmdbMovieGenres: Record<string, number> = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  crime: 80,
+  documentary: 99,
+  drama: 18,
+  family: 10751,
+  fantasy: 14,
+  history: 36,
+  horror: 27,
+  music: 10402,
+  mystery: 9648,
+  romance: 10749,
+  science_fiction: 878,
+  tv_movie: 10770,
+  thriller: 53,
+  war: 10752,
+  western: 37,
+};
--- src/film-database/composables/tmdb-api/hooks/useTmdbGenres.tsx
+++ src/film-database/composables/tmdb-api/hooks/useTmdbGenres.tsx
@@ -1,50 +1,13 @@
-export type Type_MovieGenre_Keys =
-  | 'action'
-  | 'adventure'
-  | 'animation'
-  | 'comedy'
-  | 'crime'
-  | 'documentary'
-  | 'drama'
-  | 'family'
-  | 'fantasy'
-  | 'history'
-  | 'horror'
-  | 'music'
-  | 'mystery'
-  | 'romance'
-  | 'science_fiction'
-  | 'tv_movie'
-  | 'thriller'
-  | 'war'
-  | 'western';
+import { Type_MovieGenre_Keys, tmdbMovieGenres } from '../data/tmdbMovieGenres';
 
-const movieGenres: Record<string, number> = {
-  action: 28,
-  adventure: 12,
-  animation: 16,
-  comedy: 35,
-  crime: 80,
-  documentary: 99,
-  drama: 18,
-  family: 10751,
-  fantasy: 14,
-  history: 36,
-  horror: 27,
-  music: 10402,
-  mystery: 9648,
-  romance: 10749,
-  science_fiction: 878,
-  tv_movie: 10770,
-  thriller: 53,
-  war: 10752,
-  western: 37,
+const map = (): string[] => {
+  return [...Object.keys(tmdbMovieGenres)].sort().map((key) => key.replaceAll('_', ' '));
 };
 
-export const useTmdbGenres = (genre?: Type_MovieGenre_Keys): number | string[] => {
-  if (genre) {
-    return movieGenres[genre];
-  } else {
-    return [...Object.keys(movieGenres)].sort().map((key) => key.replaceAll('_', ' '));
-  }
+const genreId = (genre: Type_MovieGenre_Keys): number => {
+  return tmdbMovieGenres[genre];
+};
+
+export const useTmdbGenres = () => {
+  return { map, genreId };
 };
