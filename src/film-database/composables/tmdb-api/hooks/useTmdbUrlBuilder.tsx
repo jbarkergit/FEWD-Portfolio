f687e8d0080825cbc2ee3e501d9ed8c3578bf6d0
Justin Barker <jbarkerpoc@gmail.com>
2024-06-30 21:13:31 -0700

    fix(useTmdbUrlBuilder): types, data flatmap

--- src/film-database/composables/tmdb-api/hooks/useTmdbUrlBuilder.tsx
+++ src/film-database/composables/tmdb-api/hooks/useTmdbUrlBuilder.tsx
@@ -1,4 +1,4 @@
-import { Type_Tmdb_Movie_Keys_Union, tmdbMovieEndpointsFlatmap } from '../data/tmdbEndPoints';
+import { Type_Tmdb_Movie_Keys_Union, tmdbMovieEndpoints } from '../data/tmdbEndPoints';
 
 type Type_Tmdb_EndpointBuilder_Arr_Opt = Partial<{
   language: 'en-US';
@@ -8,49 +8,44 @@ type Type_Tmdb_EndpointBuilder_Arr_Opt = Partial<{
 }>;
 
 export const useTmdbUrlBuilder = (key: Type_Tmdb_Movie_Keys_Union, args?: Type_Tmdb_EndpointBuilder_Arr_Opt[]) => {
-  // Target entry
-  let keyEntry = tmdbMovieEndpointsFlatmap.find((entry) => entry.key === key);
+  const tmdbMovieEndpointsFlatmap: string[][] = Object.entries(tmdbMovieEndpoints).flatMap(([key, endpoint]) =>
+    Object.entries(endpoint).map(([endpointKey, endpointValue]) => [endpointKey, endpointValue])
+  );
 
-  // Short return
-  if (!keyEntry) {
-    console.error('Failure at Tmdb Url Builder: Could not find ${key}.');
-    return undefined;
-  }
+  // Target entry (autofill provides type security; therefore, we're set to use the non-null assertion operator)
+  let keyEntry: string[] = tmdbMovieEndpointsFlatmap.find((entry) => entry[0] === key)!;
 
   // If args, iterate args, mutate endpoint
   if (args) {
-    const buildUrl = () => {
-      // Init mutatable variable with endpoint (by-pass custom types with loose type cast)
-      let endpoint = keyEntry?.endpoint as unknown as string;
+    // Init mutatable variable with endpoint (by-pass custom types with loose type cast)
+    let endpoint = keyEntry[1] as unknown as string;
 
-      args.forEach((arg) => {
-        switch (true) {
-          case !!arg.append_to_response:
-            endpoint = endpoint + `&append_to_response=${arg.append_to_response}`;
-            break;
+    args.forEach((arg) => {
+      switch (true) {
+        case !!arg.append_to_response:
+          endpoint = endpoint + `&append_to_response=${arg.append_to_response}`;
+          break;
 
-          case !!arg.language:
-            endpoint = endpoint + `?language=${arg.language}`;
-            break;
+        case !!arg.language:
+          endpoint = endpoint + `?language=${arg.language}`;
+          break;
 
-          case !!arg.movie_id:
-            endpoint = endpoint.replace('3/movie/', `3/movie/${arg.movie_id}`);
-            break;
+        case !!arg.movie_id:
+          endpoint = endpoint.replace('3/movie/', `3/movie/${arg.movie_id}`);
+          break;
 
-          case !!arg.page:
-            endpoint = endpoint + `&page=${arg.page}`;
-            break;
+        case !!arg.page:
+          endpoint = endpoint + `&page=${arg.page}`;
+          break;
 
-          default:
-            break;
-        }
-      });
+        default:
+          endpoint = keyEntry[1] as unknown as string;
+          break;
+      }
+    });
 
-      return endpoint;
-    };
-
-    return { key: keyEntry.key, endpoint: buildUrl() };
+    return { key: keyEntry[0], endpoint: endpoint };
   } else {
-    return keyEntry;
+    return { key: keyEntry[0], endpoint: keyEntry[1] };
   }
 };
