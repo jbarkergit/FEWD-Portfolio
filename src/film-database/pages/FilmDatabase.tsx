c6f90f1a57b2059f03a02911bd6f3cbe6525ba0b
Justin Barker <jbarkerpoc@gmail.com>
2024-06-30 21:11:29 -0700

    perf(fd): logic cleanup and func isolations

--- src/film-database/pages/FilmDatabase.tsx
+++ src/film-database/pages/FilmDatabase.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-// Api Data
-import { Type_Tmdb_Movie_Keys_Union, tmdbMovieEndpoints } from '../composables/tmdb-api/data/tmdbEndPoints';
 // Api Types
 import { Type_Tmdb_Api_Union } from '../composables/tmdb-api/types/TmdbDataTypes';
+import { Type_MovieGenre_Keys } from '../composables/tmdb-api/data/tmdbMovieGenres';
 // Api Hooks
-import { Type_useFetchTmdbResponse_KeyValuePairArr, useFetchTmdbResponse } from '../composables/tmdb-api/hooks/useFetchTmdbResponse';
+import { useFetchTmdbResponse } from '../composables/tmdb-api/hooks/useFetchTmdbResponse';
 import { useFilmDatabaseWebStorage } from '../composables/web-storage-api/useFilmDatabaseWebStorage';
+import { useTmdbUrlBuilder } from '../composables/tmdb-api/hooks/useTmdbUrlBuilder';
 // Features
 import FDDetails from '../features/details/FDDetails';
 import FDiFrame from '../features/iframe/FDiFrame';
@@ -14,83 +14,86 @@ import FDMenu from '../features/menu/FDMenu';
 import FDMediaCarousel from '../features/media/FDMediaCarousel';
 import FDCarousel from '../components/carousel/FDCarousel';
 
-// const maxCarouselNodes: number = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--my-variable')); revisit
-
 const FilmDatabase = () => {
-  // Global dependencies
-  const maxVisibleCarouselNodes: number = 7;
-  // Fetcher && session storage dependency
+  // Spa navigation state
+  const [route, setRoute] = useState<Type_MovieGenre_Keys | undefined>(undefined);
+  // Cross-origin safety layer
   const useLocationPathname = useLocation().pathname;
 
-  /** */
-  const getMapEntry = (key: Type_Tmdb_Movie_Keys_Union) => {
-    const tmdbEntriesArr = Object.values(tmdbMovieEndpoints).flatMap((map) => [...map.entries()]);
-    // Explicitly cast parameter provides confidence behind the non-null assertion operator
-    const targetEntry = tmdbEntriesArr.find((entry) => entry[0] === key)!;
-    return targetEntry;
-  };
-
-  /** */
-  const fetchDataByPathname = () => {
+  /** Assign fetch data based on state route */
+  const gatherDataByRoute = () => {
     // Init key-endpoint pair arr, store key-endpoint pairs
-    let keyEndpointPairArr: [Type_Tmdb_Movie_Keys_Union, string | Type_Tmdb_Api_Union[]][] = [];
+    let keyEndpointPairArr: ReturnType<typeof useTmdbUrlBuilder>[];
 
     // Assign keyEndpointPairArr
-    switch (useLocationPathname) {
-      case '/film-database':
-        keyEndpointPairArr = [getMapEntry('now_playing'), getMapEntry('upcoming'), getMapEntry('trending_today'), getMapEntry('trending_this_week')];
-        break;
-
-      default:
-        keyEndpointPairArr = [];
-        break;
+    if (route !== undefined) {
+      keyEndpointPairArr = [useTmdbUrlBuilder('top_rated')];
+    } else {
+      keyEndpointPairArr = [
+        useTmdbUrlBuilder('now_playing'),
+        useTmdbUrlBuilder('upcoming'),
+        useTmdbUrlBuilder('trending_today'),
+        useTmdbUrlBuilder('trending_this_week'),
+      ];
     }
 
-    // Prevent unnecessary api calls(keyEndpointPair): Store data in place of endpoint if it exists in sessionStorage
-    keyEndpointPairArr.forEach((keyEndpointPair) => {
-      const getCachedDataByKey: Type_Tmdb_Api_Union[] | undefined = useFilmDatabaseWebStorage(useLocationPathname).getData(keyEndpointPair[0]);
+    fetchDesiredData(keyEndpointPairArr as { key: string; endpoint: string }[]);
+  };
+
+  useEffect(() => gatherDataByRoute(), [route]);
+
+  /** Fetch data */
+  const fetchDesiredData = (keyEndpointPairArr: { key: string; endpoint: string }[]) => {
+    // Prevent unnecessary api calls(keyEndpointPair): Store data in place of endpoint if it exists in sessionStorage via mutation
+    const sessionSafeKeyEndpointPairArr = keyEndpointPairArr.map((entry) => {
+      const getCachedDataByKey: Type_Tmdb_Api_Union[] | undefined = useFilmDatabaseWebStorage(useLocationPathname).getData(entry.key);
       const isKeyCached: boolean = !!getCachedDataByKey;
-      if (isKeyCached && getCachedDataByKey && getCachedDataByKey.length > 0) keyEndpointPair[1] = getCachedDataByKey!;
+
+      if (isKeyCached && getCachedDataByKey && getCachedDataByKey.length > 0) {
+        return { key: entry.key, endpoint: getCachedDataByKey };
+      } else {
+        return entry;
+      }
     });
 
     // Fetch, pass data to pagination
-    useFetchTmdbResponse(keyEndpointPairArr).then((data) => {
+    useFetchTmdbResponse(sessionSafeKeyEndpointPairArr).then((data) => {
       if (data) {
         paginateData(data);
-
         // Prevent unnecessary api calls(session storage safeguard)
-        data.forEach((entry) => useFilmDatabaseWebStorage(useLocationPathname).setData(entry[0], entry[1]));
+        data.forEach((entry) => useFilmDatabaseWebStorage(useLocationPathname).setData(entry.key, entry.endpoint));
       }
     });
   };
 
-  useEffect(() => fetchDataByPathname(), []);
-
-  /** */
+  /** Carousel components, hero data, pagination */
   const [carouselComponents, setCarouselComponents] = useState<JSX.Element[]>([]);
   const [heroData, setHeroData] = useState<Type_Tmdb_Api_Union | null>(null);
 
-  const paginateData = (data: Type_useFetchTmdbResponse_KeyValuePairArr) => {
+  const paginateData = (data: { key: string; endpoint: Type_Tmdb_Api_Union[] }[] | undefined) => {
+    const maxVisibleCarouselNodes: number = 7;
+
     // Init mutatable map in outter scope (helps reduce state updates)
     let dataMap: Map<string, Type_Tmdb_Api_Union[][]> = new Map([]);
 
-    data.forEach(([key, value]) => {
-      // Init entry with empty array or unpaginated data for pages
-      dataMap.set(key, []);
+    if (data)
+      data.forEach(({ key, endpoint }) => {
+        // Init entry with empty array or unpaginated data for pages
+        dataMap.set(key, []);
 
-      // Pagination iteration dependency calculation
-      const maxIteratorIndex: number = Math.ceil((value.length - 1) / maxVisibleCarouselNodes);
+        // Pagination iteration dependency calculation
+        const maxIteratorIndex: number = Math.ceil((endpoint.length - 1) / maxVisibleCarouselNodes);
 
-      // Pagination
-      for (let iteratorCounter = 0; iteratorCounter < maxIteratorIndex; iteratorCounter++) {
-        const startingSliceIndex: number = maxVisibleCarouselNodes * iteratorCounter + iteratorCounter;
-        const endingSliceIndex: number = iteratorCounter === 0 ? maxVisibleCarouselNodes + 1 : maxVisibleCarouselNodes * (iteratorCounter + 1) + 1;
-        const paginatedData: Type_Tmdb_Api_Union[] = value.slice(startingSliceIndex, endingSliceIndex);
-        const iterableMap: Type_Tmdb_Api_Union[][] = dataMap.get(key)!;
+        // Pagination
+        for (let iteratorCounter = 0; iteratorCounter < maxIteratorIndex; iteratorCounter++) {
+          const startingSliceIndex: number = maxVisibleCarouselNodes * iteratorCounter + iteratorCounter;
+          const endingSliceIndex: number = iteratorCounter === 0 ? maxVisibleCarouselNodes + 1 : maxVisibleCarouselNodes * (iteratorCounter + 1) + 1;
+          const paginatedData: Type_Tmdb_Api_Union[] = endpoint.slice(startingSliceIndex, endingSliceIndex);
+          const iterableMap: Type_Tmdb_Api_Union[][] = dataMap.get(key)!;
 
-        iterableMap.push(paginatedData);
-      }
-    });
+          iterableMap.push(paginatedData);
+        }
+      });
 
     // JSX carousel component creation
     const dataEntries: JSX.Element[] = [...dataMap.entries()].map(([key, value]) => {
@@ -99,7 +102,7 @@ const FilmDatabase = () => {
     setCarouselComponents(dataEntries);
 
     // Create hero data
-    setHeroData([...data.values()][0][1][0]);
+    if (data) setHeroData(data![0].endpoint[0]);
   };
 
   /** filmDatabase Breakpoint Attr
