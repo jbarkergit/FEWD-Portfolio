11015d9c593727158a5c23972d5fde85e6cfe92f
Justin Barker <jbarkerpoc@gmail.com>
2024-07-02 18:55:29 -0700

    perf(useTmdbUrlBuilder): introduced api key from fetchTmdbResponse, api has dynamic key positioning

--- src/film-database/composables/tmdb-api/hooks/useTmdbUrlBuilder.tsx
+++ src/film-database/composables/tmdb-api/hooks/useTmdbUrlBuilder.tsx
@@ -18,41 +18,47 @@ export const useTmdbUrlBuilder = (key: Type_Tmdb_Movie_Keys_Union, args?: Type_T
   // Target entry (autofill provides type security; therefore, we're set to use the non-null assertion operator)
   let keyEntry: string[] = tmdbMovieEndpointsFlatmap.find((entry) => entry[0] === key)!;
 
+  // Api Key
+  const apiKey: string = import.meta.env.VITE_TMDB_API_KEY;
+
   // If args, iterate args, mutate endpoint
   if (args) {
     // Init mutatable variable with endpoint (by-pass custom types with loose type cast)
-    let endpoint = keyEntry[1] as unknown as string;
+    let keyValuePair = { key: keyEntry[0], endpoint: keyEntry[1] as unknown as string };
 
     args.forEach((arg) => {
       switch (true) {
         case !!arg.append_to_response:
-          endpoint = endpoint + `&append_to_response=${arg.append_to_response}`;
+          keyValuePair = { key: keyEntry[0], endpoint: keyEntry[1] + `&append_to_response=${arg.append_to_response}` + `/movie?api_key=${apiKey}` };
           break;
 
         case !!arg.language:
-          endpoint = endpoint + `?language=${arg.language}`;
+          keyValuePair = { key: keyEntry[0], endpoint: keyEntry[1] + `?language=${arg.language}` + `/movie?api_key=${apiKey}` };
           break;
 
         case !!arg.movie_id:
-          endpoint = endpoint.replace('3/movie/', `3/movie/${arg.movie_id}`);
+          keyValuePair = { key: keyEntry[0], endpoint: keyEntry[1].replace('3/movie/', `3/movie/${arg.movie_id}`) + `/movie?api_key=${apiKey}` };
           break;
 
         case !!arg.page:
-          endpoint = endpoint + `&page=${arg.page}`;
+          keyValuePair = { key: keyEntry[0], endpoint: keyEntry[1] + `&page=${arg.page}` + `/movie?api_key=${apiKey}` };
           break;
 
         case !!arg.genre:
           const routeId: number = useTmdbGenres().id(arg.genre as Type_MovieGenre_Keys);
-          endpoint = endpoint.replace('{genre_ids}', `&with_genres=${routeId}`);
+          keyValuePair = {
+            key: arg.genre,
+            endpoint: keyEntry[1].replace('/movie', `/movie?api_key=${apiKey}`).replace('{genre_ids}', `&with_genres=${routeId}`),
+          };
           break;
 
         default:
-          endpoint = keyEntry[1] as unknown as string;
+          keyValuePair = { key: keyEntry[0], endpoint: keyEntry[1] + `/movie?api_key=${apiKey}` };
           break;
       }
     });
 
-    return { key: keyEntry[0], endpoint: endpoint };
+    return keyValuePair;
   } else {
     return { key: keyEntry[0], endpoint: keyEntry[1] };
   }
