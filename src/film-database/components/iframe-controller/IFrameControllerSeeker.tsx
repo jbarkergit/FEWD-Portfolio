70e612a6437ccd1604cd432356b279f210ba61cb
Justin Barker <jbarkerpoc@gmail.com>
2024-07-03 16:12:48 -0700

    feature(iframe-controller-seeker)

---
+++ src/film-database/components/iframe-controller/IFrameControllerSeeker.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from 'react';
+import { YouTubePlayer } from 'react-youtube';
+
+const IFrameControllerSeeker = ({ player }: { player: YouTubePlayer | undefined }) => {
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const seekerRef = useRef<HTMLButtonElement>(null);
+  const sliderRef = useRef<HTMLSpanElement>(null);
+
+  const useSeeker = async (travelDistance: number) => {
+    if (!player || !seekerRef.current || !sliderRef.current) return;
+
+    const time = await player.getCurrentTime();
+    const duration = await player.getDuration();
+
+    if (time !== null && duration !== null) {
+      console.log(seekerRef.current);
+      const sliderWidth: number = seekerRef.current.clientWidth;
+      const ratio: number = travelDistance / sliderWidth;
+      const seekTime: number = ratio * duration;
+
+      console.log('Slider Width:', sliderWidth);
+      // console.log('Travel Distance:', travelDistance);
+      // console.log('Ratio:', ratio);
+      // console.log('Seek Time:', seekTime);
+
+      // Ensure seekTime is within valid bounds (0 to duration)
+      const clampedSeekTime: number = Math.max(0, Math.min(seekTime, duration));
+
+      // console.log('Clamped Seek Time:', clampedSeekTime);
+
+      // Perform seek operation
+      player.seekTo(clampedSeekTime, true);
+    }
+  };
+
+  const useSlider = (e: React.PointerEvent<HTMLButtonElement>) => {
+    if (player && seekerRef.current && sliderRef.current && isDragging) {
+      const seekerRect: DOMRect = seekerRef.current.getBoundingClientRect();
+      const xOffset: number = e.clientX - seekerRect.left;
+
+      sliderRef.current.style.width = `${xOffset}px`;
+      useSeeker(xOffset);
+    }
+  };
+
+  return (
+    <button
+      className='iFrameController__seeker'
+      ref={seekerRef}
+      onPointerDown={() => setIsDragging(true)}
+      onPointerUp={() => setIsDragging(false)}
+      onPointerLeave={() => setIsDragging(false)}
+      onPointerMove={(e: React.PointerEvent<HTMLButtonElement>) => useSlider(e)}>
+      <span className='iFrameController__seeker--range' />
+      <span className='iFrameController__seeker--slider' ref={sliderRef} style={{ width: '0px' }} />
+    </button>
+  );
+};
+
+export default IFrameControllerSeeker;
