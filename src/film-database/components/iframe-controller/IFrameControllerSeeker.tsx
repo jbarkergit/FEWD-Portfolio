23df04daf60b925a98f304d1ebd6f0ac86635e5f
Justin Barker <jbarkerpoc@gmail.com>
2024-07-04 00:02:01 -0700

    feature(iFrameControllerSeeker): auto seeking

--- src/film-database/components/iframe-controller/IFrameControllerSeeker.tsx
+++ src/film-database/components/iframe-controller/IFrameControllerSeeker.tsx
@@ -1,16 +1,18 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { YouTubePlayer } from 'react-youtube';
 
 const IFrameControllerSeeker = ({ player }: { player: YouTubePlayer }) => {
   const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [dragPos, setDragPos] = useState<number>(0);
+  useEffect(() => setDragPos(0), [player]);
+  const [time, setTime] = useState<number>(0);
 
   const seekerRef = useRef<HTMLButtonElement>(null);
   const sliderRef = useRef<HTMLSpanElement>(null);
 
-  const useSeeker = async (travelDistance: number) => {
-    if (!player || !seekerRef.current || !sliderRef.current) return;
+  const useSeeker = async (travelDistance: number): Promise<void> => {
+    if (!seekerRef.current || !sliderRef.current) return;
 
-    const time = await player.getCurrentTime();
     const duration = await player.getDuration();
 
     if (time !== null && duration !== null) {
@@ -23,15 +25,36 @@ const IFrameControllerSeeker = ({ player }: { player: YouTubePlayer }) => {
     }
   };
 
-  const useSlider = (e: React.PointerEvent<HTMLButtonElement>) => {
+  const useSlider = (e: React.PointerEvent<HTMLButtonElement>): void => {
     if (player && seekerRef.current && sliderRef.current && isDragging) {
       const seekerRect: DOMRect = seekerRef.current.getBoundingClientRect();
       const xOffset: number = e.clientX - seekerRect.left;
-
-      sliderRef.current.style.width = `${xOffset}px`;
+      setDragPos(xOffset);
       useSeeker(xOffset);
     }
   };
+
+  const watchTime = async (): Promise<void> => {
+    const currentTime: number = await player.getCurrentTime();
+    setTime(currentTime);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => watchTime(), 1000);
+    return () => clearInterval(intervalId);
+  }, [player]);
+
+  const autoSlide = async () => {
+    if (seekerRef.current && !isDragging) {
+      const sliderWidth: number = seekerRef.current.clientWidth;
+      const offset: number = (time / (await player.getDuration())) * sliderWidth;
+      setDragPos(offset);
+    }
+  };
+
+  useEffect(() => {
+    autoSlide();
+  }, [player, time]);
 
   return (
     <button
@@ -46,7 +69,7 @@ const IFrameControllerSeeker = ({ player }: { player: YouTubePlayer }) => {
       onPointerLeave={() => setIsDragging(false)}
       onPointerMove={(e: React.PointerEvent<HTMLButtonElement>) => useSlider(e)}>
       <span className='iFrameController__seeker--range' />
-      <span className='iFrameController__seeker--slider' ref={sliderRef} style={{ width: '0px' }} />
+      <span className='iFrameController__seeker--slider' ref={sliderRef} style={{ width: `${dragPos}px` }} />
     </button>
   );
 };
