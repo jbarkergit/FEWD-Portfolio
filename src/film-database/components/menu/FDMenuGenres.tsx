cca266a795de76fa05a9c302e4dde2e7c14f6623
Justin Barker <jbarkerpoc@gmail.com>
2024-07-12 08:32:39 -0700

    refactor(menuGenres): genres var

--- src/film-database/components/menu/FDMenuGenres.tsx
+++ src/film-database/components/menu/FDMenuGenres.tsx
@@ -1,33 +1,46 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Type_MovieGenre_Keys } from '../../composables/tmdb-api/data/tmdbMovieGenres';
 import { useTmdbGenres } from '../../composables/tmdb-api/hooks/useTmdbGenres';
-import { forwardRef, RefObject } from 'react';
+import { forwardRef, RefObject, useRef } from 'react';
 
 type Type_PropDrill = {
-  setRoute: React.Dispatch<React.SetStateAction<Type_MovieGenre_Keys | undefined>>;
+  isMenuOpen: boolean;
   toggleMenus: (refParam: RefObject<HTMLElement> | undefined) => void;
+  setRoute: React.Dispatch<React.SetStateAction<Type_MovieGenre_Keys | undefined>>;
+  menuSearchReference: HTMLElement | null;
 };
 
-const FDMenuGenres = forwardRef<HTMLElement, Type_PropDrill>(({ setRoute, toggleMenus }, menuGenresRef) => {
+const FDMenuGenres = forwardRef<HTMLElement, Type_PropDrill>(({ isMenuOpen, toggleMenus, setRoute, menuSearchReference }, menuGenresRef) => {
+  /** Add genres to visibleGenres on scroll */
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const genres: string[] = useTmdbGenres()
+    .sortedMap()
+    .map((genre: string) => genre);
+
   return (
     <section className='fdMenu__menu__genres' ref={menuGenresRef} data-menu='closed'>
-      <nav className='fdMenu__menu__genres__nav'>
-        <ul className='fdMenu__menu__genres__nav__ul'>
-          {useTmdbGenres()
-            .sortedMap()
-            .map((genre) => (
+      <div className='fdMenu__menu__genres__container'>
+        <h2 className='fdMenu__menu__genres__container__heading'>Select genre menu</h2>
+        <nav className='fdMenu__menu__genres__container__nav'>
+          <ul className='fdMenu__menu__genres__container__nav__ul' ref={ulRef}>
+            {genres.map((genre: string) => (
               <li
-                className='fdMenu__menu__genres__nav__ul__li'
+                className='fdMenu__menu__genres__container__nav__ul__li'
                 key={uuidv4()}
+                data-activity-tracker='disabled'
                 onClick={() => {
                   toggleMenus(undefined);
                   setRoute(genre.replace(' ', '_') as Type_MovieGenre_Keys);
                 }}>
-                <button className='fdMenu__menu__genres__nav__ul__li--button'>{genre}</button>
+                <button className='fdMenu__menu__genres__container__nav__ul__li--button' aria-label={`Select genre ${genre}`}>
+                  {genre}
+                </button>
               </li>
             ))}
-        </ul>
-      </nav>
+          </ul>
+        </nav>
+      </div>
     </section>
   );
 });
