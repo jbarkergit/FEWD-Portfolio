9ec1774ed86f79609ca0b3f9dd8c968e25bb729c
Justin Barker <jbarkerpoc@gmail.com>
2024-07-12 08:33:54 -0700

    refactor(menu): prop drill isMenuOpen

--- src/film-database/features/menu/FDMenu.tsx
+++ src/film-database/features/menu/FDMenu.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useRef } from 'react';
+import { RefObject, useRef, useState } from 'react';
 
 import { Type_MovieGenre_Keys } from '../../composables/tmdb-api/data/tmdbMovieGenres';
 
@@ -9,6 +9,8 @@ import FDMenuGenres from '../../components/menu/FDMenuGenres';
 import FDMenuSearchBar from '../../components/menu/FDMenuSearchBar';
 
 const FDMenu = ({ setRoute }: { setRoute: React.Dispatch<React.SetStateAction<Type_MovieGenre_Keys | undefined>> }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
   /** Toggle menus */
   const menuRef = useRef<HTMLDivElement>(null);
   const menuSearchRef = useRef<HTMLElement>(null);
@@ -24,23 +26,23 @@ const FDMenu = ({ setRoute }: { setRoute: React.Dispatch<React.SetStateAction<Ty
     if (!menuRef.current) return;
 
     if (refParam !== undefined) {
-      menuRef.current.setAttribute('data-modal', 'open');
+      setIsMenuOpen(true);
       toolbarObjArr.forEach((obj) => {
         if (!refParam || !refParam.current || !obj.ref || !obj.ref.current) return;
         obj.ref.current !== refParam.current ? obj.ref.current.setAttribute('data-menu', 'closed') : obj.ref.current.setAttribute('data-menu', 'open');
       });
     } else {
-      menuRef.current.setAttribute('data-modal', 'closed');
+      setIsMenuOpen(false);
     }
   };
 
   return (
-    <div className='fdMenu' ref={menuRef} data-modal='closed'>
+    <div className='fdMenu' ref={menuRef} data-modal={isMenuOpen ? 'open' : 'closed'}>
       <FDMenuToolbar setRoute={setRoute} toolbarObjArr={toolbarObjArr} toggleMenus={toggleMenus} />
 
       <div className='fdMenu__menu'>
         <FDMenuSearchBar ref={menuSearchRef} />
-        <FDMenuGenres setRoute={setRoute} toggleMenus={toggleMenus} ref={menuGenresRef} />
+        <FDMenuGenres isMenuOpen={isMenuOpen} toggleMenus={toggleMenus} setRoute={setRoute} ref={menuGenresRef} menuSearchReference={menuSearchRef.current} />
       </div>
     </div>
   );
