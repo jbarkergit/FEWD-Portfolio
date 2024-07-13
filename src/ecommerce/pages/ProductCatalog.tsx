6fa5e3dba22fc4d154b4c1549f1a87670f539247
Justin Barker <jbarkerpoc@gmail.com>
2024-07-13 04:15:13 -0700

    refactor(productCatalog): import paths

--- src/ecommerce/pages/ProductCatalog.tsx
+++ src/ecommerce/pages/ProductCatalog.tsx
@@ -7,9 +7,9 @@ import { CartProvider } from '../context/CartContext';
 import Header from '../components/navigation/header/EcoHeader';
 import EFooter from '../components/navigation/footer/eFooter';
 import ProductProvider from '../components/product/product-provider/ProductProvider';
-import ProductFilterConstructor from '../components/features/product-filters/ProductFilterConstructor';
 import { useUniqueData } from '../hooks/useUniqueData';
-import ConditionallyRenderedProductFilters from '../components/features/product-filters/ConditionallyRenderedProductFilters';
+import ConditionallyRenderedProductFilters from '../features/product-filters/ConditionallyRenderedProductFilters';
+import ProductFilterConstructor from '../features/product-filters/ProductFilterConstructor';
 
 const ProductCatalog = (): JSX.Element => {
   const CompanyFilter = (): JSX.Element => ProductFilterConstructor('Filter by Company', useUniqueData().useUniqueCompanies);
