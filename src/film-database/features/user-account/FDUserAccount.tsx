7d3fa406bc713eb75c7bd0de499cfd15368f0a01
Justin Barker <jbarkerpoc@gmail.com>
2024-07-15 12:56:55 -0700

    feat(userAccount): init

--- src/film-database/features/user-account/FDUserAccount.tsx
+++ src/film-database/features/user-account/FDUserAccount.tsx
@@ -1,5 +1,18 @@
-const FDUserAccount = () => {
-  return <main>FDUserAccount</main>;
+type Type_PropDrill = {
+  carouselComponents: JSX.Element[];
+  isMenuOpen: boolean;
+};
+
+const FDUserAccount = ({ carouselComponents, isMenuOpen }: Type_PropDrill) => {
+  // console.log(carouselComponents);
+  return (
+    <main className='fdUserAccount'>
+      <article className='fdUserAccount__article'>
+        <h1 className='fdUserAccount__article--h1'>Film Database</h1>
+      </article>
+      <section className='fdUserAccount__display'>{carouselComponents.slice(0, 7).map((component) => component.props)}</section>
+    </main>
+  );
 };
 
 export default FDUserAccount;
