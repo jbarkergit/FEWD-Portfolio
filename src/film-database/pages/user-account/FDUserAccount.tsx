49c31d96e183ac4127ef4e491207247e8ea1456c
Justin Barker <jbarkerpoc@gmail.com>
2024-08-01 17:17:34 -0500

    refactor(fduseraccount): article text

--- src/film-database/pages/user-account/FDUserAccount.tsx
+++ src/film-database/pages/user-account/FDUserAccount.tsx
@@ -4,13 +4,15 @@ type Type_PropDrill = {
 };
 
 const FDUserAccount = ({ carouselComponents, isMenuOpen }: Type_PropDrill) => {
-  // console.log(carouselComponents);
+  console.log(carouselComponents);
   return (
     <main className='fdUserAccount'>
       <article className='fdUserAccount__article'>
-        <h1 className='fdUserAccount__article--h1'>Film Database</h1>
+        <h1 className='fdUserAccount__article--h1'>Stay in the know about the hottest new movies.</h1>
+        <h2>Create an account to queue release notifications via sms.</h2>
+        <h3>Powered by TMDB</h3>
       </article>
-      <section className='fdUserAccount__display'>{carouselComponents.slice(0, 7).map((component) => component.props)}</section>
+      {/* <section className='fdUserAccount__display'>{carouselComponents.slice(0, 7).map((component) => component.props)}</section> */}
     </main>
   );
 };
