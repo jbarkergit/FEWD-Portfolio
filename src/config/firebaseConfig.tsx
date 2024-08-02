cff22c0af4a5c9bd540171dd5e9f17a5968f5b93
Justin Barker <jbarkerpoc@gmail.com>
2024-08-01 20:01:41 -0500

    refactor(firebaseConfig): exports / renames for clarity

--- src/config/firebaseConfig.tsx
+++ src/config/firebaseConfig.tsx
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -11,4 +11,4 @@ export const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const firebaseAuth = getAuth(app);
