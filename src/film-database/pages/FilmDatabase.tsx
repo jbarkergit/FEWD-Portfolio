9376671d9ab803619433391beac91526133196f8
Justin Barker <jbarkerpoc@gmail.com>
2024-08-01 22:05:31 -0500

    refactor(fd,fdCatalog): moved layout logic to fd

--- src/film-database/pages/FilmDatabase.tsx
+++ src/film-database/pages/FilmDatabase.tsx
@@ -1,5 +1,60 @@
+import { useState, useEffect } from 'react';
+
 const FilmDatabase = () => {
-  return <div className='filmDatabase'></div>;
+  /** filmDatabase Breakpoint Attr
+   * Breakpoints at 25% of second row's poster
+   */
+  const [layoutAttr, setLayoutAttr] = useState<string>('xxl');
+
+  const getDataLayout = (): void => {
+    let layout: string;
+
+    switch (true) {
+      case window.innerWidth >= 1410:
+        layout = 'xxl';
+        break;
+
+      case window.innerWidth < 1410 && window.innerWidth > 1212:
+        layout = 'xl';
+        break;
+
+      case window.innerWidth <= 1212 && window.innerWidth > 1032:
+        layout = 'l';
+        break;
+
+      case window.innerWidth <= 1032 && window.innerWidth > 836:
+        layout = 'm';
+        break;
+
+      case window.innerWidth <= 836 && window.innerWidth > 632:
+        layout = 's';
+        break;
+
+      case window.innerWidth <= 632 && window.innerWidth > 0:
+        layout = 'xs';
+        break;
+
+      default:
+        layout = 'xxl';
+        break;
+    }
+
+    setLayoutAttr((prevLayout) => {
+      if (prevLayout !== layout) {
+        return layout;
+      } else {
+        return prevLayout;
+      }
+    });
+  };
+
+  useEffect(() => {
+    getDataLayout();
+    window.addEventListener('resize', getDataLayout);
+    return () => window.removeEventListener('resize', getDataLayout);
+  }, []);
+
+  return <div className='filmDatabase' data-layout-carousel={layoutAttr}></div>;
 };
 
 export default FilmDatabase;
--- src/film-database/pages/catalog/FDCatalog.tsx
+++ src/film-database/pages/catalog/FDCatalog.tsx
@@ -141,65 +141,12 @@ const FDCatalog = () => {
     if (data) setHeroData(data![0].endpoint[0]);
   };
 
-  /** filmDatabase Breakpoint Attr
-   * Breakpoints at 25% of second row's poster
-   */
-  const [layoutAttr, setLayoutAttr] = useState<string>('xxl');
-
-  const getDataLayout = (): void => {
-    let layout: string;
-
-    switch (true) {
-      case window.innerWidth >= 1410:
-        layout = 'xxl';
-        break;
-
-      case window.innerWidth < 1410 && window.innerWidth > 1212:
-        layout = 'xl';
-        break;
-
-      case window.innerWidth <= 1212 && window.innerWidth > 1032:
-        layout = 'l';
-        break;
-
-      case window.innerWidth <= 1032 && window.innerWidth > 836:
-        layout = 'm';
-        break;
-
-      case window.innerWidth <= 836 && window.innerWidth > 632:
-        layout = 's';
-        break;
-
-      case window.innerWidth <= 632 && window.innerWidth > 0:
-        layout = 'xs';
-        break;
-
-      default:
-        layout = 'xxl';
-        break;
-    }
-
-    setLayoutAttr((prevLayout) => {
-      if (prevLayout !== layout) {
-        return layout;
-      } else {
-        return prevLayout;
-      }
-    });
-  };
-
-  useEffect(() => {
-    getDataLayout();
-    window.addEventListener('resize', getDataLayout);
-    return () => window.removeEventListener('resize', getDataLayout);
-  }, []);
-
   /** Menu */
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   /** Component */
   return (
-    <div className='fdCatalog' data-layout-carousel={layoutAttr}>
+    <div className='fdCatalog'>
       <FDMenu setRoute={setRoute} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <FDDetails heroData={heroData} />
       <FDiFrame heroData={heroData} />
