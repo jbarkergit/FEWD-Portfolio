3dcf46e7f20d63c63c6c9176321b1ad8315d5cca
Justin Barker <jbarkerpoc@gmail.com>
2024-08-04 13:31:11 -0500

    refactor(fdAccountArticle): html

--- src/film-database/features/account/article/FDUserAccountArticle.tsx
+++ src/film-database/features/account/article/FDUserAccountArticle.tsx
@@ -2,13 +2,13 @@ const FDUserAccountArticle = () => {
   return (
     <article className='fdUserAccount__article'>
       <section className='fdUserAccount__article__about'>
-        <hgroup className='fdUserAccount__article__about__hgroup'>
-          <h1 className='fdUserAccount__article__about__hgroup--h1'>Stay in the know about the hottest new movies.</h1>
-          <h2 className='fdUserAccount__article__about__hgroup--h2'>Never miss a new release with sms alerts.</h2>
-        </hgroup>
         <blockquote className='fdUserAccount__article__about--blockquote' cite='https://www.themoviedb.org/?language=en-US'>
           Powered by TMDB
         </blockquote>
+        <hgroup className='fdUserAccount__article__about__hgroup'>
+          <h1 className='fdUserAccount__article__about__hgroup--h1'>Stay in the know with the latest releases</h1>
+          <h2 className='fdUserAccount__article__about__hgroup--h2'>Never miss a new release with sms alerts.</h2>
+        </hgroup>
       </section>
       <section className='fdUserAccount__article__cta'>
         <button className='fdUserAccount__article__cta--accBtn fdUserAccount__article--signin' aria-label='Sign in'>
