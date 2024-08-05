adc370ba31669d76adb52310a849f3ed3c93f208
Justin Barker <jbarkerpoc@gmail.com>
2024-08-04 22:37:48 -0500

    refactor(accArticle): tmdb link

--- src/film-database/features/account/article/FDUserAccountArticle.tsx
+++ src/film-database/features/account/article/FDUserAccountArticle.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const FDUserAccountArticle = () => {
   return (
     <article className='fdUserAccount__article'>
       <section className='fdUserAccount__article__about'>
         <blockquote className='fdUserAccount__article__about--blockquote' cite='https://www.themoviedb.org/?language=en-US'>
-          Powered by TMDB
+          Powered by{' '}
+          <Link
+            to='https://www.themoviedb.org/?language=en-US'
+            className='fdUserAccount__article__about--blockquote--tmdb'
+            aria-label='Visit The Movie Database'
+            target='_blank'>
+            TMDB
+          </Link>
         </blockquote>
         <hgroup className='fdUserAccount__article__about__hgroup'>
           <h1 className='fdUserAccount__article__about__hgroup--h1'>Stay in the know with the latest releases</h1>
