9e4a1cfea1aa8cd75e6036cba6f28735555f7f1b
Justin Barker <jbarkerpoc@gmail.com>
2024-08-10 23:40:32 -0500

    chore(menuSymbols): movie symbol

--- src/film-database/assets/google-material-symbols/menuSymbols.tsx
+++ src/film-database/assets/google-material-symbols/menuSymbols.tsx
@@ -1,11 +1,11 @@
 import { SVGProps } from 'react';
 
-export function MaterialSymbolsSearch(props: SVGProps<SVGSVGElement>) {
+export function IcBaselinePerson2(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
       <path
         fill='currentColor'
-        d='m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14'></path>
+        d='M18.39 14.56C16.71 13.7 14.53 13 12 13s-4.71.7-6.39 1.56A2.97 2.97 0 0 0 4 17.22V20h16v-2.78c0-1.12-.61-2.15-1.61-2.66M9.78 12h4.44c1.21 0 2.14-1.06 1.98-2.26l-.32-2.45C15.57 5.39 13.92 4 12 4S8.43 5.39 8.12 7.29L7.8 9.74c-.16 1.2.77 2.26 1.98 2.26'></path>
     </svg>
   );
 }
@@ -26,12 +26,22 @@ export function MaterialSymbolsAnimatedImagesSharp(props: SVGProps<SVGSVGElement
   );
 }
 
-export function IcBaselinePerson2(props: SVGProps<SVGSVGElement>) {
+export function MaterialSymbolsSearch(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
       <path
         fill='currentColor'
-        d='M18.39 14.56C16.71 13.7 14.53 13 12 13s-4.71.7-6.39 1.56A2.97 2.97 0 0 0 4 17.22V20h16v-2.78c0-1.12-.61-2.15-1.61-2.66M9.78 12h4.44c1.21 0 2.14-1.06 1.98-2.26l-.32-2.45C15.57 5.39 13.92 4 12 4S8.43 5.39 8.12 7.29L7.8 9.74c-.16 1.2.77 2.26 1.98 2.26'></path>
+        d='m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14'></path>
+    </svg>
+  );
+}
+
+export function MaterialSymbolsMovie(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
+      <path
+        fill='currentColor'
+        d='m4 4l2 4h3L7 4h2l2 4h3l-2-4h2l2 4h3l-2-4h3q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20H4q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4'></path>
     </svg>
   );
 }
