171f8fb002c9cc9906a5449c380999441297a575
Justin Barker <jbarkerpoc@gmail.com>
2024-08-12 21:43:38 -0500

    feat(carouselSearch): label logic

--- src/film-database/components/carousel-search/FDCarouselSearch.tsx
+++ src/film-database/components/carousel-search/FDCarouselSearch.tsx
@@ -20,8 +20,6 @@ const FDCarouselSearch = () => {
 
   /** Handle label */
   const labelRef = useRef<HTMLLabelElement>(null);
-  const handleLabel = (): void => labelRef.current?.setAttribute('data-status', searchTerm.length > 0 ? 'disabled' : 'enabled');
-  useEffect(() => handleLabel(), [searchTerm]);
 
   /** Fetch */
   const [searchResults, setSearchResults] = useState<Type_Tmdb_QuerieMovie_Obj[] | undefined>(undefined);
@@ -39,17 +37,18 @@ const FDCarouselSearch = () => {
     });
   };
 
+  /** Invoke fetch with timeout */
   const invokeFetch = (): void => {
     if (searchTerm.length > 0) {
       setIsTyping(false);
       fetchResults();
-    } else {
-      // hide carousel
     }
   };
 
-  /** Invoke fetch with timeout */
   useEffect(() => {
+    if (searchTerm.length === 0) setSearchResults([]);
+    if (searchTerm.length > 0) setIsTyping(true);
+
     if (timerRef.current) window.clearTimeout(timerRef.current);
     timerRef.current = window.setTimeout(() => invokeFetch(), 850) as unknown as NodeJS.Timeout;
 
@@ -62,11 +61,15 @@ const FDCarouselSearch = () => {
   }, [searchTerm]);
 
   return (
-    <section className='fdSearchBar' data-menu='closed'>
-      <section className='fdSearchBar__header' data-status='enabled'>
+    <section className='fdSearchBar'>
+      <section className='fdSearchBar__header'>
         <fieldset className='fdSearchBar__header__fieldset'>
           <legend className='fdSearchBar__header__fieldset__legend'>Find the movies you're interested in</legend>
-          <label className='fdSearchBar__header__fieldset__label' htmlFor='fdSearchBar__fieldset__input' data-status='disabled' ref={labelRef}>
+          <label
+            className='fdSearchBar__header__fieldset__label'
+            htmlFor='fdSearchBar__fieldset__input'
+            data-opacity={searchTerm.length > 0 ? 'hidden' : 'barelyVisible'}
+            ref={labelRef}>
             <MaterialSymbolsSearch />
             Find the movies you're interested in
           </label>
@@ -75,39 +78,48 @@ const FDCarouselSearch = () => {
             type='search'
             pattern='search'
             autoFocus={true}
+            onPointerOver={() => {
+              if (searchTerm.length === 0) labelRef.current?.setAttribute('data-opacity', 'visible');
+            }}
+            onPointerLeave={() => {
+              if (searchTerm.length === 0) labelRef.current?.setAttribute('data-opacity', 'barelyVisible');
+            }}
+            onFocus={() => {
+              if (searchTerm.length === 0) labelRef.current?.setAttribute('data-opacity', 'visible');
+            }}
+            onBlur={() => {
+              if (searchTerm.length === 0) labelRef.current?.setAttribute('data-opacity', 'barelyVisible');
+            }}
             onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e?.target.value.replace(' ', '-').toLowerCase())}
           />
+          {isTyping ? <SvgSpinnersRingResize /> : null}
         </fieldset>
       </section>
 
-      <section className='fdSearchBar__results' data-status='disabled'>
+      <section className='fdSearchBar__results' data-status={searchResults && searchResults.length > 0 ? 'enabled' : 'disabled'}>
         <ul className='fdSearchBar__results__ul'>
-          {isTyping ? (
-            <li className='fdSearchBar__results__ul__li'>
-              <SvgSpinnersRingResize />
-            </li>
-          ) : (
-            searchResults?.map((props) => (
-              <li className='fdSearchBar__results__ul__li' key={uuidv4()}>
-                <article className='fdSearchBar__results__ul__li__article'>
-                  <header className='fdSearchBar__results__ul__li__article__header'>
-                    <h2 className='fdSearchBar__results__ul__li__article__header--h2'>{props?.title}</h2>
-                  </header>
-                  <figure className='fdSearchBar__results__ul__li__article__figure'>
-                    <picture>
-                      <img src={`https://image.tmdb.org/t/p/original/${props?.poster_path}`} alt={`${props?.overview}`} />
-                      <figcaption>{`${props?.title}`}</figcaption>
-                    </picture>
-                  </figure>
-                </article>
-                <div className='fdSearchBar__results__ul__li__overlay'>
-                  <button className='fdSearchBar__results__ul__li__overlay--play' aria-label='Play trailer'>
-                    <MaterialSymbolsPlayArrow />
-                  </button>
-                </div>
-              </li>
-            ))
-          )}
+          {searchResults && !isTyping
+            ? searchResults?.map((props) => (
+                <li className='fdSearchBar__results__ul__li' key={uuidv4()}>
+                  <article className='fdSearchBar__results__ul__li__article'>
+                    <header className='fdSearchBar__results__ul__li__article__header'>
+                      <h2 className='fdSearchBar__results__ul__li__article__header--h2'>{props?.title}</h2>
+                    </header>
+                    <figure className='fdSearchBar__results__ul__li__article__figure'>
+                      <picture>
+                        <img src={`https://image.tmdb.org/t/p/original/${props?.poster_path}`} alt={`${props?.overview}`} />
+                        <figcaption>{`${props?.title}`}</figcaption>
+                      </picture>
+                    </figure>
+                  </article>
+                  <div className='fdSearchBar__results__ul__li__overlay'>
+                    <button className='fdSearchBar__results__ul__li__overlay--play' aria-label='Play trailer'>
+                      <MaterialSymbolsPlayArrow />
+                    </button>
+                  </div>
+                </li>
+              ))
+            : null}
         </ul>
         <nav className='fdSearchBar__results__navigation'>
           <FDCarouselButton caption={'Show Previous'} icon={<MaterialSymbolsChevronLeft />} func={() => {}} funcDelta={-1} />
