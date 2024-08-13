a0eab892c712b900b8e847ad8ea133777dc0411c
Justin Barker <jbarkerpoc@gmail.com>
2024-08-12 23:43:53 -0500

    feat(menu): account sign out

--- src/film-database/features/menu/FDMenu.tsx
+++ src/film-database/features/menu/FDMenu.tsx
@@ -1,5 +1,8 @@
 // Deps
 import { Dispatch, RefObject, SetStateAction, useRef } from 'react';
+// Firebase
+import { signOut } from 'firebase/auth';
+import { firebaseAuth } from '../../../config/firebaseConfig';
 // Composable types
 import { Type_MovieGenre_Keys } from '../../composables/tmdb-api/data/tmdbMovieGenres';
 // Assets
@@ -30,13 +33,13 @@ const FDMenu = ({
   /** Toggle menus */
   const menuAccountRef = useRef<HTMLElement>(null);
   const menuGenresRef = useRef<HTMLElement>(null);
-  const menuQueueRef = useRef<HTMLElement>(null);
+  // const menuQueueRef = useRef<HTMLElement>(null);
 
   const toolbarObjArr = [
     { key: 'Home', icon: <MaterialSymbolsHome />, ref: undefined },
     { key: 'Genres', icon: <MaterialSymbolsAnimatedImagesSharp />, ref: menuGenresRef },
     // { key: 'Queue', icon: <MaterialSymbolsMovie />, ref: menuQueueRef },
-    { key: 'Account', icon: <IcOutlinePowerSettingsNew />, ref: menuAccountRef },
+    // { key: 'Account', icon: <IcOutlinePowerSettingsNew />, ref: menuAccountRef },
   ];
 
   const toggleMenus = (refParam: RefObject<HTMLElement> | undefined) => {
@@ -55,7 +58,25 @@ const FDMenu = ({
 
   return (
     <section className='fdMenu' ref={menuRef} data-modal={isMenuOpen ? 'open' : 'closed'}>
-      <FDMenuToolbar setRoute={setRoute} toolbarObjArr={toolbarObjArr} toggleMenus={toggleMenus} />
+      <section className='fdMenu__toolbar'>
+        <ul className='fdMenu__toolbar__ul'>
+          <FDMenuToolbar setRoute={setRoute} toolbarObjArr={toolbarObjArr} toggleMenus={toggleMenus} />
+          <li className='fdMenu__toolbar__ul__li'>
+            <button
+              className='fdMenu__toolbar__ul__li--button'
+              aria-label={'Sign out'}
+              onClick={() => {
+                setRoute('home');
+                signOut(firebaseAuth);
+              }}>
+              <span className='fdMenu__toolbar__ul__li--button--icon'>
+                <IcOutlinePowerSettingsNew />
+              </span>
+            </button>
+          </li>
+        </ul>
+      </section>
+      <IcOutlinePowerSettingsNew />
       <div className='fdMenu__menu'>
         <FDMenuGenres toggleMenus={toggleMenus} setRoute={setRoute} ref={menuGenresRef} />
         {/* <FDMenuQueue ref={menuQueueRef} /> */}
