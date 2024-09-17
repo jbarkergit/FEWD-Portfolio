5179b118bc31652f198b42ba0427c2571068d1a4
justin barker <jbarkerpoc@gmail.com>
2024-09-17 10:24:25 -0700

    feat(insights): init fd

--- src/portfolio/components/insights/FilmDatabaseInsights.tsx
+++ src/portfolio/components/insights/FilmDatabaseInsights.tsx
@@ -1,31 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const FilmDatabaseInsights = (): JSX.Element => {
   return (
     <>
       <h2>Film Database</h2>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, labore iusto. Aut, pariatur voluptas soluta modi fugiat sed beatae explicabo aliquam magnam
-        expedita, eveniet quaerat, ipsum quas a iusto iure!
+        From the core architecture to the user interface elements, this project represents a self-directed effort to build a movie-database website from scratch,
+        without relying on external templates or guides. It provided a valuable opportunity to deepen my JavaScript skills and enhance my critical thinking
+        abilities.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, corporis voluptate nihil facilis eligendi nulla autem, cumque voluptatum ratione obcaecati omnis
-        hic vel consectetur sequi voluptatem magni? Ipsa quae exercitationem, temporibus excepturi suscipit, ullam in minus, necessitatibus architecto et dicta.
+        One of the major challenges was working with the <Link to='https://www.themoviedb.org/'>TheMovieDatabase API</Link> and the{' '}
+        <Link to='https://www.npmjs.com/package/react-youtube'>React-YouTube</Link> library. TMDB does not offer WEBP images or consistently formatted URLs, which
+        required additional logic and custom TypeGuards to handle image fetching. I addressed this through performance checks and iterative refactoring, developing
+        custom hooks to process data efficiently. While there is always room for optimization, the initial page loads now perform satisfactorily.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime illo similique quae ipsa sint tempora corrupti odit iure optio officia unde minima
-        repellendus aspernatur explicabo alias, officiis eveniet laborum impedit? Perferendis tempora fugit numquam obcaecati voluptatibus, voluptate earum veniam
-        praesentium facilis, exercitationem iure aperiam omnis aspernatur quia corrupti eligendi quisquam nemo quam? Voluptate odio necessitatibus voluptatum quasi
-        soluta quia minima voluptas corrupti deserunt labore fuga quis eligendi, quod amet eos doloremque perspiciatis, velit eveniet. Voluptatibus, odit sapiente
-        esse quibusdam sed fugit nihil suscipit non perferendis similique laudantium aliquid labore tenetur doloremque accusantium totam porro, ab laboriosam illo!
-        Voluptas porro facilis voluptatum hic nostrum officia illo impedit labore assumenda, ratione, molestiae rem voluptatibus neque minima quasi? Saepe cumque
-        sequi ab praesentium numquam inventore facilis architecto nisi ipsam commodi ut totam dolorum dolorem minima voluptatum excepturi, consequuntur laborum rem
-        eos, minus illo? Aliquam impedit dolores autem quis voluptates saepe culpa perspiciatis eum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quas nesciunt consequatur voluptatum aut! Quam commodi hic aspernatur, ratione at sunt
-        asperiores cumque! Non voluptate expedita officiis facilis, iusto id praesentium? Quod necessitatibus blanditiis cum saepe vitae voluptas cumque sequi id
-        reprehenderit voluptatum recusandae corrupti iusto dignissimos, fugiat nisi quam excepturi dolor eum nemo soluta. Harum impedit repudiandae repellat alias
-        id. Voluptatum, sequi. Rem libero ut molestias beatae minima dolores, veniam nemo dolor at alias enim eveniet voluptate dolorem? Totam autem aspernatur
-        quaerat impedit provident corporis perferendis nulla ad animi!
+        Customizing the YouTube iFrame API presented another significant challenge. To achieve a uniform and cohesive UI similar to Netflix, I needed to create a
+        custom solution for the video player interface. Despite the limitations of the available documentation and issues with third-party React packages, I
+        succeeded in creating a functional and visually appealing custom controller overlay. This involved overcoming difficulties with the iFrame’s native behavior
+        and integrating the player seamlessly into the design.
       </p>
     </>
   );
