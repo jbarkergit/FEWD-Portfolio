16709ee0f520f036fe1a18e5cec6d28561af5180
justin barker <jbarkerpoc@gmail.com>
2024-09-20 16:46:06 -0700

    fix(fd): useTransition

--- src/film-database/pages/FilmDatabase.tsx
+++ src/film-database/pages/FilmDatabase.tsx
@@ -86,11 +86,32 @@ const FilmDatabase = () => {
     return () => window.removeEventListener('resize', getDataLayout);
   }, []);
 
-  return (
-    <div className='filmDatabase' data-layout-carousel={layoutAttr} ref={rootRef}>
-      {authorizedUser.user && authorizedUser.verified ? <FDCatalog /> : <FDUserAccount rootRef={rootRef} />}
-    </div>
-  );
+  /** Component */
+  if (!authorizedUser.user) {
+    return (
+      <div className='fdLoading'>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+          <path
+            fill='none'
+            stroke='currentColor'
+            strokeDasharray='15'
+            strokeDashoffset='15'
+            strokeLinecap='round'
+            strokeWidth='2'
+            d='M12 3C16.9706 3 21 7.02944 21 12'>
+            <animate fill='freeze' attributeName='stroke-dashoffset' dur='0.3s' values='15;0'></animate>
+            <animateTransform attributeName='transform' dur='1.5s' repeatCount='indefinite' type='rotate' values='0 12 12;360 12 12'></animateTransform>
+          </path>
+        </svg>
+      </div>
+    );
+  } else {
+    return (
+      <div className='filmDatabase' data-layout-carousel={layoutAttr} ref={rootRef}>
+        {authorizedUser.user && authorizedUser.verified ? <FDCatalog /> : <FDUserAccount rootRef={rootRef} />}
+      </div>
+    );
+  }
 };
 
 export default FilmDatabase;
