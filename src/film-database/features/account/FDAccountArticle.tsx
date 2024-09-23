b86c2e4a3ebe127d766b38b3a212739717d697f0
justin barker <jbarkerpoc@gmail.com>
2024-09-22 20:49:05 -0700

    feat(accArticle): mount animator

--- src/film-database/features/account/FDAccountArticle.tsx
+++ src/film-database/features/account/FDAccountArticle.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, LegacyRef, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { signInWithPopup } from 'firebase/auth';
 import { firebaseAuth, googleProvider } from '../../../config/firebaseConfig';
@@ -16,11 +16,26 @@ const FDAccountArticle = forwardRef<HTMLElement, Type_PropDrill>(({ toggleCompon
     }
   };
 
+  type Type_TextRefs_Mixed_Arr = HTMLElement | HTMLQuoteElement | HTMLHeadingElement;
+
+  const textRefs = useRef<Type_TextRefs_Mixed_Arr[]>([]);
+
+  const textRef: LegacyRef<Type_TextRefs_Mixed_Arr> = (ref): void => {
+    if (ref && !textRefs.current?.includes(ref)) textRefs.current?.push(ref);
+  };
+
+  const animator = (): void => {
+    if (!textRefs.current) return;
+    textRefs.current.forEach((ref) => ref.setAttribute('data-anim', 'mount'));
+  };
+
+  useEffect(() => animator(), []);
+
   return (
     <main className='fdAccountArticle' ref={articleRefReceiver} data-activity='disabled'>
       <article className='fdAccountArticle__article'>
         <section className='fdAccountArticle__article__about'>
-          <blockquote className='fdAccountArticle__article__about--blockquote' cite='https://www.themoviedb.org/?language=en-US'>
+          <blockquote className='fdAccountArticle__article__about--blockquote' cite='https://www.themoviedb.org/?language=en-US' ref={textRef}>
             Powered by{' '}
             <Link
               to='https://www.themoviedb.org/?language=en-US'
@@ -31,11 +46,15 @@ const FDAccountArticle = forwardRef<HTMLElement, Type_PropDrill>(({ toggleCompon
             </Link>
           </blockquote>
           <hgroup className='fdAccountArticle__article__about__hgroup'>
-            <h1 className='fdAccountArticle__article__about__hgroup--h1'>Stay in the know with the latest releases</h1>
-            <h2 className='fdAccountArticle__article__about__hgroup--h2'>Free now, free forever</h2>
+            <h1 className='fdAccountArticle__article__about__hgroup--h1' ref={textRef}>
+              Stay in the know with the latest releases
+            </h1>
+            <h2 className='fdAccountArticle__article__about__hgroup--h2' ref={textRef}>
+              Free now, free forever
+            </h2>
           </hgroup>
         </section>
-        <section className='fdAccountArticle__article__cta'>
+        <section className='fdAccountArticle__article__cta' ref={textRef}>
           <h2>Account options</h2>
           <button
             className='fdAccountArticle__article__cta--accBtn fdAccountArticle__article--signin'
