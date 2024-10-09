3433bbd837516e5b4ef9c58fc024327a099aefa3
justin barker <jbarkerpoc@gmail.com>
2024-10-09 15:36:55 -0700

    perf(useTmdbGenres): memoized sortedMap

--- src/film-database/composables/tmdb-api/hooks/useTmdbGenres.tsx
+++ src/film-database/composables/tmdb-api/hooks/useTmdbGenres.tsx
@@ -1,13 +1,19 @@
+import { useMemo } from 'react';
 import { Type_MovieGenre_Keys, tmdbMovieGenres } from '../data/tmdbMovieGenres';
 
-const sortedMap = (): string[] => {
-  return [...Object.keys(tmdbMovieGenres)].sort().map((key) => key.replaceAll('_', ' '));
-};
+export const useTmdbGenres = (): {
+  sortedMap: string[];
+  id: (genre: Type_MovieGenre_Keys) => number;
+} => {
+  const sortedMap = useMemo((): string[] => {
+    return Object.keys(tmdbMovieGenres)
+      .sort()
+      .map((key) => key.replaceAll('_', ' '));
+  }, [tmdbMovieGenres]);
 
-const id = (genre: Type_MovieGenre_Keys): number => {
-  return tmdbMovieGenres[genre];
-};
+  const id = (genre: Type_MovieGenre_Keys): number => {
+    return tmdbMovieGenres[genre];
+  };
 
-export const useTmdbGenres = () => {
   return { sortedMap, id };
 };
