35db2eb76be8e81e86a7233fc3e59c30e399bf18
justin barker <jbarkerpoc@gmail.com>
2024-10-11 02:40:18 -0700

    refactor(accSignIn): rewrote html

--- src/film-database/features/account/FDAccountSignIn.tsx
+++ src/film-database/features/account/FDAccountSignIn.tsx
@@ -3,13 +3,11 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { firebaseAuth } from '../../../app/config/firebaseConfig';
 
 type Type_PropDrill = {
-  toggleComponent: (modal: 'article' | 'registry' | 'signin') => void;
+  toggleComponent: (modal: 'guide' | 'registry' | 'signin') => void;
 };
 
-const FDAccountSignIn = forwardRef<HTMLDivElement, Type_PropDrill>(({ toggleComponent }, signInRefReceiver) => {
+const FDAccountSignIn = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggleComponent }, signInRefReceiver) => {
   /** Value setter */
-  const ulRef = useRef<HTMLUListElement>(null);
-
   const [values, setValues] = useState({
     emailAddress: { value: '', valid: false },
     password: { value: '', valid: false },
@@ -50,83 +48,68 @@ const FDAccountSignIn = forwardRef<HTMLDivElement, Type_PropDrill>(({ toggleComp
   useEffect(() => LIArr.forEach((li, index) => li.setAttribute('data-status', index === counter ? 'enabled' : 'disabled')), [counter]);
 
   return (
-    <div className='fdAccountSignIn' ref={signInRefReceiver} data-activity='disabled'>
-      <fieldset className='fdAccountSignIn__container'>
-        <section className='fdAccountSignIn__container__col'>
-          <div className='fdAccountSignIn__container__col__logo'>Film Database</div>
-          <legend className='fdAccountSignIn__container__legend'>
-            <h2 className='fdAccountSignIn__container__legend--h2'>Sign in</h2>
-          </legend>
-          <div className='fdAccountSignIn__container__col__hint'>to continue</div>
-        </section>
+    <ul className='fdAccount__container__form__fieldset__ul' ref={signInRefReceiver} data-activity='disabled'>
+      <li className='fdAccount__container__form__fieldset__ul__li' ref={liRef} data-status='enabled'>
+        <label id='emailAddress' htmlFor='fdUserAccountSignInEmailAddress'>
+          Email address
+        </label>
+        <input
+          form='fdRegistery'
+          id='fdUserAccountSignInEmailAddress'
+          name='emailAddress'
+          type='email'
+          inputMode='email'
+          // name, @, domain
+          minLength={3}
+          // RFC 2045
+          maxLength={76}
+          size={12}
+          required={true}
+          aria-required='true'
+          aria-invalid={values.emailAddress.valid}
+          autoCapitalize='off'
+          placeholder='johndoe@gmail.com'
+          onClick={() => focus()}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
+        />
+        <button className='fdAccount__container__form__fieldset__ul__li__forgot' aria-label='Forgot email'>
+          I forgot my email.
+        </button>
+      </li>
 
-        <ul className='fdAccountSignIn__container__ul' ref={ulRef}>
-          <li className='fdAccountSignIn__container__ul__emailAddress' ref={liRef} data-status='enabled'>
-            <div className='fdAccountSignIn__container__field__ul__emailAddress__container'>
-              <label id='emailAddress' htmlFor='fdUserAccountSignInEmailAddress'>
-                Email address
-              </label>
-              <input
-                form='fdRegistery'
-                id='fdUserAccountSignInEmailAddress'
-                name='emailAddress'
-                type='email'
-                inputMode='email'
-                // name, @, domain
-                minLength={3}
-                // RFC 2045
-                maxLength={76}
-                size={12}
-                required={true}
-                aria-required='true'
-                aria-invalid={values.emailAddress.valid}
-                autoCapitalize='off'
-                onClick={() => focus()}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
-              />
-            </div>
-            <button className='fdAccountSignIn__container__field__ul__emailAddress__forgot' aria-label='Forgot email'>
-              Forgot email?
-            </button>
-          </li>
-
-          <li className='fdAccountSignIn__container__ul__password' ref={liRef} data-status='disabled'>
-            <div className='fdAccountSignIn__container__field__ul__password__container'>
-              <label id='password' htmlFor='fdUserAccountSignInPassword'>
-                Password
-              </label>
-              <input
-                form='fdRegistery'
-                id='fdUserAccountSignInPassword'
-                name='password'
-                type='password'
-                inputMode='text'
-                // RFC 5310, NIST Special Publication 800-63B
-                minLength={8}
-                // RFC XOS
-                maxLength={32}
-                size={12}
-                required={true}
-                aria-required='true'
-                aria-invalid={values.password.valid}
-                autoCapitalize='off'
-                onClick={() => focus()}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
-              />
-            </div>
-            <button className='fdAccountSignIn__container__field__ul__password__forgot' aria-label='Forgot password'>
-              Forgot password?
-            </button>
-          </li>
-
-          <li className='fdAccountSignIn__container__ul__container__submitRegistrationForm' ref={liRef} data-status='disabled'>
-            <button id='fdUserAccountSubmitForm' aria-label='Sign in' onClick={() => authorizeSignIn()}>
-              Sign in
-            </button>
-          </li>
-        </ul>
-      </fieldset>
-    </div>
+      <li className='fdAccount__container__form__fieldset__ul__li' ref={liRef} data-status='disabled'>
+        <label id='password' htmlFor='fdUserAccountSignInPassword'>
+          Password
+        </label>
+        <input
+          form='fdRegistery'
+          id='fdUserAccountSignInPassword'
+          name='password'
+          type='password'
+          inputMode='text'
+          // RFC 5310, NIST Special Publication 800-63B
+          minLength={8}
+          // RFC XOS
+          maxLength={32}
+          size={12}
+          required={true}
+          aria-required='true'
+          aria-invalid={values.password.valid}
+          autoCapitalize='off'
+          placeholder='••••••••'
+          onClick={() => focus()}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
+        />
+        <button className='fdAccount__container__form__fieldset__ul__li__forgot' aria-label='Forgot password'>
+          I forgot my password.
+        </button>
+      </li>
+      <li className='fdAccount__container__form__fieldset__ul__cta'>
+        <button id='fdUserAccountSubmitForm' aria-label='Sign in' onClick={() => authorizeSignIn()}>
+          Sign in
+        </button>
+      </li>
+    </ul>
   );
 });
 
