159491af166146e897600558ecd74a6ee1cd95bc
justin barker <jbarkerpoc@gmail.com>
2024-10-14 00:14:54 -0700

    feat(accRegistry): signup/signin w google

--- src/film-database/features/account/FDAccountRegistry.tsx
+++ src/film-database/features/account/FDAccountRegistry.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, FormEvent, forwardRef, useEffect, useRef, useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { firebaseAuth } from '../../../app/config/firebaseConfig';
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { firebaseAuth, googleProvider } from '../../../app/config/firebaseConfig';
 
 type Type_PropDrill = {
   toggleComponent: (modal: 'registry' | 'signin') => void;
@@ -94,46 +94,6 @@ const FDAccountRegistry = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggle
     const matchingElement = labelElements.find((child: HTMLLabelElement) => child.id === key);
     if (!matchingEntry || !matchingElement) return;
     matchingElement.setAttribute('data-entry', matchingEntry[0] && matchingEntry[0].length > 0 ? 'true' : 'false');
-  };
-
-  const [formSections, setFormSections] = useState<HTMLUListElement[]>([]);
-
-  const formSectionReceiver = (ref: HTMLUListElement) => {
-    if (ref && !formSections.includes(ref))
-      setFormSections((prevArr: HTMLUListElement[]) => {
-        return [...prevArr, ref];
-      });
-  };
-
-  const [activeFormSection, setActiveFormSection] = useState<number>(0);
-
-  useEffect(() => {
-    formSections.forEach((section: HTMLUListElement, index: number) => {
-      section.setAttribute('data-visibility', index === activeFormSection ? 'visible' : 'hidden');
-    });
-  }, [activeFormSection]);
-
-  const requestNextSection = (index: number): void => {
-    let isValid: boolean = false;
-
-    switch (index) {
-      case 0:
-        isValid = values.firstName.valid && values.lastName.valid;
-        break;
-
-      case 2:
-        isValid = values.emailAddress.valid;
-        break;
-
-      case 3:
-        isValid = values.password.valid && values.passwordConfirmation.valid;
-        break;
-
-      default:
-        break;
-    }
-
-    if (isValid) setActiveFormSection((prev) => prev + 1);
   };
 
   const getOptionalYears = (): number[] => {
@@ -311,11 +271,25 @@ const FDAccountRegistry = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggle
         </li>
       </div>
       <li className='fdAccount__container__form__fieldset__ul__cta'>
-        {activeFormSection < 3 ? (
-          <button id='fdUserAccountSubmitForm' aria-label='Submit registration form' onClick={(e: React.PointerEvent<HTMLButtonElement>) => submitForm(e)}>
-            Complete sign up
-          </button>
-        ) : null}
+        <button id='fdUserAccountSubmitForm' aria-label='Submit registration form' onClick={(e: React.PointerEvent<HTMLButtonElement>) => submitForm(e)}>
+          Complete sign up
+        </button>
+        <button aria-label='Sign in with Google' onClick={() => signInWithPopup(firebaseAuth, googleProvider)}>
+          <svg xmlns='http://www.w3.org/2000/svg' width='0.98em' height='1em' viewBox='0 0 256 262'>
+            <path
+              fill='#4285F4'
+              d='M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027'></path>
+            <path
+              fill='#34A853'
+              d='M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1'></path>
+            <path
+              fill='#FBBC05'
+              d='M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z'></path>
+            <path
+              fill='#EB4335'
+              d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'></path>
+          </svg>
+        </button>
       </li>
     </ul>
   );
