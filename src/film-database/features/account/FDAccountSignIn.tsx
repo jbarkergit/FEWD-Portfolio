dac286a926537cb14dc17a4c5b034c226586c1a6
justin barker <jbarkerpoc@gmail.com>
2024-10-13 23:35:09 -0700

    feat(accSignIn): signin w google

--- src/film-database/features/account/FDAccountSignIn.tsx
+++ src/film-database/features/account/FDAccountSignIn.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, forwardRef, useEffect, useRef, useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { firebaseAuth } from '../../../app/config/firebaseConfig';
+import { ChangeEvent, forwardRef, useState } from 'react';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { firebaseAuth, googleProvider } from '../../../app/config/firebaseConfig';
 
 type Type_PropDrill = {
   toggleComponent: (modal: 'guide' | 'registry' | 'signin') => void;
@@ -36,20 +36,9 @@ const FDAccountSignIn = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggleCo
     }
   };
 
-  /** Form sections */
-  const [counter, setCounter] = useState<number>(0);
-
-  const LIArr: HTMLLIElement[] = [];
-
-  const liRef = (reference: HTMLLIElement): void => {
-    if (reference && !LIArr.includes(reference)) LIArr.push(reference);
-  };
-
-  useEffect(() => LIArr.forEach((li, index) => li.setAttribute('data-status', index === counter ? 'enabled' : 'disabled')), [counter]);
-
   return (
-    <ul className='fdAccount__container__form__fieldset__ul' ref={signInRefReceiver} data-activity='disabled'>
-      <li className='fdAccount__container__form__fieldset__ul__li' ref={liRef} data-status='enabled'>
+    <ul className='fdAccount__container__form__fieldset__ul' ref={signInRefReceiver}>
+      <li className='fdAccount__container__form__fieldset__ul__li' data-status='enabled'>
         <label id='emailAddress' htmlFor='fdUserAccountSignInEmailAddress'>
           Email address
         </label>
@@ -77,7 +66,7 @@ const FDAccountSignIn = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggleCo
         </button>
       </li>
 
-      <li className='fdAccount__container__form__fieldset__ul__li' ref={liRef} data-status='disabled'>
+      <li className='fdAccount__container__form__fieldset__ul__li'>
         <label id='password' htmlFor='fdUserAccountSignInPassword'>
           Password
         </label>
@@ -107,6 +96,23 @@ const FDAccountSignIn = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggleCo
       <li className='fdAccount__container__form__fieldset__ul__cta'>
         <button id='fdUserAccountSubmitForm' aria-label='Sign in' onClick={() => authorizeSignIn()}>
           Sign in
+        </button>
+        <button onClick={() => signInWithPopup(firebaseAuth, googleProvider)}>
+          <svg xmlns='http://www.w3.org/2000/svg' width='0.98em' height='1em' viewBox='0 0 256 262'>
+            <path
+              fill='#4285F4'
+              d='M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027'></path>
+            <path
+              fill='#34A853'
+              d='M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1'></path>
+            <path
+              fill='#FBBC05'
+              d='M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z'></path>
+            <path
+              fill='#EB4335'
+              d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'></path>
+          </svg>{' '}
+          Continue with Google
         </button>
       </li>
     </ul>
