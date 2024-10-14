257afc4528fa03d2b7d0e3e66245b6b6480d1bfc
justin barker <jbarkerpoc@gmail.com>
2024-10-13 23:54:32 -0700

    feat(accSignIn): registry btn

--- src/film-database/features/account/FDAccountSignIn.tsx
+++ src/film-database/features/account/FDAccountSignIn.tsx
@@ -111,8 +111,12 @@ const FDAccountSignIn = forwardRef<HTMLUListElement, Type_PropDrill>(({ toggleCo
             <path
               fill='#EB4335'
               d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'></path>
-          </svg>{' '}
-          Continue with Google
+          </svg>
+        </button>
+      </li>
+      <li className='fdAccount__container__form__fieldset__ul__signup'>
+        <button aria-label='Create a new account' onClick={() => toggleComponent('registry')}>
+          Not a member? Create a new account.
         </button>
       </li>
     </ul>
