e7f3fcf0dbf9258d7daf7a44c7e3c98c29b8b452
justin barker <jbarkerpoc@gmail.com>
2024-10-13 17:01:11 -0700

    chore(projectData): portfolio insight revision

--- src/portfolio/data/projectData.tsx
+++ src/portfolio/data/projectData.tsx
@@ -103,29 +103,30 @@ export const projectData = [
       <>
         <h2>An Application Hub with Seamless Transitions</h2>
         <p>
-          The primary goal of this portfolio was to craft an intuitive environment tailored for skill assessment. However, achieving such simplicity presented
-          challenges, especially in dealing with routing intricacies and optimizing Web Core Vital metrics. What initially appeared as an unassuming design unfolded
+          The primary goal of this portfolio was to create an intuitive environment tailored for skill assessment. However, achieving this simplicity posed
+          challenges, particularly in navigating routing complexities and optimizing Web Core Vital metrics. What initially seemed like an unassuming design evolved
           into a time-consuming process.
         </p>
         <p>
-          The first strategy involved prioritizing each project's landing page and lazy loading corresponding pages to optimize the largest contentful paint and time
-          to interact. While this standard approach seemed promising, it prompted a reconsideration of the routing strategy, especially considering the application's
-          future scalability. Utilizing the Network developer tool became crucial for visualizing file sizes, transfer times, and the import queue.
+          My first strategy focused on prioritizing each project's landing page and implementing lazy loading for corresponding pages to optimize the largest
+          contentful paint and time to interact. While this standard approach appeared promising, it prompted a reconsideration of the routing strategy, especially
+          in light of the application's future scalability. Utilizing the Network developer tool became essential for visualizing file sizes, transfer times, and the
+          import queue.
         </p>
         <p>
-          To create a module loading queue, I dynamically generated routes and spread them in state. Challenges arose, particularly with the 404 protocol error
-          handler loading before the requested page. Attempting to address this led to a somewhat hacky solution involving dismissal of the handler, mounting an
-          Observer to the Network, and prop drilling data to the Suspense loader just to prompt the user that the requested page couldn't be found.
+          To create a module loading queue, I dynamically generated routes and spread them in the state. However, challenges emerged, particularly with the 404 error
+          handler loading before the requested page. My attempts to resolve this led to a somewhat hacky solution: dismissing the handler, mounting an Observer on
+          the Network, and prop drilling data to the Suspense loader to inform the user that the requested page couldn't be found.
         </p>
         <p>
-          Navigating into the realm of custom Router development raised questions about the need for such intricacies. After careful consideration, I realized I
-          hadn't exhausted all solutions. Manually routing paths and dynamically loading only the necessary modules would solve the protocol handler issues without
-          adding complexity. This approach would also offer flexibility as the application scaled.
+          Diving into custom Router development raised questions about the necessity of such complexities. After careful consideration, I realized I hadn’t fully
+          explored all available solutions. Manually routing paths and dynamically loading only the necessary modules would address the protocol handler issues
+          without adding unnecessary complexity. This approach would also provide flexibility as the application scales.
         </p>
         <p>
-          Following this evaluation and implementation, I streamlined logic and minimalized variable memory caching to reduce Web Core Vital metrics. Accepting a
-          minuscule performance impact on initial load, the trade-off would be a synchronous background queue and seamless page transitions. While I do intend on
-          revisiting a more custom solution, the current solution is satisfactory.
+          Following this evaluation and implementation, I streamlined the logic and minimized variable memory caching to enhance Web Core Vital metrics. Accepting a
+          slight performance impact on the initial load, the trade-off resulted in a synchronous background queue and seamless page transitions. While I plan to
+          revisit a more custom solution in the future, the current implementation is satisfactory.
         </p>
       </>
     ),
