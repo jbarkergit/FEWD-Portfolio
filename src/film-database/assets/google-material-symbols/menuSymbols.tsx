41743e29d00960407be38dd17f1664568aa8c27b
justin barker <jbarkerpoc@gmail.com>
2024-10-15 01:25:10 -0700

    feat(menuSymbols): information icon

--- src/film-database/assets/google-material-symbols/menuSymbols.tsx
+++ src/film-database/assets/google-material-symbols/menuSymbols.tsx
@@ -45,3 +45,11 @@ export function IcOutlinePowerSettingsNew(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function MaterialSymbolsChatInfoSharp(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
+      <path fill='currentColor' d='M12 7q.425 0 .713-.288T13 6t-.288-.712T12 5t-.712.288T11 6t.288.713T12 7m-1 8h2V9h-2zm-9 7V2h20v16H6z'></path>
+    </svg>
+  );
+}
