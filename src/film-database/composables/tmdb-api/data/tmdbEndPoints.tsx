00f5e727b04c8311f3cea83983f978a6a2224ec7
justin barker <jbarkerpoc@gmail.com>
2024-10-15 10:14:28 -0700

    refactor(tmdbEndPoints): removed unused keys, renamed all props for use-clarity

--- src/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
+++ src/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
@@ -2,32 +2,24 @@
  * https://developer.themoviedb.org/reference/
  */
 
-type Type_GetPrefabList_Keys = 'now_playing' | 'upcoming' | 'popular' | 'top_rated' | 'trending_today' | 'trending_this_week';
+/** Category Keys */
+type Type_Prefabs_Keys = 'now_playing' | 'upcoming' | 'popular' | 'top_rated' | 'trending_today' | 'trending_this_week';
 
-type Type_GetIdData_Keys =
-  | 'discover'
-  | 'details'
-  | 'credits'
-  | 'keywords'
-  | 'recommendations'
-  | 'releaseDates'
-  | 'reviews'
-  | 'similar'
-  | 'videos'
-  | 'watchProviders';
+type Type_MovieId_Keys = 'details' | 'credits' | 'recommendations' | 'reviews' | 'watchProviders';
 
-type Type_GetSearchResult_Keys = 'querieMovie';
+type Type_Miscellaneous_Keys = 'discover' | 'genreQuerie';
 
-export type Type_Tmdb_Movie_Keys_Union = Type_GetPrefabList_Keys | Type_GetIdData_Keys | Type_GetSearchResult_Keys;
+export type Type_Tmdb_Movie_Keys_Union = Type_Prefabs_Keys | Type_MovieId_Keys | Type_Miscellaneous_Keys;
 
 type Type_Tmdb_Movie_Endpoint_Obj = {
-  getPrefabList: Record<Type_GetPrefabList_Keys, string>;
-  getIdData: Record<Type_GetIdData_Keys, string>;
-  getSearchResult: Record<Type_GetSearchResult_Keys, string>;
+  prefabs: Record<Type_Prefabs_Keys, string>;
+  movieId: Record<Type_MovieId_Keys, string>;
+  miscellaneous: Record<Type_Miscellaneous_Keys, string>;
 };
 
-export const tmdbMovieEndpoints: Type_Tmdb_Movie_Endpoint_Obj = {
-  getPrefabList: {
+/** Endpoint Collection */
+export const tmdbEndpoints: Type_Tmdb_Movie_Endpoint_Obj = {
+  prefabs: {
     now_playing: 'https://api.themoviedb.org/3/movie/now_playing',
     upcoming: 'https://api.themoviedb.org/3/movie/upcoming',
     popular: 'https://api.themoviedb.org/3/movie/popular',
@@ -35,19 +27,15 @@ export const tmdbMovieEndpoints: Type_Tmdb_Movie_Endpoint_Obj = {
     trending_today: 'https://api.themoviedb.org/3/trending/movie/day',
     trending_this_week: 'https://api.themoviedb.org/3/trending/movie/week',
   },
-  getIdData: {
-    discover: 'https://api.themoviedb.org/3/discover/movie{genre_ids}',
+  movieId: {
     details: 'https://api.themoviedb.org/3/movie/{movie_id}',
     credits: 'https://api.themoviedb.org/3/movie/{movie_id}/credits',
-    keywords: 'https://api.themoviedb.org/3/movie/{movie_id}/keywords',
     recommendations: 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations',
-    releaseDates: 'https://api.themoviedb.org/3/movie/{movie_id}/release_dates',
     reviews: 'https://api.themoviedb.org/3/movie/{movie_id}/reviews',
-    similar: 'https://api.themoviedb.org/3/movie/{movie_id}/similar',
-    videos: 'https://api.themoviedb.org/3/movie/{movie_id}/videos',
     watchProviders: 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers',
   },
-  getSearchResult: {
-    querieMovie: 'https://api.themoviedb.org/3/search/movie',
+  miscellaneous: {
+    discover: 'https://api.themoviedb.org/3/discover/movie{genre_ids}',
+    genreQuerie: 'https://api.themoviedb.org/3/search/movie',
   },
 };
