33a02e2bed34ec8bd68cc1de4d711e86f2baec71
justin barker <jbarkerpoc@gmail.com>
2024-10-16 07:20:35 -0700

    refactor(useFilmDatabaseWebStorage): rewrite, removed 'cross-origin safety protocol' layer

--- src/film-database/composables/web-storage-api/useFilmDatabaseWebStorage.tsx
+++ src/film-database/composables/web-storage-api/useFilmDatabaseWebStorage.tsx
@@ -1,10 +1,12 @@
 import { Type_Tmdb_Api_Union } from '../tmdb-api/types/TmdbDataTypes';
 
-const fireCrossOriginError = (): void => console.error('Cross-Origin access to sessionStorage is not permitted.');
 const fireRetrievalError = (): void => console.error('Could not retrieve data from Web Storage.');
 
-const getParsedData = (key: string): Type_Tmdb_Api_Union[] | undefined => {
-  const cachedDataString: string | null = sessionStorage.getItem(key);
+const setData = (cacheKey: string, data: Type_Tmdb_Api_Union[]): void => sessionStorage.setItem(cacheKey, JSON.stringify(data));
+
+const getData = (cacheKey: string): Type_Tmdb_Api_Union[] | undefined => {
+  const cachedDataString: string | null = sessionStorage.getItem(cacheKey);
+
   if (!cachedDataString) {
     fireRetrievalError();
     return undefined;
@@ -12,41 +14,14 @@ const getParsedData = (key: string): Type_Tmdb_Api_Union[] | undefined => {
 
   const parsedData: Type_Tmdb_Api_Union[] | undefined = JSON.parse(cachedDataString);
 
-  if (parsedData) {
-    return parsedData;
-  } else {
+  if (!parsedData) {
     console.error('Web Storage Key is missing or undefined.');
     return undefined;
   }
+
+  return parsedData;
 };
 
-export const useFilmDatabaseWebStorage = (userLocation: string) => {
-  /** Simple Cross-Origin restriction safety protocol */
-  const isSameOrigin: boolean = '/film-database' === userLocation;
-
-  const setData = (cacheKey: string, data: Type_Tmdb_Api_Union[]): void => {
-    if (isSameOrigin) {
-      sessionStorage.setItem(cacheKey, JSON.stringify(data));
-    } else {
-      fireCrossOriginError();
-    }
-  };
-
-  const getData = (cacheKey: string): Type_Tmdb_Api_Union[] | undefined => {
-    if (!isSameOrigin) {
-      fireCrossOriginError();
-      return undefined;
-    }
-
-    const parsedData: Type_Tmdb_Api_Union[] | undefined = getParsedData(cacheKey);
-
-    if (parsedData) {
-      return parsedData;
-    } else {
-      fireRetrievalError();
-      return undefined;
-    }
-  };
-
+export const useFilmDatabaseWebStorage = () => {
   return { setData, getData };
 };
