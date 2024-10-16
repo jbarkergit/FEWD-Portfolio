879a44a0117de852d9f9e28049a3225cb8168ba7
justin barker <jbarkerpoc@gmail.com>
2024-10-16 03:03:06 -0700

    refactor(useFetchTmdbResponse): proper types, removed junk logic

--- src/film-database/composables/tmdb-api/hooks/useFetchTmdbResponse.tsx
+++ src/film-database/composables/tmdb-api/hooks/useFetchTmdbResponse.tsx
@@ -1,37 +1,29 @@
-import { Type_Tmdb_Api_Union } from '../types/TmdbDataTypes';
 import { fetchTmdbResponse } from '../util/fetchTmdbResponse';
 
 /** Invoke fetcher util, filter fulfilled && rejected entries, return fulfilled data as a new arr */
-export const useFetchTmdbResponse = async (keyEndpointPairArr: { key: string; endpoint: string | Type_Tmdb_Api_Union[] }[]) => {
-  try {
-    const fetchEntries = await Promise.allSettled(
-      keyEndpointPairArr.map(async (keyEndpointPair) => {
-        // Prevent unnecessary api calls(keyEndpointPair): Fetch data if keyEndpointPair is of type 'string', not Type_Tmdb_Api_Union[]
-        if (typeof keyEndpointPair.endpoint === 'string') {
-          const fetchResponse = await fetchTmdbResponse(keyEndpointPair as { key: string; endpoint: string });
-          return { key: keyEndpointPair.key, endpoint: fetchResponse ? fetchResponse.results : keyEndpointPair.endpoint };
-        } else {
-          return keyEndpointPair;
-        }
-      })
-    );
+export const useFetchTmdbResponse = async (keyValuePairs: { [key: string]: string } | { [key: string]: string }[]) => {
+  if (Array.isArray(keyValuePairs)) {
+    try {
+      const responses = await Promise.allSettled(
+        keyValuePairs.map(async (keyValuePair) => {
+          const fetch = await fetchTmdbResponse(keyValuePair);
+          return fetch;
+        })
+      );
 
-    /** NOTE
-     * While handling !response.ok in fetchTmdbResponse is sufficient,
-     * filtering entries based on their status (fulfilled/rejected) adds an extra layer of safety to Promise.allSettled().
-     */
-    const fulfilledEntries = fetchEntries.filter((entry) => entry.status === 'fulfilled');
-    const rejectedEntries = fetchEntries.filter((entry) => entry.status === 'rejected');
+      // Filtering entries based on their status (fulfilled/rejected) adds an extra layer of safety to Promise.allSettled().
+      const fulfilledEntries = responses.filter((entry) => entry.status === 'fulfilled');
+      const rejectedEntries = responses.filter((entry) => entry.status === 'rejected');
 
-    if (fulfilledEntries.length === 0) throw new Error('No entries were viable.');
-    if (rejectedEntries.length > 0) throw new Error('Some entries were rejected.');
+      if (fulfilledEntries.length === 0) throw new Error('No entries were viable.');
+      if (rejectedEntries.length > 0) throw new Error('Some entries were rejected.');
 
-    //@ts-ignore (narrowing issues, not going to memory cache/type cast two variables to avoid it)
-    const values = fulfilledEntries.map((entry) => entry.value);
-    return values as { key: string; endpoint: Type_Tmdb_Api_Union[] }[];
-
-    /** Catch errors */
-  } catch (error) {
-    console.error('Failure at entries processing:', error);
+      const values = fulfilledEntries.map((entry) => entry.value);
+      return values;
+    } catch (error) {
+      console.error('Failure at entries processing:', error);
+    }
+  } else {
+    return fetchTmdbResponse(keyValuePairs);
   }
 };
