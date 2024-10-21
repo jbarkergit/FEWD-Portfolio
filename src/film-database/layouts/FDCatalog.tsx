d9c5aa2bfe28540e122e0c23b1a2c52ce8c2d0d8
justin barker <jbarkerpoc@gmail.com>
2024-10-21 09:28:44 -0700

    refactor(catalog): logic rewrite post fetcher rebuild

--- src/film-database/layouts/FDCatalog.tsx
+++ src/film-database/layouts/FDCatalog.tsx
@@ -1,9 +1,9 @@
 // Deps
-import { useState } from 'react';
-// Tmdb Api Data
+import { useEffect, useState } from 'react';
+// Composables
 import { Type_MovieGenre_Keys } from '../composables/tmdb-api/data/tmdbGenres';
-import { Type_Tmdb_Api_Union } from '../composables/tmdb-api/types/TmdbDataTypes';
-// Components
+import { Type_Tmdb_Discover_Response, Type_Tmdb_Prefabs_Response, Type_Tmdb_Response_Union, useTmdbFetcher } from '../composables/tmdb-api/hooks/useTmdbFetcher';
+// Features
 import FDDetails from '../features/details/FDDetails';
 import FDiFrame from '../features/iframe/FDiFrame';
 import FDMenu from '../features/menu/FDMenu';
@@ -14,16 +14,36 @@ const FDCatalog = () => {
   const [route, setRoute] = useState<'home' | Type_MovieGenre_Keys>('home');
   // Menu
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  // Data
-  const [heroData, setHeroData] = useState<Type_Tmdb_Api_Union | null>(null);
+  // Details, trailer data
+  const [heroData, setHeroData] = useState<Type_Tmdb_Response_Union | undefined>(undefined);
+  // Carousel data
+  const [carouselData, setCarouselData] = useState<Type_Tmdb_Prefabs_Response[] | Type_Tmdb_Discover_Response | undefined>(undefined);
+
+  /** Fetch data when user requests a route */
+  const fetchDataByRoute = async (): Promise<void> => {
+    const homeData = (await useTmdbFetcher([
+      { key: 'now_playing' },
+      { key: 'upcoming' },
+      { key: 'trending_today' },
+      { key: 'trending_this_week' },
+    ])) as Type_Tmdb_Prefabs_Response[];
+
+    const routeData = (await useTmdbFetcher({ key: 'discover', args: { genre: 'comedy' as Type_MovieGenre_Keys } })) as Type_Tmdb_Discover_Response;
+
+    setCarouselData(route === 'home' ? homeData : routeData);
+  };
+
+  useEffect(() => {
+    fetchDataByRoute();
+  }, [route]);
 
   /** Component */
   return (
     <div className='fdCatalog'>
-      <FDMenu setRoute={setRoute} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} heroData={heroData} />
-      <FDDetails heroData={heroData} />
-      <FDiFrame heroData={heroData} />
-      <FDMedia route={route} isMenuOpen={isMenuOpen} setHeroData={setHeroData} />
+      {/* <FDMenu setRoute={setRoute} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} heroData={heroData} /> */}
+      {/* <FDDetails heroData={heroData} /> */}
+      {/* <FDiFrame heroData={heroData} /> */}
+      {/* <FDMedia isMenuOpen={isMenuOpen} setHeroData={setHeroData} carouselData={carouselData} /> */}
     </div>
   );
 };
