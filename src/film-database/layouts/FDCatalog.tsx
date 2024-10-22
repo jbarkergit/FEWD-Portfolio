0b2bb1d2c1a9d22ce79ee3a5a4f20fc8230d05cc
justin barker <jbarkerpoc@gmail.com>
2024-10-22 05:48:18 -0700

    refactor(fdCatalog): reduced rerenders by moving state to appropriate file fdMedia

--- src/film-database/layouts/FDCatalog.tsx
+++ src/film-database/layouts/FDCatalog.tsx
@@ -16,30 +16,6 @@ const FDCatalog = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   // Details, trailer data
   const [heroData, setHeroData] = useState<Namespace_Tmdb.Response_Union | undefined>(undefined);
-  // Carousel data
-  const [carouselData, setCarouselData] = useState<Namespace_Tmdb.Prefabs_Obj[] | Namespace_Tmdb.Discover_Obj | undefined>(undefined);
-
-  /** Fetch data when user requests a route */
-  type Prefabs_Obj_isUndefined = Namespace_Tmdb.Prefabs_Obj | undefined;
-
-  const fetchDataByRoute = async (): Promise<void> => {
-    const homeData = (await useTmdbFetcher([{ key: 'now_playing' }, { key: 'upcoming' }, { key: 'trending_today' }, { key: 'trending_this_week' }])) as
-      | Namespace_Tmdb.Prefabs_Obj[]
-      | Prefabs_Obj_isUndefined[];
-
-    const routeData = (await useTmdbFetcher({ key: 'discover', args: { genre: 'comedy' as Type_MovieGenre_Keys } })) as Namespace_Tmdb.Discover_Obj | undefined;
-
-    if (route === 'home') {
-      const filteredHomeData = homeData.filter((obj) => obj !== undefined) as Namespace_Tmdb.Prefabs_Obj[];
-      setCarouselData(filteredHomeData);
-    } else {
-      setCarouselData(routeData);
-    }
-  };
-
-  useEffect(() => {
-    fetchDataByRoute();
-  }, [route]);
 
   /** Component */
   return (
@@ -47,7 +23,7 @@ const FDCatalog = () => {
       {/* <FDMenu setRoute={setRoute} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} heroData={heroData} /> */}
       {/* <FDDetails heroData={heroData} /> */}
       {/* <FDiFrame heroData={heroData} /> */}
-      <FDMedia isMenuOpen={isMenuOpen} setHeroData={setHeroData} carouselData={carouselData} />
+      <FDMedia route={route} isMenuOpen={isMenuOpen} setHeroData={setHeroData} />
     </div>
   );
 };
