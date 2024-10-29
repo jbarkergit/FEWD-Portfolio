4a65a8d3841a8824900420478ec45927a289c55d
justin barker <jbarkerpoc@gmail.com>
2024-10-29 09:45:30 -0700

    feat(carousel): moved carousels to /components

---
+++ src/film-database/components/FDCarousels.tsx
@@ -0,0 +1,121 @@
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Namespace_Tmdb } from '../composables/tmdb-api/hooks/useTmdbFetcher';
+import { MaterialSymbolsChevronLeft, MaterialSymbolsChevronRight } from '../assets/google-material-symbols/carouselSymbols';
+import { MaterialSymbolsPlayArrow } from '../assets/google-material-symbols/iFrameSymbols';
+
+type Variant_Provider = {
+  type: 'movies' | 'people';
+  mapIndex: number;
+  heading: string;
+  data: Namespace_Tmdb.BaseMedia_Provider[][];
+  setHeroData: Dispatch<SetStateAction<Namespace_Tmdb.BaseMedia_Provider | undefined>>;
+};
+
+type Variant = {
+  variant: Variant_Provider;
+};
+
+const FDCarousels = ({ variant }: Variant) => {
+  const { type, mapIndex, heading, data, setHeroData } = variant;
+  const [carouselIndex, setCarouselIndex] = useState<number>(0);
+  const [articles, setArticles] = useState<Namespace_Tmdb.BaseMedia_Provider[][]>([data[0]]);
+
+  /** Track carousel navigation index */
+  const updateCarouselIndex = (delta: number): void => setCarouselIndex(Math.max(0, Math.min(carouselIndex + delta, data.length - 1)));
+
+  /** Render paginated sets on navigation */
+  const renderPaginatedDataSet = (): void => {
+    const isIndexInArticles: boolean = articles.some((_, index) => index === carouselIndex);
+
+    if (!isIndexInArticles)
+      setArticles((prevState) => {
+        return [...prevState, data[carouselIndex]];
+      });
+  };
+
+  useEffect(() => renderPaginatedDataSet(), [carouselIndex]);
+
+  /** Pagination on pointer drag */
+  const carouselRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (window.innerWidth >= 1410) return;
+
+    if (!carouselRef.current || !carouselRef.current.children) return;
+    const lastCarouselNode = carouselRef.current.children[carouselRef.current.children.length - 1];
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[entries.length - 1].isIntersecting) {
+          setCarouselIndex(Math.max(0, Math.min(carouselIndex + 1, data.length - 1)));
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    observer.observe(lastCarouselNode);
+    return () => observer.unobserve(lastCarouselNode);
+  }, []);
+
+  /** Horizontal Navigation */
+  const navigate = (): void => {
+    if (!carouselRef.current || !carouselRef.current.children) return;
+    // Target indexes and elements
+    const targetIndex: number = carouselIndex * 7;
+    const targetElement = carouselRef.current.children[targetIndex] as HTMLLIElement;
+    const articlesFlatMap: Namespace_Tmdb.BaseMedia_Provider[] = articles.flatMap((innerArray) => innerArray);
+    const lastElement = carouselRef.current.children[articlesFlatMap.findLastIndex((obj) => obj)] as HTMLLIElement;
+    // Element boundaries
+    let targetElementPosition: number;
+    !targetElement ? (targetElementPosition = lastElement.offsetLeft) : (targetElementPosition = targetElement.offsetLeft);
+    // Positions
+    const carouselPosition: number = carouselRef.current.offsetLeft;
+    const scrollPosition: number = targetElementPosition - carouselPosition;
+    // Scroll
+    carouselRef.current.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+  };
+
+  useEffect(() => navigate(), [articles, carouselIndex]);
+
+  /** JSX */
+  return (
+    <section className='fdCarousel' data-anim={mapIndex === 0 ? 'active' : 'disabled'}>
+      <h2 className='fdCarousel__header'>{heading.replaceAll('_', ' ')}</h2>
+      <div className='fdCarousel__wrapper'>
+        <ul className='fdCarousel__wrapper__ul' ref={carouselRef}>
+          {articles.flat().map((article) => {
+            return (
+              <li className='fdCarousel__wrapper__ul__li' key={uuidv4()} onClick={() => setHeroData(article)}>
+                <picture className='fdCarousel__wrapper__ul__li__picture'>
+                  <img
+                    className='fdCarousel__wrapper__ul__li__picture--img'
+                    src={`https://image.tmdb.org/t/p/w780/${article?.poster_path}`}
+                    alt={`${article?.title}`}
+                    fetchPriority={mapIndex === 0 ? 'high' : 'low'}
+                  />
+                </picture>
+                <div className='fdCarousel__wrapper__ul__li__overlay'>
+                  <button className='fdCarousel__wrapper__ul__li__overlay--play' aria-label='Play trailer'>
+                    <MaterialSymbolsPlayArrow />
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <nav className='fdCarousel__wrapper__navigation'>
+          <button className='fdCarousel__wrapper__navigation__button' aria-label={'Show Previous'} onClick={() => updateCarouselIndex(-1)}>
+            <MaterialSymbolsChevronLeft />
+          </button>
+          <button className='fdCarousel__wrapper__navigation__button' aria-label={'Show More'} onClick={() => updateCarouselIndex(1)}>
+            <MaterialSymbolsChevronRight />
+          </button>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default FDCarousels;
+0;
