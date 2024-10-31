7c1d82ee0bacf0670b464602c5d7ddb3e9463130
justin barker <jbarkerpoc@gmail.com>
2024-10-31 14:07:09 -0700

    refactor(userAccount): moved state to appropriate file

--- src/film-database/pages/FDUserAccount.tsx
+++ src/film-database/pages/FDUserAccount.tsx
@@ -1,9 +1,7 @@
 // Deps
-import { RefObject, useState } from 'react';
+import { RefObject } from 'react';
 // Provider
 import { LayoutProvider } from '../context/LayoutProvider';
-// Composable Types
-import { Namespace_Tmdb } from '../composables/tmdb-api/hooks/useTmdbFetcher';
 // Features
 import FDAccountBackground from '../features/account/background/FDAccountBackground';
 import FDAccount from '../features/account/modal/FDAccount';
@@ -13,11 +11,9 @@ type Type_PropDrill = {
 };
 
 const FDUserAccount = ({ rootRef }: Type_PropDrill) => {
-  const [responseSets, setResponseSets] = useState<Namespace_Tmdb.Response_Union[][]>([]);
-
   return (
     <LayoutProvider>
-      <FDAccountBackground responseSets={responseSets} setResponseSets={setResponseSets} />
+      <FDAccountBackground />
       <FDAccount rootRef={rootRef} />
     </LayoutProvider>
   );
