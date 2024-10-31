f3aedbab9d05a00e3f634337ec2f82a43b53589d
justin barker <jbarkerpoc@gmail.com>
2024-10-31 10:35:54 -0700

    feat(context/catalog): moved global state to context

---
+++ src/film-database/context/CatalogContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, FC, ReactNode, useContext, useState } from 'react';
+import { Type_MovieGenre_Keys } from '../composables/tmdb-api/data/tmdbGenres';
+import { Namespace_Tmdb } from '../composables/tmdb-api/hooks/useTmdbFetcher';
+
+type Type_CatalogContext_State_Obj = {
+  route: 'home' | Type_MovieGenre_Keys;
+  setRoute: React.Dispatch<React.SetStateAction<'home' | Type_MovieGenre_Keys>>;
+  isMenuOpen: boolean;
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  heroData: Namespace_Tmdb.BaseMedia_Provider | undefined;
+  setHeroData: React.Dispatch<React.SetStateAction<Namespace_Tmdb.BaseMedia_Provider | undefined>>;
+};
+
+const CatalogContext = createContext<Type_CatalogContext_State_Obj | undefined>(undefined);
+
+export const CatalogProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  // Routing system, lends to menu navigation
+  const [route, setRoute] = useState<'home' | Type_MovieGenre_Keys>('home');
+  // Menu toggle boolean
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  // Details, trailer data
+  const [heroData, setHeroData] = useState<Namespace_Tmdb.BaseMedia_Provider | undefined>(undefined);
+  // Provider
+  return <CatalogContext.Provider value={{ route, setRoute, isMenuOpen, setIsMenuOpen, heroData, setHeroData }}>{children}</CatalogContext.Provider>;
+};
+
+export const useCatalogProvider = () => {
+  const context = useContext(CatalogContext);
+  if (context === undefined) throw new Error('CatalogProvider failure: Not within provider scope. Ensure your component is wrapped in the CatalogProvider.');
+  return context;
+};
