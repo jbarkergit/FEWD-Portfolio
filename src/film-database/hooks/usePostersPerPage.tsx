b63567a908e2a947211c6fae368db56669a2e2b3
justin barker <jbarkerpoc@gmail.com>
2024-10-31 08:03:18 -0700

    fix(usePostersPerPage): initialized state

--- src/film-database/hooks/usePostersPerPage.tsx
+++ src/film-database/hooks/usePostersPerPage.tsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from 'react';
 
-export const usePostersPerPage = (): number | undefined => {
-  const root = document.body.querySelector('.filmDatabase') as HTMLElement;
-  if (!root) return;
-
-  const [itemsPerPage, setItemsPerPage] = useState<number | undefined>(undefined);
-
-  const getItemsPerPage = (): void => {
-    const styles: CSSStyleDeclaration = getComputedStyle(root);
-    const value: number = parseInt(styles.getPropertyValue('--fd-carousel-items-per-page').trim());
-    setItemsPerPage(value);
-  };
+export const usePostersPerPage = (): number => {
+  const [itemsPerPage, setItemsPerPage] = useState<number>(7);
 
   useEffect(() => {
+    const root = document.body.querySelector('.filmDatabase') as HTMLElement;
+
+    const getItemsPerPage = (): void => {
+      const styles: CSSStyleDeclaration = getComputedStyle(root);
+      const value: number = parseInt(styles.getPropertyValue('--fd-carousel-items-per-page'));
+      setItemsPerPage(value);
+    };
+
     // Init fetch
     getItemsPerPage();
-    // Detect changes to data-attribute
-    const observer: MutationObserver = new MutationObserver(() => getItemsPerPage());
+
+    // Observe changes to data-attribute
+    const observer = new MutationObserver(() => getItemsPerPage());
     observer.observe(root, { attributes: true });
+
     // Cleanup
     return () => observer.disconnect();
-  }, [root]);
+  }, []);
 
   return itemsPerPage;
 };
-
-export default usePostersPerPage;
