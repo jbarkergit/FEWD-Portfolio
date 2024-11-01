6cdf3b71a102ff63ea2fca1ff58cc6187d384fae
justin barker <jbarkerpoc@gmail.com>
2024-11-01 11:21:31 -0700

    refactor(details): semantics tweak

--- src/film-database/features/catalog/hero/details/FDDetails.tsx
+++ src/film-database/features/catalog/hero/details/FDDetails.tsx
@@ -20,14 +20,20 @@ const FDDetails = () => {
         <header className='fdDetails__article__header'>
           <h2>{heroData.title}</h2>
         </header>
+        <div className='fdDetails__article__details'>
+          <div className='fdDetails__article__details__release'>
+            <span>{`${Math.round(heroData.vote_average)}/10`}</span>
+            <span>{heroData.release_date}</span>
+          </div>
+          <nav className='fdDetails__article__details__cta'>
+            <Link to={`/film-database/${heroData.id}`} aria-label={`More details about ${heroData.title}`}>
+              View more details
+            </Link>
+          </nav>
+        </div>
         <div className='fdDetails__article__description'>
           <p>{heroData.overview}</p>
         </div>
-        <nav className='fdDetails__article__cta'>
-          <Link to={`/film-database/${heroData.id}`} aria-label={`More details about ${heroData.title}`}>
-            More details
-          </Link>
-        </nav>
       </article>
     </section>
   );
