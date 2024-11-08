651fe77a74e1eb84191e2e7e72227aaf59617723
justin barker <jbarkerpoc@gmail.com>
2024-11-08 07:10:06 -0800

    refactor(media): seperated pagination function from media, moved to hook

--- src/film-database/features/catalog/media/FDMedia.tsx
+++ src/film-database/features/catalog/media/FDMedia.tsx
@@ -4,19 +4,18 @@ import { useEffect, useRef, useState } from 'react';
 import { useCatalogProvider } from '../../../context/CatalogContext';
 // Composables
 import { Namespace_Tmdb, useTmdbFetcher } from '../../../composables/tmdb-api/hooks/useTmdbFetcher';
-// Hooks
-import { usePostersPerPage } from '../../../hooks/usePostersPerPage';
-// Data
-import { Namespace_TmdbEndpointsKeys } from '../../../composables/tmdb-api/data/tmdbEndPoints';
 // Features
 import FDCarouselSearch from './media-carousel-search/FDCarouselSearch';
 import FDCarousels from '../../../components/FDCarousels';
+import { usePaginateData } from '../../../hooks/usePaginateData';
+import { usePostersPerPage } from '../../../hooks/usePostersPerPage';
 
 const FDMedia = () => {
+  const maxCarouselNodes = usePostersPerPage();
   const { route, isMenuOpen, setHeroData } = useCatalogProvider();
   const [paginatedData, setPaginatedData] = useState<Map<string, Namespace_Tmdb.BaseMedia_Provider[][]> | undefined>(undefined);
 
-  /** Fetch data when user requests a route, pass to processDataPagination() */
+  /** Fetch data when user requests a route, pass to usePaginateData() hook */
   type Prefabs_Obj_isUndefined = Namespace_Tmdb.Prefabs_Obj | undefined;
 
   const fetchDataByRoute = async (): Promise<void> => {
@@ -29,64 +28,18 @@ const FDMedia = () => {
       ])) as Namespace_Tmdb.Prefabs_Obj[] | Prefabs_Obj_isUndefined[];
 
       const filteredHomeData = routeData.filter((obj) => obj !== undefined) as Namespace_Tmdb.Prefabs_Obj[];
-      processDataPagination(filteredHomeData);
+      const data = usePaginateData(filteredHomeData, maxCarouselNodes, setHeroData);
+      setPaginatedData(data);
     } else {
       const routeData = (await useTmdbFetcher({ discover: route })) as Namespace_Tmdb.Discover_Obj;
-      processDataPagination(routeData);
+      const data = usePaginateData(routeData, maxCarouselNodes, setHeroData);
+      setPaginatedData(data);
     }
   };
 
   useEffect(() => {
     fetchDataByRoute();
   }, [route]);
-
-  /** Pagination && carousel, hero data creation */
-  const maxVisibleCarouselNodes: number | undefined = usePostersPerPage();
-
-  const processDataPagination = (rawData: Namespace_Tmdb.Prefabs_Obj[] | Namespace_Tmdb.Discover_Obj | undefined) => {
-    if (!rawData || !maxVisibleCarouselNodes) return;
-
-    // Init mutatable map in outter scope to reduce state updates when paginating data
-    let dataMap: Map<string, Namespace_Tmdb.BaseMedia_Provider[][]> = new Map([]);
-
-    // Paginate data
-    const setData = (targetKey: Namespace_TmdbEndpointsKeys.Prefabs_Keys, data: Namespace_Tmdb.BaseMedia_Provider[]): void => {
-      // Pagination iteration dependency calculation
-      const maxIteratorIndex: number = Math.ceil((data.length - 1) / maxVisibleCarouselNodes);
-      // Get map by key
-      const targetMapArr: Namespace_Tmdb.BaseMedia_Provider[][] | undefined = dataMap.get(targetKey);
-      // If key doesn't exist in map, create new entry
-      if (!targetMapArr) dataMap.set(targetKey, []);
-
-      // Paginate data
-      for (let iteratorCounter = 0; iteratorCounter < maxIteratorIndex; iteratorCounter++) {
-        const startingSliceIndex: number = maxVisibleCarouselNodes * iteratorCounter + iteratorCounter;
-        const endingSliceIndex: number = maxVisibleCarouselNodes * (iteratorCounter + 1) + 1;
-        const paginatedData: Namespace_Tmdb.BaseMedia_Provider[] = data.slice(startingSliceIndex, endingSliceIndex);
-        dataMap.get(targetKey)?.push(paginatedData); // Push data into dataMap
-      }
-    };
-
-    // Prep data for pagination
-    if (Array.isArray(rawData)) {
-      (rawData as Namespace_Tmdb.Prefabs_Obj[]).forEach((item) => {
-        const itemKey = Object.keys(item)[0] as Namespace_TmdbEndpointsKeys.Prefabs_Keys;
-        const results: Namespace_Tmdb.BaseMedia_Provider[] = item[itemKey].results;
-        setData(itemKey, results);
-      });
-    } else {
-      const itemKey = Object.keys(rawData)[0] as Namespace_TmdbEndpointsKeys.Prefabs_Keys;
-      const results = rawData.discover.results;
-      setData(itemKey, results);
-    }
-
-    // Set state
-    setPaginatedData(dataMap);
-
-    // Create hero data
-    const firstMapValue: Namespace_Tmdb.BaseMedia_Provider | undefined = dataMap.entries().next().value?.[1][0][0];
-    if (firstMapValue) setHeroData(firstMapValue);
-  };
 
   /** Carousel DeltaY scroll logic */
   const fdMediaRef = useRef<HTMLElement>(null);
