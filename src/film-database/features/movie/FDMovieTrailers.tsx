e669824fdddd48d8278e804fea927756097f4c09
Justin <jbarkerpoc@gmail.com>
2024-11-13 14:15:03 -0800

    chore(movieTrailers): removal of unused import causing visual error

--- src/film-database/features/movie/FDMovieTrailers.tsx
+++ src/film-database/features/movie/FDMovieTrailers.tsx
@@ -1,5 +1,4 @@
 import { Namespace_Tmdb } from '../../composables/tmdb-api/hooks/useTmdbFetcher';
-import FDiFramePlayer from '../catalog/hero/iframe/player/FDiFramePlayer';
 
 const FDMovieTrailers = ({ videos }: { videos: Namespace_Tmdb.Videos_Obj['videos']['results'] }) => {
   const trailers = videos.filter((obj) => obj.name.includes('Trailer'));
