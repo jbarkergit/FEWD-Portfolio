5229a94fe3e89b76e1a6d971785ef80dcfd99fa3
Justin <jbarkerpoc@gmail.com>
2024-11-14 18:34:53 -0600

    refactor(userAcc): assigned unique classname to root

--- src/film-database/pages/FDUserAccount.tsx
+++ src/film-database/pages/FDUserAccount.tsx
@@ -10,7 +10,7 @@ type Type_PropDrill = {
 
 const FDUserAccount = ({ rootRef }: Type_PropDrill) => {
   return (
-    <div>
+    <div className='fdUserAccount'>
       <FDAccountBackground />
       <FDAccount rootRef={rootRef} />
     </div>
