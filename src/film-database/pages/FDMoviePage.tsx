fd86807518a9a764578577a4a1cd9409ceded904
Justin <jbarkerpoc@gmail.com>
2024-11-26 02:33:36 -0600

    feat(moviePage): castcrew

--- src/film-database/pages/FDMoviePage.tsx
+++ src/film-database/pages/FDMoviePage.tsx
@@ -4,16 +4,8 @@ import { CatalogProvider } from '../context/CatalogContext';
 import FDCastCrew from '../features/movie/FDCastCrew';
 
 const FDMoviePage = () => {
-  // const getProp = (key: Namespace_TmdbEndpointsKeys.Keys_Union) => {
-  //   return castCrew.find((obj) => Object.keys(obj)[0] === key);
-  // };
-  // const details = getProp('details') as Namespace_Tmdb.Details_Obj;
-  // const watchProviders = getProp('watchProviders') as Namespace_Tmdb.WatchProviders_Obj;
-  // const videos = getProp('videos') as Namespace_Tmdb.Videos_Obj;
-  // const recommendations = getProp('recommendations') as Namespace_Tmdb.Recommendations_Obj;
-
   return (
-    <div className='filmDatabase' data-layout-carousel>
+    <div className='fdMoviePage' data-layout-carousel>
       <CatalogProvider>
         <FDCastCrew />
       </CatalogProvider>
