7f924d49b71e9256fbaeccdba326eb437c0eb34e
Justin <jbarkerpoc@gmail.com>
2024-12-01 07:31:09 -0600

    feat(header): init

---
+++ src/film-database/features/catalog/header/FDHeader.tsx
@@ -0,0 +1,48 @@
+import { signOut } from 'firebase/auth';
+import { firebaseAuth } from '../../../../app/config/firebaseConfig';
+import { useCatalogProvider } from '../../../context/CatalogContext';
+
+type CTAButton = {
+  key: string;
+  icon: React.ReactNode | undefined;
+  func: () => void;
+};
+
+const FDHeader = () => {
+  const { setRoute } = useCatalogProvider();
+
+  const cta: { left: CTAButton[]; right: CTAButton[] } = {
+    left: [{ key: 'Home', icon: undefined, func: (): void => setRoute('home') }],
+    right: [
+      {
+        key: 'Sign Out',
+        icon: (
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+            <path fill='currentColor' d='M5 5h7V3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h7v-2H5zm16 7l-4-4v3H9v2h8v3z'></path>
+          </svg>
+        ),
+        func: (): void => {
+          signOut(firebaseAuth);
+        },
+      },
+    ],
+  };
+
+  return (
+    <nav className='fdHeader'>
+      {(['left', 'right'] as const).map((alignment) => (
+        <ul className='fdHeader__ul' key={`fdHeader-alignment-${alignment}`}>
+          {cta[alignment].map((obj) => (
+            <li className='fdHeader__ul__li' key={`fdHeader-nav-btn-${obj.key}`}>
+              <button className='fdHeader__ul__li__button' aria-label={`Select ${obj.key}`} onClick={obj.func}>
+                {obj.icon ? obj.icon : obj.key}
+              </button>
+            </li>
+          ))}
+        </ul>
+      ))}
+    </nav>
+  );
+};
+
+export default FDHeader;
