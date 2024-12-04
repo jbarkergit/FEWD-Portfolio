fd52a40872f69ef32123c1bac78ec4b1ddba0fb3
Justin <jbarkerpoc@gmail.com>
2024-12-04 09:23:37 -0600

    chore(useMovieModal): del

--- src/film-database/hooks/useMovieModal.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export const useMovieModal = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const mount = (): void => setIsOpen(true);
-  const unmount = (): void => setIsOpen(false);
-  return { isOpen, mount, unmount };
-};
