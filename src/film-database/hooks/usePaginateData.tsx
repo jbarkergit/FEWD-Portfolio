1a3416946d27099b8f933f58e7f5b5e5198f327b
Justin <jbarkerpoc@gmail.com>
2024-12-04 03:03:50 -0600

    perf(usePaginateData): rewrite

--- src/film-database/hooks/usePaginateData.tsx
+++ src/film-database/hooks/usePaginateData.tsx
@@ -1,53 +1,51 @@
-import { Namespace_TmdbEndpointsKeys } from '../composables/tmdb-api/data/tmdbEndPoints';
-import { Namespace_Tmdb } from '../composables/tmdb-api/hooks/useTmdbFetcher';
+import { Namespace_TmdbEndpointsKeys, tmdbEndpoints } from '../composables/tmdb-api/data/tmdbEndPoints';
+import { tmdbMovieGenres } from '../composables/tmdb-api/data/tmdbGenres';
+import { Namespace_Tmdb, useTmdbFetcher } from '../composables/tmdb-api/hooks/useTmdbFetcher';
 
-export type Type_usePaginateData_Provider =
-  | Namespace_Tmdb.BaseMedia_Provider[]
-  | Namespace_Tmdb.Credits_Obj['credits']['cast']
-  | Namespace_Tmdb.Credits_Obj['credits']['crew'];
+type Type_usePaginateData_Data_Provider = Array<
+  Namespace_Tmdb.BaseMedia_Provider | Namespace_Tmdb.Credits_Obj['credits']['cast'][0] | Namespace_Tmdb.Credits_Obj['credits']['crew'][0]
+>;
 
-type Type_usePaginateData_TargetKey_Provider = Namespace_TmdbEndpointsKeys.Keys_Union | 'Cast' | 'Crew';
+export const usePaginateData = (rawData: ReturnType<typeof useTmdbFetcher>) => {
+  // Initialize mutatable map
+  type Type_usePaginateData_TargetKeys = Namespace_TmdbEndpointsKeys.Keys_Union & 'Cast' & 'Crew';
+  let store: Map<Type_usePaginateData_TargetKeys, Type_usePaginateData_Data_Provider[]> | undefined = new Map(undefined);
+  console.log(store);
 
-export const usePaginateData = (rawData: Namespace_Tmdb.Prefabs_Obj[] | Namespace_Tmdb.Discover_Obj | Namespace_Tmdb.Credits_Obj | undefined) => {
-  // Get visible carousel nodes
-  const root: Element | null = document.querySelector('[data-layout-carousel]');
-  if (!rawData || !root) return;
-  const maxCarouselNodes: number = parseInt(getComputedStyle(root).getPropertyValue('--fd-carousel-items-per-page'));
-
-  // Init mutatable map in outter scope to reduce state updates when paginating data
-  let dataMap: Map<Type_usePaginateData_TargetKey_Provider, Array<Type_usePaginateData_Provider>> = new Map();
-
-  // Paginate data
-  const setData = (targetKey: Type_usePaginateData_TargetKey_Provider, data: Type_usePaginateData_Provider): void => {
+  // Paginate data callback
+  const paginateData = (targetKey: Type_usePaginateData_TargetKeys, data: Type_usePaginateData_Data_Provider) => {
     // Get map by key
-    const isKeyMapped: boolean = dataMap.has(targetKey);
+    const isKeyMapped: boolean = store.has(targetKey);
+
     // If key doesn't exist in map, create new entry
-    if (!isKeyMapped) dataMap.set(targetKey, []);
+    if (!isKeyMapped) store.set(targetKey, []);
+
+    // Max carousel nodes
+    const root: Element | null = document.querySelector('[data-layout-carousel]');
+    const maxCarouselNodes: number = parseInt(getComputedStyle(root!).getPropertyValue('--fd-carousel-items-per-page'));
 
     // Paginate data
     for (let i = 0; i < data.length; i += maxCarouselNodes + 1) {
-      dataMap.get(targetKey)?.push(data.slice(i, i + maxCarouselNodes + 1));
+      store.get(targetKey)!.push(data.slice(i, i + maxCarouselNodes + 1));
     }
   };
 
-  // Prep data for pagination
-  if (Array.isArray(rawData)) {
-    for (const item of rawData) {
-      const itemKey = Object.keys(item)[0] as Namespace_TmdbEndpointsKeys.Prefabs_Keys;
-      const results: Namespace_Tmdb.BaseMedia_Provider[] = item[itemKey].results;
-      setData(itemKey, results);
+  // Envoke pagination
+  const process = (obj: ReturnType<typeof useTmdbFetcher>) => {
+    const key = Object.keys(obj)[0] as Type_usePaginateData_TargetKeys;
+    const data = obj[key] as unknown;
+
+    if (key in tmdbEndpoints.prefabs || key in tmdbMovieGenres) {
+      const i = (data as Namespace_Tmdb.Recommendations_Provider<Namespace_Tmdb.BaseMedia_Provider>).results;
+      paginateData(key, i);
+    } else if (key === 'cast' || key === 'crew') {
+    } else {
+      return undefined;
     }
-  } else if ('credits' in rawData) {
-    const cast: Namespace_Tmdb.Credits_Obj['credits']['cast'] = rawData.credits.cast;
-    const crew: Namespace_Tmdb.Credits_Obj['credits']['crew'] = rawData.credits.crew;
-    setData('Cast', cast);
-    setData('Crew', crew);
-  } else {
-    const itemKey = Object.keys(rawData)[0] as Namespace_TmdbEndpointsKeys.Prefabs_Keys;
-    const results: Namespace_Tmdb.BaseMedia_Provider[] = rawData.discover.results;
-    setData(itemKey, results);
-  }
+  };
+
+  Array.isArray(rawData) ? rawData.forEach((obj) => process(obj)) : process(rawData);
 
   // Return
-  return Array.from(dataMap);
+  return Array.from(store);
 };
