f7bf1a09009a87eb2417f175737cbf806cf66f42
Justin <jbarkerpoc@gmail.com>
2024-12-04 09:16:38 -0600

    fix(movieModal): wrapped async func in useEffect for safety layer

--- src/film-database/features/catalog/modals/FDMovieModal.tsx
+++ src/film-database/features/catalog/modals/FDMovieModal.tsx
@@ -3,12 +3,14 @@ import FDCarousel from '../../../components/carousel/FDCarousel';
 import { useTmdbFetcher, Namespace_Tmdb } from '../../../composables/tmdb-api/hooks/useTmdbFetcher';
 import { useCatalogProvider } from '../../../context/CatalogContext';
 import { usePaginateData } from '../../../hooks/usePaginateData';
+import { useMovieModal } from '../../../hooks/useMovieModal';
 
 const FDMovieModal = () => {
+  const { isOpen } = useMovieModal();
   const { heroData } = useCatalogProvider();
   const [castCrew, setCastCrew] = useState<ReturnType<typeof usePaginateData> | undefined>(undefined);
 
-  const fetch = async () => {
+  const fetch = async (): Promise<void> => {
     const data = (await useTmdbFetcher({ credits: (heroData as Namespace_Tmdb.BaseMedia_Provider).id })) as Namespace_Tmdb.Credits_Obj;
     const paginatedData = usePaginateData(data);
     setCastCrew(paginatedData);
@@ -18,7 +20,7 @@ const FDMovieModal = () => {
     fetch();
   }, [heroData]);
 
-  if (castCrew)
+  if (isOpen && castCrew)
     return (
       <div className='fdMovieModal'>
         <section className='fdMovieModal__container'>
