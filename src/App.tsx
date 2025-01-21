06ea794f44e02d11795631e32c1941a79f79bd9d
Justin <jbarkerpoc@gmail.com>
2025-01-21 16:12:54 -0600

    perf(App): rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,64 @@
-import { Suspense, lazy, startTransition, useEffect, useState } from 'react';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-// 404
-const ProtocolErrorHandler = lazy(() => import('./app/features/ProtocolErrorHandler'));
-// Routes data
-import { useAppRoutes } from './app/hooks/useAppRoutes';
-import { useSuspense } from './app/hooks/useSuspense';
-// Firebase
-import { onAuthStateChanged, User } from 'firebase/auth';
+/** Deps */
+import { JSX, startTransition, Suspense, useEffect, useMemo, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+/** Firebase */
 import { firebaseAuth } from './app/config/firebaseConfig';
+import { onAuthStateChanged, User } from 'firebase/auth';
+/** App Components */
+import ProtocolErrorHandler from './app/components/ProtocolErrorHandler';
+import StandardSuspense from './app/components/StandardSuspense';
+/** Componenets: Skeletons */
+import HomeSkeleton from './ecommerce/skeletons/pages/HomeSkeleton';
+import ProductCatalogSkeleton from './ecommerce/skeletons/pages/ProductCatalogSkeleton';
+import ProductDetailPageSkeleton from './ecommerce/skeletons/pages/ProductDetailPageSkeleton';
+/** Data: Ecommerce Dynamic Routes */
+import { useUniqueData } from './ecommerce/hooks/useUniqueData';
 
-function App() {
-  const appRoutes = useAppRoutes();
+/** App Routes */
+function useRouteDirectory() {
+  const { useEcommercePaths } = useUniqueData();
 
-  /** Component storage */
-  type Type_routeComponents_Object = { path: string; component: JSX.Element };
-  const [routeComponents, setRouteComponents] = useState<Type_routeComponents_Object[]>([]);
+  const routes = useMemo(() => {
+    return {
+      portfolio: { path: '/', dir: '/src/portfolio/pages/Portfolio.tsx' },
+      ecommerce: [
+        { path: '/ecommerce', dir: '/src/ecommerce/pages/Home.tsx' },
+        { path: '/ecommerce/product/:paramId', dir: '/src/ecommerce/pages/ProductDetailPage.tsx' },
+        { path: '/ecommerce/products', dir: '/src/ecommerce/pages/ProductCatalog.tsx' },
+        ...useEcommercePaths.reduce<{ path: string; dir: string }[]>((acc, path) => {
+          acc.push({ path, dir: '/src/ecommerce/pages/ProductCatalog.tsx' });
+          return acc;
+        }, []),
+      ],
+      filmDatabase: [
+        { path: '/film-database', dir: '/src/film-database/pages/FDUserAccount.tsx' },
+        { path: '/film-database/browse', dir: '/src/film-database/pages/FDCatalog.tsx' },
+      ],
+    };
+  }, [useEcommercePaths]);
 
-  /** Import module, store route component */
-  type Type_createRoute_Param = { path: string; dir: string };
+  const paths = useMemo(() => {
+    return Object.entries(routes).flatMap(([key, value]) => value);
+  }, [routes]);
 
-  const createRoute = async (param: Type_createRoute_Param): Promise<void> => {
-    const { path, dir } = param;
+  return { routes, paths };
+}
 
-    if (!routeComponents.some((obj) => obj.path === path)) {
-      try {
-        const Module = await import(/* @vite-ignore */ dir);
-        if (!Module) throw new Error(`${dir}`);
+/** Suspense */
+function useSuspense(routeDir: ReturnType<typeof useRouteDirectory>, pathname: string): JSX.Element {
+  switch (true) {
+    case pathname.startsWith('/ecommerce'):
+      if (pathname === '/ecommerce') return <HomeSkeleton />;
+      else if (routeDir.routes.ecommerce.flatMap((entries) => entries.path).includes(pathname)) return <ProductCatalogSkeleton />;
+      else return <ProductDetailPageSkeleton />;
 
-        const component: JSX.Element = <Module.default />;
-        const routeComponentObj: Type_routeComponents_Object = { path: path, component };
+    default:
+      return <StandardSuspense />;
+  }
+}
 
-        // Use functional update to avoid closure issues
-        setRouteComponents((prevRouteComponents) => {
-          if (!prevRouteComponents.some((obj) => obj.path === path)) {
-            return [...prevRouteComponents, routeComponentObj];
-          }
-          return prevRouteComponents;
-        });
-      } catch (Error) {
-        console.error('Failure to load module: ', Error);
-      }
-    }
-  };
-
-  /** Route loading queue */
-  const userLocationPathname: string = useLocation().pathname;
-
-  const queueRoute = (): void => {
-    switch (userLocationPathname) {
-      // Prioritize landing pages on portfolio mount
-      case '/':
-        const landingPageRoutes: Type_createRoute_Param[] = [
-          appRoutes.routes.portfolio,
-          appRoutes.routes.filmDatabase[0],
-          appRoutes.routes.ecommerce[0] as Type_createRoute_Param,
-        ];
-        landingPageRoutes.forEach((route) => createRoute(route));
-        break;
-
-      // Standard procedure
-      default:
-        for (const route of Object.values(appRoutes.routes)) {
-          // MPA (Routes entry is an array)
-          Array.isArray(route)
-            ? // If entry.path is an array, create individual queues
-              route.forEach(({ path, dir }) => {
-                const paths = path;
-                Array.isArray(paths) ? paths.forEach((path) => createRoute({ path, dir })) : createRoute({ path, dir } as Type_createRoute_Param);
-              }) // SPA (Routes entry is an object)
-            : createRoute(route);
-        }
-        break;
-    }
-  };
-
-  // Queue routes when userLocation().pathname changes
-  useEffect(() => queueRoute(), [userLocationPathname]);
-
-  /** Dynamic route elements */
-  const getElementByPath = (path: string): JSX.Element | undefined => {
-    return routeComponents.find((route) => route.path === path)?.component;
-  };
-
+/** Application */
+const Napp = () => {
   /** Firebase Authentication */
   const [authorizedUser, setAuthorizedUser] = useState<{
     user: undefined | User;
@@ -102,33 +79,77 @@ function App() {
     return () => authListener();
   }, []);
 
-  /** Application */
+  /** Route Store */
+  type Type_Route = { path: string; element: JSX.Element };
+  const [routes, setRoutes] = useState<Array<Type_Route> | undefined>(undefined);
+
+  /** Module Importer
+   * Opting out of Promise.all to ensure Modules are loaded as quickly as possible
+   * To enable this behavior and reduce readability issues, function loadModules() was implemented
+   */
+  type Type_ImportModule_Param = { path: string; dir: string };
+
+  async function importModule(pd: Type_ImportModule_Param) {
+    const { path, dir } = pd;
+
+    try {
+      const Module = await import(/* @vite-ignore */ dir);
+      if (!Module) throw new Error(`Module for path '${path}' could not be imported.`);
+
+      const newRoute: Type_Route = { path: path, element: <Module.default /> };
+
+      setRoutes((prevRoutes) => {
+        if (!prevRoutes) return [newRoute];
+        else if (prevRoutes.some((route) => route.path === path)) return prevRoutes;
+        else return [...prevRoutes, newRoute];
+      });
+    } catch {
+      console.error(`Failure during Application Routing: ${Error}`);
+    }
+  }
+
+  /** Module Loader */
+  type Type_LoadModule_Param = { path: string | string[]; dir: string };
+  type Type_LoadModule_Params = Type_LoadModule_Param | Array<Type_LoadModule_Param>;
+
+  async function loadModule(pd: Type_LoadModule_Params) {
+    // If parameter is an array
+    if (Array.isArray(pd)) for (const { path, dir } of pd) importModule({ path, dir } as Type_ImportModule_Param);
+    // If parameter is an object
+    else importModule(pd as Type_ImportModule_Param);
+  }
+
+  /** Queue modules for import */
+  const routeDir = useRouteDirectory();
+
+  function queueModule(pathname: string) {
+    switch (pathname) {
+      // Prioritize landing pages on portfolio mount
+      case '/':
+        loadModule([routeDir.routes.portfolio, routeDir.routes.filmDatabase[0], routeDir.routes.ecommerce[0]]);
+        break;
+
+      default:
+        loadModule(routeDir.paths.find(({ path, dir }) => path === pathname)!);
+        break;
+    }
+  }
+
+  const pathname = useLocation().pathname;
+
+  useEffect(() => queueModule(pathname), []);
+
+  /** Router */
   return (
-    <Suspense fallback={useSuspense(appRoutes, userLocationPathname)}>
+    <Suspense fallback={useSuspense(routeDir, pathname)}>
       <Routes>
         <Route path='*' element={<ProtocolErrorHandler />} />
-        <Route path='/' element={getElementByPath('/')} />
-        <Route path='/ecommerce' element={getElementByPath('/ecommerce')} />
-        <Route path='/ecommerce/products' element={getElementByPath('/ecommerce/products')} />
-        <Route path='/ecommerce/headphones' element={getElementByPath('/ecommerce/headphones')} />
-        <Route path='/ecommerce/amps-dacs' element={getElementByPath('/ecommerce/amps-dacs')} />
-        <Route path='/ecommerce/microphones' element={getElementByPath('/ecommerce/microphones')} />
-        <Route path='/ecommerce/interfaces' element={getElementByPath('/ecommerce/interfaces')} />
-        <Route path='/ecommerce/product/:paramId' element={getElementByPath('/ecommerce/product/:paramId')} />
-        {appRoutes.ecommercePaths.map((path) => (
-          <Route path={path} element={getElementByPath('/ecommerce/products')} key={path} />
+        {routeDir.paths.map(({ path, dir }, index) => (
+          <Route path={path} element={routes?.find((obj) => obj.path === path)?.element} key={`${path}-${index}`} />
         ))}
-        <Route
-          path='/film-database'
-          element={!authorizedUser.user || !authorizedUser.verified ? getElementByPath('/film-database') : <Navigate to='/film-database/browse' />}
-        />
-        <Route
-          path='/film-database/browse'
-          element={authorizedUser.user && authorizedUser.verified ? getElementByPath('/film-database/browse') : <Navigate to='/film-database' />}
-        />
       </Routes>
     </Suspense>
   );
-}
+};
 
-export default App;
+export default Napp;
