d8cb34e7e3264c4d966ea63db895b617cc0afca7
Justin <jbarkerpoc@gmail.com>
2025-01-29 13:57:16 -0600

    docs(vite.config.ts): updated vite reference url

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+/** https://vite.dev/config/ */
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
+  server: { watch: { usePolling: true } },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
   build: { copyPublicDir: false },
-  define: {
-    'process.env': process.env,
-  },
+  define: { 'process.env': process.env },
 });
