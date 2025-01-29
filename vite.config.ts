a93b089770172f51093621a786aa46e3d9daae8e
Justin <jbarkerpoc@gmail.com>
2025-01-29 17:54:01 -0600

    chore: reactRouter, tsconfigPaths plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 /** https://vite.dev/config/ */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRouter(), tsconfigPaths()],
   server: { watch: { usePolling: true } },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
-  build: { copyPublicDir: false },
+  build: { copyPublicDir: false, cssMinify: true, ssr: false },
   define: { 'process.env': process.env },
 });
