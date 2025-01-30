8a5c0f4bb3dcc2c32494118298e4436a55ad737e
Justin <jbarkerpoc@gmail.com>
2025-01-30 13:34:18 -0600

    fix(useProductDatabase): bad import path

--- app/ecommerce/hooks/useProductDatabase.tsx
+++ app/ecommerce/hooks/useProductDatabase.tsx
@@ -2,7 +2,7 @@ import type { ProductType } from '../context/CartContext';
 
 const fetchProductDatabase = async (): Promise<ProductType[]> => {
   try {
-    const productDatabase = await fetch('/src/ecommerce/data/database/productDatabase.json');
+    const productDatabase = await fetch('/app/ecommerce/data/database/productDatabase.json');
     return (await productDatabase.json()) as ProductType[];
   } catch (error) {
     console.error('Error fetching product database JSON data', error);
