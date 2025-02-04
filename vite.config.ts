f6a75246331cdcfaefce3f3e1400744aed7cfc40
Justin <jbarkerpoc@gmail.com>
2025-02-04 16:07:20 -0600

    build: -@vitejs/plugin-react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { reactRouter } from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 /** https://vite.dev/config/ */
 export default defineConfig({
-  plugins: [react(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
   server: { watch: { usePolling: true } },
   test: {
     globals: true,
