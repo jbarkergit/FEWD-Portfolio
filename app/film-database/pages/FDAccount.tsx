6a453ab60832ece787212233b422bddc7f49e214
Justin <jbarkerpoc@gmail.com>
2025-02-10 15:30:00 -0600

    chore(fdAccount): rename, imported renamed modules

---
+++ app/film-database/pages/FDAccount.tsx
@@ -0,0 +1,14 @@
+// Features
+import FDAccountBackground from '../features/account/animation/FDAccountBackground';
+import FDAccountModal from '../features/account/FDAccountModal';
+
+const FDUserAccount = () => {
+  return (
+    <div className='fdAccount'>
+      <FDAccountBackground />
+      <FDAccountModal />
+    </div>
+  );
+};
+
+export default FDUserAccount;
