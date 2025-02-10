df11f3f6fca9a4488a23b17cf1cdef0d38a6985a
Justin <jbarkerpoc@gmail.com>
2025-02-10 15:29:39 -0600

    feat(fdCatalog): init clientLoader auth

--- app/film-database/pages/FDCatalog.tsx
+++ app/film-database/pages/FDCatalog.tsx
@@ -1,10 +1,24 @@
-// Context
+import { redirect } from 'react-router';
+// Authorization && Context
+import { isAuth } from '~/base/context/authProvider';
 import { CatalogProvider } from '../context/CatalogContext';
 // Features
 import FDHeader from '../features/catalog/header/FDHeader';
 import FDHero from '../features/catalog/hero/FDHero';
 import FDMedia from '../features/catalog/media/FDMedia';
 import FDMovieModal from '../features/catalog/modals/FDMovieModal';
+
+export async function clientLoader() {
+  // await new Promise((resolve) =>
+  //   setTimeout(() => {
+  //     resolve(undefined);
+  //   }, 2000)
+  // );
+  const isLogged = await isAuth();
+  if (isLogged) {
+    return redirect('/film-database');
+  }
+}
 
 const FDCatalog = () => {
   return (
