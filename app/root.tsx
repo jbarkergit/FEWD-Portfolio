4b64d3dbadf65b12ba5e06c1d8b7db9f8a4f3acf
Justin <jbarkerpoc@gmail.com>
2025-02-11 17:13:15 -0600

    chore(root): hydrate fallback className reflects scss module

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,7 @@ export default function App() {
 
 export function HydrateFallback() {
   return (
-    <div className='standardSuspenseFallback'>
+    <div className='hydrateFallback'>
       <svg xmlns='http://www.w3.org/2000/svg' width='6em' height='6em' viewBox='0 0 24 24'>
         <path fill='currentColor' d='M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z' opacity='.5' />
         <path fill='currentColor' d='M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z'>
