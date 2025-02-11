d239026ffb37e10baae020ed6488868c595f2281
Justin <jbarkerpoc@gmail.com>
2025-02-10 18:38:37 -0600

    feat(accountArticle): isolated from parent

--- app/film-database/features/account/article/FDAccountArticle.tsx
+++ app/film-database/features/account/article/FDAccountArticle.tsx
@@ -1,7 +1,7 @@
 const FDAccountArticle = () => {
   return (
     <article className='fdAccountModal__article'>
-      <span>Film Database</span>
+      <h2>Film Database</h2>
       <p>Watch trailers, get cast details, save movies, create a watch queue and more. Free of charge now, free forever.</p>
     </article>
   );
