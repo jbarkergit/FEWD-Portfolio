f0565a3d8b9cb5afe077c492d140b300b426339a
Justin <jbarkerpoc@gmail.com>
2025-02-17 19:50:29 -0600

    refactor(auth/deauthorizedUser): cleanup

--- app/base/auth/hooks/deauthorizeUser.ts
+++ app/base/auth/hooks/deauthorizeUser.ts
@@ -2,17 +2,17 @@ import { useNavigate } from 'react-router';
 import { useAuth } from '../context/authProvider';
 import { firebaseAuth } from '~/base/config/firebaseConfig';
 
-export function deauthorizeUser() {
+export async function deauthorizeUser() {
   const navigate = useNavigate();
   const { user, setUser } = useAuth();
 
-  const handleLogout = async () => {
-    try {
-      await firebaseAuth.signOut();
-      setUser(null);
-      navigate('/');
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
+  if (!user) return;
+
+  try {
+    await firebaseAuth.signOut();
+    setUser(null);
+    navigate('/');
+  } catch (error) {
+    console.error('Error logging out:', error);
+  }
 }
