c8326d37e113448e035948e633284aef84f4a124
Justin <jbarkerpoc@gmail.com>
2025-02-18 15:43:24 -0600

    refactor(authorizeUser): moved funcs in scope, returned

--- app/base/auth/hooks/authorizeUser.ts
+++ app/base/auth/hooks/authorizeUser.ts
@@ -1,26 +1,24 @@
 import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { firebaseAuth } from '~/base/config/firebaseConfig';
 
-async function authorizeGoogleUser() {
-  try {
-    const provider = new GoogleAuthProvider();
-    await signInWithPopup(firebaseAuth, provider);
-  } catch (err) {
-    console.error('Error thrown during Google authorization: ', err);
+export function authorizeUser() {
+  async function google() {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(firebaseAuth, provider);
+    } catch (err) {
+      console.error('Error thrown during Google authorization: ', err);
+    }
   }
-}
 
-async function authorizeGithubUser() {
-  try {
-    const provider = new GithubAuthProvider();
-    await signInWithPopup(firebaseAuth, provider);
-  } catch (err) {
-    console.error('Error thrown during Github authorization: ', err);
+  async function github() {
+    try {
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(firebaseAuth, provider);
+    } catch (err) {
+      console.error('Error thrown during Github authorization: ', err);
+    }
   }
-}
 
-export default async function authorizeUser() {
-  const google = await authorizeGoogleUser();
-  const github = await authorizeGithubUser();
   return { google, github };
 }
