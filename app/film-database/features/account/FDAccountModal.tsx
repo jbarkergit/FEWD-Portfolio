d6bdcbe1ffcb59416b74580c11106b94a1bedaad
Justin <jbarkerpoc@gmail.com>
2025-02-21 01:28:15 -0600

    feat/style(accModal): shared 'login with' grouped btns component

--- app/film-database/features/account/FDAccountModal.tsx
+++ app/film-database/features/account/FDAccountModal.tsx
@@ -4,6 +4,7 @@ import FDAccountSignIn from './fieldsets/FDAccountSignIn';
 import FDAccountArticle from './article/FDAccountArticle';
 import type { Namespace_Tmdb } from '~/film-database/composables/tmdb-api/hooks/useTmdbFetcher';
 import FDAccountReset from './fieldsets/FDAccountReset';
+import { authorizeUser } from '~/base/auth/hooks/authorizeUser';
 
 const FDAccountModal = ({ responseSetArr }: { responseSetArr: Namespace_Tmdb.BaseMedia_Provider[][] }) => {
   // Primary state forcing rerenders
@@ -50,13 +51,62 @@ const FDAccountModal = ({ responseSetArr }: { responseSetArr: Namespace_Tmdb.Bas
     );
   };
 
+  // Modal components shared 'login with'
+  const LoginWithGroup = () => {
+    return (
+      <div className='fdAccountModal__modals__btns__grouped'>
+        <button
+          aria-label='Sign in with Google'
+          onPointerUp={(e) => {
+            e.preventDefault();
+            authorizeUser().google();
+          }}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 262'>
+            <path
+              fill='#4285F4'
+              d='M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027'></path>
+            <path
+              fill='#34A853'
+              d='M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1'></path>
+            <path
+              fill='#FBBC05'
+              d='M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z'></path>
+            <path
+              fill='#EB4335'
+              d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'></path>
+          </svg>
+        </button>
+        <button
+          aria-label='Sign in with Github'
+          onPointerUp={(e) => {
+            e.preventDefault();
+            authorizeUser().github();
+          }}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+            <g fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'>
+              <path
+                stroke-dasharray='32'
+                stroke-dashoffset='32'
+                d='M12 4c1.67 0 2.61 0.4 3 0.5c0.53 -0.43 1.94 -1.5 3.5 -1.5c0.34 1 0.29 2.22 0 3c0.75 1 1 2 1 3.5c0 2.19 -0.48 3.58 -1.5 4.5c-1.02 0.92 -2.11 1.37 -3.5 1.5c0.65 0.54 0.5 1.87 0.5 2.5c0 0.73 0 3 0 3M12 4c-1.67 0 -2.61 0.4 -3 0.5c-0.53 -0.43 -1.94 -1.5 -3.5 -1.5c-0.34 1 -0.29 2.22 0 3c-0.75 1 -1 2 -1 3.5c0 2.19 0.48 3.58 1.5 4.5c1.02 0.92 2.11 1.37 3.5 1.5c-0.65 0.54 -0.5 1.87 -0.5 2.5c0 0.73 0 3 0 3'>
+                <animate fill='freeze' attributeName='stroke-dashoffset' dur='0.7s' values='32;0'></animate>
+              </path>
+              <path stroke-dasharray='10' stroke-dashoffset='10' d='M9 19c-1.41 0 -2.84 -0.56 -3.69 -1.19c-0.84 -0.63 -1.09 -1.66 -2.31 -2.31'>
+                <animate fill='freeze' attributeName='stroke-dashoffset' begin='0.8s' dur='0.2s' values='10;0'></animate>
+              </path>
+            </g>
+          </svg>
+        </button>
+      </div>
+    );
+  };
+
   // JSX
   return (
     <main className='fdAccountModal' ref={accountRef} data-visible='false'>
       <FDAccountArticle responseSetArr={responseSetArr} />
       <div className='fdAccountModal__modals'>
-        {modal === 'signin' && <FDAccountSignIn ModalParent={ModalParent} setModal={setModal} ref={signInRefReceiver} />}
-        {modal === 'registry' && <FDAccountRegistry ModalParent={ModalParent} setModal={setModal} ref={registryRefReceiver} />}
+        {modal === 'signin' && <FDAccountSignIn ModalParent={ModalParent} LoginWithGroup={LoginWithGroup} setModal={setModal} ref={signInRefReceiver} />}
+        {modal === 'registry' && <FDAccountRegistry ModalParent={ModalParent} LoginWithGroup={LoginWithGroup} setModal={setModal} ref={registryRefReceiver} />}
         {modal === 'reset' && <FDAccountReset ModalParent={ModalParent} setModal={setModal} ref={resetRefReceiver} />}
       </div>
     </main>
--- app/film-database/features/account/fieldsets/FDAccountRegistry.tsx
+++ app/film-database/features/account/fieldsets/FDAccountRegistry.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, useState } from 'react';
-import type { ChangeEvent, FC, ReactNode } from 'react';
+import type { ChangeEvent, FC, JSX, ReactNode } from 'react';
 import { z } from 'zod';
 import { authorizeUser } from '~/base/auth/hooks/authorizeUser';
 import { createUser } from '~/base/auth/hooks/createUser';
@@ -7,6 +7,7 @@ import { zodSchema } from '~/base/schema/zodSchema';
 
 type Type_PropDrill = {
   ModalParent: FC<{ children: ReactNode }>;
+  LoginWithGroup: () => JSX.Element;
   setModal: React.Dispatch<React.SetStateAction<'signin' | 'registry' | 'reset'>>;
 };
 
@@ -18,7 +19,7 @@ const schema = z.object({
   password: zodSchema.account.shape.password,
 });
 
-const FDAccountRegistry = forwardRef<HTMLUListElement, Type_PropDrill>(({ ModalParent, setModal }, registryRefReceiver) => {
+const FDAccountRegistry = forwardRef<HTMLUListElement, Type_PropDrill>(({ ModalParent, LoginWithGroup, setModal }, registryRefReceiver) => {
   const [values, setValues] = useState({
     firstName: '',
     lastName: '',
@@ -65,36 +66,37 @@ const FDAccountRegistry = forwardRef<HTMLUListElement, Type_PropDrill>(({ ModalP
   // };
 
   return (
-    <ModalParent>
-      <ul className='fdAccountModal__modals__form__fieldset__ul' ref={registryRefReceiver} data-visible='false'>
-        <div className='fdAccountModal__modals__form__fieldset__ul__name'>
-          {[
-            { labelId: 'firstName', id: 'fdUserAccountFirstName', name: 'firstName', label: 'First name', isRequired: true, placeholder: 'John' },
-            { labelId: 'lastName', id: 'fdUserAccountLastName', name: 'lastName', label: 'Last name', isRequired: false, placeholder: 'Doe' },
-          ].map((field) => (
-            <li key={field.id}>
-              <label id={field.labelId} htmlFor={field.id}>
-                {field.label}
-              </label>
-              <input
-                form='fdRegistery'
-                id={field.id}
-                name={field.name}
-                type='text'
-                inputMode='text'
-                size={12}
-                required={field.isRequired}
-                aria-required={field.isRequired ? 'true' : 'false'}
-                aria-invalid={schema.safeParse(values.firstName).success}
-                autoCapitalize='words'
-                placeholder={field.placeholder}
-                onPointerUp={() => focus()}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
-              />
-            </li>
-          ))}
-        </div>
-        {/* <div className='fdAccountModal__modals__form__fieldset__ul__dob'>
+    <>
+      <ModalParent>
+        <ul className='fdAccountModal__modals__form__fieldset__ul' ref={registryRefReceiver} data-visible='false'>
+          <div className='fdAccountModal__modals__form__fieldset__ul__name'>
+            {[
+              { labelId: 'firstName', id: 'fdUserAccountFirstName', name: 'firstName', label: 'First name', isRequired: true, placeholder: 'John' },
+              { labelId: 'lastName', id: 'fdUserAccountLastName', name: 'lastName', label: 'Last name', isRequired: false, placeholder: 'Doe' },
+            ].map((field) => (
+              <li key={field.id}>
+                <label id={field.labelId} htmlFor={field.id}>
+                  {field.label}
+                </label>
+                <input
+                  form='fdRegistery'
+                  id={field.id}
+                  name={field.name}
+                  type='text'
+                  inputMode='text'
+                  size={12}
+                  required={field.isRequired}
+                  aria-required={field.isRequired ? 'true' : 'false'}
+                  aria-invalid={schema.safeParse(values.firstName).success}
+                  autoCapitalize='words'
+                  placeholder={field.placeholder}
+                  onPointerUp={() => focus()}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
+                />
+              </li>
+            ))}
+          </div>
+          {/* <div className='fdAccountModal__modals__form__fieldset__ul__dob'>
         <li>
           <label id='dobMonth' htmlFor='fdUserAccountDobMonth'>
             Month
@@ -156,113 +158,117 @@ const FDAccountRegistry = forwardRef<HTMLUListElement, Type_PropDrill>(({ ModalP
           </select>
         </li>
       </div> */}
-        <li className='fdAccountModal__modals__form__fieldset__ul__emailAddress'>
-          <label id='emailAddress' htmlFor='fdUserAccountEmailAddress'>
-            Email address
-          </label>
-          <input
-            form='fdRegistery'
-            id='fdUserAccountEmailAddress'
-            name='emailAddress'
-            type='email'
-            inputMode='email'
-            // name, @, domain
-            minLength={3}
-            // RFC 2045
-            maxLength={76}
-            size={12}
-            required={true}
-            aria-required='true'
-            aria-invalid={schema.safeParse(values.emailAddress).success}
-            autoCapitalize='off'
-            placeholder='johndoe@gmail.com'
-            onPointerUp={() => focus()}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
-          />
-        </li>
-        <div className='fdAccountModal__modals__form__fieldset__ul__password'>
-          <li>
-            <label id='password' htmlFor='fdUserAccountPassword'>
-              Password
+          <li className='fdAccountModal__modals__form__fieldset__ul__emailAddress'>
+            <label id='emailAddress' htmlFor='fdUserAccountEmailAddress'>
+              Email address
             </label>
             <input
               form='fdRegistery'
-              id='fdUserAccountPassword'
-              name='password'
-              type='password'
-              inputMode='text'
-              // RFC 5310, NIST Special Publication 800-63B
-              minLength={8}
-              // RFC XOS
-              maxLength={32}
+              id='fdUserAccountEmailAddress'
+              name='emailAddress'
+              type='email'
+              inputMode='email'
+              // name, @, domain
+              minLength={3}
+              // RFC 2045
+              maxLength={76}
               size={12}
               required={true}
               aria-required='true'
-              aria-invalid={schema.safeParse(values.password).success}
+              aria-invalid={schema.safeParse(values.emailAddress).success}
               autoCapitalize='off'
-              placeholder='••••••••'
+              placeholder='johndoe@gmail.com'
               onPointerUp={() => focus()}
               onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
             />
           </li>
-          <li>
-            <label id='passwordConfirmation' htmlFor='fdUserAccountPasswordConfirmation'>
-              Retype password
-            </label>
-            <input
-              form='fdRegistery'
-              id='fdUserAccountPasswordConfirmation'
-              name='passwordConfirmation'
-              type='password'
-              inputMode='text'
-              // RFC 5310, NIST Special Publication 800-63B
-              minLength={8}
-              // RFC XOS
-              maxLength={32}
-              size={12}
-              required={true}
-              aria-required='true'
-              aria-invalid={schema.safeParse(values.password).success}
-              autoCapitalize='off'
-              placeholder='••••••••'
-              onPointerUp={() => focus()}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
-            />
+          <div className='fdAccountModal__modals__form__fieldset__ul__password'>
+            <li>
+              <label id='password' htmlFor='fdUserAccountPassword'>
+                Password
+              </label>
+              <input
+                form='fdRegistery'
+                id='fdUserAccountPassword'
+                name='password'
+                type='password'
+                inputMode='text'
+                // RFC 5310, NIST Special Publication 800-63B
+                minLength={8}
+                // RFC XOS
+                maxLength={32}
+                size={12}
+                required={true}
+                aria-required='true'
+                aria-invalid={schema.safeParse(values.password).success}
+                autoCapitalize='off'
+                placeholder='••••••••'
+                onPointerUp={() => focus()}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
+              />
+            </li>
+            <li>
+              <label id='passwordConfirmation' htmlFor='fdUserAccountPasswordConfirmation'>
+                Retype password
+              </label>
+              <input
+                form='fdRegistery'
+                id='fdUserAccountPasswordConfirmation'
+                name='passwordConfirmation'
+                type='password'
+                inputMode='text'
+                // RFC 5310, NIST Special Publication 800-63B
+                minLength={8}
+                // RFC XOS
+                maxLength={32}
+                size={12}
+                required={true}
+                aria-required='true'
+                aria-invalid={schema.safeParse(values.password).success}
+                autoCapitalize='off'
+                placeholder='••••••••'
+                onPointerUp={() => focus()}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => valueSetter(e)}
+              />
+            </li>
+          </div>
+          <li className='fdAccountModal__modals__form__fieldset__ul__cta'>
+            <button id='fdUserAccountSubmitForm' aria-label='Submit registration form' onPointerUp={(e: React.PointerEvent<HTMLButtonElement>) => submitForm(e)}>
+              Complete sign up
+            </button>
+            <button
+              aria-label='Sign in with Google'
+              onPointerUp={(e) => {
+                e.preventDefault();
+                authorizeUser().google();
+              }}>
+              <svg xmlns='http://www.w3.org/2000/svg' width='0.98em' height='1em' viewBox='0 0 256 262'>
+                <path
+                  fill='#4285F4'
+                  d='M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027'></path>
+                <path
+                  fill='#34A853'
+                  d='M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1'></path>
+                <path
+                  fill='#FBBC05'
+                  d='M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z'></path>
+                <path
+                  fill='#EB4335'
+                  d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'></path>
+              </svg>
+            </button>
           </li>
-        </div>
-        <li className='fdAccountModal__modals__form__fieldset__ul__cta'>
-          <button id='fdUserAccountSubmitForm' aria-label='Submit registration form' onPointerUp={(e: React.PointerEvent<HTMLButtonElement>) => submitForm(e)}>
-            Complete sign up
-          </button>
-          <button
-            aria-label='Sign in with Google'
-            onPointerUp={(e) => {
-              e.preventDefault();
-              authorizeUser().google();
-            }}>
-            <svg xmlns='http://www.w3.org/2000/svg' width='0.98em' height='1em' viewBox='0 0 256 262'>
-              <path
-                fill='#4285F4'
-                d='M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027'></path>
-              <path
-                fill='#34A853'
-                d='M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1'></path>
-              <path
-                fill='#FBBC05'
-                d='M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z'></path>
-              <path
-                fill='#EB4335'
-                d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'></path>
-            </svg>
-          </button>
-        </li>
-        <li className='fdAccountModal__modals__form__fieldset__ul__signup'>
-          <button aria-label='Sign in' onPointerUp={() => setModal('signin')}>
-            Already a member? Sign in.
-          </button>
-        </li>
-      </ul>
-    </ModalParent>
+          <li className='fdAccountModal__modals__form__fieldset__ul__signup'>
+            <button aria-label='Sign in' onPointerUp={() => setModal('signin')}>
+              Already a member? Sign in.
+            </button>
+          </li>
+        </ul>
+      </ModalParent>
+      <div className='fdAccountModal__modals__btns'>
+        <LoginWithGroup />
+      </div>
+    </>
   );
 });
 
