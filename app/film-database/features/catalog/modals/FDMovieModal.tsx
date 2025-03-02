db4e9cd455f6e8de9e65d2373596ccd3f3eabd94
Justin <jbarkerpoc@gmail.com>
2025-03-01 18:18:25 -0600

    refactor(fdMovieModal): removal of watchProviders, importation of fdDetails

--- app/film-database/features/catalog/modals/FDMovieModal.tsx
+++ app/film-database/features/catalog/modals/FDMovieModal.tsx
@@ -3,33 +3,22 @@ import FDCarousel from '../../../components/carousel/FDCarousel';
 import { useTmdbFetcher, type Namespace_Tmdb } from '../../../composables/tmdb-api/hooks/useTmdbFetcher';
 import { useCatalogProvider } from '../../../context/CatalogContext';
 import FDiFrame from '../../../components/iframe/FDiFrame';
-import { tmdbMovieGenres } from '../../../composables/tmdb-api/data/tmdbGenres';
 import { usePaginateData } from '../../../hooks/usePaginateData';
-
-type ModalData = {
-  castCrew: ReturnType<typeof usePaginateData> | undefined;
-  watchProviders: Namespace_Tmdb.WatchProviders_Obj['watchProviders']['results'] | undefined;
-};
+import FDDetails from '../../../components/movie/FDDetails';
 
 const FDMovieModal = () => {
   const { heroData, isModalOpen, setIsModalOpen } = useCatalogProvider();
-  const [modalData, setModalData] = useState<ModalData>({
-    castCrew: undefined,
-    watchProviders: undefined,
-  });
+  const [castCrew, setCastCrew] = useState<ReturnType<typeof usePaginateData> | undefined>(undefined);
   const fdMovieModal = useRef<HTMLDivElement>(null);
 
-  // Fetch
   const fetch = async (): Promise<void> => {
-    if (!heroData) return;
     const castCrewResponse = (await useTmdbFetcher({ credits: (heroData as Namespace_Tmdb.BaseMedia_Provider).id })) as Namespace_Tmdb.Credits_Obj;
     const castCrew = usePaginateData(castCrewResponse);
-    // const watchProviders = (await useTmdbFetcher({ watchProviders: heroData.id })) as Namespace_Tmdb.WatchProviders_Obj;
-    setModalData({ castCrew: castCrew, watchProviders: undefined });
+    setCastCrew(castCrew);
   };
 
   useEffect(() => {
-    if (heroData) fetch();
+    fetch();
   }, [heroData]);
 
   const handleExteriorClicks = (event: PointerEvent) => {
@@ -38,48 +27,29 @@ const FDMovieModal = () => {
 
   useEffect(() => {
     const unmount = () => document.body.removeEventListener('pointerdown', handleExteriorClicks);
-
-    if (isModalOpen) document.body.addEventListener('pointerdown', handleExteriorClicks);
+    if (isModalOpen && fdMovieModal.current) document.body.addEventListener('pointerdown', handleExteriorClicks);
     else unmount();
-
     return () => unmount();
   }, [isModalOpen]);
 
-  if (!isModalOpen || !heroData || !modalData.castCrew || !modalData.watchProviders) return;
-
-  return (
-    <div className='fdMovieModal'>
-      <div className='fdMovieModal__container' ref={fdMovieModal}>
-        <div className='fdMovieModal__container__player'>
-          <FDiFrame />
-        </div>
-        <article className='fdMovieModal__article'>
-          <div className='fdMovieModal__article__container'>
-            <div className='fdMovieModal__article__container__wrapper'>
-              <span>Release Date: {new Date(heroData.release_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</span>
-            </div>
-            <ul className='fdMovieModal__article__container__wrapper'>
-              Genres:
-              {heroData?.genre_ids.map((genreId) => {
-                const genreName = Object.keys(tmdbMovieGenres).find((key) => tmdbMovieGenres[key as keyof typeof tmdbMovieGenres] === genreId);
-                return genreName ? <li key={`genreId-${genreId}`}>{genreName.replaceAll('_', ' ')}</li> : null;
-              })}
-            </ul>
-            {/* <ul className='fdMovieModal__article__container__wrapper'>{modalData.watchProviders}</ul> */}
+  if (isModalOpen && heroData && castCrew)
+    return (
+      <div className='fdMovieModal'>
+        <div className='fdMovieModal__container' ref={fdMovieModal}>
+          <section className='fdMovieModal__container__player'>
+            <FDiFrame />
+          </section>
+          <div className='fdMovieModal__container__article'>
+            <FDDetails modal={true} />
           </div>
-          <header>
-            <h2>{heroData?.title}</h2>
-          </header>
-          <p>{heroData?.overview}</p>
-        </article>
-        <div className='fdMovieModal__container__castCrew'>
-          {modalData.castCrew.map(([key, value], index) => (
-            <FDCarousel type={key === 'cast' ? 'cast' : 'crew'} mapIndex={index} heading={key} data={value} key={`${key}-${index}`} />
-          ))}
+          <section className='fdMovieModal__container__castCrew'>
+            {castCrew.map(([key, value], index) => (
+              <FDCarousel type={key === 'cast' ? 'cast' : 'crew'} mapIndex={index} heading={key} data={value} key={`${key}-${index}`} />
+            ))}
+          </section>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default FDMovieModal;
