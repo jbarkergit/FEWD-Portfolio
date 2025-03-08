62639080dd930244aa1ac2a208a60158fa9e6d50
Justin <jbarkerpoc@gmail.com>
2025-03-08 17:35:51 -0600

    feat(modal): shared container/wrapper for modals

---
+++ app/film-database/features/catalog/modals/FDModal.tsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect } from 'react';
+import { useCatalogProvider } from '~/film-database/context/CatalogContext';
+import FDMovieModal from './FDMovieModal';
+import FDMovieList from './FDMovieList';
+
+const FDModal = () => {
+  // Context
+  const { isMovieModal, setIsMovieModal, isListModal, setIsListModal } = useCatalogProvider();
+
+  // References
+  const modal = useRef<HTMLDivElement>(null);
+
+  /**
+   * @function handleExteriorClicks
+   * @returns void
+   * Sets modal state (in context) to false when the user isn't directly interacting with modal
+   */
+  const handleExteriorClicks = (event: PointerEvent): void => {
+    if (!modal.current?.contains(event.target as Node)) {
+      if (isMovieModal) setIsMovieModal(false);
+      if (isListModal) setIsListModal(false);
+    }
+  };
+
+  /**
+   * @function mountListeners
+   * @returns (() => void
+   * Mount event listeners for @handleExteriorClicks
+   */
+  const mountListeners = (): (() => void) => {
+    const unmount = () => document.body.removeEventListener('pointerup', handleExteriorClicks);
+
+    if (modal.current) {
+      if (isMovieModal || isListModal) {
+        document.body.addEventListener('pointerup', handleExteriorClicks);
+      }
+    } else unmount();
+
+    return () => unmount();
+  };
+
+  useEffect(() => mountListeners(), [isMovieModal, isListModal]);
+
+  /** JSX */
+  if (isMovieModal || isListModal)
+    return (
+      <div className='fdMovieModal'>
+        <div className='fdMovieModal__container' ref={modal}>
+          {isMovieModal && <FDMovieModal />}
+          {isListModal && <FDMovieList />}
+        </div>
+      </div>
+    );
+};
+
+export default FDModal;
