9514e9bd6629e0956ccdcaa8d22108a7557ca18c
Justin <jbarkerpoc@gmail.com>
2025-03-10 01:05:14 -0500

    feat(getFirestoreCollection): init

--- app/base/firebase/firestore/hooks/getFirestoreCollection.tsx
+++ app/base/firebase/firestore/hooks/getFirestoreCollection.tsx
@@ -1,15 +1,19 @@
 import { getDocs, collection } from 'firebase/firestore';
-import { database } from '../../config/firebaseConfig';
+import { database, firebaseAuth } from '../../config/firebaseConfig';
+import isUserAuthorized from '../../authentication/hooks/isUserAuthorized';
 
-const getFirestoreCollection = async (docs: string) => {
+export async function getFirestoreCollection(docs: string) {
+  if (!isUserAuthorized) return undefined;
+
   try {
     const querySnapshot = await getDocs(collection(database, docs));
-    if (!querySnapshot) throw new Error();
+    if (!querySnapshot) throw new Error('Failure to retrieve collection from Firestore.');
     const users = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data }));
     return users;
   } catch (error) {
     console.error(error);
+    return undefined;
   }
-};
+}
 
 export default getFirestoreCollection;
