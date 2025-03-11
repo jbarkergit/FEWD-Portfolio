cb92367879189072df24e0de8751b97d387ca203
Justin <jbarkerpoc@gmail.com>
2025-03-11 01:26:13 -0500

    refactor(createFirestoreCollectionUser): auth verification

--- app/base/firebase/firestore/hooks/useFirestoreCollection.tsx
+++ app/base/firebase/firestore/hooks/useFirestoreCollection.tsx
@@ -2,17 +2,16 @@ import { getDocs, collection } from 'firebase/firestore';
 import { database } from '../../config/firebaseConfig';
 import isUserAuthorized from '../../authentication/utility/isUserAuthorized';
 
-export async function useFirestoreCollection(docs: string) {
-  const isUserAuth = await isUserAuthorized();
-  if (!isUserAuth) return undefined;
-
-  try {
-    const querySnapshot = await getDocs(collection(database, docs));
-    if (!querySnapshot) throw new Error('Failure to retrieve collection from Firestore.');
-    const users = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data }));
-    return users;
-  } catch (error) {
-    console.error(error);
-    return undefined;
+export async function useFirestoreCollection(col: string) {
+  if (await isUserAuthorized()) {
+    try {
+      const querySnapshot = await getDocs(collection(database, col));
+      if (!querySnapshot) throw new Error('Failure to retrieve collection from Firestore.');
+      const users = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data }));
+      return users;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   }
 }
