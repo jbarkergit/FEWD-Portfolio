03cef66abed714102b57457915c405ee31dbb378
Justin <jbarkerpoc@gmail.com>
2025-03-11 01:26:37 -0500

    refactor(createFirestoreCollectionUser): auth verification

--- app/base/firebase/firestore/utility/createFirestoreCollectionUser.ts
+++ app/base/firebase/firestore/utility/createFirestoreCollectionUser.ts
@@ -1,37 +1,36 @@
 import { doc, DocumentReference, DocumentSnapshot, getDoc, setDoc, type DocumentData } from 'firebase/firestore';
 import { database, firebaseAuth } from '../../config/firebaseConfig';
 import type { User } from 'firebase/auth';
+import isUserAuthorized from '../../authentication/utility/isUserAuthorized';
+
+export type Firestore_User_Document = {
+  email: string | null;
+  verified: boolean;
+};
 
 export async function createFirestoreCollectionUser() {
-  try {
-    // Get user
-    const user: User | null = firebaseAuth.currentUser;
-    if (!user) throw new Error('User missing.');
+  if (await isUserAuthorized()) {
+    try {
+      // Get user
+      const user: User | null = firebaseAuth.currentUser;
+      if (!user) throw new Error('User missing.');
 
-    // Document name
-    const documentName: string = 'users';
+      // Reference to 'users' collection and the user's document
+      const userDocRef: DocumentReference<DocumentData, DocumentData> = doc(database, 'users', user.uid);
+      const userDoc: DocumentSnapshot<unknown, DocumentData> = await getDoc(userDocRef);
 
-    // Reference to 'users' collection and the user's document
-    const userDocRef: DocumentReference<DocumentData, DocumentData> = doc(database, documentName, user.uid);
-    const userDoc: DocumentSnapshot<unknown, DocumentData> = await getDoc(userDocRef);
+      // If document doesn't exist, create it
+      if (!userDoc.exists()) {
+        const newUserDocument: Firestore_User_Document = {
+          email: user.email,
+          verified: user.emailVerified,
+        };
 
-    // If document doesn't exist, create it
-    if (!userDoc.exists()) {
-      // New user object
-      type NewUser = {
-        email: string | null;
-        verified: boolean;
-      };
-
-      const newUser: NewUser = {
-        email: user.email,
-        verified: user.emailVerified,
-      };
-
-      // Document creation
-      await setDoc(userDocRef, newUser);
+        // Document creation
+        await setDoc(userDocRef, newUserDocument);
+      }
+    } catch (error) {
+      console.error(`Failure to fetch or create user document: ${error}`);
     }
-  } catch (error) {
-    console.error(`Failure to fetch or create user document: ${error}`);
   }
 }
