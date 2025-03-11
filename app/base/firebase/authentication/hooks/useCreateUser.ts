62bd293fa2bfd4fa9b37f1969312908efb9ed3e9
Justin <jbarkerpoc@gmail.com>
2025-03-11 01:57:35 -0500

    chore(useCreateUser): removal of firestore collection func invoke

--- app/base/firebase/authentication/hooks/useCreateUser.ts
+++ app/base/firebase/authentication/hooks/useCreateUser.ts
@@ -1,11 +1,9 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { firebaseAuth } from '../../config/firebaseConfig';
-import { createFirestoreCollectionUser } from '../../firestore/utility/createFirestoreCollectionUser';
 
 export async function useCreateUser(emailAddress: string, password: string) {
   try {
     await createUserWithEmailAndPassword(firebaseAuth, emailAddress, password);
-    await createFirestoreCollectionUser();
   } catch (error) {
     console.error(error);
   }
