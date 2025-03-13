b982f8d7b9b66cf393ccad7d9ce87fb2360b44fc
Justin <jbarkerpoc@gmail.com>
2025-03-13 02:12:06 -0500

    fix(useFirestore): updateDocumentMovies removal

--- app/base/firebase/firestore/hooks/useFirestore.ts
+++ app/base/firebase/firestore/hooks/useFirestore.ts
@@ -86,7 +86,7 @@ const updateDocumentMovies = async (collectionName: Firestore_CollectionNames, m
     const docRef: DocumentReference<DocumentData, DocumentData> = doc(database, collectionName, user.uid);
 
     // Create new movies object
-    const userMovies = movies.concat ? [...userDoc.movies, movies.movieId] : userDoc.movies.filter((id) => id === movies.movieId);
+    const userMovies = movies.concat ? [...userDoc.movies, movies.movieId] : userDoc.movies.filter((id) => id !== movies.movieId);
 
     // Overwrite existing document
     await updateDoc(docRef, { ...userDoc, movies: userMovies });
