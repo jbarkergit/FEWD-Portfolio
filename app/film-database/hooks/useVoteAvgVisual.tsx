fb08fe77e01c081ed6c80955d13bd210fec4d5c7
Justin <jbarkerpoc@gmail.com>
2025-03-13 16:13:43 -0500

    chore(useVoteAvgVisual): general cleanup, full jsx.element return

--- app/film-database/hooks/useVoteAvgVisual.tsx
+++ app/film-database/hooks/useVoteAvgVisual.tsx
@@ -5,7 +5,7 @@ import { EmptyStar, FullStar, HalfStar } from '../assets/google-material-symbols
  * @function useVoteAvgVisual
  * @returns Vote average in svg star visual reference
  */
-const useVoteAvgVisual = (voteAvg: number): JSX.Element[] | undefined => {
+const useVoteAvgVisual = (voteAvg: number): JSX.Element | undefined => {
   // 0-10 vote scale (contains floating point value) floored and converted to 0-5 vote scale
   const flooredVoteAverage: number = Math.floor(voteAvg / 2);
 
@@ -17,9 +17,17 @@ const useVoteAvgVisual = (voteAvg: number): JSX.Element[] | undefined => {
   const halfStars: 1 | 0 = hasFloatingValue ? 1 : 0;
   const emptyStars: number = maxStars - fullStars - halfStars;
 
-  return [...Array(fullStars).fill(<FullStar />), ...Array(halfStars).fill(<HalfStar />), ...Array(emptyStars).fill(<EmptyStar />)].map((Star, index) => {
-    return <span key={`vote-average-star-${index}`}>{Star}</span>;
-  });
+  const stars: JSX.Element[] = [...Array(fullStars).fill(<FullStar />), ...Array(halfStars).fill(<HalfStar />), ...Array(emptyStars).fill(<EmptyStar />)];
+
+  return (
+    <ul className='voteAvgVisual' aria-label={`Vote Average ${voteAvg / 2} out of 5`}>
+      {stars.map((Star, index) => (
+        <li className='voteAvgVisual__star' key={`star-${index}`}>
+          {Star}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default useVoteAvgVisual;
