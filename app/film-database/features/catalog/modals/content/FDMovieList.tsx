aee55346bfc245fda0312090c9229c7b122f3f51
Justin <jbarkerpoc@gmail.com>
2025-03-13 21:42:31 -0500

    fix/docs/feat(movieList): fix fetchMovies dupe, doc funcs, handleSelect, fixed semantics

--- app/film-database/features/catalog/modals/content/FDMovieList.tsx
+++ app/film-database/features/catalog/modals/content/FDMovieList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { useFirestore, type Firestore_UserDocument } from '~/base/firebase/firestore/hooks/useFirestore';
 import { type Namespace_Tmdb } from '~/film-database/composables/tmdb-api/hooks/useTmdbFetcher';
@@ -12,7 +12,15 @@ const FDMovieList = () => {
   const [movies, setMovies] = useState<Namespace_Tmdb.BaseMedia_Provider[]>([]);
   const { isListModal } = useCatalogProvider();
 
-  const fetchMovies = async () => {
+  /**
+   * @function fetchMovies
+   * @returns Promise<void>
+   * Utilizes primaryData from loaderData to prevent an api call
+   * Flattens primary data from useLoaderData
+   * Gets user document from Firestore
+   * Filters flattenedPrimaryData to retrieve a list of user's saved movies
+   */
+  const fetchMovies = async (): Promise<void> => {
     // Flatten primaryData from loaderData
     if (!flattenedPrimaryData) {
       flattenedPrimaryData = (primaryData as Namespace_Tmdb.Response_Union[]).flatMap((entry) =>
@@ -26,7 +34,9 @@ const FDMovieList = () => {
 
     // Get, filter and set movies from clientLoader's primaryData to prevent api call
     const movieSet: Set<number> = new Set(collection.movies);
-    const moviesArr: Namespace_Tmdb.BaseMedia_Provider[] = flattenedPrimaryData.filter((movie) => movieSet.has(movie.id));
+    const moviesArr: Namespace_Tmdb.BaseMedia_Provider[] = flattenedPrimaryData.filter(
+      (movie, index, self) => movieSet.has(movie.id) && self.findIndex((m) => m.id === movie.id) === index
+    );
     setMovies(moviesArr);
   };
 
@@ -35,32 +45,44 @@ const FDMovieList = () => {
     if (isListModal) fetchMovies();
   }, [isListModal]);
 
-  // JSX
+  /**
+   * @function handleSelect
+   * @returns void
+   * Creates an array of ulRef's children, iterates to enable/disable data-attr
+   */
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const handleSelect = (index: number): void => {
+    // Get all unordered list elements
+    if (!ulRef.current) return;
+    const elements: Element[] = [...ulRef.current.children];
+
+    // Iterate elements to disable/enable data-attr
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].setAttribute('data-vis', i === index ? 'true' : 'false');
+    }
+  };
+
+  /** @returns */
   return movies.length ? (
-    <ul className='fdMovieModal__ul'>
-      {movies.map((movie) => (
-        <button onPointerUp={() => {}}>
-          <li key={`movie-list-key-${movie}`}>
-            <picture>
-              <img
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w780/${movie.poster_path}`
-                    : `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg`
-                }
-                alt={`${movie.title}`}
-                fetchPriority={'high'}
-              />
-            </picture>
-            <header>
-              <h2>{movie.title}</h2>
-              {useFormattedDate(movie.release_date)}
-              {useVoteAvgVisual(movie.vote_average)}
-            </header>
+    <div className='fdMovieModal__carousel'>
+      <ul ref={ulRef}>
+        {movies.map((movie, index) => (
+          <li key={`movie-list-key-${movie.id}`} data-vis={index === 0 ? 'true' : 'false'}>
+            <button onPointerUp={() => handleSelect(index)}>
+              <picture>
+                <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={`${movie.title}`} fetchPriority={'high'} />
+              </picture>
+            </button>
           </li>
-        </button>
-      ))}
-    </ul>
+        ))}
+      </ul>
+      <article>
+        <header>{/* <h2>{movie.title}</h2> */}</header>
+        {/* {useFormattedDate(movie.release_date)} */}
+        {/* {useVoteAvgVisual(movie.vote_average)} */}
+      </article>
+    </div>
   ) : (
     <p>Whoops! It appears you haven't saved a movie yet!</p>
   );
