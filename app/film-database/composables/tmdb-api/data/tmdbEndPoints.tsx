58b802fc8aa025ee2bb76f4c38baa5d87f7b620a
Justin <jbarkerpoc@gmail.com>
2025-03-16 11:43:28 -0500

    chore(tmdbEndPoints): simplified strings for legibility

--- app/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
+++ app/film-database/composables/tmdb-api/data/tmdbEndPoints.tsx
@@ -16,25 +16,27 @@ export namespace Namespace_TmdbEndpointsKeys {
 }
 
 /** Endpoint Collection */
+const tmdb: string = 'https://api.themoviedb.org/3/';
+
 export const tmdbEndpoints: Namespace_TmdbEndpointsKeys.Endpoint_Obj = {
   prefabs: {
-    now_playing: 'https://api.themoviedb.org/3/movie/now_playing',
-    upcoming: 'https://api.themoviedb.org/3/movie/upcoming',
-    popular: 'https://api.themoviedb.org/3/movie/popular',
-    top_rated: 'https://api.themoviedb.org/3/movie/top_rated',
-    trending_today: 'https://api.themoviedb.org/3/trending/movie/day',
-    trending_this_week: 'https://api.themoviedb.org/3/trending/movie/week',
+    now_playing: tmdb + 'movie/now_playing',
+    upcoming: tmdb + 'movie/upcoming',
+    popular: tmdb + 'movie/popular',
+    top_rated: tmdb + 'movie/top_rated',
+    trending_today: tmdb + 'trending/movie/day',
+    trending_this_week: tmdb + 'trending/movie/week',
   },
   movieId: {
-    details: 'https://api.themoviedb.org/3/movie/{movie_id}',
-    credits: 'https://api.themoviedb.org/3/movie/{movie_id}/credits',
-    videos: 'https://api.themoviedb.org/3/movie/{movie_id}/videos',
-    watchProviders: 'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers',
-    reviews: 'https://api.themoviedb.org/3/movie/{movie_id}/reviews',
-    recommendations: 'https://api.themoviedb.org/3/movie/{movie_id}/recommendations',
+    details: tmdb + 'movie/{movie_id}',
+    credits: tmdb + 'movie/{movie_id}/credits',
+    videos: tmdb + 'movie/{movie_id}/videos',
+    watchProviders: tmdb + 'movie/{movie_id}/watch/providers',
+    reviews: tmdb + 'movie/{movie_id}/reviews',
+    recommendations: tmdb + 'movie/{movie_id}/recommendations',
   },
   miscellaneous: {
-    discover: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&with_genres=',
-    search: 'https://api.themoviedb.org/3/search/movie&query={search_term}&include_adult=false&language=en-US',
+    discover: tmdb + 'discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&with_genres=',
+    search: tmdb + 'search/movie&query={search_term}&include_adult=false&language=en-US',
   },
 };
