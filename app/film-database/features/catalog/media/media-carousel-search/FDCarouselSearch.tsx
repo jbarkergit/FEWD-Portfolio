bd71536833c474928dd6d3fe6ecfac55782c6f40
Justin <jbarkerpoc@gmail.com>
2025-03-16 12:43:19 -0500

    chore(carouselSearch): misc

--- app/film-database/features/catalog/media/media-carousel-search/FDCarouselSearch.tsx
+++ app/film-database/features/catalog/media/media-carousel-search/FDCarouselSearch.tsx
@@ -1,5 +1,5 @@
 // Deps
-import { useState, useEffect, useRef, type ChangeEvent, useCallback } from 'react';
+import { useState, useEffect, useRef, type ChangeEvent } from 'react';
 // Context
 import { useCatalogProvider } from '../../../../context/CatalogContext';
 // Composables
@@ -39,13 +39,13 @@ const FDCarouselSearch = () => {
    * @returns Promise<void>
    * Invoke debounced fetch
    */
-  const invokeFetch = useCallback(async () => {
+  const invokeFetch = async () => {
     if (searchTerm.length > 0) {
       setIsTyping(false);
       const data = (await useTmdbFetcher({ search: searchTerm })) as Namespace_Tmdb.Search_Obj;
       if (data) setSearchResults(data.search.results);
     }
-  }, [searchTerm]);
+  };
 
   useEffect(() => {
     if (searchTerm.length === 0) setSearchResults([]);
@@ -73,7 +73,7 @@ const FDCarouselSearch = () => {
             htmlFor='fdSearchBar__fieldset__input'
             data-opacity={searchTerm.length > 0 ? 'hidden' : 'barelyVisible'}
             ref={labelRef}>
-            <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='3em' height='1em' viewBox='0 0 24 24'>
               <path
                 fill='currentColor'
                 d='M15.5 14h-.79l-.28-.27A6.47 6.47 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14'></path>
