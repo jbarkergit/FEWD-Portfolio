5a96ad8a300300fdaa75a2de46c7fd6f92d8a481
Justin <jbarkerpoc@gmail.com>
2025-03-17 08:05:17 -0500

    wip/feat(userCarousel): init

---
+++ app/film-database/components/carousel/FDUserCarousel.tsx
@@ -0,0 +1,121 @@
+import { useRef, useEffect, useState } from 'react';
+import { TablerCategoryFilled, TablerCategoryPlus } from '~/film-database/assets/google-material-symbols/GoogleMaterialIcons';
+import type { Namespace_Tmdb } from '~/film-database/composables/tmdb-api/hooks/useTmdbFetcher';
+
+const FDUserCarousel = ({ header, data, display }: { header?: string; data: Namespace_Tmdb.BaseMedia_Provider[] | undefined[]; display: 'flex' | 'grid' }) => {
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  /** Carousel interactivity */
+  const [layout, setLayout] = useState<'flex' | 'grid'>(display);
+  let isInteract: boolean = false;
+  let isDragging: boolean = false;
+
+  /**
+   * @function pointerDown
+   */
+  const pointerDown = (): void => {
+    isInteract = true;
+  };
+
+  /**
+   * @function pointerMove
+   */
+  let timeoutId: NodeJS.Timeout;
+
+  const pointerMove = (): void => {
+    if (isInteract) {
+      // Debounce isDragging to allow for minor mouse movement post click to allow for wiggle room
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => (isDragging = true), 50);
+    }
+  };
+
+  /**
+   * @function pointerLeave
+   */
+  const pointerLeave = (): void => {
+    if (isDragging) {
+      clearTimeout(timeoutId);
+      isInteract = false;
+      isDragging = false;
+    }
+  };
+
+  /**
+   * @function pointerUp
+   */
+  const pointerUp = (event: PointerEvent): void => {
+    clearTimeout(timeoutId);
+    if (!isDragging) animateListItems(event);
+
+    isInteract = false;
+    isDragging = false;
+  };
+
+  useEffect(() => {
+    if (header && ulRef.current) {
+      ulRef.current.addEventListener('pointerdown', pointerDown);
+      ulRef.current.addEventListener('pointermove', pointerMove);
+      ulRef.current.addEventListener('pointerleave', pointerLeave);
+      ulRef.current.addEventListener('pointerup', pointerUp);
+    }
+
+    return () => {
+      if (header && ulRef.current) {
+        ulRef.current.removeEventListener('pointerdown', pointerDown);
+        ulRef.current.removeEventListener('pointermove', pointerMove);
+        ulRef.current.removeEventListener('pointerleave', pointerLeave);
+        ulRef.current.removeEventListener('pointerup', pointerUp);
+      }
+    };
+  }, []);
+
+  /**
+   * @function animateListItems
+   * Scales all list items and applies filters to their images
+   */
+  function animateListItems(event: PointerEvent) {
+    if (!isDragging) {
+      const listItems: Element[] | null = ulRef.current ? [...ulRef.current.children] : null;
+
+      if (listItems) {
+        const element: Node | null = !isDragging ? (event.target as Node) : null;
+        const elementIndex: number = listItems.findIndex((item) => item === element);
+
+        if (element && elementIndex !== -1) {
+          // Iterate ulRef.current.children to disable/enable data-attr
+          for (let i = 0; i < listItems.length; i++) {
+            listItems[i].setAttribute('data-vis', i === elementIndex ? 'true' : 'false');
+          }
+        }
+      }
+    }
+  }
+
+  if (data.length > 0)
+    return (
+      <section className='fdUserList__collection'>
+        {header ? (
+          <header>
+            <TablerCategoryFilled />
+            <h2>{header}</h2>
+          </header>
+        ) : (
+          <button className='fdUserList__collection--create' aria-label='Create new list'>
+            <TablerCategoryPlus />
+            <h2>Create new list</h2>
+          </button>
+        )}
+
+        <ul ref={ulRef} data-layout={layout}>
+          {data.map((movie, index) => (
+            <li key={`movie-list-key-${movie ? movie.id : index}`} data-vis={index === 0 ? 'true' : 'false'}>
+              <picture>{movie && <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={`${movie.title}`} fetchPriority={'high'} />}</picture>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+};
+
+export default FDUserCarousel;
