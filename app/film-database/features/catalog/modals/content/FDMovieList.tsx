728a8f195be00c93fcd5591cc8fa34281b315378
Justin <jbarkerpoc@gmail.com>
2025-03-17 08:05:05 -0500

    wip(movieList)

--- app/film-database/features/catalog/modals/content/FDMovieList.tsx
+++ app/film-database/features/catalog/modals/content/FDMovieList.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useReducer, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { useFirestore, type Firestore_UserDocument } from '~/base/firebase/firestore/hooks/useFirestore';
+import FDUserCarousel from '~/film-database/components/carousel/FDUserCarousel';
 import { type Namespace_Tmdb } from '~/film-database/composables/tmdb-api/hooks/useTmdbFetcher';
 import { useCatalogProvider } from '~/film-database/context/CatalogContext';
 import useFormattedDate from '~/film-database/hooks/useFormattedDate';
@@ -10,8 +11,7 @@ const FDMovieList = () => {
   const { primaryData } = useLoaderData();
   let flattenedPrimaryData: Namespace_Tmdb.BaseMedia_Provider[] | undefined = undefined;
   const [movies, setMovies] = useState<Namespace_Tmdb.BaseMedia_Provider[]>([]);
-  const { isListModal } = useCatalogProvider();
-  const ulRef = useRef<HTMLUListElement>(null);
+  const { isListModal, maxCarouselNodes } = useCatalogProvider();
 
   /**
    * @function fetchMovies
@@ -46,96 +46,10 @@ const FDMovieList = () => {
     if (isListModal) fetchMovies();
   }, [isListModal]);
 
-  /** Carousel interactivity
-   * @function pointerDown
-   *
-   * @function pointerMove
-   *
-   * @function pointerLeave
-   *
-   * @function pointerUp
-   *
-   */
-  let isInteract: boolean = false;
-  let isDragging: boolean = false;
-
-  const pointerDown = () => {
-    isInteract = true;
-  };
-
-  const pointerMove = () => {
-    if (isInteract) {
-      isDragging = true;
-    }
-  };
-
-  const pointerLeave = () => {
-    if (isDragging) {
-      isInteract = false;
-      isDragging = false;
-    }
-  };
-
-  const pointerUp = (event: PointerEvent) => {
-    if (!isDragging) animateListItems(event);
-
-    isInteract = false;
-    isDragging = false;
-  };
-
-  useEffect(() => {
-    ulRef.current?.addEventListener('pointerdown', pointerDown);
-    ulRef.current?.addEventListener('pointermove', pointerMove);
-    ulRef.current?.addEventListener('pointerleave', pointerLeave);
-    ulRef.current?.addEventListener('pointerup', pointerUp);
-
-    return () => {
-      ulRef.current?.removeEventListener('pointerdown', pointerDown);
-      ulRef.current?.removeEventListener('pointermove', pointerMove);
-      ulRef.current?.removeEventListener('pointerleave', pointerLeave);
-      ulRef.current?.removeEventListener('pointerup', pointerUp);
-    };
-  }, []);
-
-  /**
-   * @function animateListItems
-   * Scales all list items and applies filters to their images
-   */
-  function animateListItems(event: PointerEvent) {
-    if (!isDragging) {
-      const listItems: Element[] | null = ulRef.current ? [...ulRef.current.children] : null;
-
-      if (listItems) {
-        const element: Node | null = !isDragging ? (event.target as Node) : null;
-        const elementIndex: number = listItems.findIndex((item) => item === element);
-
-        if (element && elementIndex !== -1) {
-          // Iterate ulRef.current.children to disable/enable data-attr
-          for (let i = 0; i < listItems.length; i++) {
-            listItems[i].setAttribute('data-vis', i === elementIndex ? 'true' : 'false');
-          }
-        }
-      }
-    }
-  }
-
-  /** @returns */
   return (
     <div className='fdUserList'>
-      <section className='fdUserList__collection'>
-        <header>
-          <h2>Uncategorized Movies</h2>
-        </header>
-        <ul ref={ulRef}>
-          {movies.map((movie, index) => (
-            <li key={`movie-list-key-${movie.id}`} data-vis={index === 0 ? 'true' : 'false'}>
-              <picture>
-                <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={`${movie.title}`} fetchPriority={'high'} />
-              </picture>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <FDUserCarousel header={'Uncategorized Movies'} data={movies} display='grid' />
+      <FDUserCarousel data={Array(maxCarouselNodes + 1).fill(undefined)} display='flex' />
     </div>
   );
 };
