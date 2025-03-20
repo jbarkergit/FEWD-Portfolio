1e9050163bb9397e746f6af2389836dc154143db
Justin <jbarkerpoc@gmail.com>
2025-03-20 00:58:02 -0500

    chore(svg/icons): removal of user list text icon

--- app/film-database/assets/svg/icons.tsx
+++ app/film-database/assets/svg/icons.tsx
@@ -277,18 +277,3 @@ export function TablerEyeOff(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
-
-export function TablerCursorText(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' {...props}>
-      <path
-        fill='none'
-        stroke='currentColor'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        d='M10 12h4M9 4a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3m6-16a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3'
-      />
-    </svg>
-  );
-}
