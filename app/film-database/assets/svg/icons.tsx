da2902b5c05c01f27c0ff3c546345d5bd6c50c41
Justin <jbarkerpoc@gmail.com>
2025-03-21 22:54:38 -0500

    chore(assets/svg): spinner ring resize dimension removal

--- app/film-database/assets/svg/icons.tsx
+++ app/film-database/assets/svg/icons.tsx
@@ -33,7 +33,7 @@ export function Exit(props: SVGProps<SVGSVGElement>) {
 
 export function SvgSpinnersRingResize(props: SVGProps<SVGSVGElement>): JSX.Element {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' {...props}>
       <g stroke='currentColor'>
         <circle cx='12' cy='12' r='9.5' fill='none' strokeLinecap='round' strokeWidth='3'>
           <animate
