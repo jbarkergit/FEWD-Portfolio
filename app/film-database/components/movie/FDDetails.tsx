37064e3aa05b03828467c04c2318a60f5292fd56
Justin <jbarkerpoc@gmail.com>
2025-04-23 02:41:48 -0500

    chore(details): moved useFormattedDate hook to local scope

--- app/film-database/components/movie/FDDetails.tsx
+++ app/film-database/components/movie/FDDetails.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, type JSX } from 'react';
 import { Link } from 'react-router';
 // Context
 import { useCatalogProvider } from '../../context/CatalogContext';
@@ -11,7 +11,6 @@ import useVoteAvgVisual from '~/film-database/hooks/useVoteAvgVisual';
 import { tmdbMovieGenres } from '~/film-database/composables/tmdb-api/data/tmdbGenres';
 // Assets
 import { Exit, MaterialSymbolsHeartPlus, MaterialSymbolsLightHeartMinusRounded, SvgSpinnersRingResize, TheMovieDatabaseLogo } from '../../assets/svg/icons';
-import useFormattedDate from '~/film-database/hooks/useFormattedDate';
 
 const FDDetails = (modal: { modal: boolean }) => {
   const { heroData, setIsMovieModal } = useCatalogProvider();
@@ -58,6 +57,37 @@ const FDDetails = (modal: { modal: boolean }) => {
   // useEffect(() => {
   //   getMovieBtn();
   // }, [heroData]);
+
+  /**
+   * @function useFormattedDate
+   * @returns Movie release date OR 'Now Available'
+   * Compares current date to movie release date to determine if the film has been released
+   */
+  const useFormattedDate = (release: string): JSX.Element => {
+    // Handle dates
+    const currentDate: string = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+    const releaseDate: string = release.replaceAll('-', '');
+
+    // Convert releaseDate to Date object
+    const releaseDateObj = new Date(`${releaseDate.slice(0, 4)}-${releaseDate.slice(4, 6)}-${releaseDate.slice(6, 8)}`);
+
+    // Return JSX.Element indicating release status
+    if (releaseDate > currentDate) {
+      return (
+        <div className='formattedReleaseDate'>{`Available ${releaseDateObj.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}`}</div>
+      );
+    }
+
+    return (
+      <div className='formattedReleaseDate' data-status='green'>
+        {'Now Available'}
+      </div>
+    );
+  };
 
   /** @returns */
   return (
