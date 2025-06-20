44c58e91b75aff99fc934937d9922ab86b2cf8dc
jbarker <121387493+jbarkergit@users.noreply.github.com>
2025-06-20 01:43:29 -0700

    feat(accountModalPoster): random poster queue with fade effect

--- app/film-database/features/account/features/FDAccountModalPoster.tsx
+++ app/film-database/features/account/features/FDAccountModalPoster.tsx
@@ -1,39 +1,68 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, type CSSProperties } from 'react';
+import { array } from 'zod/v4';
 import type { Namespace_Tmdb } from '~/film-database/composables/tmdb-api/hooks/useTmdbFetcher';
 
 const FDAccountModalPoster = ({ films }: { films: Namespace_Tmdb.BaseMedia_Provider[] }) => {
-  const randomPoster = (): Namespace_Tmdb.BaseMedia_Provider => films[Math.floor(Math.random() * films.length)];
+  /** @state */
+  const [posters, setPosters] = useState<Namespace_Tmdb.BaseMedia_Provider[]>([]);
 
-  const [previousPoster, setPreviousPoster] = useState<Namespace_Tmdb.BaseMedia_Provider>(randomPoster);
-  const [poster, setPoster] = useState<Namespace_Tmdb.BaseMedia_Provider>(randomPoster);
-  const [dissolve, setDissolve] = useState(false);
+  /** @ref */
+  const indexQueue = useRef<number[]>([]);
 
-  const previousRef = useRef<HTMLPictureElement>(null);
-  const currentRef = useRef<HTMLPictureElement>(null);
+  /**
+   * @function generateOrShuffleIndexes()
+   * @description Generate or shuffle randomly selected indexes within bounds of the films array.
+   */
+  const generateOrShuffleIndexes = (): void => {
+    let nextQueue: number[] = [...indexQueue.current];
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setDissolve(false); // Reset dissolve
-  //     setPreviousPoster(poster);
-  //     setPoster(randomPoster());
-  //     setDissolve(true); // Trigger dissolve effect
-  //   }, 6000);
+    // If index queue length is equal to or greater than the even films length, shuffle the queue via Fisher-Yates shuffle algorithm
+    if (indexQueue.current.length >= films.length) {
+      for (let i = nextQueue.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [nextQueue[i], nextQueue[j]] = [nextQueue[j], nextQueue[i]];
+      }
+    }
+    // Generate unique indexes until the queue length matches the even films length
+    else {
+      while (nextQueue.length < films.length) {
+        const randomIndex = Math.floor(Math.random() * films.length);
+        if (!nextQueue.includes(randomIndex)) nextQueue.push(randomIndex);
+      }
+    }
 
-  //   setDissolve(true); // Initial dissolve
+    indexQueue.current = nextQueue;
+  };
 
-  //   return () => clearInterval(interval);
-  // }, [poster, films]);
+  /**
+   * @function createPosters
+   * @description Generate a random queue of film posters for the fade effect.
+   */
+  const createPosters = (): void => {
+    generateOrShuffleIndexes();
+    let tempPosterQueue: typeof posters = [];
+    for (let i = 0; i < films.length; i++) tempPosterQueue.push(films[indexQueue.current[i]]);
+    setPosters(tempPosterQueue);
+  };
 
+  useEffect(() => {
+    createPosters();
+    setInterval(() => createPosters(), films.length * 7500);
+  }, [films]);
+
+  /** @JSX */
   return (
     <div className='fdAccount__container__wrapper'>
       <picture>
-        <img
-          src={`https://image.tmdb.org/t/p/original/${previousPoster?.poster_path}`}
-          alt={`${previousPoster?.title}`}
-          fetchPriority={'high'}
-          data-dissolve={dissolve}
-        />
-        <img src={`https://image.tmdb.org/t/p/original/${poster.poster_path}`} alt={`${poster.title}`} fetchPriority={'high'} data-dissolve={!dissolve} />
+        {posters.map((poster, index) => (
+          <img
+            key={poster.poster_path}
+            fetchPriority={index === 0 ? 'high' : 'low'}
+            src={`https://image.tmdb.org/t/p/original/${poster.poster_path}`}
+            alt={poster.title}
+            style={{ '--i': posters.length - index - 1 } as CSSProperties}
+          />
+        ))}
       </picture>
     </div>
   );
