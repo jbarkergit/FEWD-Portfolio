f39959f17a51cde31abd6a6932cf9877c068d902
jbarker <121387493+jbarkergit@users.noreply.github.com>
2025-07-22 22:10:24 -0500

    refactor(tmdbChunk): simplified logic with generic types

--- app/film-database/utility/tmdbChunk.ts
+++ app/film-database/utility/tmdbChunk.ts
@@ -1,19 +1,8 @@
-import type { TmdbResponseFlat } from '~/film-database/composables/types/TmdbResponse';
+export const tmdbChunk = <T>(data: T[], chunkSize: number): T[][] => {
+  const chunked: T[][] = [];
 
-type DataGeneric =
-  | Exclude<TmdbResponseFlat[keyof TmdbResponseFlat], TmdbResponseFlat['credits']>
-  | TmdbResponseFlat['credits']['cast']
-  | TmdbResponseFlat['credits']['crew'];
-
-type ChunkedGeneric<T> = DataGeneric[];
-
-export const tmdbChunk = <T extends DataGeneric>(data: T, chunkSize: number): ChunkedGeneric<T> => {
-  const dataToArray = Array.isArray(data) ? data : Object.values(data);
-
-  let chunked: ChunkedGeneric<T> = [];
-
-  for (let i = 0; i < dataToArray.length; i += chunkSize + 1) {
-    chunked.push(dataToArray.slice(i, i + chunkSize + 1));
+  for (let i = 0; i < data.length; i += chunkSize + 1) {
+    chunked.push(data.slice(i, i + chunkSize + 1));
   }
 
   return chunked;
