3b5ea0d16f17cf145800e80b0d6af36dfc4704ba
jbarker <121387493+jbarkergit@users.noreply.github.com>
2025-07-25 12:40:27 -0500

    refactor(accountModal): removal of clientLoader data

--- app/film-database/features/account/FDAccountModal.tsx
+++ app/film-database/features/account/FDAccountModal.tsx
@@ -1,32 +1,25 @@
-import { forwardRef, useMemo } from 'react';
+import { forwardRef } from 'react';
 import FDAccountModalPoster from './features/FDAccountModalPoster';
 import FDAccountFieldset from './fieldsets/FDAccountFieldset';
-import { useFLoader } from '~/film-database/routes/FilmDatabase';
 
 const FDAccountModal = forwardRef<HTMLDivElement, {}>(({}, accountRef) => {
-  /** @loaderData */
-  const { primaryData } = useFLoader();
-  const films = useMemo(() => primaryData[0]?.response.results, [primaryData[0]]);
-
-  /** @JSX */
-  if (films)
-    return (
+  return (
+    <div
+      className='fdAccount'
+      data-layout-carousel>
       <div
-        className='fdAccount'
-        data-layout-carousel>
-        <div
-          className='fdAccount__container'
-          ref={accountRef}
-          data-visible='false'>
-          <main className='fdAccount__container__wrapper'>
-            <form className='fdAccount__container__wrapper__form'>
-              <FDAccountFieldset />
-            </form>
-          </main>
-          <FDAccountModalPoster films={films} />
-        </div>
+        className='fdAccount__container'
+        ref={accountRef}
+        data-visible='false'>
+        <main className='fdAccount__container__wrapper'>
+          <form className='fdAccount__container__wrapper__form'>
+            <FDAccountFieldset />
+          </form>
+        </main>
+        <FDAccountModalPoster />
       </div>
-    );
+    </div>
+  );
 });
 
 export default FDAccountModal;
