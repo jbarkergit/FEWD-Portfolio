078b381003bb53e175ea8e99758557f2d1058c9d
Justin Barker <jbarkerpoc@gmail.com>
2025-08-24 11:35:13 -0500

    chore(misc carousel): removal of duplicate carousels in favor of generic carousel

--- app/film-database/utility/tmdbChunk.ts
+++
@@ -1,9 +0,0 @@
-export const tmdbChunk = <T>(data: T[], chunkSize: number): T[][] => {
-  const chunked: T[][] = [];
-
-  for (let i = 0; i < data.length; i += chunkSize + 1) {
-    chunked.push(data.slice(i, i + chunkSize + 1));
-  }
-
-  return chunked;
-};
