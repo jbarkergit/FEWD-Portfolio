2093557d918d4d437538e40748fbb7acd8f1990c
Justin Barker <jbarkerpoc@gmail.com>
2025-09-03 06:53:15 -0500

    fix(fdSearch): failed placeholders

--- app/film-database/features/catalog/media/search/FDSearch.tsx
+++ app/film-database/features/catalog/media/search/FDSearch.tsx
@@ -1,67 +1,58 @@
 import { useState, useEffect, useRef, type ChangeEvent } from 'react';
-import { SvgSpinnersRingResize, IcOutlinePlayCircle, IcBaselineSearch } from '~/film-database/assets/svg/icons';
+import { IcOutlinePlayCircle, IcBaselineSearch } from '~/film-database/assets/svg/icons';
 import { tmdbCall } from '~/film-database/composables/tmdbCall';
-import type { TmdbResponse } from '~/film-database/composables/types/TmdbResponse';
+import type { TmdbResponseFlat } from '~/film-database/composables/types/TmdbResponse';
 import { useCatalogProvider } from '~/film-database/context/CatalogContext';
 
 const FDSearch = ({ orientation }: { orientation: 'desktop' | 'mobile' }) => {
-  // Context
   const { setHeroData, viewportChunkSize } = useCatalogProvider();
 
-  // State
-  const [isTyping, setIsTyping] = useState(false);
+  const isTypingRef = useRef<boolean>(false);
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [searchResults, setSearchResults] = useState<TmdbResponse['string']['search']['results'] | undefined>(
-    undefined
-  );
+  const [searchResults, setSearchResults] = useState<TmdbResponseFlat['search']['results'] | undefined>(undefined);
 
-  // Debounce
+  /**
+   * @function fetch
+   * @returns Promise<void>
+   * Invoke debounced fetch
+   */
+  const fetch = async () => {
+    const search = await tmdbCall({ search: searchTerm });
+    if (search) setSearchResults(search.response.results);
+  };
+
   const timeoutId = useRef<NodeJS.Timeout | null>(null);
 
-  // References
-  const labelRef = useRef<HTMLLabelElement>(null);
+  useEffect(() => {
+    // Clear timer
+    if (timeoutId.current) window.clearTimeout(timeoutId.current);
+
+    // Assign new timer
+    timeoutId.current = window.setTimeout(() => {
+      if (searchTerm.length > 0) fetch();
+    }, 850) as unknown as NodeJS.Timeout;
+
+    return () => {
+      if (timeoutId.current) {
+        isTypingRef.current = false;
+        window.clearTimeout(timeoutId.current);
+      }
+    };
+  }, [searchTerm]);
 
   /**
    * @function handleLabelVisibility
    * @returns void undefined
    * Alters visibility of the input label
    */
+  const labelRef = useRef<HTMLLabelElement>(null);
+
   const handleLabelVisibility = (prop: 'visible' | 'barelyVisible'): void | undefined => {
     if (labelRef.current && searchTerm.length === 0) {
       return labelRef.current?.setAttribute('data-opacity', prop);
     }
     return undefined;
   };
-
-  /**
-   * @function invokeFetch
-   * @returns Promise<void>
-   * Invoke debounced fetch
-   */
-  const invokeFetch = async () => {
-    if (searchTerm.length > 0) {
-      setIsTyping(false);
-      const search = await tmdbCall({ search: searchTerm });
-      if (search) setSearchResults(search.response.results);
-    }
-  };
-
-  useEffect(() => {
-    if (searchTerm.length === 0) setSearchResults([]);
-    if (searchTerm.length > 0) setIsTyping(true);
-
-    // Clear timer
-    if (timeoutId.current) window.clearTimeout(timeoutId.current);
-    // Assign new timer
-    timeoutId.current = window.setTimeout(() => invokeFetch(), 850) as unknown as NodeJS.Timeout;
-
-    return () => {
-      if (timeoutId.current) {
-        setIsTyping(false);
-        window.clearTimeout(timeoutId.current);
-      }
-    };
-  }, [searchTerm]);
 
   return (
     <section
@@ -86,44 +77,45 @@ const FDSearch = ({ orientation }: { orientation: 'desktop' | 'mobile' }) => {
             onPointerLeave={() => handleLabelVisibility('barelyVisible')}
             onFocus={() => handleLabelVisibility('visible')}
             onBlur={() => handleLabelVisibility('barelyVisible')}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setSearchTerm(e?.target.value.replace(' ', '-').toLowerCase())
-            }
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              isTypingRef.current = true;
+              setSearchTerm(e?.target.value.replace(' ', '-').toLowerCase());
+            }}
           />
-          {isTyping ? <SvgSpinnersRingResize /> : null}
         </fieldset>
       </div>
 
       <div
         className='fdSearchBar__results'
-        data-anim={searchResults && searchResults.length > 0 ? 'enabled' : 'disabled'}>
+        data-anim={searchResults && searchResults.length ? 'enabled' : 'disabled'}>
         <ul className='fdSearchBar__results__ul'>
-          {searchResults && !isTyping
-            ? searchResults.splice(0, viewportChunkSize).map((props, index) => {
-                if (props.poster_path)
-                  return (
-                    <li
-                      className='fdSearchBar__results__ul__li'
-                      key={`fd-search-result-${index}`}>
-                      <picture className='fdSearchBar__results__ul__li__article'>
-                        <img
-                          src={`https://image.tmdb.org/t/p/w780${props.poster_path}`}
-                          alt={`${props.title}`}
-                        />
-                      </picture>
-                      <div
-                        className='fdSearchBar__results__ul__li__overlay'
-                        onClick={() => setHeroData(props)}>
-                        <button
-                          className='fdSearchBar__results__ul__li__overlay--play'
-                          aria-label='Play trailer'>
-                          <IcOutlinePlayCircle />
-                        </button>
-                      </div>
-                    </li>
-                  );
-              })
-            : null}
+          {!isTypingRef.current && searchResults
+            ? searchResults.slice(0, viewportChunkSize).map((props, index) => (
+                <li
+                  className='fdSearchBar__results__ul__li'
+                  key={`fd-search-result-${index}`}>
+                  <picture className='fdSearchBar__results__ul__li__article'>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w780${props.poster_path}`}
+                      alt={`${props.title}`}
+                    />
+                  </picture>
+                  <div
+                    className='fdSearchBar__results__ul__li__overlay'
+                    onClick={() => setHeroData(props)}>
+                    <button
+                      className='fdSearchBar__results__ul__li__overlay--play'
+                      aria-label='Play trailer'>
+                      <IcOutlinePlayCircle />
+                    </button>
+                  </div>
+                </li>
+              ))
+            : Array.from({ length: viewportChunkSize }).map((_, i) => (
+                <li
+                  className='fdSearchBar__results__ul__li'
+                  key={`fd-search-result-placeholder-${i}`}></li>
+              ))}
         </ul>
       </div>
     </section>
