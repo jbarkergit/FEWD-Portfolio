90f8bae637991cd6c3c399d86a98c7635bec53a8
Justin Barker <jbarkerpoc@gmail.com>
2025-09-16 09:39:14 -0500

    perf: removed additional forwardRef layer, placed background animator logic within scope of accountAnimation component

--- app/film-database/features/account/animation/FDAccountAnimation.tsx
+++ app/film-database/features/account/animation/FDAccountAnimation.tsx
@@ -1,79 +1,78 @@
-import { forwardRef, useEffect, useImperativeHandle, useMemo, useRef, useState, type CSSProperties } from 'react';
+import { useEffect, useMemo, useRef, useState, type CSSProperties } from 'react';
 import type { TmdbMovieProvider } from '~/film-database/composables/types/TmdbResponse';
 import { useFLoader } from '~/film-database/routes/FilmDatabase';
 
-const FDAccountAnimation = forwardRef<HTMLDivElement, { unmountAnimation: () => void }>(
-  ({ unmountAnimation }, animationRef) => {
-    const exposedRef = useRef<HTMLDivElement>(null);
-    useImperativeHandle(animationRef, () => exposedRef.current!, []);
-    const { primaryData } = useFLoader();
-    const [posters, setPosters] = useState<TmdbMovieProvider[][]>();
+// Find the visual center of an array's length
+const getCenteredIndex = (length: number) => Math.round((length - 1) / 2);
 
-    /**
-     * @function useEffect
-     * @description Handles animation on mount
-     */
-    useEffect(() => exposedRef.current?.setAttribute('data-visible', 'true'), []);
+const FDAccountAnimation = ({ accountRef }: { accountRef: React.RefObject<HTMLDivElement | null> }) => {
+  const { primaryData } = useFLoader();
+  const [posters, setPosters] = useState<TmdbMovieProvider[][]>();
+  const animationRef = useRef<HTMLDivElement>(null);
 
-    /**
-     * @function mostCenteredImageID
-     * @description Aims to abstract logic from map scope in order to leverage TMDB image paths with numerous dimensions to improve image load times
-     */
-    const mostCenteredImageID: number = useMemo(() => {
-      if (!primaryData[0]) return -1;
+  /** Aims to abstract logic from map scope in order to leverage TMDB image paths with numerous dimensions to improve image load times */
+  const mostCenteredImageID: number = useMemo(() => {
+    if (!primaryData[0]) return -1;
 
-      const nowPlaying = primaryData[0].response.results;
-      let posters: TmdbMovieProvider[][] = [];
+    const nowPlaying = primaryData[0].response.results;
+    let posters: TmdbMovieProvider[][] = [];
 
-      for (let i = 0; i < Math.ceil(nowPlaying.length / 4); i++) {
-        posters.push(nowPlaying.slice(i * 4, i * 4 + 4));
-      }
+    for (let i = 0; i < Math.ceil(nowPlaying.length / 4); i++) {
+      posters.push(nowPlaying.slice(i * 4, i * 4 + 4));
+    }
 
-      if (!posters.length) return -1;
+    if (!posters.length) return -1;
 
-      setPosters(posters);
+    setPosters(posters);
 
-      // Find the visual center of an array's length
-      const getCenteredIndex = (length: number) => Math.round((length - 1) / 2);
+    const middleSet = posters[getCenteredIndex(posters.length)];
+    if (!middleSet?.length) return -1;
 
-      const middleSet = posters[getCenteredIndex(posters.length)];
-      if (!middleSet?.length) return -1;
+    const middleItem = middleSet?.[getCenteredIndex(middleSet.length)];
+    return middleItem?.id ?? -1;
+  }, [primaryData[0]]);
 
-      const middleItem = middleSet?.[getCenteredIndex(middleSet.length)];
-      return middleItem?.id ?? -1;
-    }, [primaryData[0]]);
+  /** Mount animator */
+  useEffect(() => animationRef.current?.setAttribute('data-visible', 'true'), []);
 
-    return (
-      <div className='fdAccountAnimation'>
-        <div
-          className='fdAccountAnimation__backdrop'
-          ref={exposedRef}
-          data-visible='false'
-          onAnimationEnd={unmountAnimation}>
-          {posters?.map((set: TmdbMovieProvider[], setIndex: number) => (
-            <ul
-              className='fdAccountAnimation__backdrop__set'
-              key={`backdrop-set-${setIndex}`}>
-              {set.map((article: TmdbMovieProvider, index: number) => (
-                <li
-                  className='fdAccountAnimation__backdrop__set__li'
-                  key={`backdrop-image-${article.id}`}
-                  style={{ '--i': index } as CSSProperties}>
-                  <picture className='fdAccountAnimation__backdrop__set__li__container'>
-                    <img
-                      className='fdAccountAnimation__backdrop__set__li__container--img'
-                      src={`https://image.tmdb.org/t/p/${article.id === mostCenteredImageID ? `original` : `w780`}/${article?.backdrop_path}`}
-                      alt={article?.title}
-                    />
-                  </picture>
-                </li>
-              ))}
-            </ul>
-          ))}
-        </div>
+  /** Unmount animator */
+  const unmountAnimation = (): void => {
+    if (!animationRef.current || !accountRef.current) return;
+    const attribute: string = 'data-visible';
+    animationRef.current.setAttribute(attribute, 'false');
+    accountRef.current.setAttribute(attribute, 'true');
+  };
+
+  return (
+    <div className='fdAccountAnimation'>
+      <div
+        className='fdAccountAnimation__backdrop'
+        ref={animationRef}
+        data-visible='false'
+        onAnimationEnd={unmountAnimation}>
+        {posters?.map((set: TmdbMovieProvider[], setIndex: number) => (
+          <ul
+            className='fdAccountAnimation__backdrop__set'
+            key={`backdrop-set-${setIndex}`}>
+            {set.map((article: TmdbMovieProvider, index: number) => (
+              <li
+                className='fdAccountAnimation__backdrop__set__li'
+                key={`backdrop-image-${article.id}`}
+                style={{ '--i': index } as CSSProperties}>
+                <picture className='fdAccountAnimation__backdrop__set__li__container'>
+                  <img
+                    className='fdAccountAnimation__backdrop__set__li__container--img'
+                    src={`https://image.tmdb.org/t/p/${article.id === mostCenteredImageID ? `original` : `w780`}/${article?.backdrop_path}`}
+                    alt={article?.title}
+                  />
+                </picture>
+              </li>
+            ))}
+          </ul>
+        ))}
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
 export default FDAccountAnimation;
--- app/film-database/routes/FilmDatabase.tsx
+++ app/film-database/routes/FilmDatabase.tsx
@@ -1,21 +1,21 @@
 import { useRef } from 'react';
-import type { Route } from './+types/FilmDatabase';
 import { useLoaderData } from 'react-router';
-// Auth
+import type { Route } from './+types/FilmDatabase';
+
 import isUserAuthorized from '~/base/firebase/authentication/utility/isUserAuthorized';
-// Context
+
+import { tmdbCall } from '../composables/tmdbCall';
+
 import { HeroDataProvider } from '~/film-database/context/HeroDataContext';
 import { ChunkSizeProvider } from '~/film-database/context/ChunkSizeContext';
 import { RootRefProvider } from '~/film-database/context/RootRefContext';
 import { TrailerQueueProvider } from '~/film-database/context/ModalTrailerContext';
 import { UserCollectionProvider } from '~/film-database/context/UserCollectionContext';
 import { ModalProvider } from '~/film-database/context/ModalContext';
-// Composables
-import { tmdbCall } from '../composables/tmdbCall';
-// Features: Account Page
+
 import FDAccountAnimation from '../features/account/animation/FDAccountAnimation';
 import FDAccountModal from '../features/account/FDAccountModal';
-// Features: Catalog page
+
 import FDHeader from '../features/catalog/navigation/FDHeader';
 import FDCatalog from '../features/catalog/FDCatalog';
 
@@ -58,15 +58,7 @@ export const useFLoader = () => useLoaderData() as Awaited<ReturnType<typeof cli
 export default function FilmDatabase({ loaderData }: Route.ComponentProps) {
   const { isAuth } = loaderData;
 
-  const animationRef = useRef<HTMLDivElement>(null);
   const accountRef = useRef<HTMLDivElement>(null);
-
-  const unmountAnimation = (): void => {
-    if (!animationRef.current || !accountRef.current) return;
-    const attribute: string = 'data-visible';
-    animationRef.current.setAttribute(attribute, 'false');
-    accountRef.current.setAttribute(attribute, 'true');
-  };
 
   return isAuth ? (
     <RootRefProvider>
@@ -87,10 +79,7 @@ export default function FilmDatabase({ loaderData }: Route.ComponentProps) {
     </RootRefProvider>
   ) : (
     <>
-      <FDAccountAnimation
-        ref={animationRef}
-        unmountAnimation={unmountAnimation}
-      />
+      <FDAccountAnimation accountRef={accountRef} />
       <FDAccountModal ref={accountRef} />
     </>
   );
