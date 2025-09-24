a7ba173a07e5c568b1da0171d41a8372c910875f
Justin Barker <jbarkerpoc@gmail.com>
2025-09-24 11:35:34 -0500

    chore(vite.config): fix reactRouter preamble error during testing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 /** https://vite.dev/config/ */
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
   server: { watch: { usePolling: true } },
   test: {
     globals: true,
