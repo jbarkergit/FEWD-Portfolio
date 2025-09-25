70fe5c46213b20c92eb99b176fb923315f30453a
Justin Barker <jbarkerpoc@gmail.com>
2025-09-25 11:06:44 -0500

    chore(userCollectionContext): prevent anonymous users from utilizing firebase utilities

--- app/film-database/context/UserCollectionContext.tsx
+++ app/film-database/context/UserCollectionContext.tsx
@@ -7,6 +7,8 @@ import {
   type ReactNode,
   type SetStateAction,
 } from 'react';
+import { useAuth } from '~/base/firebase/authentication/context/authProvider';
+import isUserAuthorized from '~/base/firebase/authentication/utility/isUserAuthorized';
 import { getFirestoreUserDocument } from '~/base/firebase/firestore/utility/getFirestoreUserDocument';
 import { updateFirestoreUserDocument } from '~/base/firebase/firestore/utility/updateFirestoreUserDocument';
 import type { TmdbMovieProvider } from '~/film-database/composables/types/TmdbResponse';
@@ -31,9 +33,20 @@ const Context = createContext<
 
 export const UserCollectionProvider = ({ children }: { children: ReactNode }) => {
   const { primaryData } = useFLoader();
-  const [userCollections, setUserCollections] = useState<Record<string, UserCollection>>({});
+  const [userCollections, setUserCollections] = useState<Record<string, UserCollection>>({
+    'user-collection-0': {
+      header: 'Trailer Queue',
+      data: [],
+    },
+    'user-collection-1': {
+      header: 'Unnamed Collection',
+      data: primaryData[0]?.response.results!,
+    },
+  });
 
   useEffect(() => {
+    if (!isUserAuthorized()) return;
+
     /**
      * If the user document has no collection, pre-populate it with film intelligence from clientLoader
      * If the user already has a collection, restore it from their document
@@ -72,6 +85,8 @@ export const UserCollectionProvider = ({ children }: { children: ReactNode }) =>
   }, []);
 
   useEffect(() => {
+    if (!isUserAuthorized()) return;
+
     /** Update user document collection */
     updateFirestoreUserDocument({ movies: userCollections });
   }, [userCollections]);
