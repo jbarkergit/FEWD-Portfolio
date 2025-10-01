76af54586eeaa6f5e4ea219d048b6b5b80bd18ba
Justin Barker <jbarkerpoc@gmail.com>
2025-09-30 23:54:50 -0500

    perf(ProductProvider): rewrite starter code

--- app/ecommerce/components/product/product-provider/ProductProvider.tsx
+++ app/ecommerce/components/product/product-provider/ProductProvider.tsx
@@ -1,109 +1,93 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useMemo } from 'react';
 import { useLocation } from 'react-router';
 import type { ProductType } from '~/ecommerce/context/CartContext';
 import { commerceDatabase } from '~/ecommerce/data/commerceDatabase';
 import ProductProp from './ProductProp';
 
-// Custom hook to filter products based on the location (category or other attributes)
-const useProductFilter = (location: string): ProductType[] => {
-  const filteredData = commerceDatabase.reduce((result: ProductType[], product: ProductType) => {
-    if (product.category) {
-      switch (location) {
-        case 'products':
-          return commerceDatabase.sort((a, b) => a.company.localeCompare(b.company));
-
-        case 'headphones':
-        case 'microphones':
-        case 'interfaces':
-          if (Array.isArray(product.category) && product.category.includes(location)) result.push(product);
-          else if (typeof product.category === 'string' && product.category.includes(location)) result.push(product);
-          break;
-
-        case 'amps-dacs':
-          if (
-            Array.isArray(product.category) &&
-            (product.category as string[]).some((cat) => ['amps', 'dacs', 'amps-dacs'].includes(cat))
-          )
-            result.push(product);
-          else if (typeof product.category === 'string' && ['amps', 'dacs', 'amps-dacs'].includes(product.category))
-            result.push(product);
-          break;
-
-        default:
-          if (product.company?.includes(location)) result.push(product);
-          else if (product.wearStyle?.includes(location)) result.push(product);
-          else if (product.polarPattern?.includes(location)) result.push(product);
-          else
-            console.error(
-              'Failure at ProductProvider: Location unavailable or property are unavailable in default case.'
-            );
-      }
-    }
-    return result;
-  }, []);
-
-  return filteredData.sort((a, b) => a.company.localeCompare(b.company));
-};
-
 /** Product Display */
 const ProductProvider = () => {
-  // State for paginated products
-  const [paginatedProducts, setPaginatedProducts] = useState<ProductType[][]>([]);
-  // State for currently visible products
-  const [visibleProducts, setVisibleProducts] = useState<ProductType[]>([]);
-  // Ref to track the index of the currently visible product set
-  const visibleArrayIndex = useRef(0);
-  // Ref for the last product, used for IntersectionObserver
-  const lastProductRef = useRef<HTMLLIElement>(null);
-  // Get current location for filtering
+  /** Produce filtered, sorted and paginated products */
   const location = useLocation().pathname.split('/').pop() || 'products';
 
-  /**
-   * Paginate filtered data: chunks of 7 products
-   * Fetch filtered and paginated products when location changes
-   */
+  const paginatedProducts = useMemo(() => {
+    const filteredData = commerceDatabase.reduce((result: ProductType[], product: ProductType) => {
+      if (product.category) {
+        switch (location) {
+          case 'products':
+            return [...commerceDatabase].sort((a, b) => a.company.localeCompare(b.company));
 
-  const getPaginatedProducts = (filteredData: ProductType[]) => {
-    const newPaginatedData = [];
-    for (let i = 0; i < filteredData.length; i += 7) newPaginatedData.push(filteredData.slice(i, i + 7));
-    setPaginatedProducts(newPaginatedData);
-  };
+          case 'headphones':
+          case 'microphones':
+          case 'interfaces':
+            if (Array.isArray(product.category) && product.category.includes(location)) result.push(product);
+            else if (typeof product.category === 'string' && product.category.includes(location)) result.push(product);
+            break;
 
-  useEffect(() => {
-    const filteredData = useProductFilter(location);
-    getPaginatedProducts(filteredData);
+          case 'amps-dacs':
+            if (
+              Array.isArray(product.category) &&
+              (product.category as string[]).some((cat) => ['amps', 'dacs', 'amps-dacs'].includes(cat))
+            )
+              result.push(product);
+            else if (typeof product.category === 'string' && ['amps', 'dacs', 'amps-dacs'].includes(product.category))
+              result.push(product);
+            break;
+
+          default:
+            if (product.company?.includes(location)) result.push(product);
+            else if (product.wearStyle?.includes(location)) result.push(product);
+            else if (product.polarPattern?.includes(location)) result.push(product);
+            else
+              console.error(
+                'Failure at ProductProvider: Location unavailable or property are unavailable in default case.'
+              );
+        }
+      }
+      return result;
+    }, []);
+
+    const filteredSortedData = [...filteredData].sort((a, b) => a.company.localeCompare(b.company));
+
+    let paginatedData = [];
+
+    for (let i = 0; i < filteredSortedData.length; i += 7) {
+      paginatedData.push(filteredSortedData.slice(i, i + 7));
+    }
+
+    return paginatedData;
   }, [location]);
 
-  // Set the first set of paginated products when the paginated products state is updated
+  /** Product scroll intersectional observer */
+  const [visibleProducts, setVisibleProducts] = useState<ProductType[]>(
+    paginatedProducts[0] ? paginatedProducts[0] : []
+  );
+
+  const visibleArrayIndex = useRef<number>(0);
+  const lastProductRef = useRef<HTMLLIElement>(null);
+
   useEffect(() => {
-    if (paginatedProducts.length) {
-      setVisibleProducts(paginatedProducts[0]);
-      // Reset the index when paginated products change
-      visibleArrayIndex.current = 0;
-    }
+    visibleArrayIndex.current = 0;
+    setVisibleProducts(paginatedProducts[0] ?? []);
   }, [paginatedProducts]);
 
-  /** Observer: Handle new products when the last product comes into view */
-  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
-    const entry = entries[0];
-
-    // If the last product is in view and more products are available
-    if (entry.isIntersecting && visibleArrayIndex.current + 1 < paginatedProducts.length) {
-      visibleArrayIndex.current += 1;
-      // Append new products to visibleProducts
-      setVisibleProducts((prev) => [...prev, ...paginatedProducts[visibleArrayIndex.current]]);
-    }
-  };
-
   useEffect(() => {
+    // If the last rendered product is in view and more products can be appended, do so
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      if (!!entries[0] && entries[0]?.isIntersecting) {
+        visibleArrayIndex.current += 1;
+        const newProducts = paginatedProducts[visibleArrayIndex.current];
+        if (newProducts) setVisibleProducts((state) => [...state, ...newProducts]);
+      }
+    };
+
     const observer = new IntersectionObserver(handleIntersection, { root: null, threshold: 0.1 });
     if (lastProductRef.current) observer.observe(lastProductRef.current);
+
     return () => {
       if (lastProductRef.current) observer.unobserve(lastProductRef.current!);
     };
-  }, [visibleProducts]);
+  }, [visibleProducts.length]);
 
-  /** JSX */
   return (
     <ul className='productGrid'>
       {visibleProducts.map((product, index) => (
