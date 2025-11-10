48c77f8058325893eff277a9938a7f0655fd77c5
Justin Barker <jbarkerpoc@gmail.com>
2025-11-10 16:19:50 -0600

    feat(SpotifyVisualizer): clientLoader mount

--- app/spotify-visualizer/routes/SpotifyVisualizer.tsx
+++ app/spotify-visualizer/routes/SpotifyVisualizer.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router';
 import { callSpotify } from '~/spotify-visualizer/composables/callSpotify';
-import type { SpotifyResponse } from '~/spotify-visualizer/composables/types/SpotifyResponse';
+import type { SpotifyResponseFor } from '~/spotify-visualizer/composables/types/SpotifyResponse';
 
-export default function () {
-  const [userData, setUserData] = useState<SpotifyResponse[]>();
+export async function clientLoader() {
+  const userPlaylists = await callSpotify('getCurrentUsersPlaylists', { limit: 50, offset: 0 });
+  return { userPlaylists };
+}
 
-  useEffect(() => {
-    const userPlaylists = callSpotify('getCurrentUsersPlaylists');
-    setUserData(userPlaylists);
-  }, []);
+export const useSpotifyLoader = () => useLoaderData() as Awaited<ReturnType<typeof clientLoader>>;
+
+export default function SpotifyVisualizer() {
+  const { userPlaylists } = useSpotifyLoader();
+  console.log(userPlaylists);
 
   return (
     <div className='spotify'>
-      <main></main>
+      <div></div>
     </div>
   );
 }
