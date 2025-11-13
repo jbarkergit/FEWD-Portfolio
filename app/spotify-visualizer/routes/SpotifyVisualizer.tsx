702199c1683b6d2518e494c8efe8d03d80509c1b
Justin Barker <jbarkerpoc@gmail.com>
2025-11-13 15:23:53 -0600

    feat(spotify misc): basic app arch

--- app/spotify-visualizer/routes/SpotifyVisualizer.tsx
+++ app/spotify-visualizer/routes/SpotifyVisualizer.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router';
 import { initSpotifyAuth } from '~/spotify-visualizer/composables/auth/SpotifyAuthFlow';
-import { callSpotify } from '~/spotify-visualizer/composables/callSpotify';
-import type { SpotifyResponseFor } from '~/spotify-visualizer/composables/types/SpotifyResponse';
+import Library from '~/spotify-visualizer/features/library/Library';
+import Main from '~/spotify-visualizer/features/main/Main';
+import Navigation from '~/spotify-visualizer/features/navigation/Navigation';
+import Overlay from '~/spotify-visualizer/features/overlay/Overlay';
 
 export async function clientLoader() {
   void (await initSpotifyAuth());
@@ -11,23 +11,18 @@ export async function clientLoader() {
 // export const useSpotifyLoader = () => useLoaderData() as Awaited<ReturnType<typeof clientLoader>>;
 
 export default function SpotifyVisualizer() {
-  const [userPlaylists, setState] = useState<SpotifyResponseFor<'getCurrentUsersPlaylists'>>();
-
-  useEffect(() => {
-    const fetch = async () => {
-      const userPlaylists = await callSpotify('getCurrentUsersPlaylists', { limit: 50, offset: 0 });
-      setState(userPlaylists);
-    };
-    fetch();
-  }, []);
-
   return (
     <div className='spotify'>
-      <ul className='spotify__fonts'>
-        {userPlaylists?.items.map((item) => (
-          <li>{item.name}</li>
-        ))}
-      </ul>
+      <div className='spotify__wrapper'>
+        <div className='spotify__wrapper__overlay'>
+          <Overlay />
+        </div>
+        <div className='spotify__wrapper__content'>
+          <Navigation />
+          <Library />
+          <Main />
+        </div>
+      </div>
     </div>
   );
 }
