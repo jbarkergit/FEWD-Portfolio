ad7646e70c22f7385d5e203737866a98f30d7e6a
Justin Barker <jbarkerpoc@gmail.com>
2025-11-17 17:43:24 -0600

    wip(contact): rewrite

--- app/portfolio/features/contact/Contact.tsx
+++ app/portfolio/features/contact/Contact.tsx
@@ -1,15 +1,19 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, type SVGProps } from 'react';
 import { contactSchema } from '~/base/validation/zodSchema';
 import { useFeatureState } from '~/portfolio/context/FeatureStateContext';
 
-const inputs = {
-  firstName: { htmlFor: 'first-name', inputType: 'text' },
-  lastName: { htmlFor: 'last-name', inputType: 'text' },
+const contactInformationInputs = {
+  name: { htmlFor: 'name', inputType: 'text' },
   emailAddress: { htmlFor: 'email', inputType: 'email' },
   phoneNumber: { htmlFor: 'phone', inputType: 'tel' },
   business: { htmlFor: 'business', inputType: 'text' },
   role: { htmlFor: 'role', inputType: 'text' },
+} as const;
+
+const inquiryInputs = {
   message: { htmlFor: 'message', inputType: 'text' },
+  location: { htmlFor: 'location', inputType: 'text' },
+  otherDetails: { htmlFor: 'other', inputType: 'text' },
 } as const;
 
 const submitToWeb3Forms = async (formData: FormData) => {
@@ -26,7 +30,7 @@ const submitToWeb3Forms = async (formData: FormData) => {
 };
 
 const Contact = () => {
-  const { setFeatureState } = useFeatureState();
+  const { featureState, setFeatureState } = useFeatureState();
 
   const [errors, setErrors] = useState<Record<string, string>>({});
   const submittingRef = useRef<boolean>(false);
@@ -74,6 +78,143 @@ const Contact = () => {
 
   return (
     <section className='contact'>
+      <header className='contact__heading'>
+        <h2>Contact Form</h2>
+      </header>
+      <form
+        className='contact__form'
+        action=''>
+        <section
+          className='contact__form__step'
+          data-toggle='true'>
+          <header className='contact__form__step__header'>
+            <div>
+              <span>
+                <MaterialSymbolsShieldLock />
+              </span>
+              <h3>Contact Information</h3>
+            </div>
+            <h2>Let's get started with your contact information.</h2>
+          </header>
+          <ul className='contact__form__step__ul'>
+            {Object.entries(contactInformationInputs).map(([key, { htmlFor, inputType }]) => (
+              <li
+                className='contact__form__step__ul__li'
+                key={`contact-form-information-${key}`}>
+                <label
+                  htmlFor={htmlFor}
+                  className='contact__form__step__ul__li__label'>
+                  {htmlFor === 'business'
+                    ? 'Agency (optional)'
+                    : htmlFor === 'role'
+                      ? 'Role (optional)'
+                      : htmlFor.replaceAll('-', ' ')}
+                </label>
+                <input
+                  className='contact__form__step__ul__li__input'
+                  id={htmlFor}
+                  type={inputType}
+                  name={key}
+                  placeholder=' '
+                  aria-invalid={!!errors[key]}
+                  aria-describedby={`${htmlFor}-error`}
+                />
+                {errors[key] && (
+                  <div
+                    className='contact__form__step__ul__li__error'
+                    id={`${htmlFor}-error`}
+                    role='alert'>
+                    {errors[key]}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+          <button className='contact__form__step__button'>Next</button>
+        </section>
+        <section
+          className='contact__form__step'
+          data-toggle='false'>
+          <header className='contact__form__step__header'>
+            <nav className='contact__form__step__nav'>
+              <span>
+                <MaterialSymbolsArrowLeftAlt />
+              </span>
+              <h3>Inquiry Details</h3>
+            </nav>
+            <h2>What’s the focus of your inquiry, and how can I be of service?</h2>
+          </header>
+          <ul className='contact__form__step__ul'>
+            {Object.entries(inquiryInputs).map(([key, { htmlFor, inputType }]) => (
+              <li
+                className='contact__form__step__ul__li'
+                key={`contact-form-inquiry-${key}`}>
+                <label
+                  htmlFor={htmlFor}
+                  className='contact__form__step__ul__li__label'>
+                  {htmlFor === 'location'
+                    ? 'Location'
+                    : htmlFor === 'message'
+                      ? 'Message'
+                      : htmlFor === 'other'
+                        ? 'Other Details'
+                        : ''}
+                </label>
+                {htmlFor === 'message' ? (
+                  <textarea
+                    id='message'
+                    name='message'
+                    aria-invalid={!!errors.message}
+                    aria-describedby='message-error'
+                  />
+                ) : (
+                  <input
+                    className='contact__form__step__ul__li__input'
+                    id={htmlFor}
+                    type={inputType}
+                    name={key}
+                    placeholder={key}
+                    aria-invalid={!!errors[key]}
+                    aria-describedby={`${htmlFor}-error`}
+                  />
+                )}
+                {errors[key] && (
+                  <div
+                    className='contact__form__step__ul__li__error'
+                    id={`${htmlFor}-error`}
+                    role='alert'>
+                    {errors[key]}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+          <button className='contact__form__step__button'>Next</button>
+        </section>
+        <section
+          className='contact__form__step'
+          data-toggle='false'>
+          <header className='contact__form__step__header'>
+            <nav className='contact__form__step__nav'>
+              <span>
+                <MaterialSymbolsArrowLeftAlt />
+              </span>
+              <h3>Booking</h3>
+            </nav>
+            <h2>Let's book a call.</h2>
+          </header>
+          <ul className='contact__form__step__ul'></ul>
+          <button className='contact__form__step__button'>Submit</button>
+        </section>
+      </form>
+      {/* <nav className='contact__nav'>
+        <button
+          className='contact__container--return'
+          aria-label='Return to project hub'
+          onPointerUp={() => setFeatureState((p) => ({ ...p, contactFormActive: false }))}>
+          Return to Project Hub
+        </button>
+      </nav>
       <div
         className='contact__container'
         ref={contactContainerRef}>
@@ -145,16 +286,39 @@ const Contact = () => {
             you for your consideration.
           </p>
         )}
-
-        <button
-          className='contact__container--return'
-          aria-label='Return to project hub'
-          onPointerUp={() => setFeatureState((p) => ({ ...p, contactFormActive: false }))}>
-          Return to Project Hub
-        </button>
-      </div>
+      </div> */}
     </section>
   );
 };
 
 export default Contact;
+
+function MaterialSymbolsShieldLock(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      {...props}>
+      {/* Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE */}
+      <path
+        fill='currentColor'
+        d='M12 22q-3.475-.875-5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.262 6.913T12 22m-2-6h4q.425 0 .713-.288T15 15v-3q0-.425-.288-.712T14 11v-1q0-.825-.587-1.412T12 8t-1.412.588T10 10v1q-.425 0-.712.288T9 12v3q0 .425.288.713T10 16m1-5v-1q0-.425.288-.712T12 9t.713.288T13 10v1z'
+      />
+    </svg>
+  );
+}
+
+function MaterialSymbolsArrowLeftAlt(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      {...props}>
+      {/* Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE */}
+      <path
+        fill='currentColor'
+        d='m10 18l-6-6l6-6l1.4 1.45L7.85 11H20v2H7.85l3.55 3.55z'
+      />
+    </svg>
+  );
+}
